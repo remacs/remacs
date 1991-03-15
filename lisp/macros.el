540671f32b14364f13618c99f8b6772c1387550c
Richard M. Stallman <rms@gnu.org>
1991-03-15 20:39:25 +0000

    *** empty log message ***

--- lisp/macros.el
+++ lisp/macros.el
@@ -22,8 +22,7 @@
   "Assign a name to the last keyboard macro defined.
 Argument SYMBOL is the name to define.
 The symbol's function definition becomes the keyboard macro string.
-Such a \"function\" cannot be called from Lisp, but it is a valid command
-definition for the editor command loop."
+Such a \"function\" cannot be called from Lisp, but it is a valid editor command."
   (interactive "SName for last kbd macro: ")
   (or last-kbd-macro
       (error "No keyboard macro defined"))
@@ -35,14 +34,14 @@ definition for the editor command loop."
 
 (defun insert-kbd-macro (macroname &optional keys)
   "Insert in buffer the definition of kbd macro NAME, as Lisp code.
-Second argument KEYS non-nil means also record the keys it is on.
- (This is the prefix argument, when calling interactively.)
+Optional second arg KEYS means also record the keys it is on
+(this is the prefix argument, when calling interactively).
 
-This Lisp code will, when executed, define the kbd macro with the
-same definition it has now.  If you say to record the keys,
-the Lisp code will also rebind those keys to the macro.
-Only global key bindings are recorded since executing this Lisp code
-always makes global bindings.
+This Lisp code will, when executed, define the kbd macro with the same
+definition it has now.  If you say to record the keys, the Lisp code
+will also rebind those keys to the macro.  Only global key bindings
+are recorded since executing this Lisp code always makes global
+bindings.
 
 To save a kbd macro, visit a file of Lisp code such as your ~/.emacs,
 use this command, and then save the file."
@@ -64,15 +63,15 @@ use this command, and then save the file."
 
 (defun kbd-macro-query (flag)
   "Query user during kbd macro execution.
-With prefix argument, enters recursive edit,
- reading keyboard commands even within a kbd macro.
- You can give different commands each time the macro executes.
-Without prefix argument, reads a character.  Your options are:
- Space -- execute the rest of the macro.
- DEL -- skip the rest of the macro; start next repetition.
- C-d -- skip rest of the macro and don't repeat it any more.
- C-r -- enter a recursive edit, then on exit ask again for a character
- C-l -- redisplay screen and ask again."
+  With prefix argument, enters recursive edit, reading keyboard
+commands even within a kbd macro.  You can give different commands
+each time the macro executes.
+  Without prefix argument, reads a character.  Your options are:
+Space -- execute the rest of the macro.
+DEL -- skip the rest of the macro; start next repetition.
+C-d -- skip rest of the macro and don't repeat it any more.
+C-r -- enter a recursive edit, then on exit ask again for a character
+C-l -- redisplay screen and ask again."
   (interactive "P")
   (or executing-macro
       defining-kbd-macro
