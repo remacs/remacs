ab40ac3ace46e3517eae3cf9e6a6a74cddf9a5fd
Brian Preble <rassilon@gnu.org>
1991-05-20 19:05:21 +0000

    Initial revision

---
+++ lisp/forms-pass.el
@@ -0,0 +1,20 @@
+;; demo for forms-mode
+;;
+;; This demo visits /etc/passwd.
+
+(setq forms-file "/etc/passwd")
+(setq forms-read-only t)		; to make sure
+(setq forms-field-sep ":")
+(setq forms-number-of-fields 7)
+(setq forms-format-list
+     '("====== Visiting /etc/passwd ======\n\n"
+       "User : "	1
+       "   Uid: "	3
+       "   Gid: "	4
+       "\n\n"
+       "Name : "	5
+       "\n\n"
+       "Home : "	6
+       "\n\n"
+       "Shell: "	7
+       "\n"))
