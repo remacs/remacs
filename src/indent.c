e74928fc4babcb5d8e498e62743135d3674dccf6
Jim Blandy <jimb@redhat.com>
1991-07-13 21:22:40 +0000

    *** empty log message ***

--- src/indent.c
+++ src/indent.c
@@ -83,6 +83,13 @@ however, ^M is treated as end of line when `selective-display' is t.")
   return temp;
 }
 
+/* Cancel any recorded value of the horizontal position.  */
+
+invalidate_current_column ()
+{
+  last_known_column_point = 0;
+}
+
 int
 current_column ()
 {
