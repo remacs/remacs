279cc2b84ccbbb7796dc0081bcd829c7771b291c
Jim Blandy <jimb@redhat.com>
1992-07-17 20:35:53 +0000

    entered into RCS

--- src/emacs.c
+++ src/emacs.c
@@ -549,6 +549,15 @@ main (argc, argv, envp)
 
   initialized = 1;
 
+#ifdef sun
+  /* sun's localtime() has a bug.  it caches the value of the time
+     zone rather than looking it up every time.  Since localtime() is
+     called to bolt the undumping time into the undumped emacs, this
+     results in localtime() ignoring the TZ environment variable.
+     This flushes the new TZ value into localtime(). */
+  tzset();
+#endif /* sun */
+
   /* Enter editor command loop.  This never returns.  */
   Frecursive_edit ();
   /* NOTREACHED */
