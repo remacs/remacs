ac501cd970e09514b7435b9aacdc9a054cde38fe
Jim Blandy <jimb@redhat.com>
1992-10-02 23:56:44 +0000

    * keyboard.h (EVENT_HAS_PARAMETERS): Definition changed - all events 	are not 5 elements long. 	(EVENT_HEAD): Work correctly on all sorts of events, not just 	events with parameters. 	(EVENT_WINDOW, EVENT_BUFFER_POSN, EVENT_SCROLLBAR_BUTTON, 	EVENT_WINDOW_POSN, EVENT_TIMESTAMP): These aren't useful 	anymore; this information may appear twice in an event.  These 	are replaced by the POSN_ accessors, which are meant to be 	composed with the EVENT_START and EVENT_END accessors. 	(EVENT_START, EVENT_END): Two new accessors, to get at the 	starting and ending position of an event. 	(POSN_WINDOW, POSN_BUFFER_POSN, PONS_SCROLLBAR_BUTTON, 	POSN_WINDOW_POSN, POSN_TIMESTAMP): New macros, to futher 	diasassemble the values returned by EVENT_START and EVENT_END.
    	* keyboard.h (Qevent_unmodified): Extern declaration replaced by...
    	(Qevent_symbol_elements): This.
    	(EVENT_HEAD_UNMODIFIED): Use the Qevent_symbol_elements
    	property, rather than the Qevent_unmodified property.

--- src/keyboard.h
+++ src/keyboard.h
@@ -35,15 +35,23 @@ extern Lisp_Object Vfunction_key_map;
 
 /* True iff EVENT has data fields describing it (i.e. a mouse click).  */
 #define EVENT_HAS_PARAMETERS(event) \
-  (XTYPE (event) == Lisp_Cons && Flength (event) == 5)
+  (XTYPE (event) == Lisp_Cons)
 
-/* Access the components of composite events.  */
-#define EVENT_HEAD(event) 		(Fnth (0, (event)))
-#define EVENT_WINDOW(event)		(Fnth (1, (event)))
-#define EVENT_BUFFER_POSN(event)	(Fnth (2, (event)))
-#define EVENT_SCROLLBAR_BUTTON EVENT_BUFFER_POSN
-#define EVENT_WINDOW_POSN(event)	(Fnth (3, (event)))
-#define EVENT_TIMESTAMP(event)		(Fnth (4, (event)))
+/* Return the head of an event.  This works on composite and simple events.  */
+#define EVENT_HEAD(event) \
+  (EVENT_HAS_PARAMETERS (event) ? XCONS (event)->car : (event))
+
+/* Return the starting and ending locations of a composite event.  */
+#define EVENT_START(event) (XCONS (XCONS (event)->cdr)->car)
+#define EVENT_END(event) (XCONS (XCONS (XCONS (event)->cdr)->cdr)->car)
+
+/* Return the fields of a position.  */
+#define POSN_WINDOW(posn) (XCONS (posn)->car)
+#define POSN_BUFFER_POSN(posn) (XCONS (XCONS (posn)->cdr)->car)
+#define POSN_SCROLLBAR_BUTTON POSN_BUFFER_POSN
+#define POSN_WINDOW_POSN(posn) (XCONS (XCONS (XCONS (posn)->cdr)->cdr)->car)
+#define POSN_TIMESTAMP(posn) \
+  (XCONS (XCONS (XCONS (XCONS (posn)->cdr)->cdr)->cdr)->car)
 
 /* Some of the event heads.  */
 extern Lisp_Object Qvscrollbar_part, Qvslider_part;
@@ -52,11 +60,11 @@ extern Lisp_Object Qhscrollbar_part, Qhslider_part;
 extern Lisp_Object Qhthumbleft_part, Qhthumbright_part;
 
 /* Properties on event heads.  */
-extern Lisp_Object Qevent_kind, Qevent_unmodified;
+extern Lisp_Object Qevent_kind, Qevent_symbol_elements;
 
 /* Getting an unmodified version of an event head.  */
 #define EVENT_HEAD_UNMODIFIED(event_head) \
-  (Fget ((event_head), Qevent_unmodified))
+  (Fcar (Fget ((event_head), Qevent_symbol_elements)))
 
 /* The values of Qevent_kind properties.  */
 extern Lisp_Object Qfunction_key, Qmouse_click, Qmouse_movement;
