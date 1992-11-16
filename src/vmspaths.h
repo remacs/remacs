7104f9b06c8901fdc7a92a443050a6f06cd2aeb2
Jim Blandy <jimb@redhat.com>
1992-11-16 00:58:28 +0000

    * vmspaths.h: New version from Roland Roberts.

--- src/vmspaths.h
+++ src/vmspaths.h
@@ -1,9 +1,26 @@
-/* the default search path for Lisp function "load" */
+/* Hey Emacs, this is -*- C -*- code!  */
+
+/* The default search path for Lisp function "load".
+   This sets load-path.  */
 #define PATH_LOADSEARCH "EMACS_LIBRARY:[LISP]"
 
-/* the extra search path for programs to invoke.
-  This is appended to whatever the PATH environment variable says. */
+/* Like PATH_LOADSEARCH, but used only when Emacs is dumping.  This
+   path is usually identical to PATH_LOADSEARCH except that the entry
+   for the directory containing the installed lisp files has been
+   replaced with ../lisp.  */
+#define PATH_DUMPLOADSEARCH "[-.LISP]"
+
+/* The extra search path for programs to invoke.  This is appended to
+   whatever the PATH environment variable says to set the Lisp
+   variable exec-path and the first file name in it sets the Lisp
+   variable exec-directory.  exec-directory is used for finding
+   executables and other architecture-dependent files.  */
 #define PATH_EXEC "EMACS_LIBRARY:[ETC]"
+
+/* Where Emacs should look for its architecture-independent data
+   files, like the docstring file.  The lisp variable data-directory
+   is set to this value.  */
+#define PATH_DATA "EMACS_LIBRARY:[ETC]"
 
 /* the name of the directory that contains lock files
   with which we record what files are being modified in Emacs.
