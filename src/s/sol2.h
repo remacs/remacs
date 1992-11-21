7fe7fd40d2cbe4623a31d73a500ef407df882448
Richard M. Stallman <rms@gnu.org>
1992-11-21 22:12:53 +0000

    (LD_SWITCH_SYSTEM): Make alternate version for GCC.

--- src/s/sol2.h
+++ src/s/sol2.h
@@ -4,8 +4,12 @@
 
 /* Here is how to find X Windows.  The -L option tells the linker where
    to find the libraries at link time, the -R option at run time.  */
-#define LD_SWITCH_SYSTEM -L/usr/openwin/lib -R/usr/openwin/lib
 #define C_SWITCH_X_SYSTEM -I/usr/openwin/include
+#ifndef __GNUC__
+#define LD_SWITCH_SYSTEM -L/usr/openwin/lib -R/usr/openwin/lib
+#else /* GCC */
+#define LD_SWITCH_SYSTEM -L/usr/openwin/lib -Xlinker -R/usr/openwin/lib
+#endif /* GCC */
 
 /* Compile in non-ansi fashion to work around bugs in system header files.  */
 #ifndef __GNUC__
