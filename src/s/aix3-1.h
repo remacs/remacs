beb593aa1538ffbf4bafd14a92f5d3dc00c3872a
Jim Blandy <jimb@redhat.com>
1993-02-22 14:21:19 +0000

    * aix3-1.h: Changes from 18.58. 	(HAVE_TERMIOS): Changed from HAVE_TERMIO. 	(unix): Define.

--- src/s/aix3-1.h
+++ src/s/aix3-1.h
@@ -1,22 +1,21 @@
 /* Definitions file for GNU Emacs running on IBM AIX version 3.1
-   Copyright (C) 1985, 1986 Free Software Foundation, Inc.
+   Copyright (C) 1985, 1986, 1990 Free Software Foundation, Inc.
 
 This file is part of GNU Emacs.
 
-GNU Emacs is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY.  No author or distributor
-accepts responsibility to anyone for the consequences of using it
-or for whether it serves any particular purpose or works at all,
-unless he says so in writing.  Refer to the GNU Emacs General Public
-License for full details.
+GNU Emacs is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 1, or (at your option)
+any later version.
 
-Everyone is granted permission to copy, modify and redistribute
-GNU Emacs, but only under the conditions described in the
-GNU Emacs General Public License.   A copy of this license is
-supposed to have been given to you along with GNU Emacs so you
-can know your rights and responsibilities.  It should be in a
-file named COPYING.  Among other things, the copyright notice
-and this notice must be preserved on all copies.  */
+GNU Emacs is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU Emacs; see the file COPYING.  If not, write to
+the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 
 /*
@@ -77,7 +76,7 @@ and this notice must be preserved on all copies.  */
  *	for terminal control.
  */
 
-#define HAVE_TERMIO
+#define HAVE_TERMIOS
 
 /*
  *	Define HAVE_TIMEVAL if the system supports the BSD style clock values.
@@ -232,3 +231,6 @@ and this notice must be preserved on all copies.  */
 
 /* Use the gethostname system call.  */
 #define HAVE_GETHOSTNAME
+
+/* AIX doesn't define this.  */
+#define unix 1
