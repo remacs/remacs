383aee86216dfa30202f761f779b8744c0d19606
Richard M. Stallman <rms@gnu.org>
1993-03-06 06:13:50 +0000

    (te-escape-extended-command-unread): Handle any key sequence.

--- lisp/terminal.el
+++ lisp/terminal.el
@@ -223,7 +223,7 @@ Other chars following \"%s\" are interpreted as follows:\n"
 ;; not used.
 (defun te-escape-extended-command-unread ()
   (interactive)
-  (setq unread-command-events (list last-input-char))
+  (setq unread-command-events (listify-key-sequence (this-command-keys)))
   (te-escape-extended-command))
 
 (defun te-set-escape-char (c)
