07f6d4ae18f768326e24a6559d0808f5f01aec32
Richard M. Stallman <rms@gnu.org>
1993-03-06 06:06:10 +0000

    (electric-help-command-loop): Handle any kind of event.

--- lisp/ehelp.el
+++ lisp/ehelp.el
@@ -119,7 +119,7 @@ BUFFER is put into `default-major-mode' (or `fundamental-mode') when we exit"
   (catch 'exit
     (if (pos-visible-in-window-p (point-max))
 	(progn (message "<<< Press Space to bury the help buffer >>>")
-	       (if (= (setq unread-command-events (list (read-char))) ?\  )
+	       (if (eq (setq unread-command-events (list (read-event))) ?\  )
 		   (progn (setq unread-command-events nil)
 			  (throw 'exit t)))))
     (let (up down both neither
