a97beda4865c2d3f4c2341daa9d35e6e56de9ac9
Eric S. Raymond <esr@snark.thyrsus.com>
1993-03-11 08:48:43 +0000

    Added headers, changes some keycap names.

--- lisp/term/tvi970.el
+++ lisp/term/tvi970.el
@@ -1,51 +1,85 @@
-;;; Terminal support for the Televideo 970.
-;;; Jim Blandy <jimb@occs.cs.oberlin.edu>, January 1992
+;;; tvi970.el --- terminal support for the Televideo 970
 
-
-;;; Define the escape codes sent by the function keys.
+;; Author: Jim Blandy <jimb@occs.cs.oberlin.edu>, January 1992
+;; Keywords: terminals
+
+;; Copyright (C) 1992 Free Software Foundation, Inc.
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+
+;;; Commentary:
+
+;;; Uses the Emacs 19 terminal initialization features --- won't work with 18.
+
+;;; Code:
+
 (or (lookup-key function-key-map "\e[")
     (define-key function-key-map "\e[" (make-keymap)))
-(or (lookup-key function-key-map "\eO")
-    (define-key function-key-map "\eO" (make-keymap)))
+;; (or (lookup-key function-key-map "\eO")
+;;    (define-key function-key-map "\eO" (make-keymap)))
 
 ;; Miscellaneous keys
 (mapcar (function (lambda (key-binding)
 		    (define-key function-key-map
 		      (car key-binding) (nth 1 key-binding))))
-	'(("\e[H" [home])
-	  ("\e[Z" [backtab])
-	  ("\e[i" [print])
-	  ("\e[2J" [clear])
-	  ("\e[@" [insert])
-	  ("\e[P" [delete])
-	  ("\e[L" [insertline])
-	  ("\e[M" [deleteline])
-	  ("\e[K" [eraseline])
-	  ("\e[J" [erasepage])
-	  ("\e[U" [page])
-	  ("\e[g" [S-tab])
-	  ("\e[2N" [clearentry])
-	  ("\e[2K" [S-clearentry])
-	  ("\e[E" [?\C-j])
-	  ("\e[g" [S-backtab])
-	  ("\e[?1i" [S-print])
-	  ("\e[4h" [S-insert])
-	  ("\e[4l" [S-delete])
-	  ("\e[Q"  [S-insertline])
-	  ("\e[1Q" [S-deleteline])
-	  ("\e[19l" [S-eraseline])
-	  ("\e[19h" [S-erasepage])
-	  ("\e[V" [S-page])
-	  ("\eS" [send])
-	  ("\e5" [S-send])
-	  ("\eOm" [kp-subtract])
-	  ("\eOl" [kp-separator])
-	  ("\eOn" [kp-decimal])
-	  ("\eOM" [enter])
-	  ("\eOP" [kp-f1])
-	  ("\eOQ" [kp-f2])
-	  ("\eOR" [kp-f3])
-	  ("\eOS" [kp-f4])))
+	'(
+	  ;; These are set up by termcap or terminfo
+	  ;; ("\eOP"	[kp-f1])
+	  ;; ("\eOQ"	[kp-f2])
+	  ;; ("\eOR"	[kp-f3])
+	  ;; ("\eOS"	[kp-f4])
+
+	  ;; These are set by terminfo
+	  ("\e[H"	[home])
+	  ("\e[Z"	[backtab])
+	  ("\e[i"	[print])
+	  ("\e[@"	[insert])
+	  ("\e[L"	[insertline])
+	  ("\e[M"	[deleteline])
+
+	  ;; These won't be set up by either
+	  ("\eOm"	[kp-subtract])
+	  ("\eOl"	[kp-separator])
+	  ("\eOn"	[kp-decimal])
+	  ("\eOM"	[kp-enter])
+
+	  ;; These won't be set up by either either
+	  ("\e[K"	[eraseline])	;; Not an X keysym
+	  ("\e[J"	[erasepage])	;; Not an X keysym
+	  ("\e[2J"	[clear])	;; Not an X keysym
+	  ("\e[P"	[delete])	;; Not an X keysym
+	  ("\e[U"	[page])		;; Not an X keysym
+	  ("\e[g"	[S-tab])	;; Not an X keysym
+	  ("\e[2N"	[clearentry])	;; Not an X keysym
+	  ("\e[2K"	[S-clearentry])	;; Not an X keysym
+	  ("\e[E"	[?\C-j])	;; Not an X keysym
+	  ("\e[g"	[S-backtab])	;; Not an X keysym
+	  ("\e[?1i"	[S-print])	;; Not an X keysym
+	  ("\e[4h"	[S-insert])	;; Not an X keysym
+	  ("\e[4l"	[S-delete])	;; Not an X keysym
+	  ("\e[Q"	[S-insertline])	;; Not an X keysym
+	  ("\e[1Q"	[S-deleteline])	;; Not an X keysym
+	  ("\e[19l"	[S-eraseline])	;; Not an X keysym
+	  ("\e[19h"	[S-erasepage])	;; Not an X keysym
+	  ("\e[V"	[S-page])	;; Not an X keysym
+	  ("\eS"	[send])		;; Not an X keysym
+	  ("\e5"	[S-send])	;; Not an X keysym
+	  ))
+
 ;; The numeric keypad keys.
 (let ((i 0))
   (while (< i 10)
@@ -87,3 +121,5 @@ With a negative argument, select numeric keypad mode."
   (send-string-to-terminal (if tvi970-keypad-numeric "\e=" "\e>")))
 
 (tvi970-set-keypad-mode 1)
+
+;;; tv970 ends here
