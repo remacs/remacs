c3498e64386cc83fd0d13bb2f069e9379fe597a9
Jim Blandy <jimb@redhat.com>
1993-03-25 23:27:42 +0000

    * xselect.c (SELECTION_QUANTUM): Don't use XMaxRequestSize on R3; 	access the display structure directly.

--- src/xselect.c
+++ src/xselect.c
@@ -63,8 +63,11 @@ Lisp_Object Vx_sent_selection_hooks;
  */
 #define MAX_SELECTION_QUANTUM 0xFFFFFF
 
-#define SELECTION_QUANTUM(dpy) ((XMaxRequestSize (dpy) << 2) - 100)
-
+#ifdef HAVE_X11R4
+#define SELECTION_QUANTUM(dpy) ((XMaxRequestSize(dpy) << 2) - 100)
+#else
+#define SELECTION_QUANTUM(dpy) (((dpy)->max_request_size << 2) - 100)
+#endif
 
 /* The timestamp of the last input event Emacs received from the X server.  */
 unsigned long last_event_timestamp;
