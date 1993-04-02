8b20806d3cf5d0d82719e0913caffd8ce111552a
Jim Blandy <jimb@redhat.com>
1993-04-02 23:17:33 +0000

    *** empty log message ***

--- src/regex.c
+++ src/regex.c
@@ -1,5 +1,5 @@
 /* Extended regular expression matching and search library,
-   version 0.11.
+   version 0.12.
    (Implements POSIX draft P10003.2/D11.2, except for
    internationalization features.)
 
