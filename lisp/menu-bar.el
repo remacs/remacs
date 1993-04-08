1db87953e5c8532ca725a72b5676b4a1005748f5
Richard M. Stallman <rms@gnu.org>
1993-04-08 07:10:13 +0000

    entered into RCS

--- lisp/menu-bar.el
+++ lisp/menu-bar.el
@@ -1,3 +1,23 @@
+;;; menu-bar.el --- Define a default menu bar.
+;; Copyright (C) 1993 Free Software Foundation, Inc.
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+
+
 (define-key global-map [menu-bar] (make-sparse-keymap "menu-bar"))
 (setq menu-bar-help-menu (make-sparse-keymap "Help"))
 (define-key global-map [menu-bar help] (cons "Help" menu-bar-help-menu))
@@ -168,10 +188,15 @@ and selects that window."
 ;;;	       mode-name
 ;;;	       (or (buffer-file-name) ""))))))
 
+;; Give all existing frames a menu bar.
 (let ((frames (frame-list)))
   (while frames
     (modify-frame-parameters (car frames) '((menu-bar-lines . 1)))
     (setq frames (cdr frames))))
+
+;; Make frames created from now on have a menu bar.
 (or (assq 'menu-bar-lines default-frame-alist)
     (setq default-frame-alist
 	  (cons '(menu-bar-lines . 1) default-frame-alist)))
+
+;;; end of menu-bar.el
