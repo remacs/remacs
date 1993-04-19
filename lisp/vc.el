f495d400ead063b2ad26c3210edf68d9410b106d
Roland McGrath <roland@gnu.org>
1993-04-19 21:19:10 +0000

    (vc-comment-to-change-log): Restored interactive spec.  Why was it removed?  Why does the only log entry mentioning this function contain no actual information?

--- lisp/vc.el
+++ lisp/vc.el
@@ -547,9 +547,10 @@ popped up to accept a comment."
 ;;; Here is a checkin hook that may prove useful to sites using the
 ;;; ChangeLog facility supported by Emacs.
 (defun vc-comment-to-change-log (&optional file)
-   "Update change log from comments entered into VC for the current file.
+  "Update change log from comments entered into VC for the current file.
 Optional FILE specifies the change log file name; see `find-change-log'.
 See `vc-update-change-log'."
+  (interactive)
   (let ((log (find-change-log file)))
     (if log
 	(let ((default-directory (or (file-name-directory log)
