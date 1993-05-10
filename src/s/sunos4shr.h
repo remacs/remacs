16f7413a03dce7b6ddbb7998b489082af66c597b
Jim Blandy <jimb@redhat.com>
1993-05-10 00:34:31 +0000

    * s/sunos4shr.h: Apply changes from David J. Mackenzie; this isn't 	used by any configuration right now, but he's trying to make it 	work. 	#include "sunos4-1.h" instead of "bsd4-2.h". 	(O_NDELAY): Don't define this. 	(SYSTEM_MALLOC): Don't define this, either. 	(LD_SWITCH_SYSTEM): Remove the definition for this.

--- src/s/sunos4shr.h
+++ src/s/sunos4shr.h
@@ -1,16 +1,8 @@
-#include "bsd4-2.h"
+#include "sunos4-1.h"
 
 /* Say that the text segment of a.out includes the header;
    the header actually occupies the first few bytes of the text segment
    and is counted in hdr.a_text.  */
-
-#define O_NDELAY        FNDELAY /* Non-blocking I/O (4.2 style) */
-
-/* In SunOS 4.1, a static function called by tzsetwall reportedly
-   clears the byte just past an eight byte region it mallocs, corrupting
-   GNU malloc's memory pool.  But Sun's malloc doesn't seem to mind. */
-
-#define SYSTEM_MALLOC
 
 /*  Misleading!  Actually gets loaded after crt0.o */
 #define START_FILES pre-crt0.o
@@ -28,3 +20,5 @@
 #define UNEXEC	unexsunos4.o
 #define RUN_TIME_REMAP
 #define LINKER cc
+
+#undef LD_SWITCH_SYSTEM
