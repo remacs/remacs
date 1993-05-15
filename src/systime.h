d712c26dd5c1057a845c162ce61c965ff01ea01d
Jim Blandy <jimb@redhat.com>
1993-05-15 20:47:28 +0000

    * systime.h: Borrow CPP sequence from getdate.y to include the 	proper combination of <time.h> and <sys/time.h>.

--- src/systime.h
+++ src/systime.h
@@ -17,12 +17,14 @@ You should have received a copy of the GNU General Public License
 along with GNU Emacs; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
-#ifdef TM_IN_SYS_TIME
+#ifdef TIME_WITH_SYS_TIME
+#include <sys/time.h>
+#include <time.h>
+#else
+#ifdef HAVE_SYS_TIME_H
 #include <sys/time.h>
 #else
 #include <time.h>
-#ifdef HAVE_SYS_TIME_H
-#include <sys/time.h>
 #endif
 #endif
 
