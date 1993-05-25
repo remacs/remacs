e3971158d959ef3f1dd5c5e384ded5093114ed21
Richard M. Stallman <rms@gnu.org>
1993-05-25 20:52:11 +0000

    (C_SWITCH_X_SYSTEM, LD_SWITCH_SYSTEM): Put in #if 0.

--- src/s/sol2.h
+++ src/s/sol2.h
@@ -2,6 +2,7 @@
 
 #define POSIX
 
+#if 0
 /* Here is how to find X Windows.  The -L option tells the linker where
    to find the libraries at link time, the -R option at run time.  */
 #define C_SWITCH_X_SYSTEM -I/usr/openwin/include
@@ -10,6 +11,7 @@
 #else /* GCC */
 #define LD_SWITCH_SYSTEM -L/usr/openwin/lib -Xlinker -R/usr/openwin/lib
 #endif /* GCC */
+#endif /* 0 */
 
 /* Compile in non-ansi fashion to work around bugs in system header files.  */
 #ifndef __GNUC__
