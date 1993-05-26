c12710e962f40ef17c65672c1b78c762ce2a8ed1
Jim Blandy <jimb@redhat.com>
1993-05-26 23:08:55 +0000

    * s/hpux8.h (LIB_X11_LIB, C_SWITCH_SYSTEM, LD_SWITCH_SYSTEM, 	LD_SWITCH_SYSTEM, OLDXMENU_OPTIONS): Add X11R5 directories to the 	search paths in these lists; they shouldn't do any harm if they 	don't have X11R5.

--- src/s/hpux8.h
+++ src/s/hpux8.h
@@ -8,23 +8,23 @@
 
 #define HPUX8
 
-#define LIB_X11_LIB -L/usr/lib/X11R4 -lX11
-#define C_SWITCH_SYSTEM -I/usr/include/X11R4
+#define LIB_X11_LIB -L/usr/lib/X11R5 -L/usr/lib/X11R4 -lX11
+#define C_SWITCH_SYSTEM -I/usr/lib/X11R5 -I/usr/include/X11R4
 
 /* Don't use shared libraries.  unexec doesn't handle them.
    Note GCC automatically passes -a archive to ld, and it has its own
    conflicting -a.  */
 #ifdef __GNUC__
-#define LD_SWITCH_SYSTEM  -L/usr/lib/X11R4 -Xlinker -a -Xlinker archive
+#define LD_SWITCH_SYSTEM  -L/usr/lib/X11R5 -L/usr/lib/X11R4 -Xlinker -a -Xlinker archive
 
 /* No need to specify roundabout way of linking temacs.  */
 #define ORDINARY_LINK
 #else
-#define LD_SWITCH_SYSTEM -a archive  -L/usr/lib/X11R4
+#define LD_SWITCH_SYSTEM -a archive -L/usr/lib/X11R5 -L/usr/lib/X11R4
 #endif
 
 /* Specify compiler options for compiling oldXMenu.  */
-#define OLDXMENU_OPTIONS CFLAGS=-I/usr/include/X11R4
+#define OLDXMENU_OPTIONS CFLAGS=-I/usr/include/X11R5 -I/usr/include/X11R4
 
 /* Some hpux 8 machines seem to have TIOCGWINSZ,
    and none have sioctl.h, so might as well define this.  */
