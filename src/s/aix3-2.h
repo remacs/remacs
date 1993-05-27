7ad1552990092f3c0301a2bb946bf368def41b08
Jim Blandy <jimb@redhat.com>
1993-05-27 02:08:14 +0000

    * s/aix3-2.h (C_SWITCH_SYSTEM): Don't define this to be "-ma" if 	we're using GCC - that's an XLC switch.
    	* s/aix3-2.h (LIBS_SYSTEM): Put -LIM -Liconv here.

--- src/s/aix3-2.h
+++ src/s/aix3-2.h
@@ -6,7 +6,9 @@
    and that's supposedly what 3.2 will come with.  */
 #undef SPECIFY_X11R4
 
+#ifndef __GNUC__
 #define C_SWITCH_SYSTEM -ma
+#endif
 #define HAVE_ALLOCA
 #undef rindex
 #undef index
@@ -17,3 +19,8 @@
    3.2, and a cc-compiled Emacs works with this undefined.
    --karl@cs.umb.edu.  */
 #undef SYSTEM_MALLOC
+
+/*
+   IBM's X11R5 use these libraries in AIX 3.2.2.  */
+
+#define LIBS_SYSTEM -lIM -liconv
