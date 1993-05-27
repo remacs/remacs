8897c5c21cc3a62ce3c872d4e421acda4dec056e
Jim Blandy <jimb@redhat.com>
1993-05-27 03:05:02 +0000

    * s/sol2.h: (SOLARIS_BROKEN_ACCESS): Don't define this.

--- src/s/sol2.h
+++ src/s/sol2.h
@@ -23,4 +23,8 @@ If you have the misfortune to be running Solaris 2.1, you may have
 noticed that the access system call does not check the readonlyness of
 the filesystem the path refers to.  This is a bug, according to
 access(2), but in the meantime, some of us need the right behavior.  */
-#define SOLARIS_BROKEN_ACCESS
+
+/* Well, we released Emacs with this change, and fixed a typo, but
+   people keep saying that it doesn't work, and that the patch is easy
+   to install.  */
+#undef SOLARIS_BROKEN_ACCESS
