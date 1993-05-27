6f51e3df798f3ec03ae7435aafc10cfa219e2f0f
Jim Blandy <jimb@redhat.com>
1993-05-27 03:02:21 +0000

    * s/sol2.h (C_SWITCH_X_SYSTEM): Deleted. 	(LD_SWITCH_SYSTEM): Delete the -L option, leave just -R.

--- src/s/sol2.h
+++ src/s/sol2.h
@@ -2,16 +2,13 @@
 
 #define POSIX
 
-#if 0
-/* Here is how to find X Windows.  The -L option tells the linker where
-   to find the libraries at link time, the -R option at run time.  */
-#define C_SWITCH_X_SYSTEM -I/usr/openwin/include
+/* Here is how to find X Windows.  The -R option says where
+   to find X windows at run time.  */
 #ifndef __GNUC__
-#define LD_SWITCH_SYSTEM -L/usr/openwin/lib -R/usr/openwin/lib
+#define LD_SWITCH_SYSTEM -R/usr/openwin/lib
 #else /* GCC */
-#define LD_SWITCH_SYSTEM -L/usr/openwin/lib -Xlinker -R/usr/openwin/lib
+#define LD_SWITCH_SYSTEM -Xlinker -R/usr/openwin/lib
 #endif /* GCC */
-#endif /* 0 */
 
 /* Compile in non-ansi fashion to work around bugs in system header files.  */
 #ifndef __GNUC__
