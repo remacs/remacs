879c0d9f284a568b5c462f51cb9eeeaf7d2215db
Richard M. Stallman <rms@gnu.org>
1993-05-30 19:18:28 +0000

    *** empty log message ***

--- src/s/sunos413.h
+++ src/s/sunos413.h
@@ -1,5 +1,8 @@
 #include "sunos4-1.h"
 
+#if 0 /* jik@gza.com says this didn't work.  Too bad.
+	 Can anyone find out why this loses?  */
 /* The bug that corrupts GNU malloc's memory pool is fixed in SunOS 4.1.3. */
 
 #undef SYSTEM_MALLOC
+#endif
