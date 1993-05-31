cd1168e194b9883a4415e4c5720452885bf44f51
Richard M. Stallman <rms@gnu.org>
1993-05-31 22:25:20 +0000

    [__bsdi__]: Define BSD4_2.

--- src/mem-limits.h
+++ src/mem-limits.h
@@ -5,7 +5,7 @@ This file is part of GNU Emacs.
 
 GNU Emacs is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 1, or (at your option)
+the Free Software Foundation; either version 2, or (at your option)
 any later version.
 
 GNU Emacs is distributed in the hope that it will be useful,
@@ -20,6 +20,10 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 #if defined(__osf__) && (defined(__mips) || defined(mips))
 #include <sys/time.h>
 #include <sys/resource.h>
+#endif
+
+#ifdef __bsdi__
+#define BSD4_2
 #endif
 
 #ifndef BSD4_2
