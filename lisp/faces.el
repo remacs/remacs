7cf6fac18064e404a26b2d633f9cbde5f73c0123
Richard M. Stallman <rms@gnu.org>
1993-05-31 18:02:03 +0000

    (x-resolve-font-name): Clean up error messages.

--- lisp/faces.el
+++ lisp/faces.el
@@ -428,9 +428,9 @@ also the same size as FACE on FRAME."
       (let ((fonts (x-list-fonts pattern face frame)))
 	(or fonts
 	    (if face
-		(error "no fonts matching pattern are the same size as `%s'."
+		(error "No fonts matching pattern are the same size as `%s'"
 		       face)
-	      (error "no fonts match `%S'." pattern)))
+	      (error "No fonts match `%s'" pattern)))
 	(car fonts))
     (cdr (assq 'font (frame-parameters (selected-frame))))))
 
