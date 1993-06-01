2986d5d9c0058101bfbc2cc5f74068102ccf475a
Richard M. Stallman <rms@gnu.org>
1993-06-01 04:07:09 +0000

    Initial revision

---
+++ src/s/386bsd.h
@@ -0,0 +1,13 @@
+/* Get most of the stuff from bsd4.3 */
+#include "bsd4-3.h"
+
+/* The folowing should be set to /netbsd if you are running netbsd > 0.8 
+   Or just link /netbsd -> /386bsd  */
+#undef KERNEL_FILE
+#define KERNEL_FILE "/386bsd"
+
+#define PENDING_OUTPUT_COUNT(FILE) ((FILE)->_p - (FILE)->_bf._base)
+
+/* Need to use GNU make, as system make has problems */
+#define MAKE_COMMAND gmake
+#define LIBS_DEBUG
