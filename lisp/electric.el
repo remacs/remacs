7dc8eff950a11c718e9dade91060058453825c13
Richard M. Stallman <rms@gnu.org>
1993-06-03 03:48:37 +0000

    (Electric-command-loop): Use eq to compare events.

--- lisp/electric.el
+++ lisp/electric.el
@@ -58,7 +58,7 @@
 	    this-command (key-binding cmd)
 	    cmd this-command)
       (if (or (prog1 quit-flag (setq quit-flag nil))
-	      (= last-input-char ?\C-g))
+	      (eq last-input-char ?\C-g))
 	  (progn (setq unread-command-events nil
 		       prefix-arg nil)
 		 ;; If it wasn't cancelling a prefix character, then quit.
@@ -74,7 +74,7 @@
 	      (progn (command-execute cmd)
 		     (setq last-command this-command)
 		     (if (or (prog1 quit-flag (setq quit-flag nil))
-			     (= last-input-char ?\C-g))
+			     (eq last-input-char ?\C-g))
 			 (progn (setq unread-command-events nil)
 				(if (not inhibit-quit)
 				    (progn (ding)
