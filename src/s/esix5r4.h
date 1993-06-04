b9078b9d167434ea8ac6d63527381dfcef97d001
Richard M. Stallman <rms@gnu.org>
1993-06-04 05:34:47 +0000

    Redone by Joe Kelsey.

--- src/s/esix5r4.h
+++ src/s/esix5r4.h
@@ -1,13 +1,24 @@
 /* Definitions for ESIX System V 4.0.4, a variant of V.4 for the 386.  */
+/* Redone by zircon!joe@uunet.uu.net (Joe Kelsey).  */
 
 #include "usg5-4.h"
 
-#define LIB_X11_LIB -lsocket -lc -lX11
-#undef LIB_STANDARD
-#define LIB_STANDARD -lnsl -lns -lelf /usr/ucblib/libucb.a /usr/ccs/lib/crtn.o
+#define SYSTEM_MALLOC 1
+#if defined (HAVE_XFREE386)
+# undef LIB_STANDARD
+# define LIB_STANDARD -lc
+#else
+# define LIB_X11_LIB -lsocket -lc -lX11
+# undef LIB_STANDARD
+# ifdef ORDINARY_LINK
+#   define LIB_STANDARD -lnsl -lns -lelf /usr/ucblib/libucb.a
+# else
+#   define LIB_STANDARD -lnsl -lns -lelf /usr/ucblib/libucb.a /usr/ccs/lib/crtn.o
+# endif
 
 /* Resolve BSD string functions in X Window library from libucb.a.  */
-#define BSTRING
+# define BSTRING
 
-/* kelsey@mdd.comm.mot.com says this makes X windows work.  */
-#define BROKEN_FIONREAD
+/* zircon!joe says this makes X windows work.  */
+# define BROKEN_FIONREAD
+#endif
