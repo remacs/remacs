d5dd7d18cf7c08744e01900a1799a42a68ef6042
Richard M. Stallman <rms@gnu.org>
1993-06-04 05:21:01 +0000

    [__GNUC__ >= 2]: Don't include alloca.h.

--- src/m/sparc.h
+++ src/m/sparc.h
@@ -5,7 +5,7 @@ This file is part of GNU Emacs.
 
 GNU Emacs is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 1, or (at your option)
+the Free Software Foundation; either version 2, or (at your option)
 any later version.
 
 GNU Emacs is distributed in the hope that it will be useful,
@@ -81,7 +81,7 @@ NOTE-END  */
    in the file alloca.s should be used.  */
 
 #define HAVE_ALLOCA
-#if __GNUC__ >= 2 /* Modern versions of GCC handle alloca directly.  */
+#if __GNUC__ < 2 /* Modern versions of GCC handle alloca directly.  */
 #include <alloca.h>
 #endif
 
