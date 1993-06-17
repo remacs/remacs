1bf41cb0afa65fd2e39ba07ed5bdb0a08af47dea
Richard M. Stallman <rms@gnu.org>
1993-06-17 07:31:16 +0000

    entered into RCS

--- lisp/term/vt240.el
+++ lisp/term/vt240.el
@@ -1,1 +1,6 @@
+;; For our purposes we can treat the vt200 and vt100 almost alike.
+;; Most differences are handled by the termcap entry.
 (load "term/vt100" nil t)
+
+;; Make F11 an escape key.
+(define-key function-key-map "\e[23~" [?\e])
