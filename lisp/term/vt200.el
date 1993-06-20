ed8d30ae815616cd8edae3eb44488d281e57bd3f
Richard M. Stallman <rms@gnu.org>
1993-06-20 08:49:42 +0000

    Translate F11 (ESC [ 23 ~) to ESC.

--- lisp/term/vt200.el
+++ lisp/term/vt200.el
@@ -1,3 +1,6 @@
-;; For our purposes we can treat the vt200 and vt100 alike.
-;; The differences are handled by the termcap entry.
+;; For our purposes we can treat the vt200 and vt100 almost alike.
+;; Most differences are handled by the termcap entry.
 (load "term/vt100" nil t)
+
+;; Make F11 an escape key.
+(define-key function-key-map "\e[23~" [?\e])
