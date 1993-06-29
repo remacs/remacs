e788f29183f4a7c725cf2e73037810d8e9aa6d36
Jim Blandy <jimb@redhat.com>
1993-06-29 04:03:22 +0000

    *** empty log message ***

--- lisp/paren.el
+++ lisp/paren.el
@@ -1,5 +1,5 @@
 ;;; paren.el --- highlight matching paren.
-;; Copyright (C) 1993 Software Foundation, Inc.
+;; Copyright (C) 1993 Free Software Foundation, Inc.
 
 ;; This file is part of GNU Emacs.
 
