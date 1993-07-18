8a30241de33ee92f343ee7726869b5a5fc6f688f
Jim Blandy <jimb@redhat.com>
1993-07-18 06:18:03 +0000

    * ange-ftp.el: Install the correct regexp in 	file-name-handler-alist.

--- lisp/ange-ftp.el
+++ lisp/ange-ftp.el
@@ -856,7 +856,7 @@ SIZE, if supplied, should be a prime number."
 ;;;; Internal variables.
 ;;;; ------------------------------------------------------------
 
-(defconst ange-ftp-version "$Revision: 1.28 $")
+(defconst ange-ftp-version "$Revision: 1.29 $")
 
 (defvar ange-ftp-data-buffer-name " *ftp data*"
   "Buffer name to hold directory listing data received from ftp process.")
@@ -3755,7 +3755,7 @@ NEWNAME should be the name to give the new compressed or uncompressed file.")
 ;;;###autoload
 (or (assoc "^/[^/:]*\\([^/:]:\\|\\'\\)" file-name-handler-alist)
     (setq file-name-handler-alist
-	  (cons '("^/[^/:]+:" . ange-ftp-hook-function)
+	  (cons '("^/[^/:]*\\([^/:]:\\|\\'\\)" . ange-ftp-hook-function)
 		file-name-handler-alist)))
 
 ;;; The above two forms are sufficient to cause this file to be loaded
