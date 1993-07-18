af40bbfa99a32434416278aff411cd2a2dd05db4
Jim Blandy <jimb@redhat.com>
1993-07-18 06:13:07 +0000

    * wakeup.c: Use CPP tangle from autoconf manual to #include the 	correct combination of <time.h> and <sys/time.h>.

--- lib-src/wakeup.c
+++ lib-src/wakeup.c
@@ -1,9 +1,20 @@
 /* Program to produce output at regular intervals.  */
 
+#include "config.h"
+
 #include <stdio.h>
-#include <time.h>
 #include <sys/types.h>
+
+#ifdef TIME_WITH_SYS_TIME
 #include <sys/time.h>
+#include <time.h>
+#else
+#ifdef HAVE_SYS_TIME_H
+#include <sys/time.h>
+#else
+#include <time.h>
+#endif
+#endif
 
 struct tm *localtime ();
 
