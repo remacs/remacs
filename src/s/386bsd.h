1c3d8eebf512a693b1a37970b44b67acf480c5aa
Richard M. Stallman <rms@gnu.org>
1993-07-25 06:34:47 +0000

    (LOCALTIME_CACHE): Defined. (HAVE_SETSID): Defined. (LIB_STANDARD): Override, adding $(GNULIB_VAR).

--- src/s/386bsd.h
+++ src/s/386bsd.h
@@ -3,6 +3,9 @@
 /* Get most of the stuff from bsd4.3 */
 #include "bsd4-3.h"
 
+#undef LIB_STANDARD
+#define LIB_STANDARD -lc $(GNULIB_VAR)
+
 /* The following should be set to /netbsd if you are running netbsd > 0.8 
    Or just link /netbsd -> /386bsd  */
 #undef KERNEL_FILE
@@ -19,3 +22,9 @@
 
 /* This affects a declaration in xrdb.c.  */
 #define DECLARE_GETPWUID_WITH_UID_T
+
+/* Reread the time zone on startup.  */
+#define LOCALTIME_CACHE
+
+/* rich@lamprey.utmb.edu says needed for job control in subshells.  */
+#define HAVE_SETSID
