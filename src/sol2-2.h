c6e5e6e0835ee6d7ea9fa649e3b78b118c6fadf7
Richard M. Stallman <rms@gnu.org>
1993-07-27 20:21:14 +0000

    entered into RCS

--- src/sol2-2.h
+++ src/sol2-2.h
@@ -1,6 +1,6 @@
-#include "s/sol2.h"
+#include "sol2.h"
 
-/* Take care of libucb as well as X Windows.  */
+/* Take care of libucb.a as well as X Windows.  */
 #undef LD_SWITCH_SYSTEM
 #ifndef __GNUC__
 #define LD_SWITCH_SYSTEM -R/usr/openwin/lib:/usr/ucblib
@@ -8,6 +8,7 @@
 #define LD_SWITCH_SYSTEM -Xlinker -R/usr/openwin/lib:/usr/ucblib
 #endif /* GCC */
 
+/* Link with libucb.a.  */
 #ifdef LIB_STANDARD
 #undef LIB_STANDARD
 #define LIB_STANDARD -lc -L/usr/ucblib -lucb
