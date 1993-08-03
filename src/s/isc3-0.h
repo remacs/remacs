2f1895276fad9b6d979c6cba955b7eaa80020413
Richard M. Stallman <rms@gnu.org>
1993-08-03 04:30:43 +0000

    Don't mention dir `s/' in #include.

--- src/s/isc3-0.h
+++ src/s/isc3-0.h
@@ -1,8 +1,8 @@
 /* s- file for Interactive (ISC) Unix version 3.0 on the 386.  */
 
-#include "s/isc2-2.h"
+#include "isc2-2.h"
 
-/* These have been moved into s-isc2-2.h.  */
+/* These have been moved into isc2-2.h.  */
 /* #define HAVE_SOCKETS
 #define HAVE_SELECT */
 
