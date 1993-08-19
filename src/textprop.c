252e4dba33c9b2d13b540c09d91ee9b38bed696e
Roland McGrath <roland@gnu.org>
1993-08-19 20:18:46 +0000

    (F{next,previous}_single_property_change): Doc fix.

--- src/textprop.c
+++ src/textprop.c
@@ -586,6 +586,7 @@ DEFUN ("next-single-property-change", Fnext_single_property_change,
 Scans characters forward from POS till it finds\n\
 a change in the PROP property, then returns the position of the change.\n\
 The optional third argument OBJECT is the string or buffer to scan.\n\
+The property values are compared with `eq'.
 Return nil if the property is constant all the way to the end of OBJECT.\n\
 If the value is non-nil, it is a position greater than POS, never equal.")
   (pos, prop, object)
@@ -649,6 +650,7 @@ DEFUN ("previous-single-property-change", Fprevious_single_property_change,
 Scans characters backward from POS till it finds\n\
 a change in the PROP property, then returns the position of the change.\n\
 The optional third argument OBJECT is the string or buffer to scan.\n\
+The property values are compared with `eq'.
 Return nil if the property is constant all the way to the start of OBJECT.\n\
 If the value is non-nil, it is a position less than POS, never equal.")
      (pos, prop, object)
