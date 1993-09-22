08ae9134867ac4e7235d2f36ab9daf9db46b8cc9
Brian Fox <bfox@gnu.org>
1993-09-22 18:27:28 +0000

    * Added fixes from Karl Berry for ISC-2.2. (MEMMOVE_MISSING): Define. (SIGNALS_VIA_CHARACTERS): Define. (LIB_STANDARD): Do not include -lcposix. (C_SWITCH_SYSTEM) [__GNUC__]: Do not use -traditional.

--- src/s/isc2-2.h
+++ src/s/isc2-2.h
@@ -15,7 +15,7 @@
 
 
 /* This keeps the .cdbx section that gcc puts out when generating
-   stabs-in-coff output, so Emacs can be debugged.  --karl@cs.umb.edu*/
+   stabs-in-coff output, so Emacs can be debugged.  --karl@cs.umb.edu. */
 #define USG_SHARED_LIBRARIES
 
 /* We can support lock files.  */
@@ -27,12 +27,21 @@
 #define O_NDELAY O_NONBLOCK
 #define MEMORY_IN_STRING_H
 
-/* -lcposix is always needed for rename.  -lPW is only needed if not gcc.  */
-#ifdef __GNUC__
-#define LIB_STANDARD -lcposix -lc
-#else
-#define LIB_STANDARD -lPW -lcposix -lc
-#endif
+/* Tell gmalloc.c that we don't have memmove (system include files to the
+   contrary!). */
+#define MEMMOVE_MISSING
+
+/* Send a signal to a subprocess by "typing" a signal character. */
+#define SIGNALS_VIA_CHARACTERS
+
+/* -lPW is only needed if not using Gcc.  We used to include -lcposix here
+   for the rename function, but some pepople saus ISC's renames doesn't
+   work correctly with Emacs so we use Emacs' emulation instead. */
+#if defined (__GNUC__)
+#  define LIB_STANDARD -lcposix -lc
+#else /* !__GNUC__ */
+#  define LIB_STANDARD -lPW -lc
+#endif /* !__GNUC__ */
 
 /* mt00@etherm.co.uk says this is needed for process.c.  */
 #define USE_UTIME
@@ -57,9 +66,11 @@
    unconditionally define this.  */
 #define NO_ASM
 
-/* The POSIX-ified system headers don't work with GCC.  */
-#ifdef __GNUC__
-#define C_SWITCH_SYSTEM -traditional
+/* -traditional is not necessary if the system header files are fixed to
+   define getc and putc in the absence of _POSIX_SOURCE.  GCC's from 2.4.4
+   on do this. */
+#if !defined (__GNUC__) || __GNUC__ < 2
+#  define C_SWITCH_SYSTEM -traditional
 #endif
 
 /* Some versions of ISC are said to define S_IFLNK even tho
