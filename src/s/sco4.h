c5cb15cb104b3a3020620fd6cceedc847166579f
Richard M. Stallman <rms@gnu.org>
1993-10-08 15:28:23 +0000

    (BSTRING): Define only if HAVE_BCOPY.

--- src/s/sco4.h
+++ src/s/sco4.h
@@ -57,8 +57,10 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 #define PTY_TTY_NAME_SPRINTF \
   sprintf (pty_name, "/dev/ttyp%d", i);
 
-/* SCO has bcopy, et. al.  */
+/* SCO perhaps has bcopy, et. al.  */
+#ifdef HAVE_BCOPY
 #define BSTRING
+#endif
 
 /* Sockets are an option on SCO.  If you have X, you have them.
    They also exist if you have TCP, but we don't know how to test
