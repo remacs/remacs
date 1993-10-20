13726a3bfce7e85a1662c5145f21eee578fcab2d
Richard M. Stallman <rms@gnu.org>
1993-10-20 18:02:46 +0000

    Initial revision

---
+++ lisp/cdl.el
@@ -0,0 +1,40 @@
+;;; cdl.el -- Common Data Language (CDL) utility functions for Gnu Emacs
+
+;; Copyright (C) 1993 Free Software Foundation, Inc.
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+
+;;; Code:
+
+(defun cdl-get-file (filename)
+  "Run file through ncdump and insert result into buffer after point."
+  (interactive "fCDF file: ")
+  (message "ncdump in progress...")
+  (let ((start (point)))
+    (call-process  "ncdump" nil t nil (expand-file-name filename))
+    (goto-char start))
+  (message "ncdump in progress...done"))
+
+(defun cdl-put-region (filename start end)
+  "Run region through ncgen and write results into a file."
+  (interactive "FNew CDF file: \nr")
+  (message "ncgen in progress...")
+  (call-process-region start end "ncgen"
+		       nil nil nil "-o" (expand-file-name filename))
+  (message "ncgen in progress...done"))
+
+;;; cdl.el ends here.
