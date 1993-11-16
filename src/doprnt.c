6d291527afe29bdc9b96cf660c56b8bc634b9b19
Richard M. Stallman <rms@gnu.org>
1993-11-16 05:45:11 +0000

    Include config.h.

--- src/doprnt.c
+++ src/doprnt.c
@@ -20,6 +20,7 @@ along with GNU Emacs; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 
+#include <config.h>
 #include <stdio.h>
 #include <ctype.h>
 
