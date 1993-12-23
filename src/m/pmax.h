0d4c1a6e0b4ef4ff6e5fbb4b5b42fdb47add6bd3
Richard M. Stallman <rms@gnu.org>
1993-12-23 02:08:11 +0000

    (SET_CHILD_PTY_PGRP): Defined.

--- src/m/pmax.h
+++ src/m/pmax.h
@@ -79,3 +79,6 @@ NOTE-END  */
    Using the MIT X11 distribution instead of the one provided by Dec will
    also solve the problem, but I doubt you can convince everyone to do this. */
 #define C_SWITCH_X_MACHINE -DNeedFunctionPrototypes=0
+
+/* Enable a fix in process.c.  */
+#define SET_CHILD_PTY_PGRP
