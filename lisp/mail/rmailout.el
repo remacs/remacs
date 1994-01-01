5cb1595fb3d9ec9b63ef322d3750aba9320331f7
Richard M. Stallman <rms@gnu.org>
1994-01-01 08:31:33 +0000

    (rmail-output-to-rmail-file): If just a dir is specified, use default name in specified dir.

--- lisp/mail/rmailout.el
+++ lisp/mail/rmailout.el
@@ -61,16 +61,19 @@ starting with the current one.  Deleted messages are skipped and don't count."
 	     ;; If not suggestions, use same file as last time.
 	     (or answer rmail-last-rmail-file))))
      (list (setq rmail-last-rmail-file
-		 (expand-file-name
-		  (or
-		   (read-file-name
-		    (concat "Output message to Rmail file: (default "
-			    (file-name-nondirectory default-file)
-			    ") ")
-		    (file-name-directory default-file)
-		    default-file)
-		   default-file)
-		  (file-name-directory default-file)))
+		 (let ((read-file
+			(read-file-name
+			 (concat "Output message to Rmail file: (default "
+				 (file-name-nondirectory default-file)
+				 ") ")
+			 (file-name-directory default-file)
+			 default-file)))
+		   (if (file-directory-p read-file)
+		       (expand-file-name (file-name-nondirectory default-file)
+					 read-file)
+		     (expand-file-name
+		      (or read-file default-file)
+		      (file-name-directory default-file)))))
 	   (prefix-numeric-value current-prefix-arg))))
   (or count (setq count 1))
   (setq file-name
