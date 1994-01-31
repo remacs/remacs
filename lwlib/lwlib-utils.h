ab9d3e3c85c284fb4efde1a78ebab37ada89516b
Richard M. Stallman <rms@gnu.org>
1994-01-31 04:34:11 +0000

    entered into RCS

--- lwlib/lwlib-utils.h
+++ lwlib/lwlib-utils.h
@@ -1,20 +1,20 @@
 #ifndef _LWLIB_UTILS_H_
 #define _LWLIB_UTILS_H_
 
-void XtNoClearRefreshWidget (Widget);
+void XtNoClearRefreshWidget (/* Widget */);
 
-typedef void (*XtApplyToWidgetsProc) (Widget, XtPointer);
-typedef void* (*XtApplyUntilToWidgetsProc) (Widget, XtPointer);
+typedef void (*XtApplyToWidgetsProc) (/* Widget, XtPointer */);
+typedef void* (*XtApplyUntilToWidgetsProc) (/* Widget, XtPointer */);
 
-void XtApplyToWidgets (Widget, XtApplyToWidgetsProc, XtPointer);
-void *XtApplyUntilToWidgets (Widget, XtApplyUntilToWidgetsProc, XtPointer);
+void XtApplyToWidgets (/* Widget, XtApplyToWidgetsProc, XtPointer */);
+void *XtApplyUntilToWidgets (/* Widget, XtApplyUntilToWidgetsProc, XtPointer */);
 
-Widget *XtCompositeChildren (Widget, unsigned int *);
+Widget *XtCompositeChildren (/* Widget, unsigned int * */);
 
 /* returns True is the widget is being destroyed, False otherwise */
 Boolean
-XtWidgetBeingDestroyedP (Widget widget);
+XtWidgetBeingDestroyedP (/* Widget widget */);
 
-void XtSafelyDestroyWidget (Widget);
+void XtSafelyDestroyWidget (/* Widget */);
 
 #endif /* _LWLIB_UTILS_H_ */
