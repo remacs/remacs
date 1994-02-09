c7d7ed2771c270d51951ff97f2655abdb2aed932
Richard M. Stallman <rms@gnu.org>
1994-02-09 05:36:48 +0000

    (disabled-command-hook): Use eq to compare elts of this-command-keys.

--- lisp/novice.el
+++ lisp/novice.el
@@ -42,7 +42,7 @@
   (let (char)
     (save-window-excursion
      (with-output-to-temp-buffer "*Help*"
-       (if (= (aref (this-command-keys) 0) ?\M-x)
+       (if (eq (aref (this-command-keys) 0) ?\M-x)
 	   (princ "You have invoked the disabled command ")
 	 (princ "You have typed ")
 	 (princ (key-description (this-command-keys)))
