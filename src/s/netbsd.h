e7e0c779e1b0761ee89fd3f6bae93b3cfbca0949
Charles Hannum <mycroft@gnu.org>
1994-02-11 17:52:05 +0000

    Use -ltermcap, not Emacs's own version.  #undef symbols used for replacement getloadavg(), since we have our own.  Don't redefine symbols from a.out.h if they already exit (i.e. this is NetBSD >0.9).

--- src/s/netbsd.h
+++ src/s/netbsd.h
@@ -3,14 +3,15 @@
 /* Get most of the stuff from bsd4.3 */
 #include "bsd4-3.h"
 
+/* For mem-limits.h.  */
+#define BSD4_2
+
 #undef SYSTEM_TYPE
 #define SYSTEM_TYPE "netbsd"
 
 #undef KERNEL_FILE
-#define KERNEL_FILE "/netbsd"
-
 #undef LDAV_SYMBOL
-#define LDAV_SYMBOL "_averunnable"
+#define HAVE_GETLOADAVG
 
 #define SIGNALS_VIA_CHARACTERS
 
@@ -22,14 +23,9 @@
 #define HAVE_SETSID
 
 #define LIBS_DEBUG
+/* -lutil is not needed for NetBSD >0.9.  */
 #define LIBS_SYSTEM -lutil
-
-#define HAVE_GETLOADAVG
-
-/* For mem-limits.h.  */
-#define BSD4_2
-
-#define TERMCAP_NAME "/usr/share/misc/termcap"
+#define LIBS_TERMCAP -ltermcap
 
 #define SYSV_SYSTEM_DIR
 
@@ -37,9 +33,14 @@
    whichever is the default for `cc -nostdlib'.  */
 #define BROKEN_START
 #define TEXT_START ({ extern void start() asm ("start"); &start; })
+
 #define START_FILES pre-crt0.o /usr/lib/crt0.o
-#define UNEXEC	unexsunos4.o
+#define UNEXEC unexsunos4.o
 #define RUN_TIME_REMAP
+
+/* Try to make this work for both 0.9 and >0.9.  */
+#ifndef N_TRELOFF
 #define N_PAGSIZ(x) __LDPGSZ
 #define N_BSSADDR(x) (N_ALIGN(x, N_DATADDR(x)+x.a_data))
 #define N_TRELOFF(x) N_RELOFF(x)
+#endif
