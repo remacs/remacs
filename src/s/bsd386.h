8ffcf3f1fbdb95ad9768344c71b58df055d7f449
Richard M. Stallman <rms@gnu.org>
1994-02-12 00:30:26 +0000

    *** empty log message ***

--- src/s/bsd386.h
+++ src/s/bsd386.h
@@ -8,8 +8,6 @@
 #define A_TEXT_OFFSET(x)    (sizeof (struct exec))
 #define A_TEXT_SEEK(hdr) (N_TXTOFF(hdr) + A_TEXT_OFFSET(hdr))
 
-#define HAVE_SETSID
-
 #define LIBS_DEBUG
 #define LIB_X11_LIB -L/usr/X11/lib -lX11
 #define LIBS_SYSTEM -lutil -lkvm -lcompat
