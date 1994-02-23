7aa5f3bc6da3b9174e8b8e3d941d562c5c4e627f
Richard M. Stallman <rms@gnu.org>
1994-02-23 19:47:30 +0000

    [NEWSOS5] (C_DEBUG_SWITCH, C_OPTIMIZE_SWITCH) (LD_SWITCH_MACHINE): Definitions moved here.

--- src/m/news-risc.h
+++ src/m/news-risc.h
@@ -2,11 +2,27 @@
 
 #include "mips.h"
 
+#ifdef NEWSOS5
+
+/* NEWS-OS 5.0.2 */
+
+#define LIBS_MACHINE -lmld
+
+#ifdef __GNUC__
+#define C_DEBUG_SWITCH -g
+#define C_OPTIMIZE_SWITCH -g -O
+#define LD_SWITCH_MACHINE -g -Xlinker -D -Xlinker 800000
+#else
+#define C_DEBUG_SWITCH -g3
+#define C_OPTIMIZE_SWITCH -g3
+#define LD_SWITCH_MACHINE -g3 -D 800000 -non_shared
+#endif
+
+#else /* not NEWSOS5 */
+
 /* The following line tells the configuration script what sort of 
    operating system this machine is likely to run.
    USUAL-OPSYS="bsd4-3"  */
-
-#define LIBS_MACHINE -lmld
 
 #define COFF
 #undef LD_SWITCH_MACHINE
@@ -25,3 +41,6 @@
 /* Don't use the definitions in m/mips.h.  */
 #undef LINKER
 #undef LIBS_MACHINE
+#define LIBS_MACHINE -lmld
+
+#endif /* not NEWSOS5 */
