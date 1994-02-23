1c5ceaf673898137f03c2851f4befb4fc2b2b89a
Richard M. Stallman <rms@gnu.org>
1994-02-23 19:47:45 +0000

    Include usg5-4.h. (LIBS_SYSTEM, LINKER, START_FILES, LIB_STANDARD): Definitions moved here.

--- src/s/newsos5.h
+++ src/s/newsos5.h
@@ -1,5 +1,5 @@
-/* Definitions file for GNU Emacs running on Sony's NEWS-OS 5.0.1
-   Copyright (C) 1992 Free Software Foundation, Inc.
+/* Definitions file for GNU Emacs running on Sony's NEWS-OS 5.0.2
+   Copyright (C) 1992, 1994 Free Software Foundation, Inc.
 
 This file is part of GNU Emacs.
 
@@ -19,7 +19,7 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 /* Use the SysVr4 file for at least base configuration. */
 
-#include "s-usg5-4.h"
+#include "usg5-4.h"
 
 #define NEWSOS5
 
@@ -27,10 +27,21 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 #undef START_FILES
 #undef LIB_STANDARD
 
-/* Disable use of "unexelf.c" and shared libraries,
-   because NEWS-OS on MIPS doesn't support ELF. */
+#undef LIBS_SYSTEM
+#define LIBS_SYSTEM -lsocket -lnsl -lgen
+
+/* Disable use of "unexelf.c" and shared libraries, because
+   "unexelf.c" doesn't work correctly on NEWS-OS.  "unexmips.c" does
+   work correctly if the program is linked statically without ELF. */
 #undef UNEXEC
 #undef USG_SHARED_LIBRARIES
+
+/* Use `ld' directly rather than ordinary link, because ordinary link
+   can't produce a non-ELF executable.  */
+#undef ORDINARY_LINK
+#define LINKER /usr/lib/cmplrs/cc/ld
+#define START_FILES pre-crt0.o /usr/ccs/lib/crt1.o
+#define LIB_STANDARD -lc /usr/ccs/lib/crtn.o /usr/ccs/lib/values-Xt.o
 
 #ifndef HAVE_SOCKETS
 #define HAVE_SOCKETS
