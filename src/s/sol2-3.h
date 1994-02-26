b1d07009bf9537461790fdf3192ac0fcb27e1b42
Richard M. Stallman <rms@gnu.org>
1994-02-26 20:49:21 +0000

    (LD_SWITCH_SYSTEM): New definitions, with #undef.

--- src/s/sol2-3.h
+++ src/s/sol2-3.h
@@ -2,3 +2,13 @@
 
 /* Solaris 2.3 has a bug in XListFontsWithInfo.  */
 #define BROKEN_XLISTFONTSWITHINFO
+
+/* Override LD_SWITCH_SYSTEM: add  -L /usr/ccs/lib to the sol2.h value.  */
+
+#undef LD_SWITCH_SYSTEM
+
+#ifndef __GNUC__
+#define LD_SWITCH_SYSTEM -L /usr/ccs/lib LD_SWITCH_X_SITE_AUX
+#else /* GCC */
+#define LD_SWITCH_SYSTEM -L /usr/ccs/lib -Xlinker LD_SWITCH_X_SITE_AUX
+#endif /* GCC */
