f2ac12d570e415ac054846cb4b9e74c6acc7b00d
Karl Heuer <kwzh@gnu.org>
1994-03-02 03:19:12 +0000

    Use -g with the native compiler.

--- src/s/aix3-2.h
+++ src/s/aix3-2.h
@@ -21,3 +21,11 @@
    3.2, and a cc-compiled Emacs works with this undefined.
    --karl@cs.umb.edu.  */
 #undef SYSTEM_MALLOC
+
+/* For AIX, it turns out compiling emacs under AIX 3.2.4 REQUIRES "cc -g"
+   because "cc -O" crashes. Under AIX 3.2.5, "cc -O" is required because
+   "cc -g" crashes. Go figure.  --floppy@merlin.mit.edu */
+#ifndef __GNUC__
+#define C_SWITCH_DEBUG -g
+#define C_SWITCH_OPTIMIZE
+#endif
