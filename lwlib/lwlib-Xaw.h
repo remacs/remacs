15ba97a047472b9b6056f41425ffebad8fdbb430
Fred Pierresteguy <F.Pierresteguy@frcl.bull.fr>
1994-03-14 17:02:21 +0000

    entered into RCS

--- lwlib/lwlib-Xaw.h
+++ lwlib/lwlib-Xaw.h
@@ -6,26 +6,24 @@
 extern widget_creation_entry xaw_creation_table [];
 
 Widget 
-xaw_create_dialog (widget_instance* instance);
+xaw_create_dialog ();
 
 Boolean
-lw_xaw_widget_p (Widget widget);
+lw_xaw_widget_p ();
 
 void
-xaw_update_one_widget (widget_instance *instance, Widget widget,
-		       widget_value *val, Boolean deep_p);
+xaw_update_one_widget ();
 
 void
-xaw_update_one_value (widget_instance* instance, Widget widget,
-		      widget_value* val);
+xaw_update_one_value ();
 
 void
-xaw_destroy_instance (widget_instance* instance);
+xaw_destroy_instance ();
 
 void
-xaw_popup_menu (Widget widget);
+xaw_popup_menu ();
 
 void
-xaw_pop_instance (widget_instance* instance, Boolean up);
+xaw_pop_instance ();
 
 #endif /* LWLIB_XAW_H */
