c0096c73d7638eb7f172bd953fceccab199b4414
Richard M. Stallman <rms@gnu.org>
1994-05-05 19:18:57 +0000

    Don't use netbsd.h.  Do all that right here.

--- src/s/freebsd.h
+++ src/s/freebsd.h
@@ -6,8 +6,76 @@
 #define NO_SHARED_LIBS
 #endif
 
+
+#if 0 /* This much, alone, seemed sufficient as of 19.23.
+	 But it seems better to be independent of netbsd.h.  */
 #include "netbsd.h"
 
 #undef LIB_GCC
 #define LIB_GCC -lgcc
 #undef NEED_ERRNO
+#endif /* 0 */
+
+
+/* Get most of the stuff from bsd4.3 */
+#include "bsd4-3.h"
+
+/* For mem-limits.h. */
+#define BSD4_2
+
+/* thses aren't needed, since we have getloadavg() */
+#undef KERNEL_FILE
+#undef LDAV_SYMBOL
+
+#define PENDING_OUTPUT_COUNT(FILE) ((FILE)->_p - (FILE)->_bf._base)
+
+#define A_TEXT_OFFSET(x) (sizeof (struct exec))
+#define A_TEXT_SEEK(hdr) (N_TXTOFF(hdr) + A_TEXT_OFFSET(hdr))
+
+#define LIBS_DEBUG
+#define LIBS_SYSTEM -lutil
+#define LIBS_TERMCAP -ltermcap
+#define LIB_GCC -lgcc
+
+/* Reread the time zone on startup. */
+#define LOCALTIME_CACHE
+
+#define SYSV_SYSTEM_DIR
+
+/* freebsd has POSIX-style pgrp behavior. */
+#undef BSD_PGRPS
+
+#ifndef NO_SHARED_LIBS
+/* These definitions should work for either dynamic or static linking,
+   whichever is the default for `cc -nostdlib'. */
+#define HAVE_TEXT_START		/* No need to define `start_of_text'. */
+#define START_FILES pre-crt0.o /usr/lib/crt0.o
+#define UNEXEC unexsunos4.o
+#define RUN_TIME_REMAP
+
+#ifndef N_TRELOFF
+#define N_PAGSIZ(x) __LDPGSZ
+#define N_BSSADDR(x) (N_ALIGN(x, N_DATADDR(x)+x.a_data))
+#define N_TRELOFF(x) N_RELOFF(x)
+#endif
+#endif /* not NO_SHARED_LIBS */
+
+#define HAVE_WAIT_HEADER
+#define HAVE_GETLOADAVG
+#define HAVE_TERMIOS
+#define NO_TERMIO
+
+/* freebsd uses OXTABS instead of the expected TAB3. */
+#define TABDLY OXTABS
+#define TAB3 OXTABS
+
+/* this silences a few compilation warnings */
+#undef BSD
+#define BSD 199103
+
+#define WAITTYPE int
+/* get this since it won't be included if WAITTYPE is defined */
+#ifdef emacs
+#include <sys/wait.h>
+#endif
+#define WRETCODE(w) (_W_INT(w) >> 8)
