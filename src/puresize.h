21f4aec6915b5543b6630f7deda6b065aae919df
Richard M. Stallman <rms@gnu.org>
1994-05-07 02:26:40 +0000

    (PURESIZE): Increase to 235000.

--- src/puresize.h
+++ src/puresize.h
@@ -29,7 +29,7 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 #ifndef PURESIZE
 #ifdef MULTI_FRAME
-#define PURESIZE 232000
+#define PURESIZE 235000
 #else
 #define PURESIZE 200000
 #endif
