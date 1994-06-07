4808385995bac27216803cb4f66a4a51960bf7d4
Richard M. Stallman <rms@gnu.org>
1994-06-07 15:36:36 +0000

    *** empty log message ***

--- src/m/mips-siemens.h
+++ src/m/mips-siemens.h
@@ -1,15 +1,5 @@
-#include "mips.h"
+#include "pyramid.h"
 
-
-/* Data type of load average, as read out of kmem.  */
-
-#undef LOAD_AVE_TYPE
-#define LOAD_AVE_TYPE double
-
-/* Convert that into an integer that is 100 for a load average of 1.0  */
-
-#undef LOAD_AVE_CVT
-#define LOAD_AVE_CVT(x) ((int) ((x) * 100.0))
 
 /* Don't use the ordinary -g for debugging in cc */
 
@@ -23,4 +13,8 @@
 
 #define BSTRING
 
-#define LIB_STANDARD /usr/ccs/lib/libc.a
+#define NO_ARG_ARRAY
+
+#undef BROKEN_FIONREAD
+
+#define HAVE_ALLOCA
