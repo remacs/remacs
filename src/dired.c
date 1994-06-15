3964b9a75d5a7e3c721fb7b4dbc924791c6b5dab
Richard M. Stallman <rms@gnu.org>
1994-06-15 03:43:43 +0000

    Put stdio.h, sys/types.h and sys/stat.h after config.h.

--- src/dired.c
+++ src/dired.c
@@ -18,11 +18,11 @@ along with GNU Emacs; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 
+#include <config.h>
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-
-#include <config.h>
 
 #ifdef VMS
 #include <string.h>
