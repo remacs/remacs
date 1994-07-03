de0724367ce37e0fd26c033dd30316567a14266f
Richard M. Stallman <rms@gnu.org>
1994-07-03 18:47:37 +0000

    Use add-hook, (provide 'meese). (protect-innocence-hook): compare expanded file names for the sake of non-unix file systems.  Use expand-file-name instead of concat to create "celibacy.1" file name.  Check that the "sex.6" exists.

--- lisp/play/meese.el
+++ lisp/play/meese.el
@@ -9,18 +9,19 @@
 ;;; Code:
 
 (defun protect-innocence-hook ()
-  (if (and (equal (file-name-nondirectory buffer-file-name) "sex.6")
-	   (not (y-or-n-p "Are you over 18? ")))
-      (progn
-	(clear-visited-file-modtime)
-	(setq buffer-file-name (concat (file-name-directory buffer-file-name)
-				       "celibacy.1"))
-	(let (buffer-read-only)	; otherwise (erase-buffer) may bomb.
-	  (erase-buffer)
-	  (insert-file-contents buffer-file-name t))
-	(rename-buffer (file-name-nondirectory buffer-file-name)))))
+  (let ((dir (file-name-directory buffer-file-name)))
+    (if (and (equal buffer-file-name (expand-file-name "sex.6" dir))
+	     (file-exists-p buffer-file-name)
+	     (not (y-or-n-p "Are you over 18? ")))
+	(progn
+	  (clear-visited-file-modtime)
+	  (setq buffer-file-name (expand-file-name "celibacy.1" dir))
+	  (let ((inhibit-read-only t))	; otherwise (erase-buffer) may bomb.
+	    (erase-buffer)
+	    (insert-file-contents buffer-file-name t))
+	  (rename-buffer (file-name-nondirectory buffer-file-name))))))
 
-(or (memq 'protect-innocence-hook find-file-hooks)
-    (setq find-file-hooks (cons 'protect-innocence-hook find-file-hooks)))
+(add-hook 'find-file-hooks 'protect-innocence-hook)
+(provide 'meese)
 
 ;;; meese.el ends here
