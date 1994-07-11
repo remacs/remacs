52ae2b425612bea551fac9d3274d8a9e6ed257f2
Richard M. Stallman <rms@gnu.org>
1994-07-11 21:26:44 +0000

    (lisp-complete-symbol): Bind completion-fixup-function.

--- lisp/emacs-lisp/lisp.el
+++ lisp/emacs-lisp/lisp.el
@@ -277,7 +277,12 @@ function definitions, values or properties are considered."
 	   (insert completion))
 	  (t
 	   (message "Making completion list...")
-	   (let ((list (all-completions pattern obarray predicate)))
+	   (let ((list (all-completions pattern obarray predicate))
+		 (completion-fixup-function
+		  (function (lambda () (if (save-excursion
+					     (goto-char (max (point-min) (- (point) 4)))
+					     (looking-at " <f>"))
+					   (forward-char -4))))))
 	     (or (eq predicate 'fboundp)
 		 (let (new)
 		   (while list
