db18af03cfa402a61fc879f4433d4eb99b3de8b1
Richard M. Stallman <rms@gnu.org>
1994-07-19 22:16:18 +0000

    (EmacsFrameSetCharSize): Delete the code to make rows at least 3.

--- src/widget.c
+++ src/widget.c
@@ -880,7 +880,6 @@ EmacsFrameSetCharSize (widget, columns, rows)
   int ac = 0;
   
   if (columns < 3) columns = 3;  /* no way buddy */
-  if (rows < 3) rows = 3;
 
   check_frame_size (f, &rows, &columns);
   f->display.x->vertical_scroll_bar_extra
