a17c29e05e587d07b755d96d9cba924036bc7f91
Richard M. Stallman <rms@gnu.org>
1994-07-25 00:34:55 +0000

    (c++-mode-syntax-table): Give RET the same syntax as newline.

--- lisp/progmodes/cplus-md.el
+++ lisp/progmodes/cplus-md.el
@@ -125,7 +125,8 @@
   (setq c++-mode-syntax-table (copy-syntax-table c-mode-syntax-table))
   (modify-syntax-entry ?* ". 23b" c++-mode-syntax-table)
   (modify-syntax-entry ?/ ". 124" c++-mode-syntax-table)
-  (modify-syntax-entry ?\n ">" c++-mode-syntax-table))
+  (modify-syntax-entry ?\n ">" c++-mode-syntax-table)
+  (modify-syntax-entry ?\^m ">" c++-mode-syntax-table))
 
 (defvar c++-continued-member-init-offset nil
   "*Extra indent for continuation lines of member inits;
