d1fa6affefe6ad289f87d07059a53a47a553aba4
Richard M. Stallman <rms@gnu.org>
1994-07-29 20:00:00 +0000

    (hide-ifdefs): Doc fix.

--- lisp/progmodes/hideif.el
+++ lisp/progmodes/hideif.el
@@ -914,7 +914,7 @@ Assume that defined symbols have been added to `hide-ifdef-env'.
 The text hidden is the text that would not be included by the C
 preprocessor if it were given the file with those symbols defined.
 
-Turn off hiding by calling `show-ifdef'."
+Turn off hiding by calling `show-ifdefs'."
 
   (interactive)
   (message "Hiding...")
