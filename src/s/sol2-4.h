da6aae60a479603fbe762d14409492fba6a3e10a
Richard M. Stallman <rms@gnu.org>
1994-08-07 07:58:18 +0000

    (PTY_TTY_NAME_SPRINTF): New definition. (sigsetmask): Add #undef.
    (SOLARIS2_4): Defined.
    
    Discard old contents of file.  Instead, undef C_SWITCH_SYSTEM and
    const, so that we use GCC without -traditional.
    (HAVE_VFORK): Defined.

--- src/s/sol2-4.h
+++ src/s/sol2-4.h
@@ -1,11 +1,35 @@
 /* Handle Solaris 2.4.  */
 
+#include "sol2-3.h"
+
+#define SOLARIS2_4
+
+/* Get rid of -traditional and let const really do its thing.  */
+
 #ifdef __GNUC__
-#ifdef i386
-/* Since we use gcc -traditional, we have to work around a bogus "volatile" in
-   <sys/machlock.h>.  Must do this before #include "sol2-3.h".  */
-#define volatile
-#endif /* i386 */
+#undef C_SWITCH_SYSTEM
+#undef const
 #endif /* __GNUC__ */
 
-#include "sol2-3.h"
+#define HAVE_VFORK
+
+/* solaris does POSIX signals.  this bit is from s/usg-5-4-2.h */
+
+#define POSIX_SIGNALS
+#undef sigsetmask
+#undef PTY_TTY_NAME_SPRINTF
+#define PTY_TTY_NAME_SPRINTF			\
+  {						\
+    char *ptsname(), *ptyname;			\
+						\
+    sigblock(sigmask(SIGCLD));			\
+    if (grantpt(fd) == -1)			\
+      fatal("could not grant slave pty");	\
+    sigunblock(sigmask(SIGCLD));		\
+    if (unlockpt(fd) == -1)			\
+      fatal("could not unlock slave pty");	\
+    if (!(ptyname = ptsname(fd)))		\
+      fatal ("could not enable slave pty");	\
+    strncpy(pty_name, ptyname, sizeof(pty_name)); \
+    pty_name[sizeof(pty_name) - 1] = 0;		\
+  }
