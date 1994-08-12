e7b00662769f10da7a026d2dd4d529cbc20202f9
Karl Heuer <kwzh@gnu.org>
1994-08-12 23:12:32 +0000

    NEED_SIOCTL: moved to isc-3.0.h.

--- src/s/isc4-0.h
+++ src/s/isc4-0.h
@@ -1,11 +1,8 @@
-/* This is needed for ISC 4.0, but won't be needed for 4.1.  */
-
 #include "isc3-0.h"
 
 #define LIBS_SYSTEM -linet
 
 #define ISC4_0
-#define NEED_SIOCTL
 
 /* fmcphers@csugrad.cs.vt.edu reported this was necessary.
    He used GCC.  I don't know what is needed with other compilers.  */
