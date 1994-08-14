212ea30525ded31a9cbb3b257781a856d536c37b
Richard M. Stallman <rms@gnu.org>
1994-08-14 02:47:35 +0000

    (pid_t): New definition. (vfork): Definition deleted.

--- src/s/sol2-4.h
+++ src/s/sol2-4.h
@@ -4,10 +4,12 @@
 
 #define SOLARIS2_4
 
-/* Solaris 2.4 has a broken vfork.  And a header file has a declaration 
+/* Solaris 2.4 has a broken vfork.  So we don't use it;
+   we use the alternate definition in sysdep.c.
+   But a header file has a declaration 
    that conflicts with the definition of vfork in sysdep.c.
-   This definition should avoid it.  */
-#define vfork emacs_vfork
+   This kludge should prevent the conflict.  */
+#define pid_t int
 
 /* Get rid of -traditional and let const really do its thing.  */
 
