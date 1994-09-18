712043beeddd8734cbb96cc9fef973b7cac1718d
Richard M. Stallman <rms@gnu.org>
1994-09-18 20:04:00 +0000

    (HAVE_VFORK): Defined. (C_SWITCH_SYSTEM): Deleted. (const): Definition deleted.

--- src/s/sol2.h
+++ src/s/sol2.h
@@ -23,21 +23,4 @@
 #define LD_SWITCH_SYSTEM `./prefix-args -Xlinker LD_SWITCH_X_SITE_AUX`
 #endif /* GCC */
 
-/* Compile in non-ansi fashion to work around bugs in system header files.  */
-#ifndef __GNUC__
-#define C_SWITCH_SYSTEM -Xs
-#else /* GCC */
-#define C_SWITCH_SYSTEM -traditional
-#endif /* GCC */
-#define const
-
-/* Karl Berry writes:
-If you have the misfortune to be running Solaris 2.1, you may have
-noticed that the access system call does not check the readonlyness of
-the filesystem the path refers to.  This is a bug, according to
-access(2), but in the meantime, some of us need the right behavior.  */
-
-/* Well, we released Emacs with this change, and fixed a typo, but
-   people keep saying that it doesn't work, and that the patch is easy
-   to install.  Patch number is 100947-02.  */
-#undef SOLARIS_BROKEN_ACCESS
+#define HAVE_VFORK
