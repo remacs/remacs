e4f74c7e882531505a0da7c24a19a9a3ade20bda
Richard M. Stallman <rms@gnu.org>
1994-09-20 03:18:33 +0000

    (PURESIZE_RATIO, BASE_PURESIZE): Defined. (PURESIZE): Define it based on those.

--- src/puresize.h
+++ src/puresize.h
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with GNU Emacs; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
-/* # bytes of pure Lisp code to leave space for.
+/* Define PURESIZE, the number of bytes of pure Lisp code to leave space for.
 
    At one point, this was defined in config.h, meaning that changing
    PURESIZE would make Make recompile all of Emacs.  But only a few
@@ -27,12 +27,28 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
    whether we are running X windows, which tells us how much pure
    storage to allocate.  */
 
-#ifndef PURESIZE
+/* First define a measure of the amount of data we have.  */
+
+#ifndef BASE_PURESIZE
 #ifdef MULTI_FRAME
-#define PURESIZE 255000
+#define BASE_PURESIZE 255000
 #else
-#define PURESIZE 208000
+#define BASE_PURESIZE 208000
 #endif
+#endif
+
+/* Increase BASE_PURESIZE by a ratio depending on the machine's word size.  */
+#ifndef PURESIZE_RATIO
+#if VALBITS + GCTYPEBITS + 1 > 32
+#define PURESIZE_RATIO 8/5	/* Don't surround with `()'. */
+#else
+#define PURESIZE_RATIO 1
+#endif
+#endif
+
+/* This is the actual size in bytes to allocate.  */
+#ifndef PURESIZE
+#define PURESIZE  (BASE_PURESIZE * PURESIZE_RATIO)
 #endif
 
 #ifdef VIRT_ADDR_VARIES
