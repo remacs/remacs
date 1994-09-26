fe56ec1329484031acc3e3820846ceead5f403c9
David J. MacKenzie <djm@gnu.org>
1994-09-26 21:56:21 +0000

    REMOVE CONFIG_BROKETS

--- src/getloadavg.c
+++ src/getloadavg.c
@@ -70,14 +70,7 @@
 
 
 #ifdef HAVE_CONFIG_H
-#if defined (emacs) || defined (CONFIG_BROKETS)
-/* We use <config.h> instead of "config.h" so that a compilation
-   using -I. -I$srcdir will use ./config.h rather than $srcdir/config.h
-   (which it would do because it found this file in $srcdir).  */
 #include <config.h>
-#else
-#include "config.h"
-#endif
 #endif
 
 /* Exclude all the code except the test program at the end
