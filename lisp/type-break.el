99c0333bc4ffccc455bf4e8f597b9e2581b9670a
Noah Friedman <friedman@splode.com>
1994-10-06 19:12:46 +0000

    Assign copyright to FSF, and change comments to reflect that type-break is now part of GNU Emacs.

--- lisp/type-break.el
+++ lisp/type-break.el
@@ -1,34 +1,29 @@
 ;;; type-break.el --- encourage rests from typing at appropriate intervals
 
-;;; Copyright (C) 1994 Noah S. Friedman
+;; Copyright (C) 1994 Free Software Foundation, Inc.
 
-;;; Author: Noah Friedman <friedman@prep.ai.mit.edu>
-;;; Maintainer: friedman@prep.ai.mit.edu
-;;; Keywords: extensions, timers
-;;; Status: known to work in GNU Emacs 19.25 or later.
-;;; Created: 1994-07-13
+;; Author: Noah Friedman <friedman@prep.ai.mit.edu>
+;; Maintainer: friedman@prep.ai.mit.edu
+;; Keywords: extensions, timers
+;; Status: known to work in GNU Emacs 19.25 or later.
+;; Created: 1994-07-13
+;; $Id$
 
-;;; LCD Archive Entry:
-;;; type-break|Noah Friedman|friedman@prep.ai.mit.edu|
-;;; encourage rests from typing at appropriate intervals|
-;;; $Date$|$Revision$||
+;; This file is part of GNU Emacs.
 
-;;; $Id$
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
 
-;;; This program is free software; you can redistribute it and/or modify
-;;; it under the terms of the GNU General Public License as published by
-;;; the Free Software Foundation; either version 2, or (at your option)
-;;; any later version.
-;;;
-;;; This program is distributed in the hope that it will be useful,
-;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;; GNU General Public License for more details.
-;;;
-;;; You should have received a copy of the GNU General Public License
-;;; along with this program; if not, you can either send email to this
-;;; program's maintainer or write to: The Free Software Foundation,
-;;; Inc.; 675 Massachusetts Avenue; Cambridge, MA 02139, USA.
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 
 ;;; Commentary:
 
