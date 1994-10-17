ddff705b7a85685bbeaa64c4ecfecde37573053f
Morten Welinder <terra@diku.dk>
1994-10-17 07:35:36 +0000

    *** empty log message ***

--- config.bat
+++ config.bat
@@ -29,30 +29,66 @@ rem     line length limit.
 rem   + rm, mv, chmod (From GNU file utilities).
 rem   + sed.
 rem
-rem   You must install in directory c:/emacs or change this script, the
-rem   files msdos/sed*.inp, and lisp/dos-fns.el.  (The latter must be
-rem   recompiled.)
+rem   You should be able to get all the above utilities from all
+rem   Simtel repositories, e.g., oak.oakland.edu in the directories
+rem   "/pub/msdos/djgpp" and "/pub/msdos/gnuish".  As usual, please
+rem   use your local mirroring site to reduce trans-Atlantic traffic.
 rem   ----------------------------------------------------------------------
-if not "%2" == "" goto usage
+set X11=
+set nodebug=
+:again
+if "%1" == "" goto usage
+if "%1" == "--with-x" goto withx
+if "%1" == "--no-debug" goto nodebug
 if "%1" == "msdos" goto msdos
-if "%1" == "msdos-X11" goto msdos11
 :usage
-echo Usage: config msdos
-rem echo    or  config msdos-X11 -- don't even think about it
-echo [Read the script before you run it; also check that you have all the
-echo necessary utilities.]
+echo Usage: config [--with-x] [--no-debug] msdos
+echo [Read the script before you run it.]
 goto end
 rem   ----------------------------------------------------------------------
-:msdos11
-set X11=y
-goto msdoscommon
+:withx
+set X11=Y
+shift
+goto again
+rem   ----------------------------------------------------------------------
+:nodebug
+set nodebug=Y
+shift
+goto again
 rem   ----------------------------------------------------------------------
 :msdos
-set X11=
-:msdoscommon
-rem   Change to the Emacs root -- assume we are there
-rem cd c:\emacs
-rem   ----------------------------------------------------------------------
+Echo Checking whether `sed' is available...
+Sed -e "w junk.$$$" <Nul
+If Exist junk.$$$ Goto sedOk
+Echo To configure `Emacs' you need to have `sed'!
+Goto End
+:sedOk
+Echo Checking whether `rm' is available...
+rm -f junk.$$$
+If Not Exist junk.$$$ Goto rmOk
+Echo To configure `Emacs' you need to have `rm'!
+Goto End
+:rmOk
+Echo Checking whether `mv' is available...
+rm -f junk.1 junk.2
+echo foo >junk.1
+mv junk.1 junk.2
+If Exist junk.2 Goto mvOk
+Echo To configure `Emacs' you need to have `mv'!
+rm -f junk.1
+Goto End
+:mvOk
+rm -f junk.2
+Echo Checking whether `gcc' is available...
+echo main(){} >junk.c
+gcc -c junk.c
+if exist junk.o goto gccOk
+Echo To configure `Emacs' you need to have `gcc'!
+rm -f junk.c
+Goto End
+:gccOk
+rm -f junk.c junk.o
+Rem   ----------------------------------------------------------------------
 Echo Configuring the source directory...
 cd src
 set PATHSH=paths-h.in
@@ -81,26 +117,38 @@ goto end
 :src3
 
 rem   Create "paths.h"
-rm -f paths.h
-sed -f ../msdos/sed4.inp <%PATHSH% >paths.h
+sed -f ../msdos/sed4.inp <%PATHSH% >paths.tmp
+update paths.tmp paths.h >nul
+rm -f paths.tmp
 
 rem   Create "config.h"
-rm -f config.h config.tmp
+rm -f config.h2 config.tmp
 cp %CONFIGH% config.tmp
 if "%X11%" == "" goto src4
-sed -f ../msdos/sed4.inp <%CONFIGH% >config.tmp
+sed -f ../msdos/sed2x.inp <%CONFIGH% >config.tmp
 :src4
-sed -f ../msdos/sed2.inp <config.tmp >config.h
-rm -f config.tmp
+sed -f ../msdos/sed2.inp <config.tmp >config.h2
+update config.h2 config.h >nul
+rm -f config.tmp config.h2
 
 rem   On my system dir.h gets in the way.  It's a VMS file so who cares.
 if exist dir.h ren dir.h vmsdir.h
 
-rem   Create "makefile" from "makefile.in.in" using a context patch.
+rem   Create "makefile" from "makefile.in.in".
 rm -f makefile junk.c
 sed -e "1,/cpp stuff/s@^# .*$@@" <%MAKEFILEIN% >junk.c
 gcc -E junk.c | sed -f ../msdos/sed1.inp >makefile
 rm -f junk.c
+if "%X11%" == "" goto src5
+mv makefile makefile.tmp
+sed -f ../msdos/sed1x.inp <makefile.tmp >makefile
+rm -f makefile.tmp
+:src5
+
+if "%nodebug%" == "" goto src6
+sed -e "/^CFLAGS *=/s/ *-g//" <makefile >makefile.tmp
+mv -f makefile.tmp makefile
+:src6
 cd ..
 rem   ----------------------------------------------------------------------
 Echo Configuring the library source directory...
@@ -117,14 +165,30 @@ rem   Create "makefile" from "makefile.in".
 sed -e "1,/cpp stuff/s@^# .*$@@" <%MAKEFILEIN% >junk.c
 gcc -E -I. -I../src junk.c | sed -e "s/^ /	/" -e "/^#/d" -e "/^[ 	]*$/d" >Makefile.new
 sed -f ../msdos/sed3.inp <makefile.new >makefile
+rm -f makefile.new junk.c
+if "%nodebug%" == "" goto libsrc2
+sed -e "/^CFLAGS *=/s/ *-g//" <makefile >makefile.tmp
+mv -f makefile.tmp makefile
+:libsrc2
 cd ..
+rem   ----------------------------------------------------------------------
+if "%X11%" == "" goto oldx1
+Echo Configuring the oldxmenu directory...
+cd oldxmenu
+sed -f ../msdos/sed5x.inp <makefile.in >makefile
+if "%nodebug%" == "" goto oldx2
+sed -e "/^CFLAGS *=/s/ *-g//" <makefile >makefile.tmp
+mv -f makefile.tmp makefile
+:oldx2
+cd ..
+:oldx1
 rem   ----------------------------------------------------------------------
 Echo Configuring the main directory...
 copy msdos\mainmake makefile >nul
 rem   ----------------------------------------------------------------------
 :end
 set X11=
+set nodebug=
 set MAKEFILEIN=
 set PATHSH=
 set CONFIGH=
-
