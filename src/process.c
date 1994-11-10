3ea1d2918f21eb9bd18c127f35838b4e978abb06
Richard M. Stallman <rms@gnu.org>
1994-11-10 08:12:48 +0000

    (create_process) [OSF1]: Use setpgid, not setpgrp.

--- src/process.c
+++ src/process.c
@@ -1403,7 +1403,11 @@ create_process (process, new_argv, current_dir)
 	    /* In order to get a controlling terminal on some versions
 	       of BSD, it is necessary to put the process in pgrp 0
 	       before it opens the terminal.  */
+#ifdef OSF1
+	    setpgid (0, 0);
+#else
 	    setpgrp (0, 0);
+#endif
 #endif
 	  }
 #endif /* TIOCNOTTY */
