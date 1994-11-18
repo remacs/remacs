af8eb50dd287ffeb2b6767f07c9191f946938ee1
Richard M. Stallman <rms@gnu.org>
1994-11-18 17:50:37 +0000

    (batch-byte-compile-emacs): New function.

--- lisp/patcomp.el
+++ lisp/patcomp.el
@@ -5,3 +5,10 @@
 This is used after installing the patches for a new version."
   (let ((load-path (list (expand-file-name "lisp"))))
     (byte-recompile-directory "lisp")))
+
+(defun batch-byte-compile-emacs ()
+  "Compile new files installed in the Emacs `lisp' directory.
+This is used after installing the patches for a new version.
+It uses the command line arguments to specify the files to compile."
+  (let ((load-path (list (expand-file-name "lisp"))))
+    (batch-byte-compile)))
