53ece92895439718e96fce412707df089c31485e
Richard M. Stallman <rms@gnu.org>
1994-12-23 15:06:14 +0000

    entered into RCS

--- src/s/aix4-1.h
+++ src/s/aix4-1.h
@@ -2,5 +2,7 @@
 
 #include "aix4.h"
 
-/* Tomotake FURUHATA <furuhata@trl.ibm.co.jp> says this is needed.  */
+#if 0 /* Tomotake FURUHATA <furuhata@trl.ibm.co.jp> says this is needed
+	 in Mule, but we don't know why.  Anyway, it's not needed now.  */
 #define SYSTEM_MALLOC
+#endif
