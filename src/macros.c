f1cbbff5d28b923708fa7fabf1c983c48727124f
Karl Heuer <kwzh@gnu.org>
1995-03-01 03:43:41 +0000

    (Fexecute_kbd_macro): Use clear_prefix_arg.

--- src/macros.c
+++ src/macros.c
@@ -217,7 +217,7 @@ COUNT is a repeat count, or nil for once, or 0 for infinite loop.")
 
       if (!current_perdisplay)
 	abort ();
-      current_perdisplay->Vprefix_arg = Qnil;
+      clear_prefix_arg ();
       command_loop_1 ();
 
       QUIT;
