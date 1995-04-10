30bc62fa77073f3e7270fa7384a31d815dbf7446
Richard M. Stallman <rms@gnu.org>
1995-04-10 00:34:00 +0000

    Remove function prototypes.

--- lwlib/lwlib-Xm.h
+++ lwlib/lwlib-Xm.h
@@ -6,35 +6,35 @@
 extern widget_creation_entry xm_creation_table [];
 
 Widget 
-xm_create_dialog (widget_instance* instance);
+xm_create_dialog (/* widget_instance* instance */);
 
 Boolean
-lw_motif_widget_p (Widget widget);
+lw_motif_widget_p (/* Widget widget */);
 
 void
-xm_update_one_widget (widget_instance* instance, Widget widget,
-		      widget_value* val, Boolean deep_p);
+xm_update_one_widget (/* widget_instance* instance, Widget widget,
+		      widget_value* val, Boolean deep_p */);
 
 void
-xm_update_one_value (widget_instance* instance, Widget widget,
-		     widget_value* val);
+xm_update_one_value (/* widget_instance* instance, Widget widget,
+		     widget_value* val */);
 
 void
-xm_destroy_instance (widget_instance* instance);
+xm_destroy_instance (/* widget_instance* instance */);
 
 void
-xm_set_keyboard_focus (Widget parent, Widget w);
+xm_set_keyboard_focus (/* Widget parent, Widget w */);
 
 void
-xm_popup_menu (Widget widget);
+xm_popup_menu (/* Widget widget */);
 
 void
-xm_pop_instance (widget_instance* instance, Boolean up);
+xm_pop_instance (/* widget_instance* instance, Boolean up */);
 
 void
-xm_set_main_areas (Widget parent, Widget menubar, Widget work_area);
+xm_set_main_areas (/* Widget parent, Widget menubar, Widget work_area */);
 
 void
-xm_manage_resizing (Widget w, Boolean flag);
+xm_manage_resizing (/* Widget w, Boolean flag */);
 
 #endif /* LWLIB_XM_H */
