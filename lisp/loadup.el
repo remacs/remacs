ea3e2043318327c6c8d5fcc7707d1c3d98f4b0b5
Karl Heuer <kwzh@gnu.org>
1995-04-12 03:11:40 +0000

    [WINDOWSNT]: Find doc strings

--- lisp/loadup.el
+++ lisp/loadup.el
@@ -215,7 +215,7 @@
       (dump-emacs "emacs" "temacs")
       (message "%d pure bytes used" pure-bytes-used)
       ;; Recompute NAME now, so that it isn't set when we dump.
-      (if (not (eq system-type 'ms-dos))
+      (if (not (memq system-type '(ms-dos windows-nt)))
 	  (let ((name (concat "emacs-" emacs-version)))
 	    (while (string-match "[^-+_.a-zA-Z0-9]+" name)
 	      (setq name (concat (downcase (substring name 0 (match-beginning 0)))
