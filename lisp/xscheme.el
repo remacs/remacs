390d1087a16fd7a51f8f951c764695cefb832b60
Karl Heuer <kwzh@gnu.org>
1995-04-25 22:23:31 +0000

    (xscheme-set-runlight, xscheme-set-prompt): Use force-mode-line-update.

--- lisp/xscheme.el
+++ lisp/xscheme.el
@@ -674,12 +674,7 @@ When called, the current buffer will be the Scheme process-buffer.")
 
 (defun xscheme-set-runlight (runlight)
   (setq xscheme-runlight-string runlight)
-  (xscheme-modeline-redisplay))
-
-(defun xscheme-modeline-redisplay ()
-  (save-excursion (set-buffer (other-buffer)))
-  (set-buffer-modified-p (buffer-modified-p))
-  (sit-for 0))
+  (force-mode-line-update t))
 
 ;;;; Process Filter Operations
 
@@ -811,7 +806,7 @@ the remaining input.")
   (setq xscheme-prompt string)
   (xscheme-guarantee-newlines 2)
   (setq xscheme-mode-string (xscheme-coerce-prompt string))
-  (xscheme-modeline-redisplay))
+  (force-mode-line-update t))
 
 (defun xscheme-output-goto ()
   (xscheme-goto-output-point)
