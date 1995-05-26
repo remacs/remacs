075f06eca341dbfd2b085684eb5ac5cf0a1affe2
Karl Heuer <kwzh@gnu.org>
1995-05-26 22:35:10 +0000

    (NULL): Use explicit cast.

--- src/regex.c
+++ src/regex.c
@@ -183,7 +183,7 @@ init_syntax_once ()
 #define ISXDIGIT(c) (ISASCII (c) && isxdigit (c))
 
 #ifndef NULL
-#define NULL 0
+#define NULL (void *)0
 #endif
 
 /* We remove any previous definition of `SIGN_EXTEND_CHAR',
