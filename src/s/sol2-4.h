7f0db62c95ecff38d713dc60bc80546e9aea7619
Karl Heuer <kwzh@gnu.org>
1995-05-27 01:08:20 +0000

    (VFORK_RETURN_TYPE): Moved to s/sol2-3.h.

--- src/s/sol2-4.h
+++ src/s/sol2-4.h
@@ -4,13 +4,6 @@
 
 #define SOLARIS2_4
 
-/* Solaris 2.4 has a broken vfork.  So we don't use it;
-   we use the alternate definition in sysdep.c.
-   But a header file has a declaration
-   that would conflict with the definition of vfork in sysdep.c.
-   So we'll choose the return type to match the system header.  */
-#define VFORK_RETURN_TYPE pid_t
-
 /* Get rid of -traditional and let const really do its thing.  */
 
 #ifdef __GNUC__
