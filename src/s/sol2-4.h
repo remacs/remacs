1832d1091bff55268a8c96c7282ba12d357585a2
Karl Heuer <kwzh@gnu.org>
1995-05-30 23:32:09 +0000

    (LD_SWITCH_SYSTEM): Copied back from s/sol2-3.h; added references to /usr/dt/lib.

--- src/s/sol2-4.h
+++ src/s/sol2-4.h
@@ -10,3 +10,14 @@
 #undef C_SWITCH_SYSTEM
 #undef const
 #endif /* __GNUC__ */
+
+#undef LD_SWITCH_SYSTEM
+#ifndef __GNUC__
+#define LD_SWITCH_SYSTEM -L /usr/ccs/lib LD_SWITCH_X_SITE_AUX -R /usr/dt/lib -L /usr/dt/lib
+#else /* GCC */
+/* We use ./prefix-args because we don't know whether LD_SWITCH_X_SITE_AUX
+   has anything in it.  It can be empty.
+   This works ok in src.  Luckily lib-src does not use LD_SWITCH_SYSTEM.  */
+#define LD_SWITCH_SYSTEM -L /usr/ccs/lib \
+ `./prefix-args -Xlinker LD_SWITCH_X_SITE_AUX` -R /usr/dt/lib -L /usr/dt/lib
+#endif /* GCC */
