0006272a97c3bcb3174027b9d085103452a76a38
Karl Heuer <kwzh@gnu.org>
1995-05-30 05:07:52 +0000

    (Man-default-man-entry): Return result.

--- lisp/man.el
+++ lisp/man.el
@@ -3,8 +3,8 @@
 ;; Copyright (C) 1993, 1994 Free Software Foundation, Inc.
 
 ;; Author:		Barry A. Warsaw <bwarsaw@cen.com>
-;; Last-Modified:	$Date: 1995/02/03 12:07:37 $
-;; Version:		$Revision: 1.61 $
+;; Last-Modified:	$Date: 1995/05/29 13:20:51 $
+;; Version:		$Revision: 1.62 $
 ;; Keywords:		help
 ;; Adapted-By:		ESR, pot
 
@@ -487,7 +487,7 @@ default section number is selected from `Man-auto-section-alist'."
 				  Man-section-regexp "\\)[ \t]*)"))
 			 (format "(%s)" (Man-match-substring 1))))))
 	(set-text-properties 0 (length result) nil result)
-	))))
+	result))))
 
 
 ;; ======================================================================
