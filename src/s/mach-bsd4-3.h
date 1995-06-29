caaf3c4ed0c2c64517cb5b0175326755a9904961
Richard M. Stallman <rms@gnu.org>
1995-06-29 01:39:56 +0000

    Initial revision

---
+++ src/s/mach-bsd4-3.h
@@ -0,0 +1,5 @@
+/* I don't care if this doesn't do more than including bsd4-3.h;
+   Mach is not bsd4-3 and the moment you forget it chances are that
+   you're in deep shit.  */
+
+#include "bsd4-3.h"
