f6e0680c88750eaaf8c5cb0a2db868cad8f6e861
Richard M. Stallman <rms@gnu.org>
1995-07-01 19:49:40 +0000

    Don't use sunos4shr.h; go back to 19.28 definition.

--- src/s/sunos413.h
+++ src/s/sunos413.h
@@ -1,7 +1,3 @@
-/* As of 19.29, it should work ok to use shared libs with X.  */
-#include "sunos4shr.h"
-
-#if 0
 #include "sunos4-1.h"
 
 /* jik@gza.com says this works now.  */
@@ -9,18 +5,7 @@
 
 #undef SYSTEM_MALLOC
 
-#if 0 /* This causes failure in process_send_signal (tcgetattr loses)
-	 and may also cause hanging at Emacs startup when parent
-	 is not a job control shell.  */
-/* murray@chemical-eng.edinburgh.ac.uk says this works, and avoids
-   the problem of spurious ^M in subprocess output.  */
-#define HAVE_TERMIOS
-/* This enables some #undefs in systty.h.  */
-#define BSD_TERMIOS
-#endif
-
 /* barrie@calvin.demon.co.uk says memmove is missing.  */
 #ifndef SYSTEM_MALLOC
 #define MEMMOVE_MISSING
 #endif
-#endif /* 0 */
