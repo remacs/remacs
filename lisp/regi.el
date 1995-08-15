4db66229797dba35fdb24eb53ae874b50bf27110
Richard M. Stallman <rms@gnu.org>
1995-08-15 12:31:33 +0000

    Comment change.

--- lisp/regi.el
+++ lisp/regi.el
@@ -1,5 +1,7 @@
 ;;; regi.el --- REGular expression Interpreting engine
 
+;; Copyright (C) 1993 Free Software Foundation, Inc.
+
 ;; Author: 1993 Barry A. Warsaw, Century Computing, Inc. <bwarsaw@cen.com>
 ;; Maintainer:    bwarsaw@cen.com
 ;; Created:       24-Feb-1993
@@ -7,28 +9,21 @@
 ;; Last Modified: 1993/06/01 21:33:00
 ;; Keywords:      extensions, matching
 
-;; Copyright (C) 1993 Barry A. Warsaw
+;; This file is part of GNU Emacs.
 
-;; This file is not yet part of GNU Emacs.
-;;
-;; This program is free software; you can redistribute it and/or modify
+;; GNU Emacs is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 2 of the License, or
-;; (at your option) any later version.
-;; 
-;; This program is distributed in the hope that it will be useful,
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-;; 
-;; You should have received a copy of the GNU General Public License
-;; along with this program; if not, write to the Free Software
-;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-;; LCD Archive Entry
-;; regi|Barry A. Warsaw|bwarsaw@cen.com
-;; |REGular expression Interpreting engine
-;; |1993/06/01 21:33:00|1.8|
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 
 ;;; Code:
 
