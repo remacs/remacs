37473f05cb994af6adf0ed8dd2128c804533edc2
Richard M. Stallman <rms@gnu.org>
1995-08-21 20:52:13 +0000

    (SA_RESTART): Add #undef.

--- src/s/irix5-2.h
+++ src/s/irix5-2.h
@@ -3,3 +3,7 @@
 /* as of version 5.2, irix no longer uses flock, 
    according to jackr@wpd.sgi.com.  */
 #undef MAIL_USE_FLOCK 
+
+/* C-g in select is not handled properly with restartable
+   system calls.  So don't use them.  */
+#undef SA_RESTART
