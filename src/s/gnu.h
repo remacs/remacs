afe3e774719298e84da086e8b2fa58215b414ab9
Roland McGrath <roland@gnu.org>
1995-08-28 20:38:37 +0000

    (GETPGRP_NO_ARG, WAIT_USE_INT, UNEXEC): New macros. (HAVE_SOCKETS, A_TEXT_SEEK, NO_REMAP): Macros removed.

--- src/s/gnu.h
+++ src/s/gnu.h
@@ -1,5 +1,5 @@
 /* Definitions file for GNU Emacs running on the GNU Hurd.
-   Copyright (C) 1994 Free Software Foundation, Inc.
+   Copyright (C) 1994, 1995 Free Software Foundation, Inc.
 
 This file is part of GNU Emacs.
 
@@ -30,8 +30,6 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 /* XXX should getloadavg be in libc?  Should we have a libutil?
 #define HAVE_GETLOADAVG */
 
-#define HAVE_UNION_WAIT
-
 #define SIGNALS_VIA_CHARACTERS
 
 #define HAVE_TERMIOS
@@ -48,31 +46,24 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 /* GNU has POSIX-style pgrp behavior.  */
 #undef BSD_PGRPS
+#define GETPGRP_NO_ARG
 
 /* Reread the time zone on startup.  */
 #define LOCALTIME_CACHE
 
 #define HAVE_WAIT_HEADER
+#define WAIT_USE_INT
+#define HAVE_UNION_WAIT
 
 /* GNU needs its own crt0, and libc defines data_start.  */
 #define ORDINARY_LINK
 #define DATA_START ({ extern int data_start; (char *) &data_start; })
 
-/* It is not safe to move the text/data boundary because in GNU the C
-   library will need some data and bss of its own, but which must come
-   after Emacs's data because it is linked in to satisfy references Emacs
-   makes.  Fortunately, in GNU it is also no loss for the pure Lisp data to
-   be in data instead of text, because it's all mapped copy-on-write.  */
-#define NO_REMAP
-
-/* For the i386-mach a.out format: */
-#define A_TEXT_SEEK(hdr) (sizeof (struct exec))
+/* GNU now always uses the ELF format.  */
+#define UNEXEC unexelf.o
 
 /* ptys are not done yet.  */
 #undef HAVE_PTYS
-
-/* Until we finish the network.  */
-#undef HAVE_SOCKETS
 
 /* Some losing code fails to include this and then assumes
    that because it is braindead that O_RDONLY==0.  */
