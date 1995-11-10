80c1bbc61f6509ad54a9c1611b612ff1f95f320e
Richard M. Stallman <rms@gnu.org>
1995-11-10 17:09:45 +0000

    (awk-mode): Require cc-mode.

--- lisp/progmodes/awk-mode.el
+++ lisp/progmodes/awk-mode.el
@@ -65,6 +65,7 @@ Turning on AWK mode calls the value of the variable `awk-mode-hook'
 with no args, if that value is non-nil."
   (interactive)
   (kill-all-local-variables)
+  (require 'cc-mode)
   (use-local-map c-mode-map)
   (setq major-mode 'awk-mode)
   (setq mode-name "AWK")
