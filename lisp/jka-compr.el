f4454a146791ccf47c3082a79575602a272ee4df
Richard M. Stallman <rms@gnu.org>
1995-11-11 20:29:31 +0000

    (auto-compression-mode): Make arg optional.

--- lisp/jka-compr.el
+++ lisp/jka-compr.el
@@ -5,6 +5,22 @@
 ;; Author: jka@ece.cmu.edu (Jay K. Adams)
 ;; Keywords: data
 
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+
 ;;; Commentary: 
 
 ;;; This package implements low-level support for reading, writing,
@@ -669,7 +685,7 @@ There should be no more than seven characters after the final `/'")
     (apply operation args)))
 
 ;;;###autoload
-(defun auto-compression-mode (arg)
+(defun auto-compression-mode (&optional arg)
   "Toggle automatic file compression and uncompression.
 With prefix argument ARG, turn auto compression on if positive, else off.
 Returns the new status of auto compression (non-nil means on)."
