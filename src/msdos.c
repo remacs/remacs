1d28946c4a68aa96de1849a1fb2f82329ce94a35
Richard M. Stallman <rms@gnu.org>
1995-11-25 00:20:46 +0000

    Comment change.

--- src/msdos.c
+++ src/msdos.c
@@ -1,5 +1,5 @@
 /* MS-DOS specific C utilities.
-   Copyright (C) 1993, 1994 Free Software Foundation, Inc.
+   Copyright (C) 1993, 1994, 1995 Free Software Foundation, Inc.
 
 This file is part of GNU Emacs.
 
