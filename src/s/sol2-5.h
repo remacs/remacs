6b4eb5930d41f6b188935e2c3a1e6b4f324365e8
Karl Heuer <kwzh@gnu.org>
1995-12-21 17:20:11 +0000

    (LIBXT_STATIC, STATIC_OPTION): Defined.

--- src/s/sol2-5.h
+++ src/s/sol2-5.h
@@ -1,3 +1,15 @@
 /* Handle Solaris 2.5.  */
 
 #include "sol2-4.h"
+
+/* Don't use the shared libraries for -lXt and -lXaw,
+   to work around a linker bug in Solaris 2.5.
+   (This also affects the other libraries used specifically for
+   the X toolkit, which may not be necessary.)  */
+#define LIBXT_STATIC
+
+#ifdef __GNUC__
+#define STATIC_OPTION -static
+#else
+#define STATIC_OPTION -Bstatic
+#endif
