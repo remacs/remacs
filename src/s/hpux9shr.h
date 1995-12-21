5e182e80bf157b6fc130c8c4fdca6aac93195de9
Karl Heuer <kwzh@gnu.org>
1995-12-21 17:19:24 +0000

    (C_DEBUG_SWITCH): Definition turned off.

--- src/s/hpux9shr.h
+++ src/s/hpux9shr.h
@@ -4,8 +4,10 @@
 
 #include "hpux9.h"
 
+#if 0 /* No longer needed, since in current GCC -g no longer does that.  */
 /* We must turn off -g since it forces -static.  */
 #ifdef __GNUC__
 #undef C_DEBUG_SWITCH
 #define C_DEBUG_SWITCH
+#endif
 #endif
