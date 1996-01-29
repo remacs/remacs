ba4a8e51f9c1d179c845eb717d2161cbd815d04c
Karl Heuer <kwzh@gnu.org>
1996-01-29 22:30:55 +0000

    Update FSF address in comment.

--- src/alloca.s
+++ src/alloca.s
@@ -12,9 +12,9 @@
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. */
 
 /* Both 68000 systems I have run this on have had broken versions of alloca.
    Also, I am told that non-berkeley systems do not have it at all.
--- src/termcap.c
+++ src/termcap.c
@@ -13,7 +13,8 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program; see the file COPYING.  If not, write to
-the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
 
 /* Emacs config.h may rename various library functions such as malloc.  */
 #ifdef HAVE_CONFIG_H
@@ -781,4 +782,3 @@ tprint (cap)
 }
 
 #endif /* TEST */
-
