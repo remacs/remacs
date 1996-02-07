79616aa086701ef37b20dd52211dcc1913591396
Richard M. Stallman <rms@gnu.org>
1996-02-07 22:56:22 +0000

    (LIB_STANDARD): Add -lmach. (LD_DATA_START): Definition deleted. (LD_SWITCH_SYSTEM): Definition and #undef deleted.

--- src/m/paragon.h
+++ src/m/paragon.h
@@ -4,10 +4,7 @@
 #define COFF
 #define SYSTEM_MALLOC
 #define TEXT_START 0x10000
-#define LD_DATA_START 0x140000
-#undef  LD_SWITCH_SYSTEM
-#define LD_SWITCH_SYSTEM -d LD_DATA_START
-#define LIB_STANDARD -lc -lic
+#define LIB_STANDARD -lc -lic -lmach
 #define KEEP_OLD_TEXT_SCNPTR
 #define KEEP_OLD_PADDR
 #define drem fmod
