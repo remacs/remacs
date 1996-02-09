9695bac622658ce5b6eaab169ffb55bcf812552b
Richard M. Stallman <rms@gnu.org>
1996-02-09 19:08:26 +0000

    Censored.

--- lisp/term-nasty.el
+++ lisp/term-nasty.el
@@ -1,29 +1,12 @@
 ;;; term-nasty.el --- Damned Things from terminfo.el
-;;; This file is in the public domain, and was written by Stallman and Mlynarik
 
-;;; Commentary:
+;;; This text is no longer included in Emacs, because it was censored
+;;; by the Communications Decency Act.  The law was promoted as a ban
+;;; on pornography, but it bans far more than that.  This file did not
+;;; contain pornograpy, but it was prohibited nonetheless.
 
-;; Some people used to be bothered by the following comments that were
-;; found in terminal.el.  We decided they were distracting, and that it
-;; was better not to have them there.  On the other hand, we didn't want
-;; to appear to be giving in to the pressure to censor obscenity that
-;; currently threatens freedom of speech and of the press in the US.
-;; So we decided to put the comments here.
-
-;;; Code:
-
-These comments were removed from te-losing-unix.
-  ;(what lossage)
-  ;(message "fucking-unix: %d" char)
-
-This was before te-process-output.
-;; fucking unix has -such- braindamaged lack of tty control...
-
-And about the need to handle output characters such as C-m, C-g, C-h
-and C-i even though the termcap doesn't say they may be used:
-;fuck me harder
-;again and again!
-;wa12id!!
-;(spiked)
+;;; For information on US government censorship of the Internet, and
+;;; what you can do to bring back freedom of the press, see the web
+;;; site http://www.vtw.org/
 
 ;;; term-nasty.el ends here
