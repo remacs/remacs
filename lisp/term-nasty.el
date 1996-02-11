d3cca84f53f786e6a203ef922f090939862decc3
Richard M. Stallman <rms@gnu.org>
1996-02-11 18:44:54 +0000

    Fix typo.

--- lisp/term-nasty.el
+++ lisp/term-nasty.el
@@ -3,7 +3,7 @@
 ;;; This text is no longer included in Emacs, because it was censored
 ;;; by the Communications Decency Act.  The law was promoted as a ban
 ;;; on pornography, but it bans far more than that.  This file did not
-;;; contain pornograpy, but it was prohibited nonetheless.
+;;; contain pornography, but it was prohibited nonetheless.
 
 ;;; For information on US government censorship of the Internet, and
 ;;; what you can do to bring back freedom of the press, see the web
