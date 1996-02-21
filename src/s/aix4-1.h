b11432c3898504ca54e0d2a16cdd13a88f34e12a
Karl Heuer <kwzh@gnu.org>
1996-02-21 21:13:39 +0000

    (C_DEBUG_SWITCH) [not __GNUC__]: New definition. (C_OPTIMIZE_SWITCH) [not __GNUC__]: Add #undef. (X11R5_INHIBIT_I18N): #undef deleted. (REL_ALLOC): Add #undef.

--- src/s/aix4-1.h
+++ src/s/aix4-1.h
@@ -10,6 +10,19 @@
 #undef _NO_PROTO
 #endif
 
-/* Let's hope the X internationalization stuff is working in AIX 4.1.
-   It was not working in 3.2.5.  */
-#undef X11R5_INHIBIT_I18N
+/* For AIX, it turns out compiling emacs under AIX 3.2.4 REQUIRES "cc -g"
+   because "cc -O" crashes.  Under AIX 3.2.5, "cc -O" is required because
+   "cc -g" crashes. Go figure.  --floppy@merlin.mit.edu.
+   4.1 seems to need -g again. -- larry@vaquita.mitra.com.  */
+#ifndef __GNUC__
+#undef C_DEBUG_SWITCH
+#undef C_OPTIMIZE_SWITCH
+#define C_DEBUG_SWITCH -g
+#endif
+
+/* X internationalization stuff is still not working in AIX 4.1.  */
+/* #undef X11R5_INHIBIT_I18N */
+
+/* Perry Smith <pedz@ddivt1.austin.ibm.com> says this is necessary
+   because of noncontiguous allocation.  */
+#undef REL_ALLOC
