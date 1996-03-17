c9bf9238c540cd3f0064b3d0cb03873338c2b85a
Richard M. Stallman <rms@gnu.org>
1996-03-17 18:05:32 +0000

    (NEED_LIBW): Defined.

--- src/s/irix5-2.h
+++ src/s/irix5-2.h
@@ -7,3 +7,7 @@
 /* C-g in select is not handled properly with restartable
    system calls.  So don't use them.  */
 #undef SA_RESTART
+
+/* schoepf@goofy.zdv.Uni-Mainz.de reports he needed -lw with X11R6
+   on Irix 5.3.  I don't know which Irix version that need starts with.  */
+#define NEED_LIBW
