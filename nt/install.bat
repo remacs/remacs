69c46fcf8b13294c88ef0f43da2c8d383f11854b
Richard M. Stallman <rms@gnu.org>
1996-05-13 18:28:09 +0000

    Set BUILD_TYPE and INSTALL_DIR before calling make; undo settings afterwards.

--- nt/install.bat
+++ nt/install.bat
@@ -1,6 +1,7 @@
-nmake -f makefile.nt install
 @echo off
 if (%1) == (speed) set BUILD_TYPE=spd
 if (%1) == (speed) shift
 if not (%1) == () set INSTALL_DIR=%1
-@if not (%1) == () set INSTALL_DIR=%1
+nmake -f makefile.nt install
+set INSTALL_DIR=
+set BUILD_TYPE=
