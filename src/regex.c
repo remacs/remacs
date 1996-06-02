264d34bf38d6a7f008aa95808319ba2bcd117ebe
Richard M. Stallman <rms@gnu.org>
1996-06-02 21:33:01 +0000

    (re_max_failures): Reduce to 8000.

--- src/regex.c
+++ src/regex.c
@@ -1008,7 +1008,9 @@ static const char *re_error_msgid[] =
    This is a variable only so users of regex can assign to it; we never
    change it ourselves.  */
 #if defined (MATCH_MAY_ALLOCATE)
-int re_max_failures = 20000;
+/* 8600 was enough to cause a crash on Ultrix,
+   whose default stack limit is 2mb.  */
+int re_max_failures = 8000;
 #else
 int re_max_failures = 2000;
 #endif
