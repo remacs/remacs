a013d266d17c2bc9d22de5f050de985bb7207e59
Richard M. Stallman <rms@gnu.org>
1996-06-16 23:30:31 +0000

    (local-variables-section): Deleted.

--- lisp/skeleton.el
+++ lisp/skeleton.el
@@ -382,32 +382,32 @@ automatically, and you are prompted to fill in the variable parts.")))
 
 ;; Maybe belongs into simple.el or elsewhere
 
-(define-skeleton local-variables-section
-  "Insert a local variables section.  Use current comment syntax if any."
-  ()
-  '(save-excursion
-     (if (re-search-forward page-delimiter nil t)
-	 (error "Not on last page.")))
-  comment-start "Local Variables:" comment-end \n
-  comment-start "mode: "
-  (completing-read "Mode: " obarray
-		   (lambda (symbol)
-		     (if (commandp symbol)
-			 (string-match "-mode$" (symbol-name symbol))))
-		   t)
-  & -5 | '(kill-line 0) & -1 | comment-end \n
-  ( (completing-read (format "Variable, %s: " skeleton-subprompt)
-		     obarray
-		     (lambda (symbol)
-		       (or (eq symbol 'eval)
-			   (user-variable-p symbol)))
-		     t)
-    comment-start str ": "
-    (read-from-minibuffer "Expression: " nil read-expression-map nil
-			  'read-expression-history) | _
-    comment-end \n)
-  resume:
-  comment-start "End:" comment-end)
+;;;(define-skeleton local-variables-section
+;;;  "Insert a local variables section.  Use current comment syntax if any."
+;;;  ()
+;;;  '(save-excursion
+;;;     (if (re-search-forward page-delimiter nil t)
+;;;	 (error "Not on last page.")))
+;;;  comment-start "Local Variables:" comment-end \n
+;;;  comment-start "mode: "
+;;;  (completing-read "Mode: " obarray
+;;;		   (lambda (symbol)
+;;;		     (if (commandp symbol)
+;;;			 (string-match "-mode$" (symbol-name symbol))))
+;;;		   t)
+;;;  & -5 | '(kill-line 0) & -1 | comment-end \n
+;;;  ( (completing-read (format "Variable, %s: " skeleton-subprompt)
+;;;		     obarray
+;;;		     (lambda (symbol)
+;;;		       (or (eq symbol 'eval)
+;;;			   (user-variable-p symbol)))
+;;;		     t)
+;;;    comment-start str ": "
+;;;    (read-from-minibuffer "Expression: " nil read-expression-map nil
+;;;			  'read-expression-history) | _
+;;;    comment-end \n)
+;;;  resume:
+;;;  comment-start "End:" comment-end)
 
 ;; Variables and command for automatically inserting pairs like () or "".
 
