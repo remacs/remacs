517697d92b304152326b2eca4fb31b5b15f45d79
Richard M. Stallman <rms@gnu.org>
1996-06-28 08:06:37 +0000

    (LIB_MOTIF): New definition.

--- src/s/sol2-5.h
+++ src/s/sol2-5.h
@@ -2,6 +2,11 @@
 
 #include "sol2-4.h"
 
+/* -lgen is needed for the regex and regcmp functions
+   which are used by Motif.  In the future we can try changing
+   regex.c to provide them in Emacs, but this is safer for now.  */
+#define LIB_MOTIF = -lXm -lgen
+
 #if 0 /* A recent patch in unexelf.c should eliminate the need for this.  */
 /* Don't use the shared libraries for -lXt and -lXaw,
    to work around a linker bug in Solaris 2.5.
