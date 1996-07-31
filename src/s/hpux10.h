09af8ae6afa3d3a59b7f49de2aa375d601f74a82
Richard M. Stallman <rms@gnu.org>
1996-07-31 23:18:49 +0000

    (C_SWITCH_X_SYSTEM, LD_SWITCH_X_DEFAULT): Override to add /usr/contrib directoryes.

--- src/s/hpux10.h
+++ src/s/hpux10.h
@@ -25,3 +25,10 @@
 /* Make sure we get select from libc rather than from libcurses
    because libcurses on HPUX 10.10 has a broken version of select.  */
 #define LIBS_TERMCAP -lc -lcurses
+
+#undef C_SWITCH_X_SYSTEM
+#undef LD_SWITCH_X_DEFAULT
+/* However, HPUX 10 puts Xaw and Xmu in a strange place
+   (if you install them at all).  So search that place.  */
+#define C_SWITCH_X_SYSTEM -I/usr/include/X11R5 -I/usr/include/Motif1.2 -I/usr/contrib/X11R5/include
+#define LD_SWITCH_X_DEFAULT -L/usr/lib/X11R5 -L/usr/lib/Motif1.2 -L/usr/contrib/X11R5/lib
