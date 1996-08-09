4c28806296345441e619d3c1337e59e462d5beec
Richard M. Stallman <rms@gnu.org>
1996-08-09 02:52:02 +0000

    Initial revision

---
+++ src/s/ptx4.h
@@ -0,0 +1,14 @@
+/* s/ file for Sequent "ptx 4", which is a modified SVR5.4.  */
+
+#include <s/usg5-4.h>
+
+/* Marcus Daniels <marcus@sysc.pdx.edu> says that SIGINFO is defined
+   on ptx4 but it is not a signal.  Prevent process.c from doing the
+   wrong thing.  */
+#undef SIGINFO
+
+/* Marcus Daniels <marcus@sysc.pdx.edu> says vfork does exist.  */
+#define HAVE_VFORK
+
+/* Tell usg5-4.h not to include filio.h.  */
+#define NO_FILIO_H
