5b4c672fefcfe9bbda1d916a91df4c4f158029cc
Richard M. Stallman <rms@gnu.org>
1996-08-13 15:27:23 +0000

    Fix include file name. (NO_FILIO_H): Move definition.

--- src/s/ptx4.h
+++ src/s/ptx4.h
@@ -1,6 +1,9 @@
 /* s/ file for Sequent "ptx 4", which is a modified SVR5.4.  */
 
-#include <s/usg5-4.h>
+/* Tell usg5-4.h not to include filio.h.  */
+#define NO_FILIO_H
+
+#include "usg5-4.h"
 
 /* Marcus Daniels <marcus@sysc.pdx.edu> says that SIGINFO is defined
    on ptx4 but it is not a signal.  Prevent process.c from doing the
@@ -10,5 +13,3 @@
 /* Marcus Daniels <marcus@sysc.pdx.edu> says vfork does exist.  */
 #define HAVE_VFORK
 
-/* Tell usg5-4.h not to include filio.h.  */
-#define NO_FILIO_H
