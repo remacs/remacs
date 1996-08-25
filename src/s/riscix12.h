b4d413451a0c571ecac48d456c546474c061705e
Richard M. Stallman <rms@gnu.org>
1996-08-25 02:14:42 +0000

    Initial revision

---
+++ src/s/riscix12.h
@@ -0,0 +1,25 @@
+/* Definitions file for GNU Emacs running on RISCiX 1.2 (bsd 4.3)
+   Copyright (C) 1994 Free Software Foundation, Inc.
+
+This file is part of GNU Emacs.
+
+GNU Emacs is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU Emacs is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU Emacs; see the file COPYING.  If not, write to
+the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
+
+
+#define  RISCiX_1_2	1
+#define  RISCiX		12
+#define  CRT0_O		/usr/lib/crt0.o
+#include "bsd4-3.h"
