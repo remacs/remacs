6dd4407cdc75a09a2ed28ca9ca0f2047d5da36a9
Richard M. Stallman <rms@gnu.org>
1996-08-28 20:09:51 +0000

    (sh-alias-alist): Use gnu/linux, not lignux.

--- lisp/progmodes/sh-script.el
+++ lisp/progmodes/sh-script.el
@@ -90,7 +90,7 @@ sh		Bourne Shell
 
 
 (defvar sh-alias-alist
-  (nconc (if (eq system-type 'lignux)
+  (nconc (if (eq system-type 'gnu/linux)
 	     '((csh . tcsh)
 	       (ksh . pdksh)))
 	 ;; for the time being
