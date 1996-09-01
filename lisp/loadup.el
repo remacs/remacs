424aad230d50f72c862c2433285bf6fe31ababc7
Richard M. Stallman <rms@gnu.org>
1996-09-01 17:11:14 +0000

    [windows-nt, ms-dos]: Load dos-nt.

--- lisp/loadup.el
+++ lisp/loadup.el
@@ -114,11 +114,15 @@
       (garbage-collect)
       (load "disp-table") ; needed to setup ibm-pc char set, see internal.el
       (garbage-collect)
+      (load "dos-nt")
+      (garbage-collect)
       (load "winnt")
       (garbage-collect)))
 (if (eq system-type 'ms-dos)
     (progn
       (load "ls-lisp")
+      (garbage-collect)
+      (load "dos-nt")
       (garbage-collect)
       (load "dos-fns")
       (garbage-collect)
