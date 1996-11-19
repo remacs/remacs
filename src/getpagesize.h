eb93fe6a97840318127148cb975652a1356d21fd
David J. MacKenzie <djm@gnu.org>
1996-11-19 18:34:08 +0000

    Indent for readability.

--- src/getpagesize.h
+++ src/getpagesize.h
@@ -2,38 +2,36 @@
 
 #ifndef HAVE_GETPAGESIZE
 
-#ifdef VMS
-#define getpagesize() 512
-#endif
+# ifdef VMS
+#  define getpagesize() 512
+# endif
 
-#ifdef HAVE_UNISTD_H
-#include <unistd.h>
-#endif
+# ifdef HAVE_UNISTD_H
+#  include <unistd.h>
+# endif
 
-#ifdef _SC_PAGESIZE
-#define getpagesize() sysconf(_SC_PAGESIZE)
-#else
+# ifdef _SC_PAGESIZE
+#  define getpagesize() sysconf(_SC_PAGESIZE)
+# else
+#  include <sys/param.h>
+#  ifdef EXEC_PAGESIZE
+#   define getpagesize() EXEC_PAGESIZE
+#  else /* no EXEC_PAGESIZE */
+#   ifdef NBPG
+#    define getpagesize() NBPG * CLSIZE
+#    ifndef CLSIZE
+#     define CLSIZE 1
+#    endif /* no CLSIZE */
+#   else /* no NBPG */
+#    ifdef NBPC
+#     define getpagesize() NBPC
+#    else /* no NBPC */
+#     ifdef PAGESIZE
+#      define getpagesize() PAGESIZE
+#     endif /* PAGESIZE */
+#    endif /* no NBPC */
+#   endif /* no NBPG */
+#  endif /* no EXEC_PAGESIZE */
+# endif /* no _SC_PAGESIZE */
 
-#include <sys/param.h>
-
-#ifdef EXEC_PAGESIZE
-#define getpagesize() EXEC_PAGESIZE
-#else
-#ifdef NBPG
-#define getpagesize() NBPG * CLSIZE
-#ifndef CLSIZE
-#define CLSIZE 1
-#endif /* no CLSIZE */
-#else /* no NBPG */
-#ifdef NBPC
-#define getpagesize() NBPC
-#else /* no NBPC */
-#ifdef PAGESIZE
-#define getpagesize() PAGESIZE
-#endif
-#endif /* NBPC */
-#endif /* no NBPG */
-#endif /* no EXEC_PAGESIZE */
-#endif /* no _SC_PAGESIZE */
-
-#endif /* not HAVE_GETPAGESIZE */
+#endif /* no HAVE_GETPAGESIZE */
