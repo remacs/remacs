4b45d6714a4afd55e138be89723d32472062f54e
David J. MacKenzie <djm@gnu.org>
1996-11-19 18:40:34 +0000

    If no sys/param.h, default to 8k.

--- src/getpagesize.h
+++ src/getpagesize.h
@@ -12,26 +12,30 @@
 
 # ifdef _SC_PAGESIZE
 #  define getpagesize() sysconf(_SC_PAGESIZE)
-# else
-#  include <sys/param.h>
-#  ifdef EXEC_PAGESIZE
-#   define getpagesize() EXEC_PAGESIZE
-#  else /* no EXEC_PAGESIZE */
-#   ifdef NBPG
-#    define getpagesize() NBPG * CLSIZE
-#    ifndef CLSIZE
-#     define CLSIZE 1
-#    endif /* no CLSIZE */
-#   else /* no NBPG */
-#    ifdef NBPC
-#     define getpagesize() NBPC
-#    else /* no NBPC */
-#     ifdef PAGESIZE
-#      define getpagesize() PAGESIZE
-#     endif /* PAGESIZE */
-#    endif /* no NBPC */
-#   endif /* no NBPG */
-#  endif /* no EXEC_PAGESIZE */
+# else /* no _SC_PAGESIZE */
+#  ifdef HAVE_SYS_PARAM_H
+#   include <sys/param.h>
+#   ifdef EXEC_PAGESIZE
+#    define getpagesize() EXEC_PAGESIZE
+#   else /* no EXEC_PAGESIZE */
+#    ifdef NBPG
+#     define getpagesize() NBPG * CLSIZE
+#     ifndef CLSIZE
+#      define CLSIZE 1
+#     endif /* no CLSIZE */
+#    else /* no NBPG */
+#     ifdef NBPC
+#      define getpagesize() NBPC
+#     else /* no NBPC */
+#      ifdef PAGESIZE
+#       define getpagesize() PAGESIZE
+#      endif /* PAGESIZE */
+#     endif /* no NBPC */
+#    endif /* no NBPG */
+#   endif /* no EXEC_PAGESIZE */
+#  else /* no HAVE_SYS_PARAM_H */
+#   define getpagesize() 8192	/* punt totally */
+#  endif /* no HAVE_SYS_PARAM_H */
 # endif /* no _SC_PAGESIZE */
 
 #endif /* no HAVE_GETPAGESIZE */
