bbcb3f00ac4dce90a4baf6d44a25b4e1f7574bc4
Richard M. Stallman <rms@gnu.org>
1996-12-16 07:51:24 +0000

    (mail-extract-address-components): Don't downcase the domain part of the address.

--- lisp/mail/mail-extr.el
+++ lisp/mail/mail-extr.el
@@ -1174,8 +1174,6 @@ If ADDRESS contains more than one RFC-822 address, only the first is
 	     (setq %-pos (cdr %-pos))))
 
       (setq %-pos (nreverse %-pos))
-      ;; RFC 1034 doesn't approve of this, oh well:
-      (downcase-region (or (car %-pos) @-pos (point-max)) (point-max))
       (cond (%-pos			; implies @-pos valid
 	     (setq temp %-pos)
 	     (catch 'truncated
