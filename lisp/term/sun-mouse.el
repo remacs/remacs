178663df99621384bae489a9130bb36af04c1aba
Kenichi Handa <handa@m17n.org>
1997-04-05 02:46:24 +0000

    Provide 'term/sun-mouse instead of 'sun-mouse.

--- lisp/term/sun-mouse.el
+++ lisp/term/sun-mouse.el
@@ -676,6 +676,6 @@ just close the window, and wait for reopening."
 (setq-default current-local-mousemap nil)
 (defvar current-global-mousemap (make-mousemap))
 
-(provide 'sun-mouse)
+(provide 'term/sun-mouse)
 
 ;;; sun-mouse.el ends here
