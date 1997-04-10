10ffbb91cfc3498755a8a855effe9fa93f1709c0
Karl Heuer <kwzh@gnu.org>
1997-04-10 21:16:33 +0000

    (DISP_TABLE_P): Don't use Lisp_Object as integer.

--- src/disptab.h
+++ src/disptab.h
@@ -22,7 +22,7 @@ Boston, MA 02111-1307, USA.  */
 
 #define DISP_TABLE_P(obj)						    \
   (CHAR_TABLE_P (obj)							    \
-   && XCHAR_TABLE (obj)->purpose == Qdisplay_table			    \
+   && EQ (XCHAR_TABLE (obj)->purpose, Qdisplay_table)			    \
    && CHAR_TABLE_EXTRA_SLOTS (XCHAR_TABLE (obj)) == DISP_TABLE_EXTRA_SLOTS)
 
 #define DISP_TABLE_EXTRA_SLOTS 6
