45d642dffa9f8bfc243c453907ef1c140362f39e
Richard M. Stallman <rms@gnu.org>
1997-04-23 05:06:35 +0000

    (compare-windows): Allow windows on different frames.

--- lisp/compare-w.el
+++ lisp/compare-w.el
@@ -72,7 +72,7 @@ If `compare-ignore-case' is non-nil, changes in case are also ignored."
 			     'compare-windows-skip-whitespace
 			   compare-windows-whitespace))))
     (setq p1 (point) b1 (current-buffer))
-    (setq w2 (next-window (selected-window)))
+    (setq w2 (next-window (selected-window) nil 'visible))
     (if (eq w2 (selected-window))
 	(error "No other window"))
     (setq p2 (window-point w2)
