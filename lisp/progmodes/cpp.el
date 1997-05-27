d6d920176a9ba323678ee1f40d1843c1baace6c8
Per Abrahamsen <abraham@dina.kvl.dk>
1997-05-27 21:24:07 +0000

    Update address.

--- lisp/progmodes/cpp.el
+++ lisp/progmodes/cpp.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 1994, 1995 Free Software Foundation
 
-;; Author: Per Abrahamsen <abraham@iesd.auc.dk>
+;; Author: Per Abrahamsen <abraham@dina.kvl.dk>
 ;; Keywords: c, faces, tools
 
 ;; This file is part of GNU Emacs.
