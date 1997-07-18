3219bb4315b9e1d5b54284ef13950841331d23cc
Richard M. Stallman <rms@gnu.org>
1997-07-18 02:16:42 +0000

    Comment change.

--- lisp/progmodes/cc-compat.el
+++ lisp/progmodes/cc-compat.el
@@ -5,7 +5,7 @@
 ;; Author:     1994-1997 Barry A. Warsaw
 ;; Maintainer: cc-mode-help@python.org
 ;; Created:    August 1994, split from cc-mode.el
-;; Version:    5.12
+;; Version:    5.13
 ;; Keywords:   c languages oop
 
 ;; This file is part of GNU Emacs.
