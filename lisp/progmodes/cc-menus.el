e76df57d13a045dc55a18e4b71d9d98d6dcf233e
Richard M. Stallman <rms@gnu.org>
1997-08-11 22:16:50 +0000

    Comment change.

--- lisp/progmodes/cc-menus.el
+++ lisp/progmodes/cc-menus.el
@@ -7,7 +7,7 @@
 ;;             1985 Richard M. Stallman
 ;; Maintainer: cc-mode-help@python.org
 ;; Created:    22-Apr-1997 (split from cc-mode.el)
-;; Version:    5.14
+;; Version:    5.15
 ;; Keywords:   c languages oop
 
 ;; This file is part of GNU Emacs.
