ecec411d9ec12acda99f4e0aafadd100e7b03172
Richard M. Stallman <rms@gnu.org>
1997-09-21 14:11:37 +0000

    (c-initialize-builtin-style): Don't ever try to use copy-tree.

--- lisp/progmodes/cc-styles.el
+++ lisp/progmodes/cc-styles.el
@@ -571,14 +571,11 @@ offset for that syntactic element.  Optional ADD says to add SYMBOL to
   ;; style.  Only do this once!
   (or (assoc "cc-mode" c-style-alist)
       (let (copyfunc)
-	;; use built-in copy-tree if its there.
-	(if (fboundp 'copy-tree)
-	    (setq copyfunc (symbol-function 'copy-tree))
-	  (setq copyfunc (lambda (tree)
-			    (if (consp tree)
-				(cons (funcall copyfunc (car tree))
-				      (funcall copyfunc (cdr tree)))
-			      tree))))
+	(setq copyfunc (lambda (tree)
+			 (if (consp tree)
+			     (cons (funcall copyfunc (car tree))
+				   (funcall copyfunc (cdr tree)))
+			   tree)))
 	(c-add-style "cc-mode"
 		     (mapcar
 		      (function
