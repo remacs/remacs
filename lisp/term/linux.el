952e6ec21a33594c47c54763d73cf42772c1e1e6
Karl Heuer <kwzh@gnu.org>
1997-11-15 20:52:06 +0000

    Use set-input-mode to make Latin-1 chars work.

--- lisp/term/linux.el
+++ lisp/term/linux.el
@@ -1,3 +1,11 @@
 ;; The Linux console handles Latin-1 by default.
 
 (set-terminal-coding-system 'iso-latin-1)
+
+;; Make Latin-1 input characters work, too.
+;; Meta will continue to work, because the kernel
+;; turns that into Escape.
+
+(let ((value (current-input-mode)))
+  ;; The third arg only matters in that it is not t or nil.
+  (set-input-mode (nth 0 value) (nth 1 value) 'iso-latin-1 (nth 3 value)))
