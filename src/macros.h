e7900044689018482f8a0a16e0509f9a34802ab4
Andreas Schwab <schwab@suse.de>
1997-11-21 13:24:40 +0000

    Declare finalize_kbd_macro_chars and store_kbd_macro_char.

--- src/macros.h
+++ src/macros.h
@@ -39,3 +39,12 @@ extern int executing_macro_iterations;
    so after an error, it describes the innermost interrupted macro.  */
 
 extern Lisp_Object executing_macro;
+
+/* Declare that all chars stored so far in the kbd macro being defined
+ really belong to it.  This is done in between editor commands.  */
+
+extern void finalize_kbd_macro_chars P_ ((void));
+
+/* Store a character into kbd macro being defined */
+
+extern void store_kbd_macro_char P_ ((Lisp_Object));
