d0ed5e526ec33c396e3b491d1cc93bdb7592652a
Karl Heuer <kwzh@gnu.org>
1998-01-18 03:33:26 +0000

    (etags): Fix defgroup syntax.

--- lisp/progmodes/etags.el
+++ lisp/progmodes/etags.el
@@ -34,7 +34,8 @@ Use the `etags' program to make a tags table file.")
 ;; Make M-x set-variable tags-file-name like M-x visit-tags-table.
 ;;;###autoload (put 'tags-file-name 'variable-interactive "fVisit tags table: ")
 
-(defgroup etags "Tags tables"
+(defgroup etags nil
+  "Tags tables"
   :group 'tools)
 
 ;;;###autoload
