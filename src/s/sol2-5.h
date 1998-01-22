6209024e1b10d95a8800f768a3c4e40ea2f32644
Richard M. Stallman <rms@gnu.org>
1998-01-22 04:37:13 +0000

    (SYSTEM_MALLOC): Defined.

--- src/s/sol2-5.h
+++ src/s/sol2-5.h
@@ -7,6 +7,11 @@
    regex.c to provide them in Emacs, but this is safer for now.  */
 #define LIB_MOTIF -lXm -lgen
 
+/* This is the only known way to avoid some crashes
+   that seem to relate to screwed up malloc data
+   after deleting a frame.  */
+#define SYSTEM_MALLOC
+
 #if 0 /* A recent patch in unexelf.c should eliminate the need for this.  */
 /* Don't use the shared libraries for -lXt and -lXaw,
    to work around a linker bug in Solaris 2.5.
