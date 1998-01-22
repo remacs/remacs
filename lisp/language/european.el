3617715a755ed49707da7ae99bc57c17d5b8adf0
Kenichi Handa <handa@m17n.org>
1998-01-22 01:48:25 +0000

    Remove setting up of describe-european-environment-map and setup-european-environment-map.  Exclude them in args of calls to set-language-info-alist.  Adjusted for the change of make-coding-system.  Register coding-priority key in language-info-alist.  Add "German" language env. (setup-8-bit-environment): Delete CODING-SYSTEM arg. (iso-latin-1): Make it compatible with MIME-charset "iso-8859-1" (compound-text): New coding system.  `ctext' is now alias of it. (setup-german-environment): New function.

--- lisp/language/european.el
+++ lisp/language/european.el
@@ -28,23 +28,11 @@
 
 ;;; Code:
 
-(define-prefix-command 'describe-european-environment-map)
-(define-key-after describe-language-environment-map [European]
-  '("European" . describe-european-environment-map)
-  t)
-
-(define-prefix-command 'setup-european-environment-map)
-(define-key-after setup-language-environment-map [European]
-  '("European" . setup-european-environment-map)
-  t)
-
 ;; Setup for LANGAUGE which uses one-byte 8-bit CHARSET, one-byte
-;; 8-bit CODING-SYSTEM, and INPUT-METHOD.
-(defun setup-8-bit-environment (language charset coding-system input-method)
+;; 8-bit coding system, and INPUT-METHOD.
+(defun setup-8-bit-environment (language charset input-method)
   (setup-english-environment)
-  (set-default-coding-systems coding-system)
-  (setq coding-category-iso-8-1 coding-system
-	coding-category-iso-8-2 coding-system)
+  (set-language-environment-coding-systems language)
 
   (if charset
       (let ((nonascii-offset (- (make-char charset) 128)))
@@ -65,43 +53,52 @@
 
 (make-coding-system
  'iso-latin-1 2 ?1
- "ISO 2022 based 8-bit encoding (MIME:ISO-8859-1, Compound Text Encoding)"
- '((ascii t) (latin-iso8859-1 t) nil nil
-   nil ascii-eol ascii-cntl nil nil nil nil nil nil nil nil nil t)
- '(ascii latin-iso8859-1))
+ "ISO 2022 based 8-bit encoding for Laint-1 (MIME:ISO-8859-1)"
+ '(ascii latin-iso8859-1 nil nil
+   nil nil nil nil nil nil nil nil nil nil nil nil t)
+ '((safe-charsets ascii latin-iso8859-1)
+   (mime-charset . iso-8859-1)))
 
 (define-coding-system-alias 'iso-8859-1 'iso-latin-1)
 (define-coding-system-alias 'latin-1 'iso-latin-1)
-(define-coding-system-alias 'ctext 'iso-latin-1)
+
+(make-coding-system
+ 'compound-text 2 ?1
+ "ISO 2022 based encoding used in inter client communication of X"
+ '((ascii t) (latin-iso8859-1 t) nil nil
+   nil ascii-eol ascii-cntl nil nil nil nil nil nil nil nil nil t)
+ '((safe-charsets . t)))
+
+(define-coding-system-alias 'ctext 'compound-text)
 
 (defun setup-latin1-environment ()
   "Set up multilingual environment (MULE) for European Latin-1 users."
   (interactive)
-  (setup-8-bit-environment "Latin-1" 'latin-iso8859-1 'iso-latin-1
-			   "latin-1-prefix"))
+  (setup-8-bit-environment "Latin-1" 'latin-iso8859-1 "latin-1-prefix"))
 
 (set-language-info-alist
- "Latin-1" '((setup-function . (setup-latin1-environment
-				. setup-european-environment-map))
-	     (charset . (ascii latin-iso8859-1))
-	     (coding-system . (iso-latin-1))
+ "Latin-1" '((setup-function . setup-latin1-environment)
+	     (charset ascii latin-iso8859-1)
+	     (coding-system iso-latin-1)
+	     (coding-priority iso-latin-1)
 	     (sample-text
 	      . "Hello, Hej, Tere, Hei, Bonjour, Gr,A|_(B Gott, Ciao, ,A!(BHola!")
-	     (documentation . ("\
+	     (documentation . "\
 These languages are supported with the Latin-1 (ISO-8859-1) character set:
  Danish, Dutch, English, Faeroese, Finnish, French, German, Icelandic,
- Irish, Italian, Norwegian, Portuguese, Spanish, and Swedish.
-" . describe-european-environment-map))
-	     ))
+ Irish, Italian, Norwegian, Portuguese, Spanish, and Swedish."))
+ '("European"))
+
 
 ;; Latin-2 (ISO-8859-2)
 
 (make-coding-system
  'iso-latin-2 2 ?2
  "ISO 2022 based 8-bit encoding (MIME:ISO-8859-2)"
- '((ascii t) (latin-iso8859-2 t) nil nil
-   nil ascii-eol ascii-cntl nil nil nil nil)
- '(ascii latin-iso8859-2))
+ '(ascii latin-iso8859-2 nil nil
+   nil nil nil nil nil nil nil)
+ '((safe-charsets ascii latin-iso8859-2)
+   (mime-charset . iso-8859-2)))
 
 (define-coding-system-alias 'iso-8859-2 'iso-latin-2)
 (define-coding-system-alias 'latin-2 'iso-latin-2)
@@ -109,30 +106,30 @@ These languages are supported with the Latin-1 (ISO-8859-1) character set:
 (defun setup-latin2-environment ()
   "Set up multilingual environment (MULE) for European Latin-2 users."
   (interactive)
-  (setup-8-bit-environment "Latin-2" 'latin-iso8859-2 'iso-latin-2
-			   "latin-2-prefix"))
+  (setup-8-bit-environment "Latin-2" 'latin-iso8859-2 "latin-2-prefix"))
 
 (set-language-info-alist
- "Latin-2" '((setup-function . (setup-latin2-environment
-				. setup-european-environment-map))
-	     (charset . (ascii latin-iso8859-2))
-	     (coding-system . (iso-latin-2))
-	     (documentation . ("\
+ "Latin-2" '((setup-function . setup-latin2-environment)
+	     (charset ascii latin-iso8859-2)
+	     (coding-system iso-latin-2)
+	     (coding-priority iso-latin-2)
+	     (documentation . "\
 These languages are supported with the Latin-2 (ISO-8859-2) character set:
  Albanian, Czech, English, German, Hungarian, Polish, Romanian,
  Serbo-Croatian or Croatian, Slovak, Slovene, Sorbian (upper and lower),
- and Swedish,
-" . describe-european-environment-map))
-	     ))
+ and Swedish."))
+ '("European"))
+
 
 ;; Latin-3 (ISO-8859-3)
 
 (make-coding-system
  'iso-latin-3 2 ?3
  "ISO 2022 based 8-bit encoding (MIME:ISO-8859-3)"
- '((ascii t) (latin-iso8859-3 t) nil nil
-   nil ascii-eol ascii-cntl nil nil nil nil)
- '(ascii latin-iso8859-3))
+ '(ascii latin-iso8859-3 nil nil
+   nil nil nil nil nil nil nil)
+ '((safe-charsets ascii latin-iso8859-3)
+   (mime-charset . iso-8859-3)))
 
 (define-coding-system-alias 'iso-8859-3 'iso-latin-3)
 (define-coding-system-alias 'latin-3 'iso-latin-3)
@@ -140,29 +137,29 @@ These languages are supported with the Latin-2 (ISO-8859-2) character set:
 (defun setup-latin3-environment ()
   "Set up multilingual environment (MULE) for European Latin-3 users."
   (interactive)
-  (setup-8-bit-environment "Latin-3" 'latin-iso8859-3 'iso-latin-3
-			   "latin-3-prefix"))
+  (setup-8-bit-environment "Latin-3" 'latin-iso8859-3 "latin-3-prefix"))
 
 (set-language-info-alist
- "Latin-3" '((setup-function . (setup-latin3-environment
-				. setup-european-environment-map))
-	     (charset . (ascii latin-iso8859-3))
-	     (coding-system . (iso-latin-3))
-	     (documentation . ("\
+ "Latin-3" '((setup-function . setup-latin3-environment)
+	     (charset ascii latin-iso8859-3)
+	     (coding-system iso-latin-3)
+	     (coding-priority iso-latin-3)
+	     (documentation . "\
 These languages are supported with the Latin-3 (ISO-8859-3) character set:
  Afrikaans, Catalan, Dutch, English, Esperanto, French, Galician,
- German, Italian, Maltese, Spanish, and Turkish.
-" . describe-european-environment-map))
-	     ))
+ German, Italian, Maltese, Spanish, and Turkish."))
+ '("European"))
+
 
 ;; Latin-4 (ISO-8859-4)
 
 (make-coding-system
  'iso-latin-4 2 ?4
  "ISO 2022 based 8-bit encoding (MIME:ISO-8859-4)"
- '((ascii t) (latin-iso8859-4 t) nil nil
-   nil ascii-eol ascii-cntl nil nil nil nil)
- '(ascii latin-iso8859-4))
+ '(ascii latin-iso8859-4 nil nil
+   nil nil nil nil nil nil nil)
+ '((safe-charsets ascii latin-iso8859-4)
+   (mime-charset . iso-8895-4)))
 
 (define-coding-system-alias 'iso-8859-4 'iso-latin-4)
 (define-coding-system-alias 'latin-4 'iso-latin-4)
@@ -170,29 +167,29 @@ These languages are supported with the Latin-3 (ISO-8859-3) character set:
 (defun setup-latin4-environment ()
   "Set up multilingual environment (MULE) for European Latin-4 users."
   (interactive)
-  (setup-8-bit-environment "Latin-4" 'latin-iso8859-4 'iso-latin-4
-			   "latin-4-prefix"))
+  (setup-8-bit-environment "Latin-4" 'latin-iso8859-4 "latin-4-prefix"))
 
 (set-language-info-alist
- "Latin-4" '((setup-function . (setup-latin4-environment
-				. setup-european-environment-map))
-	     (charset . (ascii latin-iso8859-4))
-	     (coding-system . (iso-8859-4))
-	     (documentation . ("\
+ "Latin-4" '((setup-function . setup-latin4-environment)
+	     (charset ascii latin-iso8859-4)
+	     (coding-system iso-8859-4)
+	     (coding-priority iso-8859-4)
+	     (documentation . "\
 These languages are supported with the Latin-4 (ISO-8859-4) character set:
  Danish, English, Estonian, Finnish, German, Greenlandic, Lappish,
- Latvian, Lithuanian, and Norwegian.
-" . describe-european-environment-map))
-	     ))
+ Latvian, Lithuanian, and Norwegian."))
+ '("European"))
+
 
 ;; Latin-5 (ISO-8859-9)
 
 (make-coding-system
  'iso-latin-5 2 ?9
  "ISO 2022 based 8-bit encoding (MIME:ISO-8859-9)"
- '((ascii t) (latin-iso8859-9 t) nil nil
-   nil ascii-eol ascii-cntl nil nil nil nil)
- '(ascii latin-iso8859-9))
+ '(ascii latin-iso8859-9 nil nil
+   nil nil nil nil nil nil nil)
+ '((safe-charsets ascii latin-iso8859-9)
+   (mime-charset . iso-8859-9)))
 
 (define-coding-system-alias 'iso-8859-9 'iso-latin-5)
 (define-coding-system-alias 'latin-5 'iso-latin-5)
@@ -200,17 +197,36 @@ These languages are supported with the Latin-4 (ISO-8859-4) character set:
 (defun setup-latin5-environment ()
   "Set up multilingual environment (MULE) for European Latin-5 users."
   (interactive)
-  (setup-8-bit-environment "Latin-5" 'latin-iso8859-9 'iso-latin-5
-			   "latin-5-prefix"))
+  (setup-8-bit-environment "Latin-5" 'latin-iso8859-9 "latin-5-prefix"))
 
 (set-language-info-alist
- "Latin-5" '((setup-function . (setup-latin5-environment
-				. setup-european-environment-map))
-	     (charset . (ascii latin-iso8859-9))
-	     (coding-system . (iso-latin-5))
-	     (documentation . ("\
-These languages are supported with the Latin-5 (ISO-8859-9) character set.
-" . describe-european-environment-map))
-	     ))
+ "Latin-5" '((setup-function . setup-latin5-environment)
+	     (charset ascii latin-iso8859-9)
+	     (coding-system iso-latin-5)
+	     (coding-priority iso-latin-5)
+	     (documentation . "\
+These languages are supported with the Latin-5 (ISO-8859-9) character set."))
+ '("European"))
+
+
+(defun setup-german-environment ()
+  "Set up multilingual environment (MULE) for German users."
+  (interactive)
+  (funcall (get-language-info "Latin-1" 'setup-function))
+  (setq default-input-method "german-postfix"))
+
+(set-language-info-alist
+ "German" '((setup-function . setup-german-environment)
+	    (tutorial . "TUTORIAL.de")
+	    (charset ascii latin-iso8859-1)
+	    (coding-system iso-latin-1)
+	    (coding-priority iso-latin-1)
+	    (sample-text . "\
+German (Deutsch Nord)	Guten Tag
+German (Deutsch S,A|(Bd)	Gr,A|_(B Gott")
+	    (documentation . "\
+This language environment is almost the same as Latin-1,
+but default input method is set to \"german-postfix\"."))
+ '("European"))
 
 ;;; european.el ends here
