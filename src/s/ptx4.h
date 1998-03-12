8828ae9b5e11e0be07716b24bb972243c62f8461
Richard M. Stallman <rms@gnu.org>
1998-03-12 06:59:25 +0000

    (SETUP_SLAVE_PTY): New definition.

--- src/s/ptx4.h
+++ src/s/ptx4.h
@@ -13,3 +13,12 @@
 /* Marcus Daniels <marcus@sysc.pdx.edu> says vfork does exist.  */
 #define HAVE_VFORK
 
+/* pae@dim.com (Phil Ernhardt) says this correction to
+   the definition in usg5-4.h is needed to prevent
+   all asynchronous subprocesses from exiting right away.  */
+#undef SETUP_SLAVE_PTY
+#define SETUP_SLAVE_PTY \
+  if (ioctl (xforkin, I_PUSH, "ldterm") == -1)	\
+    fatal ("ioctl I_PUSH ldterm", errno);	\
+  if (ioctl (xforkin, I_PUSH, "ttcompat") == -1) \
+    fatal ("ioctl I_PUSH ttcompat", errno);
