7f3bff3e4cf05e54e37047f28c15a2747404125f
Andreas Schwab <schwab@suse.de>
1998-04-06 10:13:46 +0000

    Include <stdlib.h> and <unistd.h> if available. Don't declare geteuid. (print_help_and_exit): Change return type to void.  Forward declare it.

--- lib-src/emacsclient.c
+++ lib-src/emacsclient.c
@@ -29,10 +29,12 @@ Boston, MA 02111-1307, USA.  */
 
 #include <stdio.h>
 #include <getopt.h>
+#ifdef STDC_HEADERS
+#include <stdlib.h>
+#endif
 
 char *getenv (), *getwd ();
 char *getcwd ();
-int geteuid ();
 
 /* This is defined with -D from the compilation command,
    which extracts it from ../lisp/version.el.  */
@@ -46,6 +48,8 @@ char *progname;
 
 /* Nonzero means don't wait for a response from Emacs.  --no-wait.  */
 int nowait = 0;
+
+void print_help_and_exit ();
 
 struct option longopts[] =
 {
@@ -94,6 +98,7 @@ decode_options (argc, argv)
     }
 }
 
+void
 print_help_and_exit ()
 {
   fprintf (stderr,
@@ -177,6 +182,9 @@ main (argc, argv)
 #include <sys/un.h>
 #include <sys/stat.h>
 #include <errno.h>
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
 
 extern char *strerror ();
 extern int errno;
