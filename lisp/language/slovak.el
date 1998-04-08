88337979e4b49b1e810f0e8fd93df03a2af5b4f8
Karl Heuer <kwzh@gnu.org>
1998-04-08 07:03:51 +0000

    Add coding-priority.  Use iso-8859-2 instead of iso-latin-2.  Correct starting commentary.

--- lisp/language/slovak.el
+++ lisp/language/slovak.el
@@ -7,7 +7,22 @@
 ;; Maintainer: Milan Zamazal <pdm@fi.muni.cz>
 ;; Keywords: multilingual, Slovak
 
-;; Copying and warranty: GNU General Public License, version 2
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
 
 ;;; Commentary:
 
@@ -24,7 +39,8 @@
 (set-language-info-alist
  "Slovak" '((setup-function . setup-slovak-environment)
 	    (charset . (ascii latin-iso8859-2))
-	    (coding-system . (iso-latin-2))
+	    (coding-system . (iso-8859-2))
+	    (coding-priority . (iso-8859-2))
 	    (sample-text . "Prajeme V,Ba(Bm pr,Bm(Bjemn,B}(B de,Br(B!")
 	    (documentation . t)))
 
