5360c4bbe40de49e12d72bc2149bd4fd6c2f1e1d
Karl Heuer <kwzh@gnu.org>
1998-04-08 07:03:30 +0000

    Add coding-priority.  Improve sample text. Correct starting commentary.

--- lisp/language/czech.el
+++ lisp/language/czech.el
@@ -2,11 +2,25 @@
 
 ;; Copyright (C) 1998 Free Software Foundation.
 
-;; Author:     Milan Zamazal <pdm@fi.muni.cz>
 ;; Maintainer: Milan Zamazal <pdm@fi.muni.cz>
 ;; Keywords: multilingual, Czech
 
-;; Copying and warranty: GNU General Public License, version 2
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
 
 ;;; Commentary:
 
@@ -24,8 +38,9 @@
  "Czech" '((setup-function . setup-czech-environment)
 	   (charset . (ascii latin-iso8859-2))
 	   (coding-system . (iso-8859-2))
+	   (coding-priority . (iso-8859-2))
 	   (tutorial . "TUTORIAL.cz")
-	   (sample-text . "P,Bx(Bejeme v,Ba(Bm hezk,Bi(B r,Ba(Bno!")
+	   (sample-text . "P,Bx(Bejeme v,Ba(Bm hezk,B}(B den!")
 	   (documentation . t)))
 
 (provide 'czech)
