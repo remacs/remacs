168afdaa0e33b8dcca026f3cffe6bb1e2f7f50bd
Richard M. Stallman <rms@gnu.org>
1998-04-17 07:08:02 +0000

    (Fcall_process_region): Unbind coding-system-for-write after calling Fwrite_region.

--- src/callproc.c
+++ src/callproc.c
@@ -840,8 +840,14 @@ If you quit, the process is killed with SIGINT, or SIGKILL if you quit again.")
 	val = Qnil;
     }
 
-  specbind (intern ("coding-system-for-write"), val);
-  Fwrite_region (start, end, filename_string, Qnil, Qlambda, Qnil, Qnil);
+  {
+    int count1 = specpdl_ptr - specpdl;
+
+    specbind (intern ("coding-system-for-write"), val);
+    Fwrite_region (start, end, filename_string, Qnil, Qlambda, Qnil, Qnil);
+
+    unbind_to (count1, Qnil);
+  }
 
   /* Note that Fcall_process takes care of binding 
      coding-system-for-read.  */
