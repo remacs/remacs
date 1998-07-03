3efcc61ef6c46a84e5715e0a9cdd99a325f30760
Richard M. Stallman <rms@gnu.org>
1998-07-03 19:53:47 +0000

    (dos-printer): Obsolete variable deleted. (dos-ps-printer): Likewise.

--- lisp/dos-vars.el
+++ lisp/dos-vars.el
@@ -31,37 +31,4 @@
   :type '(repeat string)
   :group 'dos-fns)
 
-;; Support for printing under MS-DOS, see lpr.el and ps-print.el.
-(defcustom dos-printer "PRN"
-  "*The name of a local MS-DOS device to which data is sent for printing.
-\(Note that PostScript files are sent to `dos-ps-printer', which see.\)
-
-Typical non-default settings would be \"LPT1\" to \"LPT3\" for
-parallel printers, or \"COM1\" to \"COM4\" or \"AUX\" for serial
-printers.  You can also set it to a name of a file, in which
-case the output gets appended to that file.
-If you want to discard the printed output, set this to \"NUL\"."
-  :type 'file ; could use string but then we lose completion for files.
-  :group 'dos-fns)
-
-(defcustom dos-ps-printer "PRN"
-  "*Method for printing PostScript files under MS-DOS.
-
-If the value is a string, then it is taken as the name of the
-device to which PostScript files are written.  By default it
-is the default printer device; typical non-default settings
-would be \"LPT1\" to \"LPT3\" for parallel printers, or \"COM1\"
-to \"COM4\" or \"AUX\" for serial printers.  You can also set it
-to a name of a file, in which case the output gets appended
-to that file.  \(Note that `ps-print' package already has
-facilities for printing to a file, so you might as well use
-them instead of changing the setting of this variable.\)  If
-you want to silently discard the printed output, set this to \"NUL\".
-
-If the value is anything but a string, PostScript files will be
-piped to the program given by `ps-lpr-command', with switches
-given by `ps-lpr-switches', which see."
-  :type '(choice file (other :tag "Pipe to ps-lpr-command" pipe))
-  :group 'dos-fns)
-
 ;;; dos-vars.el ends here
