24b0b52b1151cbf0295a383eaf1610c48cc0b143
Richard M. Stallman <rms@gnu.org>
1998-07-25 06:46:19 +0000

    (HAVE_STRING_H): Delete #undef here.

--- src/s/aix4.h
+++ src/s/aix4.h
@@ -5,11 +5,6 @@
 /* AIX 4 does not have HFT any more.  */
 #undef AIXHFT
 
-/* string.h defines rindex as a macro, at least with native cc, so we
-   lose declaring char * rindex without this.
-   It is just a guess which versions of AIX need this definition.  */
-#undef HAVE_STRING_H
-
 /* Dave Love <d.love@dl.ac.uk> reported this as needed on AIX 4.1.
    It is just a guess which versions of AIX need this definition.  */
 #define HAVE_WAIT_HEADER
