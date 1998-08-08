ec0f00faca2d90f79ac1a07c7cf6607a36df65ad
Richard M. Stallman <rms@gnu.org>
1998-08-08 03:02:37 +0000

    Initial revision

---
+++ src/s/ptx4-2.h
@@ -0,0 +1,17 @@
+#include "ptx4.h"
+
+/* Gelling Kevan (gellingk.eurostar@ems.rail.co.uk)
+   says that PTX 4.2.3 needs this version.  */
+#undef SETUP_SLAVE_PTY
+#define SETUP_SLAVE_PTY \
+    if (ioctl (xforkin, I_PUSH, "ptem") == -1)    \
+      fatal ("ioctl I_PUSH ptem", errno);        \
+    if (ioctl (xforkin, I_PUSH, "ldterm") == -1)  \
+      fatal ("ioctl I_PUSH ldterm", errno);
+
+/* Gelling Kevan (gellingk.eurostar@ems.rail.co.uk)
+   says that gmalloc.c needs _POSIX_SOURCE.
+   This defines _POSIX_SOURCE only for gmalloc.c.  */
+#ifdef _MALLOC_INTERNAL
+#define _POSIX_SOURCE
+#endif
