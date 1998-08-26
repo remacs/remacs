66236b772af493888a34148662dca58561958a6f
Karl Heuer <kwzh@gnu.org>
1998-08-26 18:00:04 +0000

    Comment fix.

--- lisp/hilit-chg.el
+++ lisp/hilit-chg.el
@@ -5,6 +5,8 @@
 ;; Author: Richard Sharman <rsharman@magma.ca>
 ;; Keywords: faces
 
+;; This file is part of GNU Emacs.
+
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 2, or (at your option)
