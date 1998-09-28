d7af002f653f49bfcd5d96995ffaa5da4ab87945
Kenichi Handa <handa@m17n.org>
1998-09-28 11:52:53 +0000

    (adjust_after_replace): Don't delete text properties here.

--- src/insdel.c
+++ src/insdel.c
@@ -1656,10 +1656,6 @@ adjust_after_replace (from, from_byte, prev_text, len, len_byte)
   if (BUF_INTERVALS (current_buffer) != 0)
     {
       offset_intervals (current_buffer, from, len - nchars_del);
-      if (len - nchars_del > 0)
-	Fset_text_properties (make_number (from),
-			      make_number (from + len - nchars_del),
-			      Qnil, Qnil);
     }
 #endif
 
