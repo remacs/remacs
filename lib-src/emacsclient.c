5fb29af43c1c5e7941645b97542a90ae5df18ebf
Richard M. Stallman <rms@gnu.org>
1998-10-10 21:20:22 +0000

    (main): Null-terminate system_name.

--- lib-src/emacsclient.c
+++ lib-src/emacsclient.c
@@ -225,6 +225,8 @@ main (argc, argv)
     struct stat statbfr;
 
     gethostname (system_name, sizeof (system_name));
+    /* system_name must be null-terminated string */
+    system_name[sizeof (system_name) - 1] = '\0';
     sprintf (server.sun_path, "/tmp/esrv%d-%s", geteuid (), system_name);
 
     if (stat (server.sun_path, &statbfr) == -1)
