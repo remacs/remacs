cb8778823b44ed8aa6d5b5c8e6c717c7ac12c87e
Kenichi Handa <handa@m17n.org>
1998-10-21 11:50:56 +0000

    (in-is13194-devanagari-pre-write-conversion): Use with-temp-buffer.

--- lisp/language/devan-util.el
+++ lisp/language/devan-util.el
@@ -1277,14 +1277,12 @@ Ligatures and special rules are processed."
 
 ;;;###autoload
 (defun in-is13194-devanagari-pre-write-conversion (from to)
-  (let ((old-buf (current-buffer))
-	(work-buf (get-buffer-create " *devanagari-work*")))
-    (set-buffer work-buf)
-    (erase-buffer)
-    (if (stringp from)
-	(insert from)
-      (insert-buffer-substring old-buf from to))
-    (devanagari-decompose-to-is13194-region (point-min) (point-max))
+  (let ((old-buf (current-buffer)))
+    (with-temp-buffer
+      (if (stringp from)
+	  (insert from)
+	(insert-buffer-substring old-buf from to))
+      (devanagari-decompose-to-is13194-region (point-min) (point-max)))
     ;; Should return nil as annotations.
     nil))
 
