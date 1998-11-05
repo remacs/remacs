635ad74809cef0070e421a5ff8e63d21fd812427
Karl Heuer <kwzh@gnu.org>
1998-11-05 19:19:03 +0000

    (Man-page-header-regexp): Alternate value for Solaris 2.6.

--- lisp/man.el
+++ lisp/man.el
@@ -254,8 +254,12 @@ the associated section number."
   "Regular expression describing a manpage section within parentheses.")
 
 (defvar Man-page-header-regexp
-  (concat "^[ \t]*\\(" Man-name-regexp
-	  "(\\(" Man-section-regexp "\\))\\).*\\1")
+  (if (and (string-match "-solaris2\\." system-configuration)
+	   (not (string-match "-solaris2\\.[123435]$" system-configuration)))
+      (concat "^[-A-Za-z0-9_].*[ \t]\\(" Man-name-regexp
+	      "(\\(" Man-section-regexp "\\))\\)$")
+    (concat "^[ \t]*\\(" Man-name-regexp
+	    "(\\(" Man-section-regexp "\\))\\).*\\1"))
   "Regular expression describing the heading of a page.")
 
 (defvar Man-heading-regexp "^\\([A-Z][A-Z ]+\\)$"
