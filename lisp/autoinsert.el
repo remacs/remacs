c5178e8e12ad7dd3eecaa838abb4571b3ebb17a4
Karl Heuer <kwzh@gnu.org>
1998-11-13 18:21:49 +0000

    (auto-insert-alist): Don't assume *.el is part of GNU Emacs.

--- lisp/autoinsert.el
+++ lisp/autoinsert.el
@@ -164,14 +164,12 @@ If this contains a %s, that will be replaced by the matching rule."
     (completing-read "Keyword, C-h: " v1 nil t))
     str ", ") & -2 "
 
-;; This file is part of GNU Emacs.
-
-;; GNU Emacs is free software; you can redistribute it and/or modify
+;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 2, or (at your option)
 ;; any later version.
 
-;; GNU Emacs is distributed in the hope that it will be useful,
+;; This file is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
