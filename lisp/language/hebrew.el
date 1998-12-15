f2719637a3e170c370e136bb00ffa69d122d6b4b
Eli Zaretskii <eliz@gnu.org>
1998-12-15 11:15:11 +0000

    Don't use sublists in the flags argument passed to make-coding-system.

--- lisp/language/hebrew.el
+++ lisp/language/hebrew.el
@@ -31,7 +31,7 @@
 (make-coding-system
  'hebrew-iso-8bit 2 ?8
  "ISO 2022 based 8-bit encoding for Hebrew (MIME:ISO-8859-8)"
- '((ascii t) (hebrew-iso8859-8 t) nil nil
+ '(ascii hebrew-iso8859-8 nil nil
    nil ascii-eol ascii-cntl nil nil nil nil nil t)
  '((safe-charsets ascii hebrew-iso8859-8)
    (mime-charset . iso-8859-8)))
