9a3f3bf4394f3c47d1b852b10e3952375debaad8
Markus Rost <rost@math.uni-bielefeld.de>
1999-01-10 06:17:49 +0000

    (custom-save-delete): First scan the custom-file.  Thus exit with an error in case the custom-file contains a non-closed open parenthesis.

--- lisp/cus-edit.el
+++ lisp/cus-edit.el
@@ -3100,6 +3100,7 @@ Leave point at the location of the call, or after the last expression."
   (let ((default-major-mode))
     (set-buffer (find-file-noselect (custom-file))))
   (goto-char (point-min))
+  (save-excursion (forward-sexp (buffer-size)))	; Test for scan errors.
   (catch 'found
     (while t
       ;; Skip all whitespace and comments.
