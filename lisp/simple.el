fc02509028b4d37124f636f798247e02f76f12eb
Dave Love <fx@gnu.org>
1999-01-15 16:57:27 +0000

    (join-line): New alias.

--- lisp/simple.el
+++ lisp/simple.el
@@ -214,6 +214,8 @@ With argument, join this line to following line."
 	    (delete-region (point) (+ (point) (length fill-prefix))))
 	(fixup-whitespace))))
 
+(defalias 'join-line #'delete-indentation) ; easier to find
+
 (defun fixup-whitespace ()
   "Fixup white space between objects around point.
 Leave one space or none, according to the context."
