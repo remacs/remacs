bf7ce02180f3b1b9941274a08e5a8bc3429a5a45
Karl Heuer <kwzh@gnu.org>
1999-02-22 20:56:34 +0000

    Initial revision

---
+++ src/m/f301.h
@@ -0,0 +1,7 @@
+/* Machine description file for Fujitsu F301 machines.  */
+
+/* The following line tells the configuration script what sort of
+   operating system this machine is likely to run.
+   USUAL-OPSYS="uxpv"  */
+
+#include "delta88k.h"
---
+++ src/s/uxpv.h
@@ -0,0 +1,11 @@
+/* System description file for Fujitsu UXP/V
+   This file describes the parameters that system description files
+   should define or not.  */
+
+#include "usg5-4-2.h"
+
+/* SYSTEM_TYPE should indicate the kind of system you are using.
+ It sets the Lisp variable system-type.  */
+
+#undef SYSTEM_TYPE
+#define SYSTEM_TYPE "uxpv"
