3f579f6af7996d411061b682c53313140fa26537
Karl Heuer <kwzh@gnu.org>
1999-03-30 21:30:12 +0000

    (which-func-mode-global): Doc fix.

--- lisp/which-func.el
+++ lisp/which-func.el
@@ -104,7 +104,8 @@ Zero means compute the Imenu menu regardless of size."
 ;;;###autoload
 (defcustom which-func-mode-global nil
   "*Toggle `which-func-mode'.
-You must modify via \\[customize] for this variable to have an effect."
+This variable should be set only with \\[customize], which is equivalent
+to using the function `which-func-mode'.
   :set #'(lambda (symbol value)
 	   (which-func-mode (if value 1 0)))
   :initialize 'custom-initialize-default
