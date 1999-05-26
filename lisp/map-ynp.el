55f7520a6e5e64dc379a6b9efe2e3b512e1f45d8
Kenichi Handa <handa@m17n.org>
1999-05-26 07:40:26 +0000

    (map-y-or-n-p): Don't inherit the current input method in read-event.

--- lisp/map-ynp.el
+++ lisp/map-ynp.el
@@ -149,7 +149,7 @@ Returns the number of actions taken."
 				(key-description (vector help-char)))
 		       (if minibuffer-auto-raise
 			   (raise-frame (window-frame (minibuffer-window))))
-		       (setq char (read-event nil t))
+		       (setq char (read-event))
 		       ;; Show the answer to the question.
 		       (message "%s(y, n, !, ., q, %sor %s) %s"
 				prompt user-keys
