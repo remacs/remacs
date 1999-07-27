17eedd00d9c0e81da9374a43954a5bdbecb70f95
Kenichi Handa <handa@m17n.org>
1999-07-27 01:32:10 +0000

    (w32_find_ccl_program): Add casting.

--- src/w32fns.c
+++ src/w32fns.c
@@ -6211,7 +6211,8 @@ w32_find_ccl_program (fontp)
     }
   if (! NILP (list))
     {
-      struct ccl_program *ccl = xmalloc (sizeof (struct ccl_program));
+      struct ccl_program *ccl
+	= (struct ccl_program *) xmalloc (sizeof (struct ccl_program));
 
       if (setup_ccl_program (ccl, XCONS (elt)->cdr) < 0)
 	xfree (ccl);
