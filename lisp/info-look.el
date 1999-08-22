57f3e9c9f6316042b10481648c02837c915ca085
Stephen Eglen <stephen@gnu.org>
1999-08-22 18:09:58 +0000

    Octave-mode support: for the function index entries, allow the prefix to also match optional return value list of form `[X Y ... Z] = '.

--- lisp/info-look.el
+++ lisp/info-look.el
@@ -761,7 +761,8 @@ Return nil if there is nothing appropriate in the buffer near point."
 (info-lookup-maybe-add-help
  :mode 'octave-mode
  :regexp "[_a-zA-Z0-9]+"
- :doc-spec '(("(octave)Function Index" nil "^ - [^:]+:[ ]+" nil)
+ :doc-spec '(("(octave)Function Index" nil 
+	      "^ - [^:]+:[ ]+\\(\\[[^=]*=[ ]+\\)?" nil)
 	     ("(octave)Variable Index" nil "^ - [^:]+:[ ]+" nil)
 	     ;; Catch lines of the form "xyz statement"
 	     ("(octave)Concept Index"
