9832d51d606ae361ebcc42d3a63678e71cbf0cd9
Dave Love <fx@gnu.org>
1999-10-11 17:14:12 +0000

    Avoid strneq.

--- lib-src/getopt.c
+++ lib-src/getopt.c
@@ -652,7 +652,7 @@ _getopt_internal (argc, argv, optstring, longopts, longind, long_only)
       /* Test all long options for either exact match
 	 or abbreviated matches.  */
       for (p = longopts, option_index = 0; p->name; p++, option_index++)
-	if (strneq (p->name, nextchar, nameend - nextchar))
+	if (!strncmp (p->name, nextchar, nameend - nextchar))
 	  {
 	    if ((unsigned int) (nameend - nextchar)
 		== (unsigned int) strlen (p->name))
@@ -839,7 +839,7 @@ _getopt_internal (argc, argv, optstring, longopts, longind, long_only)
 	/* Test all long options for either exact match
 	   or abbreviated matches.  */
 	for (p = longopts, option_index = 0; p->name; p++, option_index++)
-	  if (strneq (p->name, nextchar, nameend - nextchar))
+	  if (!strncmp (p->name, nextchar, nameend - nextchar))
 	    {
 	      if ((unsigned int) (nameend - nextchar) == strlen (p->name))
 		{
