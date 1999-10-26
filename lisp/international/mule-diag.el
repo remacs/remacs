d4b11c67a713b67bff569fda13f11b0e517d764e
Kenichi Handa <handa@m17n.org>
1999-10-26 12:53:53 +0000

    (mule-diag): Handle the case that default-input-method is symbol.

--- lisp/international/mule-diag.el
+++ lisp/international/mule-diag.el
@@ -783,7 +783,7 @@ system which uses fontsets)."
       (list-input-methods-1)
       (insert "\n")
       (if default-input-method
-	  (insert "Default input method: " default-input-method "\n")
+	  (insert (format "Default input method: %s\n" default-input-method))
 	(insert "No default input method is specified\n"))
 
       (insert-section 4 "Coding systems")
