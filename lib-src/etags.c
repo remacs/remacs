ba65dba8bbe27a68e87c5e9922ffcf7d0abdc17a
Francesco Potortì <pot@gnu.org>
1999-11-03 23:13:10 +0000

    Changed the mail addess of the maintainer.

--- lib-src/etags.c
+++ lib-src/etags.c
@@ -28,7 +28,7 @@ Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. */
  *	Francesco Potorti` reorganised C and C++ based on work by Joe Wells.
  *	Regexp tags by Tom Tromey.
  *
- *	Francesco Potorti` (F.Potorti@cnuce.cnr.it) is the current maintainer.
+ *	Francesco Potorti` (pot@gnu.org) is the current maintainer.
  */
 
 char pot_etags_version[] = "@(#) pot revision number is 13.31";
