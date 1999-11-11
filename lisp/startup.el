3b79c219074cdd7a09731f57352a910cfb418bd6
Gerd Moellmann <gerd@gnu.org>
1999-11-11 11:54:07 +0000

    (initial-scratch-message): Add ";;" to the front of lines in the message.

--- lisp/startup.el
+++ lisp/startup.el
@@ -765,9 +765,9 @@ or `CVS', and any subdirectory that contains a file named `.nosearch'."
   (if noninteractive (kill-emacs t)))
 
 (defcustom initial-scratch-message "\
-This buffer is for notes you don't want to save, and for Lisp evaluation.
-If you want to create a file, visit that file with C-x C-f,
-then enter the text in that file's own buffer.
+;; This buffer is for notes you don't want to save, and for Lisp evaluation.
+;; If you want to create a file, visit that file with C-x C-f,
+;; then enter the text in that file's own buffer.
 
 "
   "Initial message displayed in *scratch* buffer at startup.
