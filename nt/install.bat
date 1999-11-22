2c9b8e9d189b59e8e94029d74196509a41f45414
Gerd Moellmann <gerd@gnu.org>
1999-11-22 22:35:45 +0000

    Pass on command line arguments to make.

--- nt/install.bat
+++ nt/install.bat
@@ -2,6 +2,7 @@
 if (%1) == (speed) set BUILD_TYPE=spd
 if (%1) == (speed) shift
 if not (%1) == () set INSTALL_DIR=%1
-nmake -f makefile.nt install
+if not (%1) == () shift
+nmake -f makefile.nt install %1 %2 %3 %4 %5 %6 %7 %8 %9
 set INSTALL_DIR=
 set BUILD_TYPE=
