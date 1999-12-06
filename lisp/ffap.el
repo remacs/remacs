32fd7972629ec6752b4ac0f465915cc60d85e8ae
Dave Love <fx@gnu.org>
1999-12-06 16:26:36 +0000

    Autoload the ffap alias directly.

--- lisp/ffap.el
+++ lisp/ffap.el
@@ -1351,12 +1351,7 @@ See <ftp://ftp.mathcs.emory.edu/pub/mic/emacs/> for latest version."
 				filename))))))
 
 ;; Shortcut: allow {M-x ffap} rather than {M-x find-file-at-point}.
-;; The defun is for autoload.el; the defalias takes over at load time.
-;;;###autoload
-(defun ffap (&optional filename)
-  "A short alias for the find-file-at-point command.")
-(defalias 'ffap 'find-file-at-point)
-
+;;;###autoload(defalias 'ffap 'find-file-at-point)
 
 ;;; Menu support (`ffap-menu'):
 
