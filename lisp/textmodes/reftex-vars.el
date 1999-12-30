3ba2590f716a2ad4acb2eefb9f028efb241f76b7
Richard M. Stallman <rms@gnu.org>
1999-12-30 02:57:12 +0000

    Supply licensing info.

--- lisp/textmodes/reftex-vars.el
+++ lisp/textmodes/reftex-vars.el
@@ -1,7 +1,26 @@
 ;;; reftex-vars.el - Configuration variables for RefTeX
-;;; Version: 4.9
-;;;
-;;; See main file reftex.el for licensing information
+;; Copyright (c) 1997, 1998, 1999 Free Software Foundation, Inc.
+
+;; Author:     Carsten Dominik <dominik@strw.LeidenUniv.nl>
+;; Version:    4.9
+;; Keywords:   tex
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
 
 (eval-when-compile (require 'cl))
 (provide 'reftex-vars)
