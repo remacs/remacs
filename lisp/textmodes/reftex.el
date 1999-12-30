2b12cfbfd17d9864bbb816b6faf3435b3be40e2c
Richard M. Stallman <rms@gnu.org>
1999-12-30 02:50:29 +0000

    Fix in license info.

--- lisp/textmodes/reftex.el
+++ lisp/textmodes/reftex.el
@@ -5,7 +5,7 @@
 ;; Version:    4.9
 ;; Keywords:   tex
 
-;; This file is not part of GNU Emacs.
+;; This file is part of GNU Emacs.
 
 ;; GNU Emacs is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
