4c5482386b6bdc77aea564798029ce43e2d6cb29
Dave Love <fx@gnu.org>
2000-01-08 15:19:21 +0000

    Remove inappropriate customization (allowing custom.el to use backquote).

--- lisp/emacs-lisp/backquote.el
+++ lisp/emacs-lisp/backquote.el
@@ -69,27 +69,16 @@ For example (backquote-list* 'a 'b 'c) => (a b . c)"
 
 (defalias 'backquote-list* (symbol-function 'backquote-list*-macro))
 
-(defgroup backquote nil
-  "Implement the ` Lisp construct."
-  :prefix "backquote-"
-  :group 'lisp)
-
 ;; A few advertised variables that control which symbols are used
 ;; to represent the backquote, unquote, and splice operations.
-(defcustom backquote-backquote-symbol '\`
-  "*Symbol used to represent a backquote or nested backquote (e.g. `)."
-  :type 'symbol
-  :group 'backquote)
+(defconst backquote-backquote-symbol '\`
+  "*Symbol used to represent a backquote or nested backquote.")
 
-(defcustom backquote-unquote-symbol ',
-  "*Symbol used to represent an unquote (e.g. `,') inside a backquote."
-  :type 'symbol
-  :group 'backquote)
+(defconst backquote-unquote-symbol ',
+  "*Symbol used to represent an unquote inside a backquote.")
 
-(defcustom backquote-splice-symbol ',@
-  "*Symbol used to represent a splice (e.g. `,@') inside a backquote."
-  :type 'symbol
-  :group 'backquote)
+(defconst backquote-splice-symbol ',@
+  "*Symbol used to represent a splice inside a backquote.")
 
 ;;;###autoload
 (defmacro backquote (arg)
