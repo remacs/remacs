f9a70a31718a329d98272bbba17184c295994023
Gerd Moellmann <gerd@gnu.org>
2000-01-27 15:31:20 +0000

    (XMenuActivate): Add parameter HELP_CALLBACK. Call help callback.

--- oldXMenu/Activate.c
+++ oldXMenu/Activate.c
@@ -1,4 +1,4 @@
-/* $Header: /u/src/emacs/19.0/oldXMenu/RCS/Activate.c,v 1.1 1992/04/11 22:10:17 jimb Exp $ */
+/* $Header: /gd/gnu/cvsroot/emacs/oldXMenu/Activate.c,v 1.1 1999/10/03 19:34:50 fx Exp $ */
 /* Copyright    Massachusetts Institute of Technology    1985	*/
 
 #include "copyright.h"
@@ -84,7 +84,8 @@
 #include "XMenuInt.h"
 
 int
-XMenuActivate(display, menu, p_num, s_num, x_pos, y_pos, event_mask, data)
+XMenuActivate(display, menu, p_num, s_num, x_pos, y_pos, event_mask, data,
+	      help_callback)
     register Display *display;		/* Display to put menu on. */
     register XMenu *menu;		/* Menu to activate. */
     int *p_num;				/* Pane number selected. */
@@ -93,6 +94,7 @@ XMenuActivate(display, menu, p_num, s_num, x_pos, y_pos, event_mask, data)
     int y_pos;				/* Y coordinate of menu position. */
     unsigned int event_mask;		/* Mouse button event mask. */
     char **data;			/* Pointer to return data value. */
+    void (* help_callback) ();		/* Help callback.  */
 {
     int status;				/* X routine call status. */
     int orig_x;				/* Upper left menu origin X coord. */
@@ -309,6 +311,8 @@ XMenuActivate(display, menu, p_num, s_num, x_pos, y_pos, event_mask, data)
 		    }
 		}			  
 		cur_s = (XMSelect *)event_xmw;
+		help_callback (cur_s->help_string);
+		
 		/*
 		 * If the pane we are in is active and the
 		 * selection entered is active then activate
