4e4dfa78179f9e6d3375c7c0db235111d6137493
Gerd Moellmann <gerd@gnu.org>
2000-03-26 14:06:41 +0000

    (Fset_syntax_table): Use new macros for per-buffer variables.

--- src/syntax.c
+++ src/syntax.c
@@ -721,11 +721,12 @@ One argument, a syntax table.")
   (table)
      Lisp_Object table;
 {
+  int idx;
   check_syntax_table (table);
   current_buffer->syntax_table = table;
   /* Indicate that this buffer now has a specified syntax table.  */
-  current_buffer->local_var_flags
-    |= XFASTINT (buffer_local_flags.syntax_table);
+  idx = BUFFER_LOCAL_VAR_IDX (syntax_table);
+  SET_BUFFER_HAS_LOCAL_VALUE_P (current_buffer, idx, 1);
   return table;
 }
 
