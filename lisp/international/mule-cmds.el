5de75f53b1c049f1656eb94bee65364199941275
Kenichi Handa <handa@m17n.org>
2000-04-03 11:11:58 +0000

    (encoded-string-description): Use uppercase letters for hexadecimal format.

--- lisp/international/mule-cmds.el
+++ lisp/international/mule-cmds.el
@@ -1820,7 +1820,7 @@ It can be retrieved with `(get-char-code-property CHAR PROPNAME)'."
        ;; Try to get a pretty description for ISO 2022 escape sequences.
        (function (lambda (x) (or (cdr (assq x iso-2022-control-alist))
 				 (format "%02X" x))))
-     (function (lambda (x) (format "0x%02x" x))))
+     (function (lambda (x) (format "0x%02X" x))))
    str " "))
 
 (defun encode-coding-char (char coding-system)
