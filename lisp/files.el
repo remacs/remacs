01fae1df89555e7cc9f68366f0c667808387f10f
Gerd Moellmann <gerd@gnu.org>
2000-04-10 13:32:07 +0000

    (auto-mode-alist): Add `EBROWSE'.

--- lisp/files.el
+++ lisp/files.el
@@ -1415,7 +1415,9 @@ in that case, this function acts as if `enable-local-variables' were t."
     ("\\.sm2$" . snmpv2-mode)
     ("\\.\\(diffs?\\|patch\\|rej\\)\\'" . diff-mode)
     ("\\.[eE]?[pP][sS]$" . ps-mode)
-    ("configure\\.in\\'" . autoconf-mode))
+    ("configure\\.in\\'" . autoconf-mode)
+    ("EBROWSE\\'" . ebrowse-tree-mode)
+    ("\\.ebrowse\\'" . ebrowse-tree-mode))
   "\
 Alist of filename patterns vs corresponding major mode functions.
 Each element looks like (REGEXP . FUNCTION) or (REGEXP FUNCTION NON-NIL).
