643c911e6fadc7ebb110387a720bfb8a097e3f35
Dave Love <fx@gnu.org>
2000-05-22 19:00:13 +0000

    Fix boilerplate comments.

--- lisp/hl-line.el
+++ lisp/hl-line.el
@@ -6,12 +6,14 @@
 ;; Created: 1998-09-13
 ;; Keywords: faces, frames
 
-;; Hl-Line mode is free software; you can redistribute it and/or modify
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 2, or (at your option)
 ;; any later version.
 
-;; Hl-Line mode is distributed in the hope that it will be useful,
+;; GNU Emacs is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
