b5d56c1f0a369678331d52561dc2b893b6eaa213
Gerd Moellmann <gerd@gnu.org>
2000-06-08 20:58:15 +0000

    (handle_fontified_prop): Don't GCPRO local var `pos'; it's an integer.

--- src/xdisp.c
+++ src/xdisp.c
@@ -1787,7 +1787,6 @@ handle_fontified_prop (it)
 {
   Lisp_Object prop, pos;
   enum prop_handled handled = HANDLED_NORMALLY;
-  struct gcpro gcpro1;
 
   /* Get the value of the `fontified' property at IT's current buffer
      position.  (The `fontified' property doesn't have a special
@@ -1802,7 +1801,6 @@ handle_fontified_prop (it)
     {
       Lisp_Object args[2];
 
-      GCPRO1 (pos);
       /* Run the hook functions.  */
       args[0] = Qfontification_functions;
       args[1] = pos;
@@ -1813,7 +1811,6 @@ handle_fontified_prop (it)
 	 fontify the text for which reason ever.  */
       if (!NILP (Fget_char_property (pos, Qfontified, Qnil)))
 	handled = HANDLED_RECOMPUTE_PROPS;
-      UNGCPRO;
     }
 
   return handled;
