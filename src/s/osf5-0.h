93e40f4000bf5f73462e0ff06b9c8f7801de149e
Dave Love <fx@gnu.org>
2000-06-27 16:28:35 +0000

    (C_SWITCH_SYSTEM, WAIT_USE_INT, SYS_SIGLIST_DECLARED) (sys_siglist, NSIG): Definitions moved here from osf1.h.

--- src/s/osf5-0.h
+++ src/s/osf5-0.h
@@ -1,4 +1,15 @@
 #define OSF5
 #include "osf1.h"
-/* We have missing/inconsistent prototypes, at least.  */
+
+/* The -BSD loses when _XOPEN_SOURCE gets defined by configure in
+   OSF 5.  It's possible this will need to be reverted for earlier
+   versions (for which OSF5 isn't defined).  */
+#undef C_SWITCH_SYSTEM
+#define C_SWITCH_SYSTEM	-D_OSF_SOURCE
+#define WAIT_USE_INT
+#define SYS_SIGLIST_DECLARED
+#define sys_siglist __sys_siglist
+#define NSIG __sys_nsig
+
+/* We have missing/inconsistent prototypes on 5.0, at least.  */
 #define INHIBIT_X11R6_XIM
