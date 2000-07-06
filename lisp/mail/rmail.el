c70d73590c549cee68d90c761e23941756a2839e
Gerd Moellmann <gerd@gnu.org>
2000-07-06 20:08:28 +0000

    (mail-unsent-separator): Changed "the" to "\\w+", as exim can use "your message" instead of "the message".

--- lisp/mail/rmail.el
+++ lisp/mail/rmail.el
@@ -3094,7 +3094,7 @@ typically for purposes of moderating a list."
 	  "^ *---+ +Original message follows +---+ *$\\|"
 	  "^ *---+ +Your message follows +---+ *$\\|"
 	  "^|? *---+ +Message text follows: +---+ *|?$\\|"
-	  "^ *---+ +This is a copy of the message, including all the headers.*---+ *$")
+	  "^ *---+ +This is a copy of \\w+ message, including all the headers.*---+ *$")
   "A regexp that matches the separator before the text of a failed message.")
 
 (defvar mail-mime-unsent-header "^Content-Type: message/rfc822 *$"
