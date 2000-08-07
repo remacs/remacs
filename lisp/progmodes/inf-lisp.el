7749a263fa1f8502c866d9595b6ed6f4aa36dd48
Miles Bader <miles@gnu.org>
2000-08-07 14:59:23 +0000

    (inferior-lisp-prompt): Doc change.

--- lisp/progmodes/inf-lisp.el
+++ lisp/progmodes/inf-lisp.el
@@ -157,6 +157,9 @@ Defaults to \"^[^> \\n]*>+:? *\", which works pretty good for Lucid, kcl,
 and franz.  This variable is used to initialize `comint-prompt-regexp' in the 
 Inferior Lisp buffer.
 
+This variable is only used if the variable
+`comint-use-prompt-regexp-instead-of-fields' is non-nil.
+
 More precise choices:
 Lucid Common Lisp: \"^\\\\(>\\\\|\\\\(->\\\\)+\\\\) *\"
 franz: \"^\\\\(->\\\\|<[0-9]*>:\\\\) *\"
