84b87198895203b29b0188423292bc13ab5bf30f
Ken Raeburn <raeburn@raeburn.org>
2000-08-08 14:37:26 +0000

    (menu_item_eval_property): Declare.

--- src/keyboard.h
+++ src/keyboard.h
@@ -339,3 +339,4 @@ extern void show_help_echo P_ ((Lisp_Object, Lisp_Object, Lisp_Object,
 extern int gen_help_event P_ ((struct input_event *, Lisp_Object,
 			       Lisp_Object, Lisp_Object, Lisp_Object, int));
 extern void kbd_buffer_store_help_event P_ ((Lisp_Object, Lisp_Object));
+extern Lisp_Object menu_item_eval_property P_ ((Lisp_Object));
