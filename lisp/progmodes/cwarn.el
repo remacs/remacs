8251d10f50222e5a159d449a8207290040346894
Dave Love <fx@gnu.org>
2000-08-15 13:45:51 +0000

    Copyright up-date.

--- lisp/progmodes/cwarn.el
+++ lisp/progmodes/cwarn.el
@@ -1,6 +1,6 @@
 ;;; cwarn.el --- Highlight suspicious C and C++ constructions.
 
-;; Copyright (C) 1999 Free Software Foundation, Inc.
+;; Copyright (C) 1999, 2000 Free Software Foundation, Inc.
 
 ;; Author: Anders Lindgren <andersl@andersl.com>
 ;; Keywords: c, languages, faces
