3b5414898bbd86cca29a6736cd8f72243457b588
Eli Zaretskii <eliz@gnu.org>
2000-08-20 08:30:05 +0000

    (canonicalize_filename) [DOS_NT]: Upcase the first letter only if it is a drive letter.

--- lib-src/etags.c
+++ lib-src/etags.c
@@ -5484,7 +5484,7 @@ canonicalize_filename (fn)
 {
 #ifdef DOS_NT
   /* Canonicalize drive letter case.  */
-  if (islower (fn[0]))
+  if (fn[0] && fn[1] == ':' && islower (fn[0]))
     fn[0] = toupper (fn[0]);
   /* Convert backslashes to slashes.  */
   for (; *fn != '\0'; fn++)
