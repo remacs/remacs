8b1709cc9146d42cf694d87e4f97d7b9e09ad0ed
Andrew Innes <andrewi@gnu.org>
2000-08-22 21:51:15 +0000

    (PTR_TO_OFFSET): Cast ptr to unsigned char *.

--- nt/preprep.c
+++ nt/preprep.c
@@ -291,7 +291,7 @@ relocate_offset (DWORD offset,
 #define PTR_TO_RVA(ptr) ((DWORD)(ptr) - (DWORD) GetModuleHandle (NULL))
 
 #define PTR_TO_OFFSET(ptr, pfile_data) \
-          ((char *)(ptr) - (pfile_data)->file_base)
+          ((unsigned char *)(ptr) - (pfile_data)->file_base)
 
 #define OFFSET_TO_PTR(offset, pfile_data) \
           ((pfile_data)->file_base + (DWORD)(offset))
