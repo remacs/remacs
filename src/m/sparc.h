45595e0058df8e75f2bd2a67bf135eb90c35b5f9
Gerd Moellmann <gerd@gnu.org>
2000-09-26 13:02:21 +0000

    (BITS_PER_EMACS_INT) [__arch64__]: Don't define.

--- src/m/sparc.h
+++ src/m/sparc.h
@@ -106,7 +106,6 @@ NOTE-END  */
 
 #ifdef __arch64__		/* GCC, 64-bit ABI.  */
 #define BITS_PER_LONG 64
-#define BITS_PER_EMACS_INT 64
 #ifndef _LP64
 #define _LP64			/* Done on Alpha -- not sure if it
 				   should be here.  -- fx */
