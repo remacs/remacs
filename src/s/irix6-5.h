c952a617373e93ba1bb91112344877b33d44659b
Dave Love <fx@gnu.org>
2000-09-27 09:32:32 +0000

    Now works 64-bit.  Tidied.

--- src/s/irix6-5.h
+++ src/s/irix6-5.h
@@ -3,12 +3,7 @@
 #include "irix5-0.h"
 
 #if _MIPS_SZLONG == 64		/* -mabi=64 (gcc) or -64 (MIPSpro) */
-#error "The 64 bit ABI doesn't work yet.  Please fix it and send the changes!"
-#define _LP64
-#define EMACS_INT long
-#define BITS_PER_LONG 64
-#define BITS_PER_EMACS_INT BITS_PER_LONG
-#define EMACS_UINT unsigned long
+#define _LP64			/* lisp.h takes care of the rest */
 #endif /* _MIPS_SZLONG */
 
 /* This macro definition, which we inherited from irix5-0.h,
@@ -34,9 +29,6 @@
 
 #undef SA_RESTART
 
-/* It turns out that the #define in irix5-0.h is needed in Irix 6 as well.  */
-#if 0
 /* Cancel the #define that is in irix5-0.h.  */
 #undef ospeed
-#endif
 
