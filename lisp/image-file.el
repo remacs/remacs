e5a9dabf39b361c320f891435c9ad390e9110ca7
Miles Bader <miles@gnu.org>
2000-11-19 01:58:06 +0000

    (insert-image-file): Make `intangible' and `read-only' properties rear-nonsticky too.

--- lisp/image-file.el
+++ lisp/image-file.el
@@ -115,7 +115,7 @@ the command `insert-file-contents'."
 	     (props
 	      `(display ,image
 			intangible ,image
-			rear-nonsticky (display)
+			rear-nonsticky (display intangible read-only)
 			;; This a cheap attempt to make the whole buffer
 			;; read-only when we're visiting the file.
 			,@(and visit
