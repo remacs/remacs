5c9b3fac0e880bc48085d47452f3770b220eb82f
Miles Bader <miles@gnu.org>
2000-12-03 02:29:36 +0000

    (delete-trailing-whitespace): Don't delete newlines too.

--- lisp/simple.el
+++ lisp/simple.el
@@ -232,9 +232,11 @@ This respects narrowing, created by \\[narrow-to-region] and friends."
   (interactive "*")
   (save-match-data
     (save-excursion
+      (let (eol bol)         
       (goto-char (point-min))
-      (while (re-search-forward "\\s-+$" nil t)
-        (delete-region (match-beginning 0) (match-end 0))))))
+      (while (re-search-forward "\\s-$" nil t)
+	(skip-syntax-backward "-" (save-excursion (forward-line 0) (point)))
+	(delete-region (point) (match-end 0)))))))
 
 (defun newline-and-indent ()
   "Insert a newline, then indent according to major mode.
