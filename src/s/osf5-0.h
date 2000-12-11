b7d95d4220021b84527bf9263cb0255ca057b367
Gerd Moellmann <gerd@gnu.org>
2000-12-11 11:59:52 +0000

    Update comment about _XOPEN_SOURCE.

--- src/s/osf5-0.h
+++ src/s/osf5-0.h
@@ -1,11 +1,10 @@
 #define OSF5
 #include "osf1.h"
 
-/* The -BSD loses when _XOPEN_SOURCE gets defined by configure in
-   OSF 5.  It's possible this will need to be reverted for earlier
-   versions (for which OSF5 isn't defined).  */
+/* It's enough to define _OSF_SOURCE instead of _BSD.  */
 #undef C_SWITCH_SYSTEM
 #define C_SWITCH_SYSTEM	-D_OSF_SOURCE
+
 #define WAIT_USE_INT
 #define SYS_SIGLIST_DECLARED
 #define sys_siglist __sys_siglist
