6e080e4b24ab7869d1f83843c03a69458a9e9635
Dave Love <fx@gnu.org>
2000-12-12 15:11:04 +0000

    Add obsolete.

--- lisp/subdirs.el
+++ lisp/subdirs.el
@@ -1,3 +1,3 @@
 ;; In load-path, after this directory should come
 ;; certain of its subdirectories.  Here we specify them.
-(normal-top-level-add-to-load-path '("textmodes" "progmodes" "play" "net" "mail" "language" "international" "gnus" "eshell" "emulation" "emacs-lisp" "calendar" "toolbar"))
+(normal-top-level-add-to-load-path '("textmodes" "progmodes" "play" "net" "mail" "language" "international" "gnus" "eshell" "emulation" "emacs-lisp" "calendar" "toolbar" "obsolete"))
