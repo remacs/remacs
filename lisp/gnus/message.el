980984d8e35e2694ee280d1d556725f517174bd7
Miles Bader <miles@gnu.org>
2000-12-21 02:09:37 +0000

    Add comment.

--- lisp/gnus/message.el
+++ lisp/gnus/message.el
@@ -1501,6 +1501,7 @@ M-RET    message-newline-and-reformat (break the line and reformat)."
   (set (make-local-variable 'message-checksum) nil)
   (set (make-local-variable 'message-mime-part) 0)
   (message-setup-fill-variables)
+  ;; Allow using comment commands to add/remove quoting.
   (set (make-local-variable 'comment-start) message-yank-prefix)
   ;;(when (fboundp 'mail-hist-define-keys)
   ;;  (mail-hist-define-keys))
