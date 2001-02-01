e86b51338dcf074a6d48e20a9415fef48ac1818d
Eli Zaretskii <eliz@gnu.org>
2001-02-01 14:09:49 +0000

    (Fmodify_frame_parameters): Doc fix.

--- src/frame.c
+++ src/frame.c
@@ -2119,6 +2119,7 @@ If FRAME is nil, describe the currently selected frame.")
 DEFUN ("modify-frame-parameters", Fmodify_frame_parameters, 
        Smodify_frame_parameters, 2, 2, 0,
   "Modify the parameters of frame FRAME according to ALIST.\n\
+If FRAME is nil, it defaults to the selected frame.\n\
 ALIST is an alist of parameters to change and their new values.\n\
 Each element of ALIST has the form (PARM . VALUE), where PARM is a symbol.\n\
 The meaningful PARMs depend on the kind of frame.\n\
