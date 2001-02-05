b2065cb6ebaa193a00429927ba6f64a8f1a2215d
Gerd Moellmann <gerd@gnu.org>
2001-02-05 11:31:52 +0000

    (rmail-ignored-headers): Add X-Trace, X-Complaints-To, NNTP-Posting-Date, and User-Agent.

--- lisp/mail/rmail.el
+++ lisp/mail/rmail.el
@@ -152,7 +152,8 @@ It is useful to set this variable in the site customization file.")
 	  "\\|^precedence:\\|^list-help:\\|^list-post:\\|^list-subscribe:"
 	  "\\|^list-id:\\|^list-unsubscribe:\\|^list-archive:"
 	  "\\|^content-type:\\|^content-length:"
-	  "\\|^x-attribution:\\|^x-disclaimer:")
+	  "\\|^x-attribution:\\|^x-disclaimer:\\|^x-trace:"
+	  "\\|^x-complaints-to:\\|^nntp-posting-date:\\|^user-agent:")
   "*Regexp to match header fields that Rmail should normally hide.
 This variable is used for reformatting the message header,
 which normally happens once for each message,
