b3ae7a0a1e262405a11ee826a23867e52e099dd9
Gerd Moellmann <gerd@gnu.org>
2001-02-20 13:36:21 +0000

    Add copyright notice.

--- lib-src/digest-doc.c
+++ lib-src/digest-doc.c
@@ -1,7 +1,24 @@
-/* Give this program DOCSTR.mm.nn as standard input
-   and it outputs to standard output
-   a file of nroff output containing the doc strings.
+/* Give this program DOCSTR.mm.nn as standard input and it outputs to
+   standard output a file of nroff output containing the doc strings.
 
+   Copyright (C) 1987, 1994, 2001  Free Software Foundation Inc.
+
+   This file is part of GNU Emacs.
+
+   GNU Emacs is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   GNU Emacs is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with GNU Emacs; see the file COPYING.  If not, write to
+   the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+   
    See also sorted-doc.c, which produces similar output
    but in texinfo format and sorted by function/variable name.  */
 
--- src/m/ews4800.h
+++ src/m/ews4800.h
@@ -1,4 +1,23 @@
-/* m- file for NEC EWS4800 RISC series. */
+/* m- file for NEC EWS4800 RISC series.
+   Copyright (C) 1998, 2000, 2001 Free Software Foundation, Inc.
+
+This file is part of GNU Emacs.
+
+GNU Emacs is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU Emacs is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU Emacs; see the file COPYING.  If not, write to
+the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
+
 #undef LIB_STANDARD
 #undef C_DEBUG_SWITCH
 #define INHIBIT_BSD_TIME
