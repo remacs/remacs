93aba6fcd408d876343aca2f02692e52de6df4bc
Gerd Moellmann <gerd@gnu.org>
2001-08-15 09:31:06 +0000

    *** empty log message ***

---
+++ lisp/obsolete/sc.el
@@ -0,0 +1,14 @@
+;;; sc.el --- old name for supercite
+
+;; Maintainer: FSF
+
+;; This file is part of GNU Emacs.
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'supercite)
+(provide 'sc)
+
+;;; sc.el ends here
