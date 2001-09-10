e602e77917dfa7ecd956d793605acd0dd7d1bb38
Eli Zaretskii <eliz@gnu.org>
2001-09-10 14:00:55 +0000

    Update Stephen Gildea's email address.

--- lisp/time-stamp.el
+++ lisp/time-stamp.el
@@ -6,7 +6,7 @@
 ;; This file is part of GNU Emacs.
 
 ;; Maintainer's Time-stamp: <2001-07-15 12:35:58 pavel>
-;; Maintainer: Stephen Gildea <gildea@alum.mit.edu>
+;; Maintainer: Stephen Gildea <gildea@stop.mail-abuse.org>
 ;; Keywords: tools
 
 ;; This file is free software; you can redistribute it and/or modify
