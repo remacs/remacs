881aa09cb5756384b8bc8bf40883a754849f17e5
Gerd Moellmann <gerd@gnu.org>
2001-09-19 08:35:09 +0000

    (DATA_SEG_BITS): Also define for GCC 3.

--- src/m/macppc.h
+++ src/m/macppc.h
@@ -98,7 +98,10 @@ Boston, MA 02111-1307, USA.  */
 #define LD_SWITCH_MACHINE -Xlinker -m -Xlinker elf32ppc
 #endif
 
-/* GCC 2.95 on GNU/Linux PPC changed the load address to 0x10000000.  */
-#if defined(__linux__) && __GNUC__ == 2 && __GNUC_MINOR__ >= 95
+/* GCC 2.95 and newer on GNU/Linux PPC changed the load address to
+   0x10000000.  */
+#if defined __linux__
+#if __GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 95)
 #define DATA_SEG_BITS  0x10000000
+#endif
 #endif
