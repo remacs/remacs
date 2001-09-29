a7c29c442c8d8f852b3fbf41885276ba95a3b160
Eli Zaretskii <eliz@gnu.org>
2001-09-29 18:37:22 +0000

    (snake-score-file): Use temporary-file-directory instead of a literal "/tmp".  Suggested by Robert <robert@chezmarshall.freeserve.co.uk>.

--- lisp/play/snake.el
+++ lisp/play/snake.el
@@ -82,7 +82,7 @@
 (defvar snake-score-y snake-height
   "Y position of score.")
 
-(defvar snake-score-file "/tmp/snake-scores"
+(defvar snake-score-file (concat temporary-file-directory "snake-scores")
   "File for holding high scores.")
 
 ;; ;;;;;;;;;;;;; display options ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
