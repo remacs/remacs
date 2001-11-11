c77c48d99297ad605f791495f967e9718bb797a1
Eli Zaretskii <eliz@gnu.org>
2001-11-11 19:23:13 +0000

    File regenerated.

--- lisp/subdirs.el
+++ lisp/subdirs.el
@@ -1,3 +1,3 @@
 ;; In load-path, after this directory should come
 ;; certain of its subdirectories.  Here we specify them.
-(normal-top-level-add-to-load-path '("textmodes" "progmodes" "play" "net" "mail" "language" "international" "gnus" "eshell" "emulation" "emacs-lisp" "calendar" "toolbar" "obsolete"))
+(normal-top-level-add-to-load-path '("toolbar" "textmodes" "progmodes" "play" "obsolete" "net" "mail" "language" "international" "gnus" "eshell" "emulation" "emacs-lisp" "calendar" "calc" ))
