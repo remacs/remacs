843f91fdd35ba5398376751f0e89b08636f5dadf
Richard M. Stallman <rms@gnu.org>
2001-11-17 22:41:26 +0000

    Comment change.

--- lisp/international/mule-cmds.el
+++ lisp/international/mule-cmds.el
@@ -1391,8 +1391,8 @@ The default status is as follows:
   ;; Don't alter the terminal and keyboard coding systems here.
   ;; The terminal still supports the same coding system
   ;; that it supported a minute ago.
-;;  (set-terminal-coding-system-internal nil)
-;;  (set-keyboard-coding-system-internal nil)
+;;;  (set-terminal-coding-system-internal nil)
+;;;  (set-keyboard-coding-system-internal nil)
 
   (setq nonascii-translation-table nil
 	nonascii-insert-offset 0))
