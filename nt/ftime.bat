7243105827747388bec152c6c3457924c4f7ade3
Andrew Innes <andrewi@gnu.org>
2001-11-19 15:05:54 +0000

    Don't include libc.lib for profiling - the profiler corrupts part of memcpy.

--- nt/ftime.bat
+++ nt/ftime.bat
@@ -6,13 +6,13 @@ obj\i386\preprep ..\src\obj\i386\emacs.exe ..\src\obj\i386\pemacs.exe
 copy ..\src\obj\i386\temacs.map ..\src\obj\i386\pemacs.map
 rem -----------------------------------------------------------------
 rem    Use this version to profile explicit commands only.
-prep /om /ft /inc libc.lib /sf _Fexecute_extended_command ..\src\obj\i386\pemacs
+prep /om /ft /sf _Fexecute_extended_command ..\src\obj\i386\pemacs
 rem -----------------------------------------------------------------
 rem    Use this version to ignore startup code
-rem prep /om /ft /inc libc.lib /sf _command_loop_1 ..\src\obj\i386\pemacs
+rem prep /om /ft /sf _command_loop_1 ..\src\obj\i386\pemacs
 rem -----------------------------------------------------------------
 rem    Use this version to include startup code
-rem prep /om /ft /inc libc.lib ..\src\obj\i386\pemacs
+rem prep /om /ft ..\src\obj\i386\pemacs
 rem -----------------------------------------------------------------
 if errorlevel 1 goto done
 profile ..\src\obj\i386\pemacs %2 %3 %4 %5 %6 %7 %8 %9
