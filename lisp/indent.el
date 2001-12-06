f059f7032fa326cbd2cca8d46deb63c804f3231f
Stefan Monnier <monnier@iro.umontreal.ca>
2001-12-06 16:17:08 +0000

    (indent-line-function): Default is indent-relative again.

--- lisp/indent.el
+++ lisp/indent.el
@@ -37,7 +37,7 @@
   :group 'indent
   :type 'integer)
 
-(defvar indent-line-function 'indent-to-left-margin
+(defvar indent-line-function 'indent-relative
   "Function to indent the current line.
 This function will be called with no arguments.
 If it is called somewhere where auto-indentation cannot be done
