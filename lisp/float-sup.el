33aafb9bbe371734c4c397e72db5ea1bbefaad09
Pavel Janík <Pavel@Janik.cz>
2001-12-11 07:32:00 +0000

    Doc fixes.

--- lisp/float-sup.el
+++ lisp/float-sup.el
@@ -27,8 +27,6 @@
 
 ;; Provide a meaningful error message if we are running on
 ;; bare (non-float) emacs.
-;; Can't test for 'floatp since that may be defined by float-imitation
-;; packages like float.el in this very directory.
 
 (if (fboundp 'atan)
     nil
@@ -36,17 +34,17 @@
 
 ;; provide an easy hook to tell if we are running with floats or not.
 ;; define pi and e via math-lib calls. (much less prone to killer typos.)
-(defconst pi (* 4 (atan 1)) "The value of Pi (3.1415926...)")
-(defconst e (exp 1) "The value of e (2.7182818...)")
+(defconst pi (* 4 (atan 1)) "The value of Pi (3.1415926...).")
+(defconst e (exp 1) "The value of e (2.7182818...).")
 
 ;; Careful when editing this file ... typos here will be hard to spot.
 ;; (defconst pi       3.14159265358979323846264338327
 ;;  "The value of Pi (3.14159265358979323846264338327...)")
 
 (defconst degrees-to-radians (/ pi 180.0)
-  "Degrees to radian conversion constant")
+  "Degrees to radian conversion constant.")
 (defconst radians-to-degrees (/ 180.0 pi)
-  "Radian to degree conversion constant")
+  "Radian to degree conversion constant.")
 
 ;; these expand to a single multiply by a float when byte compiled
 
