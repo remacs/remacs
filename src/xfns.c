99f7c77f3162cb344cc9c779635f6ca9cdf84f8c
Eli Zaretskii <eliz@gnu.org>
2001-12-13 09:56:04 +0000

    (Frame_parameters): Makes the scroll-bar-width frame parameter have a numeric value all the time.

--- src/xfns.c
+++ src/xfns.c
@@ -1206,6 +1206,10 @@ x_report_frame_params (f, alistptr)
        	   make_number (f->output_data.x->left_fringe_width));
   store_in_alist (alistptr, Qright_fringe,
        	   make_number (f->output_data.x->right_fringe_width));
+  store_in_alist (alistptr, Qscroll_bar_width,
+           make_number (FRAME_HAS_VERTICAL_SCROLL_BARS (f)
+                        ? FRAME_SCROLL_BAR_PIXEL_WIDTH(f)
+                        : 0));
   sprintf (buf, "%ld", (long) FRAME_X_WINDOW (f));
   store_in_alist (alistptr, Qwindow_id,
        	   build_string (buf));
