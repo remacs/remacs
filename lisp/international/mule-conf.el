5a6b038a17ae09d01401e902832dff293da12e1d
Werner LEMBERG <wl@gnu.org>
2001-12-17 21:01:25 +0000

    international/mule-conf.el: Improve documentation strings of character sets.

--- lisp/international/mule-conf.el
+++ lisp/international/mule-conf.el
@@ -47,81 +47,81 @@
 
 (define-charset 129 'latin-iso8859-1
   [1 96 1 0 ?A 1 "RHP of Latin-1" "RHP of Latin-1 (ISO 8859-1): ISO-IR-100"
-     "Right-Hand Part of Latin Alphabet 1 (ISO/IEC 8859-1): ISO-IR-100"])
+     "Right-Hand Part of Latin Alphabet 1 (ISO/IEC 8859-1): ISO-IR-100."])
 (define-charset 130 'latin-iso8859-2
   [1 96 1 0 ?B 1 "RHP of Latin-2" "RHP of Latin-2 (ISO 8859-2): ISO-IR-101"
-     "Right-Hand Part of Latin Alphabet 2 (ISO/IEC 8859-2): ISO-IR-101"])
+     "Right-Hand Part of Latin Alphabet 2 (ISO/IEC 8859-2): ISO-IR-101."])
 (define-charset 131 'latin-iso8859-3
   [1 96 1 0 ?C 1 "RHP of Latin-3" "RHP of Latin-3 (ISO 8859-3): ISO-IR-109"
-     "Right-Hand Part of Latin Alphabet 3 (ISO/IEC 8859-3): ISO-IR-109"])
+     "Right-Hand Part of Latin Alphabet 3 (ISO/IEC 8859-3): ISO-IR-109."])
 (define-charset 132 'latin-iso8859-4
   [1 96 1 0 ?D 1 "RHP of Latin-4" "RHP of Latin-4 (ISO 8859-4): ISO-IR-110"
-     "Right-Hand Part of Latin Alphabet 4 (ISO/IEC 8859-4): ISO-IR-110"])
+     "Right-Hand Part of Latin Alphabet 4 (ISO/IEC 8859-4): ISO-IR-110."])
 (define-charset 133 'thai-tis620
   [1 96 1 0 ?T 1 "RHP of TIS620" "RHP of Thai (TIS620): ISO-IR-166"
-     "Right-Hand Part of TIS620.2533 (Thai): ISO-IR-166"])
+     "Right-Hand Part of TIS620.2533 (Thai): ISO-IR-166."])
 (define-charset 134 'greek-iso8859-7
   [1 96 1 0 ?F 1 "RHP of ISO8859/7" "RHP of Greek (ISO 8859-7): ISO-IR-126"
-     "Right-Hand Part of Latin/Greek Alphabet (ISO/IEC 8859-7): ISO-IR-126"])
+     "Right-Hand Part of Latin/Greek Alphabet (ISO/IEC 8859-7): ISO-IR-126."])
 (define-charset 135 'arabic-iso8859-6
   [1 96 1 1 ?G 1 "RHP of ISO8859/6" "RHP of Arabic (ISO 8859-6): ISO-IR-127"
-     "Right-Hand Part of Latin/Arabic Alphabet (ISO/IEC 8859-6): ISO-IR-127"])
+     "Right-Hand Part of Latin/Arabic Alphabet (ISO/IEC 8859-6): ISO-IR-127."])
 (define-charset 136 'hebrew-iso8859-8
   [1 96 1 1 ?H 1 "RHP of ISO8859/8" "RHP of Hebrew (ISO 8859-8): ISO-IR-138"
-     "Right-Hand Part of Latin/Hebrew Alphabet (ISO/IEC 8859-8): ISO-IR-138"])
+     "Right-Hand Part of Latin/Hebrew Alphabet (ISO/IEC 8859-8): ISO-IR-138."])
 (define-charset 137 'katakana-jisx0201
   [1 94 1 0 ?I 1 "JISX0201 Katakana" "Japanese Katakana (JISX0201.1976)"
-     "Katakana Part of JISX0201.1976"])
+     "Katakana Part of JISX0201.1976."])
 (define-charset 138 'latin-jisx0201
   [1 94 1 0 ?J 0 "JISX0201 Roman" "Japanese Roman (JISX0201.1976)"
-     "Roman Part of JISX0201.1976"])
+     "Roman Part of JISX0201.1976."])
 
 ;; CHARSET-ID is not used 139.
 
 (define-charset 140 'cyrillic-iso8859-5
   [1 96 1 0 ?L 1 "RHP of ISO8859/5" "RHP of Cyrillic (ISO 8859-5): ISO-IR-144"
-     "Right-Hand Part of Latin/Cyrillic Alphabet (ISO/IEC 8859-5): ISO-IR-144"])
+     "Right-Hand Part of Latin/Cyrillic Alphabet (ISO/IEC 8859-5): ISO-IR-144."])
 (define-charset 141 'latin-iso8859-9
   [1 96 1 0 ?M 1 "RHP of Latin-5" "RHP of Latin-5 (ISO 8859-9): ISO-IR-148"
-     "Right-Hand Part of Latin Alphabet 5 (ISO/IEC 8859-9): ISO-IR-148"])
+     "Right-Hand Part of Latin Alphabet 5 (ISO/IEC 8859-9): ISO-IR-148."])
 (define-charset 142 'latin-iso8859-15
   [1 96 1 0 ?b 1 "RHP of Latin-9" "RHP of Latin-9 (ISO 8859-15): ISO-IR-203"
-     "Right-Hand Part of Latin Alphabet 9 (ISO/IEC 8859-15): ISO-IR-203"])
+     "Right-Hand Part of Latin Alphabet 9 (ISO/IEC 8859-15): ISO-IR-203."])
 (define-charset 143 'latin-iso8859-14
   [1 96 1 0 ?_ 1 "RHP of Latin-8" "RHP of Latin-8 (ISO 8859-14): ISO-IR-199"
-     "Right-Hand Part of Latin Alphabet 8 (ISO/IEC 8859-14): ISO-IR-199"])
+     "Right-Hand Part of Latin Alphabet 8 (ISO/IEC 8859-14): ISO-IR-199."])
 
 ;; 2-byte charsets.  Valid range of CHARSET-ID is 144..153.
 
 (define-charset 144 'japanese-jisx0208-1978
   [2 94 2 0 ?@ 0 "JISX0208.1978" "JISX0208.1978 (Japanese): ISO-IR-42"
-     "JISX0208.1978 Japanese Kanji (so called \"old JIS\"): ISO-IR-42"])
+     "JISX0208.1978 Japanese Kanji (so called \"old JIS\"): ISO-IR-42."])
 (define-charset 145 'chinese-gb2312
   [2 94 2 0 ?A 0 "GB2312" "GB2312: ISO-IR-58"
-     "GB2312 Chinese simplified: ISO-IR-58"])
+     "GB2312 Chinese simplified: ISO-IR-58."])
 (define-charset 146 'japanese-jisx0208
   [2 94 2 0 ?B 0 "JISX0208" "JISX0208.1983/1990 (Japanese): ISO-IR-87"
-     "JISX0208.1983/1990 Japanese Kanji: ISO-IR-87"])
+     "JISX0208.1983/1990 Japanese Kanji: ISO-IR-87."])
 (define-charset 147 'korean-ksc5601
   [2 94 2 0 ?C 0 "KSC5601" "KSC5601 (Korean): ISO-IR-149"
-     "KSC5601 Korean Hangul and Hanja: ISO-IR-149"])
+     "KSC5601 Korean Hangul and Hanja: ISO-IR-149."])
 (define-charset 148 'japanese-jisx0212
   [2 94 2 0 ?D 0 "JISX0212" "JISX0212 (Japanese): ISO-IR-159"
-     "JISX0212 Japanese supplement: ISO-IR-159"])
+     "JISX0212 Japanese supplement: ISO-IR-159."])
 (define-charset 149 'chinese-cns11643-1
   [2 94 2 0 ?G 0 "CNS11643-1" "CNS11643-1 (Chinese traditional): ISO-IR-171"
-     "CNS11643 Plane 1 Chinese traditional: ISO-IR-171"])
+     "CNS11643 Plane 1 Chinese traditional: ISO-IR-171."])
 (define-charset 150 'chinese-cns11643-2
   [2 94 2 0 ?H 0 "CNS11643-2" "CNS11643-2 (Chinese traditional): ISO-IR-172"
-     "CNS11643 Plane 2 Chinese traditional: ISO-IR-172"])
+     "CNS11643 Plane 2 Chinese traditional: ISO-IR-172."])
 (define-charset 151 'japanese-jisx0213-1
   [2 94 2 0 ?O 0 "JISX0213-1" "JISX0213-1" "JISX0213 Plane 1 (Japanese)"])
 (define-charset 152 'chinese-big5-1
   [2 94 2 0 ?0 0 "Big5 (Level-1)" "Big5 (Level-1) A141-C67F"
-     "Frequently used part (A141-C67F) of Big5 (Chinese traditional)"])
+     "Frequently used part (A141-C67F) of Big5 (Chinese traditional)."])
 (define-charset 153 'chinese-big5-2
   [2 94 2 0 ?1 0 "Big5 (Level-2)" "Big5 (Level-2) C940-FEFE"
-     "Less frequently used part (C940-FEFE) of Big5 (Chinese traditional)"])
+     "Less frequently used part (C940-FEFE) of Big5 (Chinese traditional)."])
 
 ;; Additional (private) character sets.  These character sets are
 ;; treated less space-efficiently in the buffer.
@@ -140,46 +140,53 @@
 
 (define-charset 160 'chinese-sisheng
   [1 94 1 0 ?0 0 "SiSheng" "SiSheng (PinYin/ZhuYin)"
-     "SiSheng characters for PinYin/ZhuYin"])
+     "Sisheng characters (vowels with tone marks) for Pinyin/Zhuyin."])
 
 ;; IPA characters for phonetic symbols.
 (define-charset 161 'ipa
-  [1 96 1 0 ?0 1 "IPA" "IPA" "IPA (International Phonetic Association)"])
+  [1 96 1 0 ?0 1 "IPA" "IPA"
+     "IPA (International Phonetic Association) characters."])
 
 ;; Vietnamese VISCII.  VISCII is 1-byte character set which contains
 ;; more than 96 characters.  Since Emacs can't handle it as one
 ;; character set, it is divided into two: lower case letters and upper
 ;; case letters.
 (define-charset 162 'vietnamese-viscii-lower
-  [1 96 1 0 ?1 1 "VISCII lower" "VISCII lower-case" "VISCII1.1 lower-case"])
+  [1 96 1 0 ?1 1 "VISCII lower" "VISCII lower-case"
+     "Vietnamese VISCII1.1 lower-case characters."])
 (define-charset 163 'vietnamese-viscii-upper
-  [1 96 1 0 ?2 1 "VISCII upper" "VISCII upper-case" "VISCII1.1 upper-case"])
+  [1 96 1 0 ?2 1 "VISCII upper" "VISCII upper-case"
+     "Vietnamese VISCII1.1 upper-case characters."])
 
 ;; For Arabic, we need three different types of character sets.
 ;; Digits are of direction left-to-right and of width 1-column.
 ;; Others are of direction right-to-left and of width 1-column or
 ;; 2-column.
 (define-charset 164 'arabic-digit
-  [1 94 1 0 ?2 0 "Arabic digit" "Arabic digit" "Arabic digit"])
+  [1 94 1 0 ?2 0 "Arabic digit" "Arabic digit"
+     "Arabic digits."])
 (define-charset 165 'arabic-1-column
-  [1 94 1 1 ?3 0 "Arabic 1-col" "Arabic 1-column" "Arabic 1-column"])
+  [1 94 1 1 ?3 0 "Arabic 1-col" "Arabic 1-column"
+     "Arabic 1-column width glyphs."])
 
 ;; ASCII with right-to-left direction.
 (define-charset 166 'ascii-right-to-left
   [1 94 1 1 ?B 0 "rev ASCII" "ASCII with right-to-left direction"
-     "ASCII (left half of ISO 8859-1) with right-to-left direction"])
+     "ASCII (left half of ISO 8859-1) with right-to-left direction."])
 
 ;; Lao script.
 ;; ISO10646's 0x0E80..0x0EDF are mapped to 0x20..0x7F.
 (define-charset 167 'lao
-  [1 94 1 0 ?1 0 "Lao" "Lao" "Lao characters (ISO10646 0E80..0EDF)"])
+  [1 94 1 0 ?1 0 "Lao" "Lao"
+     "Lao characters (U+0E80..U+0EDF)."])
 
 ;; CHARSET-IDs 168..223 are not used.
 
 ;; 1-byte 2-column charsets.  Valid range of CHARSET-ID is 224..239.
 
 (define-charset 224 'arabic-2-column
-  [1 94 2 1 ?4 0 "Arabic 2-col" "Arabic 2-column" "Arabic 2-column"])
+  [1 94 2 1 ?4 0 "Arabic 2-col" "Arabic 2-column"
+     "Arabic 2-column glyphs."])
 
 ;; Indian scripts.  Symbolic charset for data exchange.  Glyphs are
 ;; not assigned.  They are automatically converted to each Indian
@@ -187,7 +194,7 @@
 
 (define-charset 225 'indian-is13194
   [1 94 2 0 ?5 1 "IS 13194" "Indian IS 13194"
-     "Generic Indian charset for data exchange with IS 13194"])
+     "Generic Indian character set for data exchange with IS 13194."])
 
 ;; CHARSET-IDs 226..239 are not used.
 
@@ -200,7 +207,8 @@
 
 ;; Actual Glyph for 1-column width.
 (define-charset 241 'tibetan-1-column
-  [2 94 1 0 ?8 0 "Tibetan 1-col" "Tibetan 1 column" "Tibetan 1 column glyph"])
+  [2 94 1 0 ?8 0 "Tibetan 1-col" "Tibetan 1 column"
+     "Tibetan 1-column glyphs."])
 
 ;; Subsets of Unicode.
 
@@ -220,43 +228,46 @@
 
 ;; Ethiopic characters (Amahric and Tigrigna).
 (define-charset 245 'ethiopic
-  [2 94 2 0 ?3 0 "Ethiopic" "Ethiopic characters" "Ethiopic characters"])
+  [2 94 2 0 ?3 0 "Ethiopic" "Ethiopic characters"
+     "Ethiopic characters."])
 
 ;; Chinese CNS11643 Plane3 thru Plane7.  Although these are official
 ;; character sets, the use is rare and don't have to be treated
 ;; space-efficiently in the buffer.
 (define-charset 246 'chinese-cns11643-3
   [2 94 2 0 ?I 0 "CNS11643-3" "CNS11643-3 (Chinese traditional): ISO-IR-183"
-     "CNS11643 Plane 3 Chinese Traditional: ISO-IR-183"])
+     "CNS11643 Plane 3 Chinese Traditional: ISO-IR-183."])
 (define-charset 247 'chinese-cns11643-4
   [2 94 2 0 ?J 0 "CNS11643-4" "CNS11643-4 (Chinese traditional): ISO-IR-184"
-     "CNS11643 Plane 4 Chinese Traditional: ISO-IR-184"])
+     "CNS11643 Plane 4 Chinese Traditional: ISO-IR-184."])
 (define-charset 248 'chinese-cns11643-5
   [2 94 2 0 ?K 0 "CNS11643-5" "CNS11643-5 (Chinese traditional): ISO-IR-185"
-     "CNS11643 Plane 5 Chinese Traditional: ISO-IR-185"])
+     "CNS11643 Plane 5 Chinese Traditional: ISO-IR-185."])
 (define-charset 249 'chinese-cns11643-6
   [2 94 2 0 ?L 0 "CNS11643-6" "CNS11643-6 (Chinese traditional): ISO-IR-186"
-     "CNS11643 Plane 6 Chinese Traditional: ISO-IR-186"])
+     "CNS11643 Plane 6 Chinese Traditional: ISO-IR-186."])
 (define-charset 250 'chinese-cns11643-7
   [2 94 2 0 ?M 0 "CNS11643-7" "CNS11643-7 (Chinese traditional): ISO-IR-187"
-     "CNS11643 Plane 7 Chinese Traditional: ISO-IR-187"])
+     "CNS11643 Plane 7 Chinese Traditional: ISO-IR-187."])
 
 ;; Actual Glyph for 2-column width.
 (define-charset 251 'indian-2-column
   [2 94 2 0 ?5 0 "Indian 2-col" "Indian 2 Column"
-     "Indian charset for 2-column width glyphs"])
+     "Indian character set for 2-column width glyphs."])
   ;; old indian-1-column characters will be translated to indian-2-column.
 (declare-equiv-charset 2 94 ?6 'indian-2-column)
 
 ;; Tibetan script.
 (define-charset 252 'tibetan
-  [2 94 2 0 ?7 0 "Tibetan 2-col" "Tibetan 2 column" "Tibetan characters"])
+  [2 94 2 0 ?7 0 "Tibetan 2-col" "Tibetan 2 column"
+     "Tibetan 2-column width glyphs."])
 
 ;; CHARSET-ID 253 is not used.
 
 ;; JISX0213 Plane 2
 (define-charset 254 'japanese-jisx0213-2
-  [2 94 2 0 ?P 0 "JISX0213-2" "JISX0213-2" "JISX0213 Plane 2 (Japanese)"])
+  [2 94 2 0 ?P 0 "JISX0213-2" "JISX0213-2"
+     "JISX0213 Plane 2 (Japanese)."])
 
 ;; Tell C code charset ID's of several charsets.
 (setup-special-charsets)
