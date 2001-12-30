c51ea62a82238479d70e8b31793946c4203560f1
Richard M. Stallman <rms@gnu.org>
2001-12-30 00:13:56 +0000

    (max_specpdl_size): Add declaration.

--- src/lisp.h
+++ src/lisp.h
@@ -1653,6 +1653,8 @@ extern struct specbinding *specpdl;
 extern struct specbinding *specpdl_ptr;
 extern int specpdl_size;
 
+extern EMACS_INT max_specpdl_size;
+
 #define BINDING_STACK_SIZE()	(specpdl_ptr - specpdl)
 
 /* Everything needed to describe an active condition case.  */
