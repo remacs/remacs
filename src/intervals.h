0277b535be09ab13c023a854e498e8bf967ba0f0
Pavel Janík <Pavel@Janik.cz>
2002-02-24 20:37:24 +0000

    Include "dispextern.h" unconditionally.

--- src/intervals.h
+++ src/intervals.h
@@ -18,9 +18,7 @@ along with GNU Emacs; see the file COPYING.  If not, write to
 the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
-#ifndef NORMAL_FACE
 #include "dispextern.h"
-#endif
 
 #define NULL_INTERVAL ((INTERVAL)0)
 #define INTERVAL_DEFAULT NULL_INTERVAL
