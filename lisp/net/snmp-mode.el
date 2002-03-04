1d09ce772edcbeecd3b89b385bb7fd75b330aa3f
Pavel Janík <Pavel@Janik.cz>
2002-03-04 09:43:40 +0000

    Require tempo when compiling to prevent warnings.  Update copyright notice.

--- lisp/net/snmp-mode.el
+++ lisp/net/snmp-mode.el
@@ -1,6 +1,6 @@
 ;;; snmp-mode.el --- SNMP & SNMPv2 MIB major mode
 
-;; Copyright (C) 1995,1998  Free Software Foundation, Inc.
+;; Copyright (C) 1995, 1998, 2002  Free Software Foundation, Inc.
 
 ;; Author: Paul D. Smith <psmith@BayNetworks.com>
 ;; Keywords: data
@@ -85,6 +85,9 @@
 ;;
 
 ;;; Code:
+
+(eval-when-compile
+  (require 'tempo))
 
 ;;;----------------------------------------------------------------------------
 ;;
