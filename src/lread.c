67868d27f8e748264af2214fc001dab826d78ccc
Pavel Janík <Pavel@Janik.cz>
2002-03-04 09:27:58 +0000

    (read_filtered_event): Do not call start_hourglass before returning.

--- src/lread.c
+++ src/lread.c
@@ -485,10 +485,15 @@ read_filtered_event (no_switch_frame, ascii_required, error_nonascii,
   if (! NILP (delayed_switch_frame))
     unread_switch_frame = delayed_switch_frame;
 
+#if 0
+
 #ifdef HAVE_WINDOW_SYSTEM
   if (display_hourglass_p)
     start_hourglass ();
 #endif
+
+#endif
+
   return val;
 }
 
