ecc8f164e8297b9e177633ede7355feeb998b7ca
Kenichi Handa <handa@m17n.org>
2002-03-05 01:04:59 +0000

    ("Czech"): Set `iso-8859-2' for `nonascii-translation'.

--- lisp/language/czech.el
+++ lisp/language/czech.el
@@ -34,7 +34,7 @@
 	   (coding-system . (iso-8859-2))
 	   (coding-priority . (iso-8859-2))
 	   (input-method . "czech")
-	   (nonascii-translation . latin-iso8859-2)
+	   (nonascii-translation . iso-8859-2)
 	   (unibyte-syntax . "latin-2")
 	   (unibyte-display . iso-8859-2)
 	   (tutorial . "TUTORIAL.cs")
