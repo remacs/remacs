fd524c21ee114c266a0c80a925da72970142512b
Gerd Moellmann <gerd@gnu.org>
2002-03-30 11:55:37 +0000

    Update copyright.

--- src/dispnew.c
+++ src/dispnew.c
@@ -1,5 +1,5 @@
 /* Updating of data structures for redisplay.
-   Copyright (C) 1985, 86, 87, 88, 93, 94, 95, 97, 98, 1999, 2000, 2001
+   Copyright (C) 1985, 86, 87, 88, 93, 94, 95, 97, 98, 1999, 2000, 2001, 2002
        Free Software Foundation, Inc.
 
 This file is part of GNU Emacs.
