5ecd7a584f75038a8cb6278eb093aea9c44fb1f2
Richard M. Stallman <rms@gnu.org>
2002-04-10 15:19:04 +0000

    (BASE_PURESIZE): Reduce again to avoid big excess.

--- src/puresize.h
+++ src/puresize.h
@@ -42,7 +42,7 @@ Boston, MA 02111-1307, USA.  */
 #endif
 
 #ifndef BASE_PURESIZE
-#define BASE_PURESIZE (900000 + SYSTEM_PURESIZE_EXTRA + SITELOAD_PURESIZE_EXTRA)
+#define BASE_PURESIZE (790000 + SYSTEM_PURESIZE_EXTRA + SITELOAD_PURESIZE_EXTRA)
 #endif
 
 /* Increase BASE_PURESIZE by a ratio depending on the machine's word size.  */
