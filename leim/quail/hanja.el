badcaf4d3ddf6214b34a060291d168eaaf607598
Pavel Janík <Pavel@Janik.cz>
2002-04-22 15:20:14 +0000

    ("Od"): Remove rule. From Koaunghi Un <koaunghi@ling.cnu.ac.kr>.

--- leim/quail/hanja.el
+++ leim/quail/hanja.el
@@ -351,7 +351,6 @@
  ("wka"	"岑暫潛箴簪蠶")
  ("wkq"	"雜")
  ("wkd"	"丈仗匠場墻壯奬將帳庄張掌暲杖樟檣欌漿牆狀獐璋章粧腸臟臧莊葬蔣薔藏裝贓醬長障")
- ("Od"	"爭箏諍錚")
  ("wo"	"再哉在宰才材栽梓渽滓災縡裁財載齋齎")
  ("wod"	"爭箏諍錚")
  ("wj"	"佇低儲咀姐底抵杵楮樗沮渚狙猪疽箸紵苧菹著藷詛貯躇這邸雎齟")
