12b309f1492d80a8d6f7441c56435869f602dfef
Eli Zaretskii <eliz@gnu.org>
2002-05-18 11:22:04 +0000

    Fix the preamble comments.

--- lisp/time-stamp.el
+++ lisp/time-stamp.el
@@ -9,7 +9,7 @@
 ;; Maintainer: Stephen Gildea <gildea@stop.mail-abuse.org>
 ;; Keywords: tools
 
-;; This file is free software; you can redistribute it and/or modify
+;; GNU Emacs is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 2, or (at your option)
 ;; any later version.
--- lisp/w32-vars.el
+++ lisp/w32-vars.el
@@ -7,7 +7,7 @@
 
 ;; This file is part of GNU Emacs.
 
-;; This file is free software; you can redistribute it and/or modify
+;; GNU Emacs is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 2, or (at your option)
 ;; any later version.
