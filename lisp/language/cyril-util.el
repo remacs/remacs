c603680d0956fbe644f8a37a6735e83a5571d12d
Dave Love <fx@gnu.org>
2002-06-11 18:44:47 +0000

    (cyrillic-encode-koi8-r-char) (cyrillic-encode-alternativnyj-char): Fixed. (standard-display-cyrillic-translit): Remove redundant unicodes.

--- lisp/language/cyril-util.el
+++ lisp/language/cyril-util.el
@@ -1,6 +1,6 @@
 ;;; cyril-util.el --- utilities for Cyrillic scripts
 
-;; Copyright (C) 1997, 2001 Free Software Foundation, Inc.
+;; Copyright (C) 1997, 2001, 2002 Free Software Foundation, Inc.
 
 ;; Keywords: mule, multilingual, Cyrillic
 
@@ -25,22 +25,17 @@
 
 ;;; Code:
 
+;; Fixme: are the next two useful?
+
 ;;;###autoload
 (defun cyrillic-encode-koi8-r-char (char)
   "Return KOI8-R external character code of CHAR if appropriate."
-  (aref (char-table-extra-slot
-	 (get 'cyrillic-koi8-r-nonascii-translation-table 'translation-table)
-	 0)
-	char))
+  (encode-char char 'koi8-r))
 
 ;;;###autoload
 (defun cyrillic-encode-alternativnyj-char (char)
   "Return ALTERNATIVNYJ external character code of CHAR if appropriate."
-  (aref (char-table-extra-slot
-	 (get 'cyrillic-alternativnyj-nonascii-translation-table
-	      'translation-table)
-	 0)
-	char))
+  (encode-char char 'alternativnyj))
 
 
 ;; Display
@@ -174,117 +169,13 @@ If the argument is nil, we return the display table to its standard state."
     (aset standard-display-table ?,L*(B  [?N?j])
     (aset standard-display-table ?,L/(B  [?D?j])
 
-    ;; Unicode version:
-    (aset standard-display-table ?$,1(P(B  [?a])
-    (aset standard-display-table ?$,1(Q(B  [?b])
-    (aset standard-display-table ?$,1(R(B  [?v])
-    (aset standard-display-table ?$,1(S(B  [?g])
-    (aset standard-display-table ?$,1(T(B  [?d])
-    (aset standard-display-table ?$,1(U(B  [?e])
-    (aset standard-display-table ?$,1(q(B  [?y?o])
-    (aset standard-display-table ?$,1(V(B  [?z?h])
-    (aset standard-display-table ?$,1(W(B  [?z])
-    (aset standard-display-table ?$,1(X(B  [?i])
-    (aset standard-display-table ?$,1(Y(B  [?j])
-    (aset standard-display-table ?$,1(Z(B  [?k])
-    (aset standard-display-table ?$,1([(B  [?l])
-    (aset standard-display-table ?$,1(\(B  [?m])
-    (aset standard-display-table ?$,1(](B  [?n])
-    (aset standard-display-table ?$,1(^(B  [?o])
-    (aset standard-display-table ?$,1(_(B  [?p])
-    (aset standard-display-table ?$,1(`(B  [?r])
-    (aset standard-display-table ?$,1(a(B  [?s])
-    (aset standard-display-table ?$,1(b(B  [?t])
-    (aset standard-display-table ?$,1(c(B  [?u])
-    (aset standard-display-table ?$,1(d(B  [?f])
-    (aset standard-display-table ?$,1(e(B  [?k?h])
-    (aset standard-display-table ?$,1(f(B  [?t?s])
-    (aset standard-display-table ?$,1(g(B  [?c?h])
-    (aset standard-display-table ?$,1(h(B  [?s?h])
-    (aset standard-display-table ?$,1(i(B  [?s?c?h])
-    (aset standard-display-table ?$,1(j(B  [?~])
-    (aset standard-display-table ?$,1(k(B  [?y])
-    (aset standard-display-table ?$,1(l(B  [?'])
-    (aset standard-display-table ?$,1(m(B  [?e?'])
-    (aset standard-display-table ?$,1(n(B  [?y?u])
-    (aset standard-display-table ?$,1(o(B  [?y?a])
-
-    (aset standard-display-table ?$,1(0(B  [?A])
-    (aset standard-display-table ?$,1(1(B  [?B])
-    (aset standard-display-table ?$,1(2(B  [?V])
-    (aset standard-display-table ?$,1(3(B  [?G])
-    (aset standard-display-table ?$,1(4(B  [?D])
-    (aset standard-display-table ?$,1(5(B  [?E])
-    (aset standard-display-table ?$,1(!(B  [?Y?o])
-    (aset standard-display-table ?$,1(6(B  [?Z?h])
-    (aset standard-display-table ?$,1(7(B  [?Z])
-    (aset standard-display-table ?$,1(8(B  [?I])
-    (aset standard-display-table ?$,1(9(B  [?J])
-    (aset standard-display-table ?$,1(:(B  [?K])
-    (aset standard-display-table ?$,1(;(B  [?L])
-    (aset standard-display-table ?$,1(<(B  [?M])
-    (aset standard-display-table ?$,1(=(B  [?N])
-    (aset standard-display-table ?$,1(>(B  [?O])
-    (aset standard-display-table ?$,1(?(B  [?P])
-    (aset standard-display-table ?$,1(@(B  [?R])
-    (aset standard-display-table ?$,1(A(B  [?S])
-    (aset standard-display-table ?$,1(B(B  [?T])
-    (aset standard-display-table ?$,1(C(B  [?U])
-    (aset standard-display-table ?$,1(D(B  [?F])
-    (aset standard-display-table ?$,1(E(B  [?K?h])
-    (aset standard-display-table ?$,1(F(B  [?T?s])
-    (aset standard-display-table ?$,1(G(B  [?C?h])
-    (aset standard-display-table ?$,1(H(B  [?S?h])
-    (aset standard-display-table ?$,1(I(B  [?S?c?h])
-    (aset standard-display-table ?$,1(J(B  [?~])
-    (aset standard-display-table ?$,1(K(B  [?Y])
-    (aset standard-display-table ?$,1(L(B  [?'])
-    (aset standard-display-table ?$,1(M(B  [?E?'])
-    (aset standard-display-table ?$,1(N(B  [?Y?u])
-    (aset standard-display-table ?$,1(O(B  [?Y?a])
-
-    (aset standard-display-table ?$,1(t(B  [?i?e])
-    (aset standard-display-table ?$,1(w(B  [?i])
-    (aset standard-display-table ?$,1(~(B  [?u])
-    (aset standard-display-table ?$,1(r(B  [?d?j])
-    (aset standard-display-table ?$,1({(B  [?c?h?j])
-    (aset standard-display-table ?$,1(s(B  [?g?j])
-    (aset standard-display-table ?$,1(u(B  [?s])
-    (aset standard-display-table ?$,1(|(B  [?k])
-    (aset standard-display-table ?$,1(v(B  [?i])
-    (aset standard-display-table ?$,1(x(B  [?j])
-    (aset standard-display-table ?$,1(y(B  [?l?j])
-    (aset standard-display-table ?$,1(z(B  [?n?j])
-    (aset standard-display-table ?$,1((B  [?d?z])
-
-    (aset standard-display-table ?$,1($(B  [?Y?e])
-    (aset standard-display-table ?$,1('(B  [?Y?i])
-    (aset standard-display-table ?$,1(.(B  [?U])
-    (aset standard-display-table ?$,1("(B  [?D?j])
-    (aset standard-display-table ?$,1(+(B  [?C?h?j])
-    (aset standard-display-table ?$,1(#(B  [?G?j])
-    (aset standard-display-table ?$,1(%(B  [?S])
-    (aset standard-display-table ?$,1(,(B  [?K])
-    (aset standard-display-table ?$,1(&(B  [?I])
-    (aset standard-display-table ?$,1(((B  [?J])
-    (aset standard-display-table ?$,1()(B  [?L?j])
-    (aset standard-display-table ?$,1(*(B  [?N?j])
-    (aset standard-display-table ?$,1(/(B  [?D?j])
-
     (when (equal cyrillic-language "Bulgarian")
       (aset standard-display-table ?,Li(B [?s?h?t])
       (aset standard-display-table ?,LI(B [?S?h?t])
       (aset standard-display-table ?,Ln(B [?i?u])
       (aset standard-display-table ?,LN(B [?I?u])
       (aset standard-display-table ?,Lo(B [?i?a])
-      (aset standard-display-table ?,LO(B [?I?a])
-      ;; Unicode version:
-      (aset standard-display-table ?$,1(i(B [?s?h?t])
-      (aset standard-display-table ?$,1(I(B [?S?h?t])
-      (aset standard-display-table ?$,1(n(B [?i?u])
-      (aset standard-display-table ?$,1(N(B [?I?u])
-      (aset standard-display-table ?$,1(o(B [?i?a])
-      (aset standard-display-table ?$,1(O(B [?I?a]))
+      (aset standard-display-table ?,LO(B [?I?a]))
 
     (when (equal cyrillic-language "Ukrainian")	; based on the official
 					; transliteration table
@@ -293,14 +184,7 @@ If the argument is nil, we return the display table to its standard state."
       (aset standard-display-table ?,LY(B [?i])
       (aset standard-display-table ?,L9(B [?Y])
       (aset standard-display-table ?,Ln(B [?i?u])
-      (aset standard-display-table ?,Lo(B [?i?a])
-      ;; Unicode version:
-      (aset standard-display-table ?$,1(X(B [?y])
-      (aset standard-display-table ?$,1(8(B [?Y])
-      (aset standard-display-table ?$,1(Y(B [?i])
-      (aset standard-display-table ?$,1(9(B [?Y])
-      (aset standard-display-table ?$,1(n(B [?i?u])
-      (aset standard-display-table ?$,1(o(B [?i?a]))))
+      (aset standard-display-table ?,Lo(B [?i?a]))))
 
 ;;
 (provide 'cyril-util)
