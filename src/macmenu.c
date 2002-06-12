b3e8cc4dfb1d02a69b6d59e944a1a935909bf853
Pavel Janík <Pavel@Janik.cz>
2002-06-12 20:55:27 +0000

    Remove declaration of Qmouse_click and Qevent_kind.

--- src/macmenu.c
+++ src/macmenu.c
@@ -176,7 +176,6 @@ Lisp_Object Vmenu_updating_frame;
 Lisp_Object Qdebug_on_next_call;
 
 extern Lisp_Object Qmenu_bar;
-extern Lisp_Object Qmouse_click, Qevent_kind;
 
 extern Lisp_Object QCtoggle, QCradio;
 
@@ -956,7 +955,7 @@ on the left of the dialog box and all following items on the right.
 
 /* Activate the menu bar of frame F.
    This is called from keyboard.c when it gets the
-   menu_bar_activate_event out of the Emacs event queue.
+   MENU_BAR_ACTIVATE_EVENT out of the Emacs event queue.
 
    To activate the menu bar, we signal to the input thread that it can
    return from the WM_INITMENU message, allowing the normal Windows
