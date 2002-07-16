77be52b77f623569329fc02d2af954e24a308c01
Ken Raeburn <raeburn@raeburn.org>
2002-07-16 19:48:08 +0000

    (xstricmp): Declaration updated.

--- src/dispextern.h
+++ src/dispextern.h
@@ -2295,7 +2295,7 @@ void unload_color P_ ((struct frame *, unsigned long));
 int frame_update_line_height P_ ((struct frame *));
 int ascii_face_of_lisp_face P_ ((struct frame *, int));
 void prepare_face_for_display P_ ((struct frame *, struct face *));
-int xstricmp P_ ((unsigned char *, unsigned char *));
+int xstricmp P_ ((const unsigned char *, const unsigned char *));
 int lookup_face P_ ((struct frame *, Lisp_Object *, int, struct face *));
 int lookup_named_face P_ ((struct frame *, Lisp_Object, int));
 int smaller_face P_ ((struct frame *, int, int));
