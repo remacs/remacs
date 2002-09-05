91fda8546ac16baabbe8462d4a0b2e2c76f23896
Richard M. Stallman <rms@gnu.org>
2002-09-05 02:37:17 +0000

    (UNEXEC): Use unexsol.o.

--- src/s/sol2-5.h
+++ src/s/sol2-5.h
@@ -38,19 +38,5 @@
 #endif
 #endif
 
-#if 0 /* A recent patch in unexelf.c should eliminate the need for this.  */
-/* Don't use the shared libraries for -lXt and -lXaw,
-   to work around a linker bug in Solaris 2.5.
-   (This also affects the other libraries used specifically for
-   the X toolkit, which may not be necessary.)  */
-#define LIBXT_STATIC
+#define UNEXEC unexsol.o
 
-#ifdef __GNUC__
-#define STATIC_OPTION -Xlinker -Bstatic
-#define DYNAMIC_OPTION -Xlinker -Bdynamic
-#else
-#define STATIC_OPTION -Bstatic
-#define DYNAMIC_OPTION -Bdynamic
-#endif
-
-#endif /* 0 */
