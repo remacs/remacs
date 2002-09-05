3bf30e3d4b9caac45bc6476e2af8c517b80f494c
Dave Love <fx@gnu.org>
2002-09-05 17:50:38 +0000

    (quoted-printable-decode-region): Use mm-insert-byte.

--- lisp/gnus/qp.el
+++ lisp/gnus/qp.el
@@ -63,7 +63,7 @@ coding-system."
 		 (let ((byte (string-to-int (buffer-substring (1+ (point))
 							      (+ 3 (point)))
 					    16)))
-		   (insert-byte byte 1)
+		   (mm-insert-byte byte 1)
 		   (delete-char 3)
 		   (unless (eq byte ?=)
 		     (backward-char))))
