3b32ae2caafe7d8e5539786c959fd3f8b0d36b75
Juanma Barranquero <lekktu@gmail.com>
2002-09-06 09:04:56 +0000

    (syms_of_w32select): Fix spacing.

--- src/w32select.c
+++ src/w32select.c
@@ -463,8 +463,8 @@ the text is encoded or decoded by this coding system.  */);
   DEFVAR_LISP ("next-selection-coding-system", &Vnext_selection_coding_system,
 	       doc: /* Coding system for the next communication with other programs.
 Usually, `selection-coding-system' is used for communicating with
-other programs.   But, if this variable is set, it is used for the
-next communication only.   After the communication, this variable is
+other programs.  But, if this variable is set, it is used for the
+next communication only.  After the communication, this variable is
 set to nil.  */);
   Vnext_selection_coding_system = Qnil;
 
