27847466a370ea6102ea3a358d04d38918e9bf27
Richard M. Stallman <rms@gnu.org>
2002-09-26 22:04:14 +0000

    (__restrict_arr): Don't define if already defined.

--- src/regex.h
+++ src/regex.h
@@ -538,7 +538,9 @@ extern int re_exec _RE_ARGS ((const char *));
 /* For now unconditionally define __restrict_arr to expand to nothing.
    Ideally we would have a test for the compiler which allows defining
    it to restrict.  */
+#ifndef __restrict_arr
 #define __restrict_arr
+#endif
 
 /* POSIX compatibility.  */
 extern int regcomp _RE_ARGS ((regex_t *__restrict __preg,
