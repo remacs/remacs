7884cd32d0eace418d11f5b478f5dda2bd04be99
Stefan Monnier <monnier@iro.umontreal.ca>
2002-10-10 15:06:28 +0000

    Remove.  They're auto-generated and not needed for bootstrap any more.

--- lisp/subdirs.el
+++
@@ -1,4 +0,0 @@
-;; -*- no-byte-compile: t -*-
-;; In load-path, after this directory should come
-;; certain of its subdirectories.  Here we specify them.
-(normal-top-level-add-to-load-path '("toolbar" "textmodes" "progmodes" "play" "obsolete" "net" "mail" "language" "international" "gnus" "eshell" "emulation" "emacs-lisp" "calendar" "calc" ))
