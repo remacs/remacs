45bec201a1e96a1ccf808c0901fbae9a8a336889
Richard M. Stallman <rms@gnu.org>
2002-11-06 04:58:50 +0000

    (POLLING_PROBLEM_IN_SELECT): Add #undef.

--- src/s/hpux11.h
+++ src/s/hpux11.h
@@ -13,3 +13,6 @@
 /* It does work on HPUX to open the pty's tty in the parent (Emacs),
    then close and reopen it in the child.  */
 #define USG_SUBTTY_WORKS
+
+/* HPUX 10.10 needs this; HPUX 10.20 does not, and HPUX 11 does not.  */
+#undef POLLING_PROBLEM_IN_SELECT
