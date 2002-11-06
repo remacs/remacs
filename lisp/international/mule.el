f4f008275cd9faa9153106e455e4f5b5e2658a9a
Kenichi Handa <handa@m17n.org>
2002-11-06 01:07:49 +0000

    (coding-system-eol-type-mnemonic): Use eol-mnemonic-undecided if CODING-SYSTEM is nil.

--- lisp/international/mule.el
+++ lisp/international/mule.el
@@ -516,11 +516,10 @@ coding system whose eol-type is N."
 (defun coding-system-eol-type-mnemonic (coding-system)
   "Return the string indicating end-of-line format of CODING-SYSTEM."
   (let* ((eol-type (coding-system-eol-type coding-system))
-	 (val (cond ((vectorp eol-type) eol-mnemonic-undecided)
-		    ((eq eol-type 0) eol-mnemonic-unix)
+	 (val (cond ((eq eol-type 0) eol-mnemonic-unix)
 		    ((eq eol-type 1) eol-mnemonic-dos)
 		    ((eq eol-type 2) eol-mnemonic-mac)
-		    (t "-"))))
+		    (t eol-mnemonic-undecided))))
     (if (stringp val)
 	val
       (char-to-string val))))
