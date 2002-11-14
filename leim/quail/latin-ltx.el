554c3968d7502c9fe8a8447b96ca4d3aedc267bb
Dave Love <fx@gnu.org>
2002-11-14 14:05:16 +0000

    Fix coding cookie.

--- leim/quail/latin-ltx.el
+++ leim/quail/latin-ltx.el
@@ -1,4 +1,4 @@
-;;; latin-ltx.el --- Quail package for TeX-style input -*-coding: iso-2022-7bit-*-
+;;; latin-ltx.el --- Quail package for TeX-style input -*-coding: iso-2022-7bit;-*-
 
 ;; Copyright (C) 2001 Electrotechnical Laboratory, JAPAN.
 ;; Licensed to the Free Software Foundation.
