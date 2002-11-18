ca55a74c0bd8ff69ee6159d9339dacd9c8522ad8
Dave Love <fx@gnu.org>
2002-11-18 16:34:38 +0000

    Remove comments.

--- src/m/powermac.h
+++ src/m/powermac.h
@@ -79,16 +79,6 @@ Boston, MA 02111-1307, USA.  */
 
 /* #define VIRT_ADDR_VARIES * */
 
-/* Define C_ALLOCA if this machine does not support a true alloca
-   and the one written in C should be used instead.
-   Define HAVE_ALLOCA to say that the system provides a properly
-   working alloca function and it should be used.
-   Define neither one if an assembler-language alloca
-   in the file alloca.s should be used.  */
-
-/* #define C_ALLOCA */
-/* #define HAVE_ALLOCA */
-
 /* Define NO_REMAP if memory segmentation makes it not work well
    to change the boundary between the text section and data section
    when Emacs is dumped.  If you define this, the preloaded Lisp
