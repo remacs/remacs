207c13a78f0791333ca476e7ad1518d65f0d433d
Dave Love <fx@gnu.org>
2002-11-21 10:58:13 +0000

    Provide ISO C arglists for functions with Boolean args.

--- lwlib/lwlib-Xaw.c
+++ lwlib/lwlib-Xaw.c
@@ -119,11 +119,16 @@ xaw_update_scrollbar (instance, widget, val)
 #endif
 
 void
+#ifdef PROTOTYPES
+xaw_update_one_widget (widget_instance *instance, Widget widget,
+		       widget_value *val, Boolean deep_p)
+#else
 xaw_update_one_widget (instance, widget, val, deep_p)
      widget_instance *instance;
      Widget widget;
      widget_value *val;
      Boolean deep_p;
+#endif
 {
 #if 0
   if (XtIsSubclass (widget, scrollbarWidgetClass))
@@ -198,9 +203,13 @@ xaw_popup_menu (widget, event)
 }
 
 void
+#ifdef PROTOTYPES
+xaw_pop_instance (widget_instance *instance, Boolean up)
+#else
 xaw_pop_instance (instance, up)
      widget_instance *instance;
      Boolean up;
+#endif
 {
   Widget widget = instance->widget;
 
