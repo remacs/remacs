f0088f4de8dfd17b6a2a1bf48cb2e1a8b7d39a3c
Richard M. Stallman <rms@gnu.org>
2002-12-21 20:25:25 +0000

    (Man-getpage-in-background): Put GROFF_NO_SGR in env.

--- lisp/man.el
+++ lisp/man.el
@@ -618,7 +618,9 @@ all sections related to a subject, put something appropriate into the
 	     (start-process manual-program buffer "sh" "-c"
 			    (format (Man-build-man-command) man-args))
 	     'Man-bgproc-sentinel)
-	  (progn
+	  (let ((process-environment 
+		 (cons "GROFF_NO_SGR=1" process-environment)))
+
 	    (let ((exit-status
 		   (call-process shell-file-name nil (list buffer nil) nil "-c"
 				 (format (Man-build-man-command) man-args)))
