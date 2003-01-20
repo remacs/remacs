945e6102fedb22aa5d200d9b60ceb7a6a256c0a9
Glenn Morris <rgm@gnu.org>
2003-01-20 21:27:41 +0000

    (calendar-only-one-frame-setup): Add autoload.

--- lisp/calendar/calendar.el
+++ lisp/calendar/calendar.el
@@ -1351,6 +1351,9 @@ The Gregorian date Sunday, December 31, 1 BC is imaginary."
   "Move cursor to DATE."
   t)
 
+(autoload 'calendar-only-one-frame-setup "cal-x"
+ "Start calendar and display it in a dedicated frame.")
+
 (autoload 'calendar-one-frame-setup "cal-x"
   "Start calendar and display it in a dedicated frame together with the diary.")
 
