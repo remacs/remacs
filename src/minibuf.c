1a9e275b0e18e956fcddcb5cd6718e6f5aefbb31
Andreas Schwab <schwab@suse.de>
2003-01-24 19:15:14 +0000

    (Fminibuffer_message): Verify type of parameter.

--- src/minibuf.c
+++ src/minibuf.c
@@ -1,6 +1,6 @@
 /* Minibuffer input and completion.
    Copyright (C) 1985, 1986, 1993, 1994, 1995, 1996, 1997, 1998, 1999,
-   2000, 2001 Free Software Foundation, Inc.
+   2000, 2001, 2003 Free Software Foundation, Inc.
 
 This file is part of GNU Emacs.
 
@@ -2431,6 +2431,7 @@ or until the next input event arrives, whichever comes first.  */)
      (string)
      Lisp_Object string;
 {
+  CHECK_STRING (string);
   temp_echo_area_glyphs (SDATA (string));
   return Qnil;
 }
