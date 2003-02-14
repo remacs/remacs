b5cb36acaa9eb077e83c019bc68bf007d13d728f
Juanma Barranquero <lekktu@gmail.com>
2003-02-14 09:57:45 +0000

    (widget-choose): Fix use of character constant.

--- lisp/wid-edit.el
+++ lisp/wid-edit.el
@@ -271,7 +271,7 @@ minibuffer."
 		 (while (not (or (and (>= char ?0) (< char next-digit))
 				 (eq value 'keyboard-quit)))
 		   ;; Unread a SPC to lead to our new menu.
-		   (setq unread-command-events (cons ?\ unread-command-events))
+		   (setq unread-command-events (cons ?\  unread-command-events))
 		   (setq keys (read-key-sequence title))
 		   (setq value
 			 (lookup-key overriding-terminal-local-map keys t)
