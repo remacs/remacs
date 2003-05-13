b0622c6d46e7109cc2afce4e8edbb1d06240420c
Stefan Monnier <monnier@iro.umontreal.ca>
2003-05-13 18:35:16 +0000

    (describe-variable): Mention permanent local status.

--- lisp/help-fns.el
+++ lisp/help-fns.el
@@ -1,6 +1,6 @@
 ;;; help-fns.el --- Complex help functions
 
-;; Copyright (C) 1985, 1986, 1993, 1994, 1998, 1999, 2000, 2001, 2002
+;; Copyright (C) 1985, 1986, 1993, 1994, 1998, 1999, 2000, 2001, 2002, 2003
 ;;   Free Software Foundation, Inc.
 
 ;; Maintainer: FSF
@@ -410,7 +410,10 @@ it is displayed along with the global value."
 		      (delete-region (1- from) from)))))
 	    (terpri)
 	    (when (local-variable-p variable)
-	      (princ (format "Local in buffer %s; " (buffer-name)))
+	      (princ (format "%socal in buffer %s; "
+			     (if (get variable 'permanent-local)
+				 "Permanently l" "L")
+			     (buffer-name)))
 	      (if (not (default-boundp variable))
 		  (princ "globally void")
 		(let ((val (default-value variable)))
