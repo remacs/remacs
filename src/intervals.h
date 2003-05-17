6d9b60995f99935be3c4e6df530dde129d77ddaa
Stefan Monnier <monnier@iro.umontreal.ca>
2003-05-17 18:54:56 +0000

    (get_pos_property): Declare.

--- src/intervals.h
+++ src/intervals.h
@@ -305,6 +305,7 @@ void extend_property_ranges P_ ((Lisp_Object, Lisp_Object, Lisp_Object));
 Lisp_Object get_char_property_and_overlay P_ ((Lisp_Object, Lisp_Object,
 					       Lisp_Object, Lisp_Object*));
 extern int text_property_stickiness P_ ((Lisp_Object prop, Lisp_Object pos, Lisp_Object buffer));
+extern Lisp_Object get_pos_property P_ ((Lisp_Object pos, Lisp_Object prop, Lisp_Object object));
 
 extern void syms_of_textprop ();
 
