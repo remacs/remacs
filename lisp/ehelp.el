6e5cbb634eb25d76d5d336685e083fcab9414016
Richard M. Stallman <rms@gnu.org>
2003-07-07 21:05:17 +0000

    (ehelp-command): Use defalias to define ehelp-command. Give it a doc string, and autoload it.

--- lisp/ehelp.el
+++ lisp/ehelp.el
@@ -418,8 +418,11 @@ will select it.)"
     (substitute-key-definition 'describe-bindings 'electric-describe-bindings map)
     (substitute-key-definition 'describe-syntax 'electric-describe-syntax map)
 
-    (setq ehelp-map map)
-    (fset 'ehelp-command map)))
+    (setq ehelp-map map)))
+
+;;;###(autoload 'ehelp-command "ehelp" "Prefix command for ehelp." t 'keymap)
+(defalias 'ehelp-command ehelp-map)
+(put 'ehelp-command 'documentation "Prefix command for ehelp.")
 
 (provide 'ehelp)
 
