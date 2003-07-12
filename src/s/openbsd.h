4a2abeb21a5870693564401ea343c0e4f1323c3e
Richard M. Stallman <rms@gnu.org>
2003-07-12 21:44:13 +0000

    Don't include bsd4-3.h. (TERMINFO): Defined. (LIBS_TERMCAP): Defined. (LD_SWITCH_SYSTEM): Defined (two definitions).

--- src/s/openbsd.h
+++ src/s/openbsd.h
@@ -1,11 +1,24 @@
 /* s/ file for openbsd systems.  */
 
-/* Get most of the stuff from bsd4.3 */
-#include "bsd4-3.h"
-
-/* Get the rest of the stuff from that less-POSIX-conformant system */
+/* Mostly the same as NetBSD.  */
 #include "netbsd.h"
 
 /*  David Mazieres <dm@reeducation-labor.lcs.mit.edu> says this
     is necessary.  Otherwise Emacs dumps core when run -nw.  */
 #undef LIBS_TERMCAP
+
+#define TERMINFO
+#define LIBS_TERMCAP -lncurses
+
+#undef LD_SWITCH_SYSTEM
+#ifdef __ELF__
+
+/*  Han Boetes <han@mijncomputer.nl> says this
+    is necessary,  otherwise Emacs dumps core on elf systems.  */
+#define LD_SWITCH_SYSTEM LD_SWITCH_SYSTEM_tmp -Z
+
+#else
+
+#define LD_SWITCH_SYSTEM LD_SWITCH_SYSTEM_tmp
+
+#endif
