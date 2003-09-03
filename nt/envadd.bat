8bf425323b25d464ae9d712a16d96a68fa9fcf6b
Jason Rumney <jasonr@gnu.org>
2003-09-03 23:09:40 +0000

    Fix copyright notice

--- nt/envadd.bat
+++ nt/envadd.bat
@@ -1,7 +1,7 @@
 rem  Hack to change/add environment variables in the makefiles for the
 rem  Windows platform.
 rem
-rem  Copyright (c) 1993-2000 Free Software Foundation, Inc.
+rem  Copyright (c) 2003 Free Software Foundation, Inc.
 rem
 rem  This file is part of GNU Emacs.
 rem
