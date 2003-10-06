e2f9e1b2adc8053e6260757659d2efcd9f645aaf
Dave Love <fx@gnu.org>
2003-10-06 16:31:17 +0000

    Change maintainer.

--- lisp/hl-line.el
+++ lisp/hl-line.el
@@ -3,6 +3,7 @@
 ;; Copyright (C) 1998, 2000, 2001, 2003 Free Software Foundation, Inc.
 
 ;; Author:  Dave Love <fx@gnu.org>
+;; Maintainer: FSF
 ;; Created: 1998-09-13
 ;; Keywords: faces, frames, emulation
 
