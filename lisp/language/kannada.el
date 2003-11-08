75b62c63e713dbdf16f4e41dbfafeb43e72739b0
Kenichi Handa <handa@m17n.org>
2003-11-08 01:40:48 +0000

    New file.

---
+++ lisp/language/kannada.el
@@ -0,0 +1,46 @@
+;;; kannada.el --- Support for Kannada -*- coding: iso-2022-7bit; no-byte-compile: t -*-
+
+;; Copyright (C) 2003 Free Software Foundation, Inc.
+
+;; Maintainer:  CHOWKSEY, Kailash C. <klchxbec@m-net.arbornet.org>
+;; Keywords: multilingual, Indian, Kannada
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;; This file defines language-info of Kannada script.
+
+;;; Code:
+
+(set-language-info-alist
+ "Kannada" '((charset mule-unicode-0100-24ff indian-is13194
+                         indian-2-column indian-glyph ;; comment out later
+                         )
+		(coding-system mule-utf-8)
+		(coding-priority mule-utf-8)
+		(input-method . "kannada-itrans")
+		(features knd-util)
+		(documentation . "\
+Kannada language and script is supported in this language
+environment.")) 
+ '("Indian"))
+
+(provide 'kannada)
+
+;;; kannada.el ends here
