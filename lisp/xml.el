5ebe0443931ba9bf4e006056a5b48283a3eb88d6
Eli Zaretskii <eliz@is.elta.co.il>
2003-12-29 12:35:35 +0000

    Fix previous change.  Remove redundant trailing whitespace.

--- lisp/xml.el
+++ lisp/xml.el
@@ -109,12 +109,10 @@ CHILD-NAME should be a lower case symbol."
 nil is returned if the attribute was not found.
 
 See also `xml-get-attribute'."
-  (if (xml-node-attributes node)
-      (let ((value (assoc attribute (xml-node-attributes node))))
-	(if value
-	    (cdr value)
-	  nil))
-    nil))
+  (when (xml-node-attributes node)
+    (let ((value (assoc attribute (xml-node-attributes node))))
+      (when value
+	(cdr value)))))
 
 (defsubst xml-get-attribute (node attribute)
   "Get from NODE the value of ATTRIBUTE.
@@ -295,7 +293,6 @@ If PARSE-NS is non-nil, then QNAMES are expanded."
    attr-list)
   attr-list)
 
-
 (defun xml-intern-attrlist (attr-list)
   "Convert attribute names to symbols for backward compatibility."
   (mapcar (lambda (attr)
@@ -358,12 +355,12 @@ Returns one of:
       (let* ((node-name (match-string 1))
 	     (attr-list (xml-parse-attlist))
 	     (children (if  (consp xml-ns) ;; take care of namespace parsing
-			    (progn 
+			    (progn
 			      (setq xml-ns (xml-ns-parse-ns-attrs
 					    attr-list xml-ns))
-			      (list (xml-ns-expand-attr 
+			      (list (xml-ns-expand-attr
 				     attr-list xml-ns)
-				    (xml-ns-expand-el 
+				    (xml-ns-expand-el
 				     node-name xml-ns)))
 			    (list (xml-intern-attrlist attr-list)
 				  (intern node-name))))
