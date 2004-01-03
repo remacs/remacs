f0e2c284732f28a4ab09fbc8a7eaeebc81f72d18
Karoly Lorentey <lorentey@elte.hu>
2004-01-03 16:51:56 +0000

    Add "multi-tty" to emacs-version output.
    lisp/version.el (emacs-version): Add multi-tty to output if (featurep
    'multi-tty).  Suggested by Romain Francoise <romain@orebokech.com>.
    
    git-archimport-id: lorentey@elte.hu--2004/emacs--multi-tty--0--patch-41

--- lisp/version.el
+++ lisp/version.el
@@ -55,8 +55,8 @@ to the system configuration; look at `system-configuration' instead."
   (interactive "P")
   (let ((version-string
          (format (if (not (interactive-p))
-		     "GNU Emacs %s (%s%s%s)\n of %s on %s"
-		   "GNU Emacs %s (%s%s%s) of %s on %s")
+		     "GNU Emacs %s (%s%s%s%s)\n of %s on %s"
+		   "GNU Emacs %s (%s%s%s%s) of %s on %s")
                  emacs-version
 		 system-configuration
 		 (cond ((featurep 'motif)
@@ -70,6 +70,7 @@ to the system configuration; look at `system-configuration' instead."
 		     (format ", %s scroll bars"
 			     (capitalize (symbol-name x-toolkit-scroll-bars)))
 		   "")
+		 (if (featurep 'multi-tty) ", multi-tty" "")
 		 (format-time-string "%Y-%m-%d" emacs-build-time)
                  emacs-build-system)))
     (if here
