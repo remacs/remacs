fc1d15f69da0808959f5d6e8c8caa0a8d0b100c7
Kenichi Handa <handa@m17n.org>
2004-02-05 02:16:36 +0000

    (Fset_buffer_multibyte): Fix docstring.

--- src/buffer.c
+++ src/buffer.c
@@ -2081,7 +2081,9 @@ DEFUN ("set-buffer-multibyte", Fset_buffer_multibyte, Sset_buffer_multibyte,
 If FLAG is t, this makes the buffer a multibyte buffer.
 If FLAG is nil, this makes the buffer a single-byte buffer.
 The buffer contents remain unchanged as a sequence of bytes
-but the contents viewed as characters do change.  */)
+but the contents viewed as characters do change.
+If the multibyte flag was really changed, undo information of the
+current buffer is cleared.  */)
      (flag)
      Lisp_Object flag;
 {
