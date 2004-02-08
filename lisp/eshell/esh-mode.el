db22de71ec917427f89dbafbc25e5e83aee65c52
Andreas Schwab <schwab@suse.de>
2004-02-08 22:40:35 +0000

    (eshell-send-invisible): Fix format string.

--- lisp/eshell/esh-mode.el
+++ lisp/eshell/esh-mode.el
@@ -1,6 +1,6 @@
 ;;; esh-mode.el --- user interface
 
-;; Copyright (C) 1999, 2000, 2001 Free Software Foundation
+;; Copyright (C) 1999, 2000, 2001, 2004 Free Software Foundation
 
 ;; Author: John Wiegley <johnw@gnu.org>
 
@@ -1017,7 +1017,7 @@ a key."
 Then send it to the process running in the current buffer."
   (interactive "P")                     ; Defeat snooping via C-x ESC ESC
   (let ((str (read-passwd
-	      (format "Password: "
+	      (format "%s Password: "
 		      (process-name (eshell-interactive-process))))))
     (if (stringp str)
 	(process-send-string (eshell-interactive-process)
