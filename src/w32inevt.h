156395866238fa97dbe03276c7e0c53679dd32ab
Kim F. Storm <storm@cua.dk>
2004-02-27 23:49:09 +0000

    (w32_console_mouse_position): Fix prototype.

--- src/w32inevt.h
+++ src/w32inevt.h
@@ -21,8 +21,8 @@ Boston, MA 02111-1307, USA.  */
 #ifndef EMACS_W32INEVT_H
 #define EMACS_W32INEVT_H
 
-extern int w32_console_read_socket (int sd, struct input_event *bufp,
-				    int numchars, int expected);
+extern int w32_console_read_socket (int sd, int numchars,
+				    struct input_event *hold_quit);
 extern void w32_console_mouse_position (FRAME_PTR *f, int insist,
 					Lisp_Object *bar_window,
 					enum scroll_bar_part *part,
