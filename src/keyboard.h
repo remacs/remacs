e0de7f88fe9935b9d400ebe80899d70b4ce88b4b
Kim F. Storm <storm@cua.dk>
2004-02-27 23:46:47 +0000

    (kbd_buffer_store_event_hold): Add prototype. (gen_help_event): Fix prototype.

--- src/keyboard.h
+++ src/keyboard.h
@@ -329,13 +329,15 @@ extern void timer_start_idle P_ ((void));
 extern void timer_stop_idle P_ ((void));
 extern int lucid_event_type_list_p P_ ((Lisp_Object));
 extern void kbd_buffer_store_event P_ ((struct input_event *));
+extern void kbd_buffer_store_event_hold P_ ((struct input_event *,
+					     struct input_event *));
 #ifdef POLL_FOR_INPUT
 extern void poll_for_input_1 P_ ((void));
 #endif
 extern void show_help_echo P_ ((Lisp_Object, Lisp_Object, Lisp_Object,
 				Lisp_Object, int));
-extern int gen_help_event P_ ((struct input_event *, int, Lisp_Object,
-			       Lisp_Object, Lisp_Object, Lisp_Object, int));
+extern void gen_help_event P_ ((Lisp_Object, Lisp_Object, Lisp_Object,
+			       Lisp_Object, int));
 extern void kbd_buffer_store_help_event P_ ((Lisp_Object, Lisp_Object));
 extern Lisp_Object menu_item_eval_property P_ ((Lisp_Object));
 extern int  kbd_buffer_events_waiting P_ ((int));
