238add5e586e100ea2f7fd243d3ce57f98449b45
Jason Rumney <jasonr@gnu.org>
2004-05-03 14:19:40 +0000

    Remove files related to old msvc only windows build.

--- nt/ebuild.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-if (%1) == () nmake -f makefile.nt all
-if not (%1) == () nmake -f makefile.nt %1 %2 %3 %4 %5 %6 %7 %8 %9
-
-goto skipArchTag
-   arch-tag: f01be12a-fd17-448d-8275-c7f527c50a1f
-:skipArchTag
