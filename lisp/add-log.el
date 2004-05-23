c58bb024cbbfe40750305b46b82e869fb18aa710
Juri Linkov <juri@jurta.org>
2004-05-23 20:57:50 +0000

    (change-log-font-lock-keywords): Remove `:' from regexps for function and variable names.

--- lisp/add-log.el
+++ lisp/add-log.el
@@ -230,13 +230,13 @@ Note: The search is conducted only within 10%, at the beginning of the file."
      ;; Possibly further names in a list:
      ("\\=, \\([^ ,:([\n]+\\)" nil nil (1 'change-log-file-face))
      ;; Possibly a parenthesized list of names:
-     ("\\= (\\([^) ,:\n]+\\)" nil nil (1 'change-log-list-face))
-     ("\\=, *\\([^) ,:\n]+\\)" nil nil (1 'change-log-list-face)))
+     ("\\= (\\([^) ,\n]+\\)" nil nil (1 'change-log-list-face))
+     ("\\=, *\\([^) ,\n]+\\)" nil nil (1 'change-log-list-face)))
     ;;
     ;; Function or variable names.
-    ("^\t(\\([^) ,:\n]+\\)"
+    ("^\t(\\([^) ,\n]+\\)"
      (1 'change-log-list-face)
-     ("\\=, *\\([^) ,:\n]+\\)" nil nil (1 'change-log-list-face)))
+     ("\\=, *\\([^) ,\n]+\\)" nil nil (1 'change-log-list-face)))
     ;;
     ;; Conditionals.
     ("\\[!?\\([^]\n]+\\)\\]\\(:\\| (\\)" (1 'change-log-conditionals-face))
