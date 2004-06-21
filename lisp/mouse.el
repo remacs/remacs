eb630d4b6b23c5c989b48233881c61f31a9a7386
Richard M. Stallman <rms@gnu.org>
2004-06-21 03:16:28 +0000

    (mouse-set-region-1): Set transient-mark-mode to `only'.

--- lisp/mouse.el
+++ lisp/mouse.el
@@ -625,7 +625,7 @@ This should be bound to a mouse drag event."
 
 (defun mouse-set-region-1 ()
   ;; Set transient-mark-mode for a little while.
-  (setq transient-mark-mode (or transient-mark-mode 'lambda))
+  (setq transient-mark-mode (or transient-mark-mode 'only))
   (setq mouse-last-region-beg (region-beginning))
   (setq mouse-last-region-end (region-end))
   (setq mouse-last-region-tick (buffer-modified-tick)))
