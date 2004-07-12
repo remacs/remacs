9ea306d1786e97efae7123d32cd59972956ab1d6
Kim F. Storm <storm@cua.dk>
2004-07-12 14:35:53 +0000

    (mark_object): Only look at Lisp_Misc_Save_Value if GC_MARK_STACK.

--- src/alloc.c
+++ src/alloc.c
@@ -5033,6 +5033,7 @@ mark_object (arg)
 	  break;
 
 	case Lisp_Misc_Save_Value:
+#if GC_MARK_STACK
 	  {
 	    register struct Lisp_Save_Value *ptr = XSAVE_VALUE (obj);
 	    /* If DOGC is set, POINTER is the address of a memory
@@ -5045,6 +5046,7 @@ mark_object (arg)
 		  mark_maybe_object (*p);
 	      }
 	  }
+#endif
 	  break;
 
 	case Lisp_Misc_Overlay:
