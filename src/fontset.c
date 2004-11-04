b912921c670907a0c62f0f6459fe5f1155eead9a
Kenichi Handa <handa@m17n.org>
2004-11-04 02:28:51 +0000

    (fontset_pattern_regexp): If '*' is preceded by '\', treat it as a literal character.

--- src/fontset.c
+++ src/fontset.c
@@ -796,7 +796,7 @@ fontset_pattern_regexp (pattern)
 	{
 	  if (*p0 == '-')
 	    ndashes++;
-	  else if (*p0 == '*')
+	  else if (*p0 == '*' && p0 > SDATA (pattern) && p0[-1] != '\\')
 	    nstars++;
 	}
 
@@ -811,7 +811,7 @@ fontset_pattern_regexp (pattern)
       *p1++ = '^';
       for (p0 = (char *) SDATA (pattern); *p0; p0++)
 	{
-	  if (*p0 == '*')
+	  if (*p0 == '*' && p0 > SDATA (pattern) && p0[-1] != '\\')
 	    {
 	      if (ndashes < 14)
 		*p1++ = '.';
