21acf124651cdcfd76317649236a25254f57de1b
Steven Tamm <steventamm@mac.com>
2004-11-07 05:57:58 +0000

    editfns.c: Adding in better patch

--- src/editfns.c
+++ src/editfns.c
@@ -39,10 +39,11 @@ Boston, MA 02111-1307, USA.  */
 #include <stdio.h>
 #endif
 
-#if defined HAVE_SYS_RESOURCE_H
-#ifdef MAC_OSX
+#ifdef HAVE_SYS_TIME_H
 #include <sys/time.h>
 #endif
+
+#if defined HAVE_SYS_RESOURCE_H
 #include <sys/resource.h>
 #endif
 
