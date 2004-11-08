4331c31e9598335f377133749f5cb239daa972e5
Richard M. Stallman <rms@gnu.org>
2004-11-08 16:42:25 +0000

    (filesets-spawn-external-viewer, filesets-run-cmd): Don't use beginning-of-buffer. (filesets-cmd-show-result): Use with-no-warnings.

--- lisp/filesets.el
+++ lisp/filesets.el
@@ -1356,7 +1356,7 @@ Use the viewer defined in EV-ENTRY (a valid element of
 		  (run-hooks 'oh))
 		(set-buffer-modified-p nil)
 		(setq buffer-read-only t)
-		(beginning-of-buffer))
+		(goto-char (point-min)))
 	    (when oh
 	      (run-hooks 'oh))))
       (filesets-error 'error
@@ -1593,7 +1593,8 @@ SAVE-FUNCTION takes no argument, but works on the current buffer."
 (defun filesets-cmd-show-result (cmd output)
   "Show OUTPUT of CMD (a shell command)."
   (pop-to-buffer "*Filesets: Shell Command Output*")
-  (end-of-buffer)
+  (with-no-warnings
+   (end-of-buffer))
   (insert "*** ")
   (insert cmd)
   (newline)
@@ -1638,7 +1639,7 @@ Replace <file-name> or <<file-name>> with filename."
 		(save-restriction
 		  (let ((buffer (filesets-find-file this)))
 		    (when buffer
-		      (beginning-of-buffer)
+		      (goto-char (point-min))
 		      (let ()
 			(cond
 			 ((stringp fn)
