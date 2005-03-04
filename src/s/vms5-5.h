f17bb0913a82da0eac22de39ff772fb8f960dd57
Thien-Thi Nguyen <ttn@gnuvola.org>
2005-03-04 21:57:07 +0000

    Undefine NO_HYPHENS_IN_FILENAMES.

--- src/s/vms5-5.h
+++ src/s/vms5-5.h
@@ -2,6 +2,8 @@
 #define VMS5_5
 #define VMS4_4
 
+#undef NO_HYPHENS_IN_FILENAMES
+
 /* The bug that SHARABLE_LIB_BUG fixes is gone in version 5.5 of VMS.
    And defining it causes lossage because sys_errlist has a different
    number of elements.  */
