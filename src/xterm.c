335fcf6c2f6ae8990d8c62589cfb3558c3f38bc0
Karoly Lorentey <lorentey@elte.hu>
2005-03-22 17:58:23 +0000

    Revert changes that were committed by mistake in the previous patch.
    git-archimport-id: lorentey@elte.hu--2004/emacs--multi-tty--0--patch-315

--- src/xterm.c
+++ src/xterm.c
@@ -10770,9 +10770,25 @@ x_delete_frame_display (struct display *display)
   struct x_display_info *dpyinfo = display->display_info.x;
   int i;
 
-  xg_display_close (dpyinfo->display);
+  BLOCK_INPUT;
+  /* Free the fonts in the font table.  */
+  for (i = 0; i < dpyinfo->n_fonts; i++)
+    if (dpyinfo->font_table[i].name)
+      {
+	XFreeFont (dpyinfo->display, dpyinfo->font_table[i].font);
+      }
+
+  x_destroy_all_bitmaps (dpyinfo);
+  XSetCloseDownMode (dpyinfo->display, DestroyAll);
+
+#ifdef USE_X_TOOLKIT
+  XtCloseDisplay (dpyinfo->display);
+#else
+  XCloseDisplay (dpyinfo->display);
+#endif
 
   x_delete_display (dpyinfo);
+  UNBLOCK_INPUT;
 }
 
 
