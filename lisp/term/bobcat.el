3e23d962ee95e70ce9208e011e33be9d1021b3d7
Eli Zaretskii <eliz@gnu.org>
2005-03-26 16:22:59 +0000

    Don't use keyswap.el, since it is now obsolete.

--- lisp/term/bobcat.el
+++ lisp/term/bobcat.el
@@ -1,6 +1,7 @@
 ;; -*- no-byte-compile: t -*-
 ;;; HP terminals usually encourage using ^H as the rubout character
-(load "term/keyswap" nil t)
+(keyboard-translate ?\177 ?\^h)
+(keyboard-translate ?\^h ?\177)
 
 ;;; arch-tag: 754e4520-0a3e-4e6e-8ca5-9481b1f85cf7
 ;;; bobcat.el ends here
