a91877d296a01133f31690c9567f4381986a67f0
Richard M. Stallman <rms@gnu.org>
2005-03-31 18:59:50 +0000

    Fix previous change.

--- lisp/progmodes/scheme.el
+++ lisp/progmodes/scheme.el
@@ -90,7 +90,7 @@
     (modify-syntax-entry ?\] ")[  " st)
     (modify-syntax-entry ?{ "(}  " st)
     (modify-syntax-entry ?} "){  " st)
-    (modify-syntax-entry ?\| "\" 23b" table)
+    (modify-syntax-entry ?\| "\" 23b" st)
 
     ;; Other atom delimiters
     (modify-syntax-entry ?\( "()  " st)
@@ -103,7 +103,7 @@
     ;; Special characters
     (modify-syntax-entry ?, "'   " st)
     (modify-syntax-entry ?@ "'   " st)
-    (modify-syntax-entry ?# "' 14bn" table)
+    (modify-syntax-entry ?# "' 14bn" st)
     (modify-syntax-entry ?\\ "\\   " st)
     st))
 
