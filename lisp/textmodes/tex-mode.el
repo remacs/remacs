797bf075e1b53ecd92fa7e4e91f893b55e4a569d
Stefan Monnier <monnier@iro.umontreal.ca>
2005-04-06 15:45:00 +0000

    (tex-font-lock-keywords-2): Add \bfseries.

--- lisp/textmodes/tex-mode.el
+++ lisp/textmodes/tex-mode.el
@@ -573,7 +573,7 @@ An alternative value is \" . \", if you use a font with a narrow period."
 	      2 '(tex-font-lock-append-prop 'italic) 'append)
 	;; This is separate from the previous one because of cases like
 	;; {\em foo {\bf bar} bla} where both match.
-	(list (concat "\\\\\\(bf\\)\\>" args)
+	(list (concat "\\\\\\(bf\\(series\\)?\\)\\>" args)
 	      2 '(tex-font-lock-append-prop 'bold) 'append)))))
    "Gaudy expressions to highlight in TeX modes.")
 
