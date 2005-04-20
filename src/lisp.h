0ddfe52ffa602315c95ba8670987961b92469825
Kenichi Handa <handa@m17n.org>
2005-04-20 07:49:35 +0000

    (CHAR_TABLE_DEFAULT_SLOT_ASCII): New macro. (CHAR_TABLE_DEFAULT_SLOT_8_BIT_CONTROL): New macro. (CHAR_TABLE_DEFAULT_SLOT_8_BIT_GRAPHIC): New macro.

--- src/lisp.h
+++ src/lisp.h
@@ -720,6 +720,14 @@ struct Lisp_Vector
    indexed by (charset-id + 128).  */
 #define CHAR_TABLE_ORDINARY_SLOTS 384
 
+/* These are the slot of the default values for single byte
+   characters.  As 0x9A is never be a charset-id, it is safe to use
+   that slot for ASCII.  0x9E and 0x80 are charset-ids of
+   eight-bit-control and eight-bit-graphic respectively.  */
+#define CHAR_TABLE_DEFAULT_SLOT_ASCII (0x9A + 128)
+#define CHAR_TABLE_DEFAULT_SLOT_8_BIT_CONTROL (0x9E + 128)
+#define CHAR_TABLE_DEFAULT_SLOT_8_BIT_GRAPHIC (0x80 + 128)
+
 /* This is the number of slots that apply to characters of ASCII and
    8-bit Europeans only.  */
 #define CHAR_TABLE_SINGLE_BYTE_SLOTS 256
