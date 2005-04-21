16a5378b45e4058b53552d7e4790c101e08f6209
Kim F. Storm <storm@cua.dk>
2005-04-21 23:45:40 +0000

    (resolve_face_name): Use Fget instead of Fsafe_get.

--- src/xfaces.c
+++ src/xfaces.c
@@ -3218,7 +3218,7 @@ resolve_face_name (face_name)
 
   while (SYMBOLP (face_name))
     {
-      aliased = Fsafe_get (face_name, Qface_alias);
+      aliased = Fget (face_name, Qface_alias);
       if (NILP (aliased))
 	break;
       if (--alias_loop_max == 0)
