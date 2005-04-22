6f2d53406a51d6d407cf17699437010b61b9e52c
YAMAMOTO Mitsuharu <mituharu@math.s.chiba-u.ac.jp>
2005-04-22 04:56:11 +0000

    (Fx_create_frame, x_create_tip_frame): Pass Lisp string as the second argument for x_new_fontset.

--- src/macfns.c
+++ src/macfns.c
@@ -2632,7 +2632,7 @@ This function is an internal primitive--use `make-frame' instead.  */)
       font = x_new_font (f, "-ETL-fixed-medium-r-*--*-160-*-*-*-*-iso8859-1");
     /* If those didn't work, look for something which will at least work.  */
     if (! STRINGP (font))
-      font = x_new_fontset (f, "fontset-mac");
+      font = x_new_fontset (f, build_string ("fontset-mac"));
     if (! STRINGP (font))
       font = x_new_font (f, "-*-monaco-*-12-*-mac-roman");
     if (! STRINGP (font))
@@ -3730,7 +3730,7 @@ x_create_tip_frame (dpyinfo, parms, text)
       font = x_new_font (f, "-ETL-fixed-medium-r-*--*-160-*-*-*-*-iso8859-1");
     /* If those didn't work, look for something which will at least work.  */
     if (! STRINGP (font))
-      font = x_new_fontset (f, "fontset-mac");
+      font = x_new_fontset (f, build_string ("fontset-mac"));
     if (! STRINGP (font))
       font = x_new_font (f, "-*-monaco-*-12-*-mac-roman");
     if (! STRINGP (font))
