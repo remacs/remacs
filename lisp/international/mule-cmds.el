901ae597141d825a65cecb812049a84982682606
Kenichi Handa <handa@m17n.org>
2005-04-25 04:30:38 +0000

    (select-safe-coding-system): Don't check consistency with coding: spec, etc if raw-text or no-conversion was found to be safe.

--- lisp/international/mule-cmds.el
+++ lisp/international/mule-cmds.el
@@ -927,7 +927,9 @@ and TO is ignored."
     ;; give when file is re-read.
     ;; But don't do this if we explicitly ignored the cookie
     ;; by using `find-file-literally'.
-    (unless (or (stringp from) find-file-literally)
+    (unless (or (stringp from)
+		find-file-literally
+		(memq (coding-system-type coding-system) '(0 5)))
       (let ((auto-cs (save-excursion
 		       (save-restriction
 			 (widen)
