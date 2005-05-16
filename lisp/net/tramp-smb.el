430c1c663e9c4fadda43836817477b93b1a36a66
Juanma Barranquero <lekktu@gmail.com>
2005-05-16 22:58:55 +0000

    (tramp-smb-open-connection): Pass `tramp-chunksize' as fifth (required) argument.

--- lisp/net/tramp-smb.el
+++ lisp/net/tramp-smb.el
@@ -954,7 +954,7 @@ Domain names in USER and port numbers in HOST are acknowledged."
       (when port   (setq args (append args (list "-p" port))))
 
       ; OK, let's go
-      (tramp-pre-connection nil tramp-smb-method user host)
+      (tramp-pre-connection nil tramp-smb-method user host tramp-chunksize)
       (tramp-message 7 "Opening connection for //%s@%s/%s..."
 		     user host (or share ""))
 
