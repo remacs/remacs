f0a31d705523185ab5507ab7d2aee00fb9d46bd4
Nick Roberts <nickrob@snap.net.nz>
2005-05-24 10:27:22 +0000

    (Fyes_or_no_p, Fy_or_n_p): Call Fx_popup_dialog with a third argument (Qnil).

--- src/fns.c
+++ src/fns.c
@@ -3276,7 +3276,7 @@ is nil and `use-dialog-box' is non-nil.  */)
 			Fcons (Fcons (build_string ("No"), Qnil),
 			       Qnil));
 	  menu = Fcons (prompt, pane);
-	  obj = Fx_popup_dialog (Qt, menu);
+	  obj = Fx_popup_dialog (Qt, menu, Qnil);
 	  answer = !NILP (obj);
 	  break;
 	}
@@ -3408,7 +3408,7 @@ is nil, and `use-dialog-box' is non-nil.  */)
 			   Qnil));
       GCPRO1 (pane);
       menu = Fcons (prompt, pane);
-      obj = Fx_popup_dialog (Qt, menu);
+      obj = Fx_popup_dialog (Qt, menu, Qnil);
       UNGCPRO;
       return obj;
     }
