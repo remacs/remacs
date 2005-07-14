d4b4dfc6c224a19009ee0911f383f6fbd9ad37e6
Stefan Monnier <monnier@iro.umontreal.ca>
2005-07-14 02:06:30 +0000

    Fix up commenting style.

--- lisp/help-fns.el
+++ lisp/help-fns.el
@@ -217,13 +217,13 @@ ARGLIST can also be t or a string of the form \"(FUN ARG1 ARG2 ...)\"."
 			(intern (upcase name))))))
 		arglist)))
 
-;;; Could be this, if we make symbol-file do the work below.
-;;; (defun help-C-file-name (subr-or-var kind)
-;;;   "Return the name of the C file where SUBR-OR-VAR is defined.
-;;; KIND should be `var' for a variable or `subr' for a subroutine."
-;;;   (symbol-file (if (symbolp subr-or-var) subr-or-var
-;;; 		 (subr-name subr-or-var))
-;;; 	       (if (eq kind 'var) 'defvar 'defun)))
+;; Could be this, if we make symbol-file do the work below.
+;; (defun help-C-file-name (subr-or-var kind)
+;;   "Return the name of the C file where SUBR-OR-VAR is defined.
+;; KIND should be `var' for a variable or `subr' for a subroutine."
+;;   (symbol-file (if (symbolp subr-or-var) subr-or-var
+;; 		 (subr-name subr-or-var))
+;; 	       (if (eq kind 'var) 'defvar 'defun)))
 ;;;###autoload
 (defun help-C-file-name (subr-or-var kind)
   "Return the name of the C file where SUBR-OR-VAR is defined.
@@ -715,5 +715,5 @@ BUFFER should be a buffer or a buffer name."
 
 (provide 'help-fns)
 
-;;; arch-tag: 9e10331c-ae81-4d13-965d-c4819aaab0b3
+;; arch-tag: 9e10331c-ae81-4d13-965d-c4819aaab0b3
 ;;; help-fns.el ends here
