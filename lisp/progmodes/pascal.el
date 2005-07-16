ca73a5b4418cde4e8024bab9fc84a6c0f89f4185
Richard M. Stallman <rms@gnu.org>
2005-07-16 18:45:25 +0000

    (pascal-outline-map): Use fboundp, not boundp.  Correctly avoid warnings. (pascal-outline): Likewise.

--- lisp/progmodes/pascal.el
+++ lisp/progmodes/pascal.el
@@ -1493,12 +1493,12 @@ The default is a name found in the buffer around point."
 
 (if pascal-outline-map
     nil
-  (if (boundp 'set-keymap-name)
+  (if (fboundp 'set-keymap-name)
       (set-keymap-name pascal-outline-map 'pascal-outline-map))
-  (if (not (boundp 'set-keymap-parent))
-      (setq pascal-outline-map (copy-keymap pascal-mode-map))
-    (setq pascal-outline-map (make-sparse-keymap))
-    (set-keymap-parent pascal-outline-map pascal-mode-map))
+  (if (fboundp 'set-keymap-parent)
+      (set-keymap-parent (setq pascal-outline-map (make-sparse-keymap))
+			 pascal-mode-map)
+    (setq pascal-outline-map (copy-keymap pascal-mode-map)))
   (define-key pascal-outline-map "\M-\C-a"  'pascal-outline-prev-defun)
   (define-key pascal-outline-map "\M-\C-e"  'pascal-outline-next-defun)
   (define-key pascal-outline-map "\C-c\C-d" 'pascal-outline-goto-defun)
@@ -1533,7 +1533,7 @@ Pascal Outline mode provides some additional commands.
   (interactive "P")
   (setq pascal-outline-mode
 	(if (null arg) (not pascal-outline-mode) t))
-  (if (boundp 'redraw-mode-line)
+  (if (fboundp 'redraw-mode-line)
       (redraw-mode-line))
   (if pascal-outline-mode
       (progn
