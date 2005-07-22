09b0b7b3889c92ffe19d745e91c4ebb78e3c7fd1
Eli Zaretskii <eliz@gnu.org>
2005-07-22 09:54:45 +0000

    (dired-move-to-filename-regexp): Recognize the B suffix of the file size (as in "177B" for 177 bytes).  This happens with "ls -lh" on FreeBSD.

--- lisp/dired.el
+++ lisp/dired.el
@@ -1865,11 +1865,11 @@ DIR must be a directory name, not a file name."
 	 ;; The "[0-9]" below requires the previous column to end in a digit.
 	 ;; This avoids recognizing `1 may 1997' as a date in the line:
 	 ;; -r--r--r--   1 may      1997        1168 Oct 19 16:49 README
-	 ;; The "[kKMGTPEZY]?" below supports "ls -alh" output.
+	 ;; The "[BkKMGTPEZY]?" below supports "ls -alh" output.
 	 ;; The ".*" below finds the last match if there are multiple matches.
 	 ;; This avoids recognizing `jservice  10  1024' as a date in the line:
 	 ;; drwxr-xr-x  3 jservice  10  1024 Jul  2  1997 esg-host
-    (concat ".*[0-9][kKMGTPEZY]?" s
+    (concat ".*[0-9][BkKMGTPEZY]?" s
 	    "\\(" western "\\|" western-comma "\\|" japanese "\\|" iso "\\)"
 	    s "+"))
   "Regular expression to match up to the file name in a directory listing.
