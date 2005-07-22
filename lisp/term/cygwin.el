19292318e867a5c47b94591c43a3c2a751cc70d2
Dan Nicolaescu <dann@ics.uci.edu>
2005-07-22 17:11:04 +0000

    * term/rxvt.el (function-key-map): Use substitute-key-definition to bind {C,S,C-S}-{f1-f10}. Add a few missing key bindings.
    * term/cygwin.el: New file.

---
+++ lisp/term/cygwin.el
@@ -0,0 +1,6 @@
+;;; cygwin.el --- support for the Cygwin terminal -*- no-byte-compile: t -*-
+
+;;; The Cygwin terminal can't really display underlines.
+(tty-no-underline)
+
+;;; cygwin.el ends here
