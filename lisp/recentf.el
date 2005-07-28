8154a06e94b2c051a72655ee69f6cfd36aa9f528
Juanma Barranquero <lekktu@gmail.com>
2005-07-28 13:00:18 +0000

    (recentf-menu-append-commands-p): Declare with `define-obsolete-variable-alias'. (recentf-max-saved-items, recentf-menu-filter, recentf-arrange-by-rule-subfilter): Doc fixes. (recentf-menu-append-commands-flag, recentf-initialize-file-name-history, recentf-expand-file-name, recentf-clear-data): Fix typos in docstrings.

--- lisp/recentf.el
+++ lisp/recentf.el
@@ -65,7 +65,7 @@ You should define the options of your own filters in this group."
 
 (defcustom recentf-max-saved-items 20
   "*Maximum number of items of the recent list that will be saved.
-nil means to save the whole list.
+A nil value means to save the whole list.
 See the command `recentf-save-list'."
   :group 'recentf
   :type 'integer)
@@ -145,7 +145,7 @@ The default is to call `find-file' to edit the selected file."
 
 (defcustom recentf-menu-filter nil
   "*Function used to filter files displayed in the recentf menu.
-nil means no filter.  The following functions are predefined:
+A nil value means no filter.  The following functions are predefined:
 
 - `recentf-sort-ascending'
     Sort menu items in ascending order.
@@ -199,16 +199,14 @@ elements (see `recentf-make-menu-element' for menu element form)."
   :set 'recentf-menu-customization-changed)
 
 (defcustom recentf-menu-append-commands-flag t
-  "*non-nil means to append command items to the menu."
+  "*Non-nil means to append command items to the menu."
   :group 'recentf
   :type 'boolean
   :set 'recentf-menu-customization-changed)
 
-(defvaralias 'recentf-menu-append-commands-p
-  'recentf-menu-append-commands-flag)
-(make-obsolete-variable 'recentf-menu-append-commands-p
-                        'recentf-menu-append-commands-flag
-                        "22.1")
+(define-obsolete-variable-alias 'recentf-menu-append-commands-p
+                                'recentf-menu-append-commands-flag
+                                "22.1")
 
 (defcustom recentf-auto-cleanup 'mode
   "*Define when to automatically cleanup the recent list.
@@ -244,7 +242,7 @@ cleanup the list."
            (recentf-auto-cleanup))))
 
 (defcustom recentf-initialize-file-name-history t
-  "*non-nil means to initialize `file-name-history' with the recent list.
+  "*Non-nil means to initialize `file-name-history' with the recent list.
 If `file-name-history' is not empty, do nothing."
   :group 'recentf
   :type  'boolean)
@@ -350,8 +348,8 @@ filenames."
 (defsubst recentf-expand-file-name (name)
   "Convert filename NAME to absolute, and canonicalize it.
 See also the function `expand-file-name'.
-If defined, call the function `recentf-filename-handler' to post
-process the canonical name."
+If defined, call the function `recentf-filename-handler'
+to postprocess the canonical name."
   (let* ((filename (expand-file-name name)))
     (or (and recentf-filename-handler
              (funcall recentf-filename-handler filename))
@@ -557,7 +555,7 @@ menu-elements (no sub-menu)."
 
 (defun recentf-clear-data ()
   "Clear data used to build the recentf menu.
-This force a rebuild of the menu."
+This forces a rebuild of the menu."
   (easy-menu-remove-item (recentf-menu-bar)
                          recentf-menu-path recentf-menu-title)
   (setq recentf-data-cache nil))
@@ -703,7 +701,7 @@ defined."
 
 (defcustom recentf-arrange-by-rule-subfilter nil
   "*Function called by a rule based filter to filter sub-menu elements.
-nil means no filter.  See also `recentf-menu-filter'.
+A nil value means no filter.  See also `recentf-menu-filter'.
 You can't use another rule based filter here."
   :group 'recentf-filters
   :type '(choice (const nil) function)
