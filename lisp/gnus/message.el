97f78c9b71b22ef2a09db90f38512346652583fa
Miles Bader <miles@gnu.org>
2005-08-05 08:37:44 +0000

    Revision: miles@gnu.org--gnu-2005/emacs--cvs-trunk--0--patch-512
    Merge from gnus--rel--5.10
    
    Patches applied:
    
     * gnus--rel--5.10  (patch 101-102)
    
       - Update from CVS
    
    2005-08-04  Lars Magne Ingebrigtsen  <larsi@gnus.org>
    
       * lisp/gnus/gnus-art.el (article-unsplit-urls): Don't anchor urls to the
       start of the lines.
       (gnus-picon-databases): Add /usr/share/picons.
    
    2005-08-04  Reiner Steib  <Reiner.Steib@gmx.de>
    
       * lisp/gnus/gnus-art.el (gnus-button-valid-localpart-regexp): New variable
       taken from `gnus-button-mid-or-mail-regexp'.
       (gnus-button-mid-or-mail-regexp, gnus-button-alist): Use it.
       (gnus-button-alist): Improve regexp for domain part of the MIDs
       for news:localpart@domain buttons.
       (gnus-button-ctan-directory-regexp): Update.
    
       * lisp/gnus/message.el (message-kill-buffer): Raise the current frame.
       (message-bury): Use `window-dedicated-p'.
    
    2005-08-02  Katsumi Yamaoka  <yamaoka@jpl.org>
    
       * lisp/gnus/sieve-manage.el (sieve-manage-interactive-login): Use
       make-local-variable rather than make-variable-buffer-local.
       (sieve-manage-open): Ditto.
       (sieve-manage-authenticate): Ditto.

--- lisp/gnus/message.el
+++ lisp/gnus/message.el
@@ -3418,8 +3418,15 @@ Instead, just auto-save the buffer and then bury it."
 			  (file-exists-p auto-save-file-name))
 		     (and file-name
 			  (file-exists-p file-name)))
-	       (yes-or-no-p (format "Remove the backup file%s? "
-				    (if modified " too" ""))))
+		 (progn
+		   ;; If the message buffer has lived in a dedicated window,
+		   ;; `kill-buffer' has killed the frame.  Thus the
+		   ;; `yes-or-no-p' may show up in a lowered frame.  Make sure
+		   ;; that the user can see the question by raising the
+		   ;; current frame:
+		   (raise-frame)
+		   (yes-or-no-p (format "Remove the backup file%s? "
+					(if modified " too" "")))))
 	(ignore-errors
 	  (delete-file auto-save-file-name))
 	(let ((message-draft-article draft-article))
@@ -3430,8 +3437,7 @@ Instead, just auto-save the buffer and then bury it."
   "Bury this mail BUFFER."
   (let ((newbuf (other-buffer buffer)))
     (bury-buffer buffer)
-    (if (and (fboundp 'frame-parameters)
-	     (cdr (assq 'dedicated (frame-parameters)))
+    (if (and (window-dedicated-p (selected-window))
 	     (not (null (delq (selected-frame) (visible-frame-list)))))
 	(delete-frame (selected-frame))
       (switch-to-buffer newbuf))))
