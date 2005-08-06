00aa417213e1668d4a86514cf5febba3e241f399
Luc Teirlinck <teirllm@auburn.edu>
2005-08-06 21:59:02 +0000

    (set-fringe-indicators-1, fringe-indicators): Delete.

--- lisp/fringe.el
+++ lisp/fringe.el
@@ -260,43 +260,6 @@ SIDE must be the symbol `left' or `right'."
 	       0)
            (float (frame-char-width))))
 
-;; Fake defvar.  Real definition using defcustom is below.  The fake
-;; defvar is necessary because `fringe-indicators' and
-;; `set-fringe-indicators-1' mutually use each other.
-(defvar fringe-indicators)
-
-(defun set-fringe-indicators-1 (ignore value)
-  "Set fringe indicators according to VALUE.
-This is usually invoked when setting `fringe-indicators' via customize."
-  (setq fringe-indicators value)
-  (setq default-indicate-empty-lines nil)
-  (setq default-indicate-buffer-boundaries
-	(cond
-	 ((memq value '(left right t))
-	  value)
-	 ((eq value 'box)
-	  '((top . left) (bottom . right)))
-	 ((eq value 'mixed)
-	  '((top . left) (t . right)))
-	 ((eq value 'empty)
-	  (setq default-indicate-empty-lines t)
-	  nil)
-	 (t nil))))
-
-;;;###autoload
-(defcustom fringe-indicators nil
-  "Visually indicate buffer boundaries and scrolling.
-Setting this variable, changes `default-indicate-buffer-boundaries'."
-  :type '(choice (const :tag "No indicators" nil)
-		 (const :tag "On left" left)
-		 (const :tag "On right" right)
-		 (const :tag "Opposite, no arrows" box)
-		 (const :tag "Opposite, arrows right" mixed)
-		 (const :tag "Empty lines" empty))
-  :group 'fringe
-  :require 'fringe
-  :set 'set-fringe-indicators-1)
-
 (provide 'fringe)
 
 ;;; arch-tag: 6611ef60-0869-47ed-8b93-587ee7d3ff5d
