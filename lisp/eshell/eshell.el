4caf4844d9591fc723f0919eb7a834bd3e84e515
Richard M. Stallman <rms@gnu.org>
2005-08-09 11:37:32 +0000

    (eshell): Avoid warnings about eshell-mode.

--- lisp/eshell/eshell.el
+++ lisp/eshell/eshell.el
@@ -374,11 +374,10 @@ buffer selected (or created)."
     ;; `same-window-buffer-names', which is done when Eshell is loaded
     (assert (and buf (buffer-live-p buf)))
     (pop-to-buffer buf)
-    (unless (fboundp 'eshell-mode)
+    (if (fboundp 'eshell-mode)
+	(unless (eq major-mode 'eshell-mode)
+	  (eshell-mode))
       (error "`eshell-auto' must be loaded before Eshell can be used"))
-    (unless (eq major-mode 'eshell-mode)
-      (eshell-mode))
-    (assert (eq major-mode 'eshell-mode))
     buf))
 
 (defun eshell-return-exits-minibuffer ()
