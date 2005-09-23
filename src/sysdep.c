3480d92bb007bebdb644afbe667a02c00edbb334
Dan Nicolaescu <dann@ics.uci.edu>
2005-09-23 17:06:03 +0000

    * s/aix4-2.h (BROKEN_GET_CURRENT_DIR_NAME): Define BROKEN_GET_CURRENT_DIR_NAME.
    * sysdep.c (get_current_dir_name): Also define if
    BROKEN_GET_CURRENT_DIR_NAME.
    
    * m/ibmrs6000.h: Test for USG5, not USG5_4.

--- src/sysdep.c
+++ src/sysdep.c
@@ -256,7 +256,7 @@ void hft_reset ();
 SIGMASKTYPE sigprocmask_set;
 
 
-#ifndef HAVE_GET_CURRENT_DIR_NAME
+#if !defined (HAVE_GET_CURRENT_DIR_NAME) || defined (BROKEN_GET_CURRENT_DIR_NAME)
 
 /* Return the current working directory.  Returns NULL on errors.
    Any other returned value must be freed with free. This is used
