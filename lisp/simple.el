56ab610b006ae0e586ee7750abc174c5ada74348
Richard M. Stallman <rms@gnu.org>
2005-10-02 03:33:02 +0000

    (next-error-internal): New function.

--- lisp/simple.el
+++ lisp/simple.el
@@ -261,6 +261,14 @@ See variables `compilation-parse-errors-function' and
       (funcall next-error-function (prefix-numeric-value arg) reset)
       (run-hooks 'next-error-hook))))
 
+(defun next-error-internal ()
+  "Visit the source code corresponding to the `next-error' message at point."
+  (setq next-error-last-buffer (current-buffer))
+  ;; we know here that next-error-function is a valid symbol we can funcall
+  (with-current-buffer next-error-last-buffer
+    (funcall next-error-function 0 nil)
+    (run-hooks 'next-error-hook)))
+
 (defalias 'goto-next-locus 'next-error)
 (defalias 'next-match 'next-error)
 
