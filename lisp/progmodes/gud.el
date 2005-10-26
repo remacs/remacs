3cae51964d23e83cef93e6d124ee0b19d92b027b
Nick Roberts <nickrob@snap.net.nz>
2005-10-26 01:02:31 +0000

    (gud-menu-map): Only display gud-until icon when the fringe is not available.

--- lisp/progmodes/gud.el
+++ lisp/progmodes/gud.el
@@ -135,13 +135,16 @@ Used to grey out relevant togolbar icons.")
 			       (memq gud-minor-mode '(gdbmi gdba gdb dbx jdb))))
     ([until]	menu-item "Continue to selection" gud-until
                   :enable (and (not gud-running)
-			       (memq gud-minor-mode '(gdbmi gdba gdb perldb))))
+			       (memq gud-minor-mode '(gdbmi gdba gdb perldb)))
+		  :visible (not (and (memq gud-minor-mode '(gdbmi gdba))
+				     (> (car (window-fringes)) 0))))
     ([remove]	menu-item "Remove Breakpoint" gud-remove
                   :enable (not gud-running)
 		  :visible (not (and (memq gud-minor-mode '(gdbmi gdba))
 				     (> (car (window-fringes)) 0))))
     ([tbreak]	menu-item "Temporary Breakpoint" gud-tbreak
-		  :enable (memq gud-minor-mode '(gdbmi gdba gdb sdb xdb bashdb)))
+		  :enable (memq gud-minor-mode
+				'(gdbmi gdba gdb sdb xdb bashdb)))
     ([break]	menu-item "Set Breakpoint" gud-break
                   :enable (not gud-running)
 		  :visible (not (and (memq gud-minor-mode '(gdbmi gdba))
