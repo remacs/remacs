2d29381ccee860dd55218a9ddb847408ec07b09d
Romain Francoise <romain@orebokech.com>
2005-10-27 06:34:30 +0000

    Change Maintainer field.

--- lisp/dired-x.el
+++ lisp/dired-x.el
@@ -2,7 +2,7 @@
 
 ;; Author: Sebastian Kremer <sk@thp.uni-koeln.de>
 ;;	Lawrence R. Dodd <dodd@roebling.poly.edu>
-;; Maintainer: nobody (want to volunteer?)
+;; Maintainer: Romain Francoise <rfrancoise@gnu.org>
 ;; Version: 2.37+
 ;; Date: 1994/08/18 19:27:42
 ;; Keywords: dired extensions files
@@ -1718,5 +1718,5 @@ variables `dired-x-variable-list' in the message."
 ;; As Barry Warsaw would say: "This might be useful..."
 (provide 'dired-x)
 
-;;; arch-tag: 71a43ba2-7a00-4793-a028-0613dd7765ae
+;; arch-tag: 71a43ba2-7a00-4793-a028-0613dd7765ae
 ;;; dired-x.el ends here
