ebce92c797ce1416a4be85b97b3b09f69251f0b8
Romain Francoise <romain@orebokech.com>
2005-11-03 14:49:14 +0000

    Now part of GNU Emacs.  Update FSF's address.  Update copyright years.

--- lisp/net/eudcb-mab.el
+++ lisp/net/eudcb-mab.el
@@ -1,11 +1,11 @@
 ;;; eudcb-mab.el --- Emacs Unified Directory Client - AddressBook backend
 
-;; Copyright (C) 2003 John Wiegley.
+;; Copyright (C) 2003, 2004, 2005 John Wiegley.
 
 ;; Author: John Wiegley <johnw@newartisans.com>
 ;; Keywords: comm
 
-;; This file is NOT part of GNU Emacs.
+;; This file is part of GNU Emacs.
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
@@ -19,8 +19,8 @@
 
 ;; You should have received a copy of the GNU General Public License
 ;; along with GNU Emacs; see the file COPYING.  If not, write to the
-;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-;; Boston, MA 02111-1307, USA.
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
 
 ;;; Commentary:
 ;;    This library provides an interface to use the Mac's AddressBook,
