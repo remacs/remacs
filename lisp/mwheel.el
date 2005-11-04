bf03bdf3bf9a71d9df3c64f32f216eef6c7d0f07
YAMAMOTO Mitsuharu <mituharu@math.s.chiba-u.ac.jp>
2005-11-04 02:15:37 +0000

    (mouse-wheel-down-event, mouse-wheel-up-event): Undo previous change.

--- lisp/mwheel.el
+++ lisp/mwheel.el
@@ -59,7 +59,7 @@
                         'mouse-wheel-down-event)
 (defcustom mouse-wheel-down-event
   ;; In the latest versions of XEmacs, we could just use mouse-%s as well.
-  (if (memq system-type '(windows-nt macos))
+  (if (memq window-system '(w32 mac))
       'wheel-up
     (intern (format (if (featurep 'xemacs) "button%s" "mouse-%s")
 		    mouse-wheel-down-button)))
@@ -73,7 +73,7 @@
                         'mouse-wheel-up-event)
 (defcustom mouse-wheel-up-event
   ;; In the latest versions of XEmacs, we could just use mouse-%s as well.
-  (if (memq system-type '(windows-nt macos))
+  (if (memq window-system '(w32 mac))
       'wheel-down
     (intern (format (if (featurep 'xemacs) "button%s" "mouse-%s")
 		    mouse-wheel-up-button)))
