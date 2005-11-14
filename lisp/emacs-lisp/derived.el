156680e2dfb90f458c2b69024ba2c455d4ce5bb2
Luc Teirlinck <teirllm@auburn.edu>
2005-11-14 23:05:53 +0000

    (define-derived-mode): Remove defvar for mode hook.  (It conflicted with defcustoms for some mode hooks.)  Use the `variable-documentation' property to give the mode hook a docstring and expand that docstring.

--- lisp/emacs-lisp/derived.el
+++ lisp/emacs-lisp/derived.el
@@ -194,7 +194,12 @@ See Info node `(elisp)Derived Modes' for more details."
 		     parent child docstring syntax abbrev))
 
     `(progn
-       (defvar ,hook nil ,(format "Hook run when entering %s mode." name))
+       (unless (get  ',hook 'variable-documentation)
+	 (put ',hook 'variable-documentation
+	      ,(format "Hook run when entering %s mode.
+No problems result if this variable is not bound.
+`add-hook' automatically binds it.  (This is true for all hook variables.)"
+		       name)))
        (defvar ,map (make-sparse-keymap))
        ,(if declare-syntax
 	    `(defvar ,syntax (make-syntax-table)))
