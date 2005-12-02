b55ccdb828f6705efc1bfda58a371eafd3c0b803
Bill Wohler <wohler@newt.com>
2005-12-02 06:44:00 +0000

    Remove errant backslash at beginning of file.

--- lisp/mh-e/mh-utils.el
+++ lisp/mh-e/mh-utils.el
@@ -1,4 +1,4 @@
-\;;; mh-utils.el --- MH-E code needed for both sending and reading
+;;; mh-utils.el --- MH-E code needed for both sending and reading
 
 ;; Copyright (C) 1993, 1995, 1997,
 ;;  2000, 2001, 2002, 2003, 2004, 2005 Free Software Foundation, Inc.
