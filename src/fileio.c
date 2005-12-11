83c1cf6df60d815df782092b8bb8c6de0b8e775d
Richard M. Stallman <rms@gnu.org>
2005-12-11 05:08:18 +0000

    (Finsert_file_contents): Set Vdeactivate_mark when we change the buffer.

--- src/fileio.c
+++ src/fileio.c
@@ -4554,6 +4554,8 @@ actually used.  */)
 #endif
       Vdeactivate_mark = old_Vdeactivate_mark;
     }
+  else
+    Vdeactivate_mark = Qt;
 
   /* Make the text read part of the buffer.  */
   GAP_SIZE -= inserted;
