fccf278488785f482c7c50ac0713aa8f60dafbe9
Chong Yidong <cyd@stupidchicken.com>
2006-02-19 17:26:44 +0000

    * custom.el (customize-mark-as-set): Push to `user' theme.

--- lisp/custom.el
+++ lisp/custom.el
@@ -653,7 +653,6 @@ default value.  Otherwise, set it to nil.
 To actually save the value, call `custom-save-all'.
 
 Return non-nil iff the `saved-value' property actually changed."
-  (custom-load-symbol symbol)
   (let* ((get (or (get symbol 'custom-get) 'default-value))
 	 (value (funcall get symbol))
 	 (saved (get symbol 'saved-value))
@@ -691,7 +690,9 @@ Return non-nil iff the `customized-value' property actually changed."
 	    (not (equal value (condition-case nil
 				  (eval (car old))
 				(error nil)))))
-	(put symbol 'customized-value (list (custom-quote value)))
+	(progn (put symbol 'customized-value (list (custom-quote value)))
+	       (custom-push-theme 'theme-value symbol 'user 'set
+				  (custom-quote value)))
       (put symbol 'customized-value nil))
     ;; Changed?
     (not (equal customized (get symbol 'customized-value)))))
