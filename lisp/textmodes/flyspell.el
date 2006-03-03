ee65a1327b4bcca71e79a4de0505b4169d8b06dd
Eli Zaretskii <eliz@gnu.org>
2006-03-03 11:49:22 +0000

    (flyspell-external-point-words): Be case-sensitive.

--- lisp/textmodes/flyspell.el
+++ lisp/textmodes/flyspell.el
@@ -1322,7 +1322,8 @@ The list of incorrect words should be in `flyspell-external-ispell-buffer'.
 The buffer to mark them in is `flyspell-large-region-buffer'."
   (let (words-not-found
 	(ispell-otherchars (ispell-get-otherchars))
-	(buffer-scan-pos flyspell-large-region-beg))
+	(buffer-scan-pos flyspell-large-region-beg)
+	(case-fold-search nil))
     (with-current-buffer flyspell-external-ispell-buffer
       (goto-char (point-min))
       ;; Loop over incorrect words, in the order they were reported,
