c60168d2e1f2fbd71bb77a9f04228c3bee268798
Eli Zaretskii <eliz@gnu.org>
2006-03-03 12:12:06 +0000

    (clean-buffer-list): Handle case where base-buffer of indirect buffer gets killed before indirect buffer.  Use dolist.

--- lisp/midnight.el
+++ lisp/midnight.el
@@ -179,20 +179,21 @@ displayed (can be nil if the buffer was never displayed) and its
 lifetime, i.e., its \"age\" when it will be purged."
   (interactive)
   (let ((tm (float-time)) bts (ts (format-time-string "%Y-%m-%d %T"))
-        (bufs (buffer-list)) buf delay cbld bn)
-    (while (setq buf (pop bufs))
-      (setq bts (midnight-buffer-display-time buf) bn (buffer-name buf)
-            delay (if bts (- tm bts) 0) cbld (clean-buffer-list-delay bn))
-      (message "[%s] `%s' [%s %d]" ts bn (if bts (round delay)) cbld)
-      (unless (or (midnight-find bn clean-buffer-list-kill-never-regexps
-                                 'string-match)
-                  (midnight-find bn clean-buffer-list-kill-never-buffer-names
-                                 'string-equal)
-                  (get-buffer-process buf)
-                  (and (buffer-file-name buf) (buffer-modified-p buf))
-                  (get-buffer-window buf 'visible) (< delay cbld))
-        (message "[%s] killing `%s'" ts bn)
-        (kill-buffer buf)))))
+        delay cbld bn)
+    (dolist (buf (buffer-list))
+      (when (buffer-live-p buf)
+	(setq bts (midnight-buffer-display-time buf) bn (buffer-name buf)
+	      delay (if bts (- tm bts) 0) cbld (clean-buffer-list-delay bn))
+	(message "[%s] `%s' [%s %d]" ts bn (if bts (round delay)) cbld)
+	(unless (or (midnight-find bn clean-buffer-list-kill-never-regexps
+				   'string-match)
+		    (midnight-find bn clean-buffer-list-kill-never-buffer-names
+				   'string-equal)
+		    (get-buffer-process buf)
+		    (and (buffer-file-name buf) (buffer-modified-p buf))
+		    (get-buffer-window buf 'visible) (< delay cbld))
+	  (message "[%s] killing `%s'" ts bn)
+	  (kill-buffer buf))))))
 
 ;;; midnight hook
 
