95c1c9018edb9797c0f5d00574d7fc64d1d53824
Kenichi Handa <handa@m17n.org>
2006-03-20 06:26:23 +0000

    (Fwrite_region): Set visit_file to Qnil before GCPRO it.

--- src/fileio.c
+++ src/fileio.c
@@ -5009,6 +5009,7 @@ This does code conversion according to the value of
   if (!NILP (start) && !STRINGP (start))
     validate_region (&start, &end);
 
+  visit_file = Qnil;
   GCPRO5 (start, filename, visit, visit_file, lockname);
 
   filename = Fexpand_file_name (filename, Qnil);
