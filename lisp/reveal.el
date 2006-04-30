cc1eecfd888839f6d75c0c7aa5d1e8e37f31d1c7
Stefan Monnier <monnier@iro.umontreal.ca>
2006-04-30 15:49:08 +0000

    Update my email address.

--- lisp/reveal.el
+++ lisp/reveal.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2000, 2001, 2002, 2003, 2004,
 ;;   2005, 2006 Free Software Foundation, Inc.
 
-;; Author: Stefan Monnier <monnier@cs.yale.edu>
+;; Author: Stefan Monnier <monnier@iro.umontreal.ca>
 ;; Keywords: outlines
 
 ;; This file is part of GNU Emacs.
