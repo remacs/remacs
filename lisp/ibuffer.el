c7f5c0a82be7ee473882924b733fdd80db4f8f7c
Dan Nicolaescu <dann@ics.uci.edu>
2006-05-05 16:19:56 +0000

    (ibuffer-compressed-file-name-regexp): Undo previous change.

--- lisp/ibuffer.el
+++ lisp/ibuffer.el
@@ -324,14 +324,13 @@ directory, like `default-directory'."
   :type '(repeat function)
   :group 'ibuffer)
 
-(eval-when-compile 
 (defcustom ibuffer-compressed-file-name-regexp
     (concat "\\.\\("
 	    (regexp-opt '("arj" "bgz" "bz2" "gz" "lzh" "taz" "tgz" "zip" "z"))
 	    "\\)$")
   "Regexp to match compressed file names."
   :type 'regexp
-  :group 'ibuffer))
+  :group 'ibuffer)
 
 (defcustom ibuffer-hook nil
   "Hook run when `ibuffer' is called."
