b9e590fc9573a874e26203ddad566325b97dc2c3
Richard M. Stallman <rms@gnu.org>
2006-07-17 21:00:08 +0000

    Fix previous change.

--- src/dispnew.c
+++ src/dispnew.c
@@ -6828,11 +6828,11 @@ For types not defined in VMS, use  define emacs_term \"TYPE\".\n\
 #else /* not VMS */
 
 #ifdef HAVE_WINDOW_SYSTEM
-      if (inhibit_window_system)
-	fprintf (stderr, "Please set the environment variable TERM; see `tset'.\n");
+      if (! inhibit_window_system)
+	fprintf (stderr, "Please set the environment variable DISPLAY or TERM (see `tset').\n");
       else
 #endif /* HAVE_WINDOW_SYSTEM */
-	fprintf (stderr, "Please set the environment variable DISPLAY or TERM (see `tset').\n");
+	fprintf (stderr, "Please set the environment variable TERM; see `tset'.\n");
 #endif /* not VMS */
       exit (1);
     }
