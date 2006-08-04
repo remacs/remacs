6f64cebf164a4a613f445aa32e70e95ba6a50d45
Kenichi Handa <handa@m17n.org>
2006-08-04 02:50:10 +0000

    (w32_query_font): Compare names by ignoring case.

--- src/w32fns.c
+++ src/w32fns.c
@@ -6207,7 +6207,7 @@ w32_query_font (struct frame *f, char *fontname)
 
   for (i = 0; i < one_w32_display_info.n_fonts ;i++, pfi++)
     {
-      if (strcmp(pfi->name, fontname) == 0) return pfi;
+      if (strcasecmp(pfi->name, fontname) == 0) return pfi;
     }
 
   return NULL;
