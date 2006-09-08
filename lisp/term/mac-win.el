dbcdba77e6d0b3ce23ceeba55ac6a1b3e25e3898
YAMAMOTO Mitsuharu <mituharu@math.s.chiba-u.ac.jp>
2006-09-08 08:17:21 +0000

    (show-hide-font-panel): New HI command ID symbol. (mac-apple-event-map): Define its handler.

--- lisp/term/mac-win.el
+++ lisp/term/mac-win.el
@@ -1533,19 +1533,20 @@ in `selection-converter-alist', which see."
 
 ;;; Event IDs
 ;; kCoreEventClass
-(put 'open-application   'mac-apple-event-id "oapp") ; kAEOpenApplication
-(put 'reopen-application 'mac-apple-event-id "rapp") ; kAEReopenApplication
-(put 'open-documents     'mac-apple-event-id "odoc") ; kAEOpenDocuments
-(put 'print-documents    'mac-apple-event-id "pdoc") ; kAEPrintDocuments
-(put 'open-contents      'mac-apple-event-id "ocon") ; kAEOpenContents
-(put 'quit-application   'mac-apple-event-id "quit") ; kAEQuitApplication
-(put 'application-died   'mac-apple-event-id "obit") ; kAEApplicationDied
-(put 'show-preferences   'mac-apple-event-id "pref") ; kAEShowPreferences
-(put 'autosave-now       'mac-apple-event-id "asav") ; kAEAutosaveNow
+(put 'open-application     'mac-apple-event-id "oapp") ; kAEOpenApplication
+(put 'reopen-application   'mac-apple-event-id "rapp") ; kAEReopenApplication
+(put 'open-documents       'mac-apple-event-id "odoc") ; kAEOpenDocuments
+(put 'print-documents      'mac-apple-event-id "pdoc") ; kAEPrintDocuments
+(put 'open-contents        'mac-apple-event-id "ocon") ; kAEOpenContents
+(put 'quit-application     'mac-apple-event-id "quit") ; kAEQuitApplication
+(put 'application-died     'mac-apple-event-id "obit") ; kAEApplicationDied
+(put 'show-preferences     'mac-apple-event-id "pref") ; kAEShowPreferences
+(put 'autosave-now         'mac-apple-event-id "asav") ; kAEAutosaveNow
 ;; kAEInternetEventClass
-(put 'get-url            'mac-apple-event-id "GURL") ; kAEGetURL
+(put 'get-url              'mac-apple-event-id "GURL") ; kAEGetURL
 ;; Converted HI command events
-(put 'about              'mac-apple-event-id "abou") ; kHICommandAbout
+(put 'about                'mac-apple-event-id "abou") ; kHICommandAbout
+(put 'show-hide-font-panel 'mac-apple-event-id "shfp") ; kHICommandShowHideFontPanel
 
 (defmacro mac-event-spec (event)
   `(nth 1 ,event))
@@ -1801,6 +1802,8 @@ With numeric ARG, display the font panel if and only if ARG is positive."
   'mac-handle-font-panel-closed)
 ;; kEventClassFont/kEventFontSelection
 (define-key mac-apple-event-map [font selection] 'mac-handle-font-selection)
+(define-key mac-apple-event-map [hi-command show-hide-font-panel]
+  'mac-font-panel-mode)
 
 (define-key-after menu-bar-showhide-menu [mac-font-panel-mode]
   (menu-bar-make-mm-toggle mac-font-panel-mode
