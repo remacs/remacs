d75a48427cc8728476a493fa44594bb598aaec40
Kim F. Storm <storm@cua.dk>
2006-09-10 00:10:46 +0000

    (get_window_cursor_type): Use hollow cursor on non-transparent images.

--- src/xdisp.c
+++ src/xdisp.c
@@ -21248,14 +21248,16 @@ get_window_cursor_type (w, glyph, width, active_cursor)
 	  if (cursor_type == FILLED_BOX_CURSOR)
 	    {
 	      /* Using a block cursor on large images can be very annoying.
-		 So use a hollow cursor for "large" images.  */
+		 So use a hollow cursor for "large" images.
+		 If image is not transparent (no mask), also use hollow cursor.  */
 	      struct image *img = IMAGE_FROM_ID (f, glyph->u.img_id);
 	      if (img != NULL && IMAGEP (img->spec))
 		{
 		  /* Arbitrarily, interpret "Large" as >32x32 and >NxN
 		     where N = size of default frame font size.
 		     This should cover most of the "tiny" icons people may use.  */
-		  if (img->width > max (32, WINDOW_FRAME_COLUMN_WIDTH (w))
+		  if (!img->mask
+		      || img->width > max (32, WINDOW_FRAME_COLUMN_WIDTH (w))
 		      || img->height > max (32, WINDOW_FRAME_LINE_HEIGHT (w)))
 		    cursor_type = HOLLOW_BOX_CURSOR;
 		}
