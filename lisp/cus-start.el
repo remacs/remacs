b1707a95e42352f260c266343f8ec264d343dd71
Jan Djärv <jan.h.d@swipnet.se>
2006-09-16 17:41:59 +0000

    * cus-start.el (all): Add x-gtk-show-chooser-help-text.

--- lisp/cus-start.el
+++ lisp/cus-start.el
@@ -373,6 +373,7 @@ since it could result in memory overflow and make Emacs crash."
 				 (repeat (directory :format "%v")))
 	     (x-use-old-gtk-file-dialog menu boolean "22.1")
 	     (x-gtk-show-hidden-files menu boolean "22.1")
+	     (x-gtk-show-chooser-help-text menu boolean "22.1")
 	     (x-gtk-whole-detached-tool-bar x boolean "22.1")
 	     ;; xterm.c
 	     (x-use-underline-position-properties display boolean "22.1")
