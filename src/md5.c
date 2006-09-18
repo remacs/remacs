423a1f3c110b90b72e6cedeb9572300c3172c5b1
Jay Belanger <jay.p.belanger@gmail.com>
2006-09-18 03:32:42 +0000

    Replace "GNU Library General Public License" by "GNU General Public License" throughout.

--- src/md5.c
+++ src/md5.c
@@ -5,17 +5,17 @@
    This file is part of the GNU Emacs.
 
    The GNU C Library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public License as
+   modify it under the terms of the GNU General Public License as
    published by the Free Software Foundation; either version 2 of the
    License, or (at your option) any later version.
 
    The GNU C Library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
+   General Public License for more details.
 
-   You should have received a copy of the GNU Library General Public
-   License along with the GNU C Library; see the file COPYING.LIB.  If not,
+   You should have received a copy of the GNU General Public
+   License along with the GNU C Library; see the file COPYING.  If not,
    write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
    Boston, MA 02110-1301, USA.  */
 
