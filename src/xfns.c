159f8da7428e89714741d4e5789c250ad67a7487
Jan Djärv <jan.h.d@swipnet.se>
2006-09-18 16:48:05 +0000

    * xfns.c (syms_of_xfns): Rename x_gtk_show_chooser_help_text to  x_gtk_file_dialog_help_text.

--- src/xfns.c
+++ src/xfns.c
@@ -160,7 +160,7 @@ int x_gtk_show_hidden_files;
 
 /* If non-zero, don't show additional help text in the GTK file chooser.  */
 
-int x_gtk_show_chooser_help_text;
+int x_gtk_file_dialog_help_text;
 
 /* If non-zero, don't collapse to tool bar when it is detached.  */
 
@@ -5818,11 +5818,11 @@ Note that this is just the default, there is a toggle button on the file
 chooser to show or not show hidden files on a case by case basis.  */);
   x_gtk_show_hidden_files = 0;
 
-  DEFVAR_BOOL ("x-gtk-show-chooser-help-text", &x_gtk_show_chooser_help_text,
+  DEFVAR_BOOL ("x-gtk-file-dialog-help-text", &x_gtk_file_dialog_help_text,
     doc: /* *If non-nil, the GTK file chooser will by show additional help text.
 If more space for files in the file chooser dialog is wanted, set this to nil
 to turn the additional text off.  */);
-  x_gtk_show_chooser_help_text = 1;
+  x_gtk_file_dialog_help_text = 1;
 
   DEFVAR_BOOL ("x-gtk-whole-detached-tool-bar", &x_gtk_whole_detached_tool_bar,
     doc: /* *If non-nil, a detached tool bar is shown in full.
