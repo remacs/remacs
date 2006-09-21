62a714fc453915ae9f7ffe2c718970e0c893851b
Kenichi Handa <handa@m17n.org>
2006-09-21 12:18:21 +0000

    (dnd-get-local-file-name): Give a unibyte string to replace-regexp-in-string as replacement.

--- lisp/dnd.el
+++ lisp/dnd.el
@@ -148,7 +148,9 @@ Return nil if URI is not a local file."
       (setq f (replace-regexp-in-string
 	       "%[A-Z0-9][A-Z0-9]"
 	       (lambda (arg)
-		 (format "%c" (string-to-number (substring arg 1) 16)))
+		 (let ((str (make-string 1 0)))
+		   (aset str 0 (string-to-number (substring arg 1) 16))
+		   str))
 	       f nil t))
       (let* ((decoded-f (decode-coding-string
 			 f
