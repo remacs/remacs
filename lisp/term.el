503bc651bd1d9cfa1079023df5249ee219dc44a0
Richard M. Stallman <rms@gnu.org>
2006-11-28 02:21:28 +0000

    (term-exec-1): Set envvar INSIDE_EMACS.

--- lisp/term.el
+++ lisp/term.el
@@ -1422,8 +1422,12 @@ The main purpose is to get rid of the local keymap."
 	   (format "TERMINFO=%s" data-directory)
 	   (format term-termcap-format "TERMCAP="
 		   term-term-name term-height term-width)
-	   ;; Breaks `./configure' of w3 and url which try to run $EMACS.
+	   ;; We are going to get rid of the binding for EMACS,
+	   ;; probably in Emacs 23, because it breaks
+	   ;; `./configure' of some packages that expect it to
+	   ;; say where to find EMACS.
 	   (format "EMACS=%s (term:%s)" emacs-version term-protocol-version)
+	   (format "INSIDE_EMACS=%s,term:%s" emacs-version term-protocol-version)
 	   (format "LINES=%d" term-height)
 	   (format "COLUMNS=%d" term-width))
 	  process-environment))
