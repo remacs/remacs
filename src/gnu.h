40e1b9663fa8af04c1927d8444cc3a4f7a8d5a68
Glenn Morris <rgm@gnu.org>
2007-02-15 03:07:00 +0000

    Add copyright and license info.

--- src/gnu.h
+++ src/gnu.h
@@ -1,3 +1,32 @@
+/* XPM */
+/* Emacs "E" icon
+
+Copyright (C) 2005, 2006, 2007  Free Software Foundation, Inc.
+
+Author: Andrew Zhilin <andrew_zhilin@yahoo.com>
+
+This file is part of GNU Emacs.
+
+GNU Emacs is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU Emacs is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU Emacs; see the file COPYING.  If not, write to
+the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+Boston, MA 02110-1301, USA.
+
+Commentary:
+
+This is a version of one of the GNU Emacs icon set posted to
+emacs-devel in October 2005 by Andrew Zhilin.
+*/
 #if defined (HAVE_XPM) && defined (HAVE_X_WINDOWS)
 static char * gnu_xpm_bits[] = {
 "32 32 173 2",
