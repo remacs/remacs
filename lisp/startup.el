2863a9be6a14eefe1f7549b99932684bdb8f73e5
Dan Nicolaescu <dann@ics.uci.edu>
2007-02-24 18:29:19 +0000

    (command-line): Also check if the abbrev file is readable.

--- lisp/startup.el
+++ lisp/startup.el
@@ -952,8 +952,12 @@ or `CVS', and any subdirectory that contains a file named `.nosearch'."
 	    (with-current-buffer (window-buffer)
 	      (deactivate-mark)))
 
-	;; If the user has a file of abbrevs, read it.
-	(if (file-exists-p abbrev-file-name)
+	;; If the user has a file of abbrevs, read it.  
+        ;; FIXME: after the 22.0 release this should be changed so
+	;; that it does not read the abbrev file when -batch is used
+	;; on the command line.
+	(when (and (file-exists-p abbrev-file-name) 
+		   (file-readable-p abbrev-file-name))
 	    (quietly-read-abbrev-file abbrev-file-name))
 
 	;; If the abbrevs came entirely from the init file or the
