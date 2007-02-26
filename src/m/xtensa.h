94218e27a558fb071f2c8d6b8e20e7b8eb27e4d6
Dan Nicolaescu <dann@ics.uci.edu>
2007-02-26 05:40:11 +0000

    * configure.in (xtensa-*-linux-gnu*): New configuration.
    * configure: Regenerate.
    
    * m/xtensa.h: New file.

---
+++ src/m/xtensa.h
@@ -0,0 +1,28 @@
+/* Machine description file for Tensilica Xtensa.
+   Copyright (C) 2007 Free Software Foundation, Inc.
+
+This file is part of GNU Emacs.
+
+GNU Emacs is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 1, or (at your option)
+any later version.
+
+GNU Emacs is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU Emacs; see the file COPYING.  If not, write to
+the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+Boston, MA 02110-1301, USA.  */
+
+#define NO_ARG_ARRAY
+#define NO_UNION_TYPE
+
+#ifdef __LITTLE_ENDIAN
+#undef WORDS_BIG_ENDIAN
+#else
+#define WORDS_BIG_ENDIAN
+#endif
