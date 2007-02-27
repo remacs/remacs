a574ff578ec8629f347408095bba69ba9a0fcb6d
Glenn Morris <rgm@gnu.org>
2007-02-27 03:07:38 +0000

    Remove FSF copyright since file does not differ significantly from original lwlib version.

--- lwlib/lwlib-Xm.h
+++ lwlib/lwlib-Xm.h
@@ -1,7 +1,5 @@
 /* The lwlib interface to Motif widgets.
-   Copyright (C) 1994, 2000, 2001, 2002, 2003, 2004,
-                 2005, 2006, 2007  Free Software Foundation, Inc.
-   Copyright (C) 1992 Lucid, Inc.
+Copyright (C) 1992 Lucid, Inc.
 
 This file is part of the Lucid Widget Library.
 
