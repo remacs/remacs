2cd3138f59fe1862b30fa548ed6bb19e04d99834
Glenn Morris <rgm@gnu.org>
2007-02-27 03:04:22 +0000

    Remove FSF copyright since file does not differ significantly from X11 version.

--- oldXMenu/XDestAssoc.c
+++ oldXMenu/XDestAssoc.c
@@ -1,6 +1,4 @@
 /* Copyright    Massachusetts Institute of Technology    1985	*/
-/* Copyright (C) 2001, 2002, 2003, 2004, 2005,
-                 2006, 2007 Free Software Foundation, Inc.  */
 
 #include "copyright.h"
 
