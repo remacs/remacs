9437b9bdbd294d47fda37e83af29f7d5f0d71d78
Carsten Dominik <dominik@science.uva.nl>
2007-02-27 07:28:48 +0000

    *** empty log message ***

--- lisp/textmodes/org.el
+++ lisp/textmodes/org.el
@@ -5,7 +5,7 @@
 ;; Author: Carsten Dominik <dominik at science dot uva dot nl>
 ;; Keywords: outlines, hypermedia, calendar, wp
 ;; Homepage: http://www.astro.uva.nl/~dominik/Tools/org/
-;; Version: 4.67
+;; Version: 4.67a
 ;;
 ;; This file is part of GNU Emacs.
 ;;
