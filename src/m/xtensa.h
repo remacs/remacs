5b5ff690a6d6d7ec9bf0634e572fe3025593f1eb
Glenn Morris <rgm@gnu.org>
2007-02-28 03:30:55 +0000

    No need for a license in such a trivial file (also, should have been GPLv2 or later, not v1).

--- src/m/xtensa.h
+++ src/m/xtensa.h
@@ -1,22 +1,6 @@
 /* Machine description file for Tensilica Xtensa.
-   Copyright (C) 2007 Free Software Foundation, Inc.
 
-This file is part of GNU Emacs.
-
-GNU Emacs is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 1, or (at your option)
-any later version.
-
-GNU Emacs is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with GNU Emacs; see the file COPYING.  If not, write to
-the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-Boston, MA 02110-1301, USA.  */
+Add a license notice if this grows to > 10 lines of code.  */
 
 #define NO_ARG_ARRAY
 #define NO_UNION_TYPE
