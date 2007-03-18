c601a8ed61f7f9e5dc2f2434dc7c391440ae4ac3
YAMAMOTO Mitsuharu <mituharu@math.s.chiba-u.ac.jp>
2007-03-18 08:07:17 +0000

    (redisplay_internal, note_mouse_highlight): Check popup_activated for MAC_OS.

--- src/xdisp.c
+++ src/xdisp.c
@@ -10872,7 +10872,7 @@ redisplay_internal (preserve_echo_area)
 	return;
     }
 
-#if defined (USE_X_TOOLKIT) || defined (USE_GTK)
+#if defined (USE_X_TOOLKIT) || defined (USE_GTK) || defined (MAC_OS)
   if (popup_activated ())
     return;
 #endif
@@ -22672,7 +22672,7 @@ note_mouse_highlight (f, x, y)
   struct buffer *b;
 
   /* When a menu is active, don't highlight because this looks odd.  */
-#if defined (USE_X_TOOLKIT) || defined (USE_GTK)
+#if defined (USE_X_TOOLKIT) || defined (USE_GTK) || defined (MAC_OS)
   if (popup_activated ())
     return;
 #endif
