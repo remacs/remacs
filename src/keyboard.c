fcd69a29888e9e578169961dae3eee350616fd1d
Juanma Barranquero <lekktu@gmail.com>
2007-06-08 20:11:04 +0000

    (cmd_error_internal): Use empty_unibyte_string.

--- src/keyboard.c
+++ src/keyboard.c
@@ -1249,7 +1249,7 @@ cmd_error_internal (data, context)
   /* Use user's specified output function if any.  */
   if (!NILP (Vcommand_error_function))
     call3 (Vcommand_error_function, data,
-	   build_string (context ? context : ""),
+	   context ? build_string (context) : empty_unibyte_string,
 	   Vsignaling_function);
   /* If the window system or terminal frame hasn't been initialized
      yet, or we're not interactive, write the message to stderr and exit.  */
