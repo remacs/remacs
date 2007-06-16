8626551860829c877899e232e4281334900f4d89
Karl Fogel <kfogel@red-bean.com>
2007-06-16 01:25:25 +0000

    * thingatpt.el (thing-at-point-email-regexp): Don't require two chars   before the "@" in an email address.  Andreas Roehler noticed this problem.

--- lisp/thingatpt.el
+++ lisp/thingatpt.el
@@ -342,7 +342,7 @@ point."
 
 ;;   Email addresses
 (defvar thing-at-point-email-regexp
-  "<?[-+_.~a-zA-Z][-+_.~:a-zA-Z0-9]+@[-.a-zA-Z0-9]+>?"
+  "<?[-+_.~a-zA-Z][-+_.~:a-zA-Z0-9]*@[-.a-zA-Z0-9]+>?"
   "A regular expression probably matching an email address.
 This does not match the real name portion, only the address, optionally
 with angle brackets.")
