862b60047ea0ae7ad8a6769259da93ee536026a2
Glenn Morris <rgm@gnu.org>
2007-07-25 07:33:54 +0000

    Switch license to GPLv3 or later.

--- oldXMenu/Create.c
+++ oldXMenu/Create.c
@@ -8,7 +8,7 @@ Copyright (C) 1993, 1994, 2001, 2002, 2003, 2004, 2005, 2006,
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2, or (at your option)
+the Free Software Foundation; either version 3, or (at your option)
 any later version.
 
 This program is distributed in the hope that it will be useful,
