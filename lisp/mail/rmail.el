11bdb42c267a5eba63edabba9f82fa2a76481002
Glenn Morris <rgm@gnu.org>
2007-08-22 03:37:21 +0000

    (rmail-movemail-variant-p): Call on load to set movemail related variables. (rmail-insert-inbox-text): Use only rmail-movemail-program, which will now be set before this is called.

--- lisp/mail/rmail.el
+++ lisp/mail/rmail.el
@@ -239,6 +239,12 @@ Currently known variants are 'emacs and 'mailutils."
     (setq rmail-movemail-variant-in-use (rmail-autodetect)))
   (not (null (member rmail-movemail-variant-in-use variants))))
 
+;; Call for effect, to set rmail-movemail-program (if not set by the
+;; user), and rmail-movemail-variant-in-use. Used by various functions.
+;; I'm not sure if M-x rmail is the only entry point to this package.
+;; If so, this can be moved there.
+(rmail-movemail-variant-p)
+
 ;;;###autoload
 (defcustom rmail-dont-reply-to-names nil "\
 *A regexp specifying addresses to prune from a reply message.
@@ -1818,10 +1824,7 @@ is non-nil if the user has supplied the password interactively.
 		 (buffer-disable-undo errors)
 		 (let ((args
 			(append
-			 (list (or rmail-movemail-program
-				   (expand-file-name "movemail"
-						     exec-directory))
-			       nil errors nil)
+			 (list rmail-movemail-program nil errors nil)
 			 (if rmail-preserve-inbox
 			     (list "-p")
 			   nil)
