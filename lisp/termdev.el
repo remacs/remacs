a4fc56e1ba738cb04f6ddc7dbaf2fb1a4dd21895
Glenn Morris <rgm@gnu.org>
2007-08-29 07:33:33 +0000

    Update copyright years, update license to GPLv3 or later.

--- lisp/termdev.el
+++ lisp/termdev.el
@@ -1,6 +1,6 @@
 ;;; termdev.el --- functions for dealing with terminals
 
-;; Copyright (C) 2005 Free Software Foundation, Inc.
+;; Copyright (C) 2005, 2006, 2007 Free Software Foundation, Inc.
 
 ;; Author: Karoly Lorentey <karoly@lorentey.hu>
 ;; Created: 2005-12-22
@@ -10,7 +10,7 @@
 
 ;; GNU Emacs is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 2, or (at your option)
+;; the Free Software Foundation; either version 3, or (at your option)
 ;; any later version.
 
 ;; GNU Emacs is distributed in the hope that it will be useful,
