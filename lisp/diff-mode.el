80a01d975acd7aaaa420458c057f29e46a8dbc8e
Stefan Monnier <monnier@iro.umontreal.ca>
2007-09-12 05:11:07 +0000

    (diff-sanity-check-hunk): Fix up the case when unified diffs are concatenated with no intervening line.

--- lisp/diff-mode.el
+++ lisp/diff-mode.el
@@ -1181,7 +1181,16 @@ Only works for unified diffs."
             (while
                 (case (char-after)
                   (?\s (decf before) (decf after) t)
-                  (?- (decf before) t)
+                  (?-
+                   (if (and (looking-at diff-file-header-re)
+                            (zerop before) (zerop after))
+                       ;; No need to query: this is a case where two patches
+                       ;; are concatenated and only counting the lines will
+                       ;; give the right result.  Let's just add an empty
+                       ;; line so that our code which doesn't count lines
+                       ;; will not get confused.
+                       (progn (save-excursion (insert "\n")) nil)
+                     (decf before) t))
                   (?+ (decf after) t)
                   (t
                    (cond
