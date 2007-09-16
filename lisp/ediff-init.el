9d2fec03a1ae3a000e02036093c8f4da6a0c1a6d
Stefan Monnier <monnier@iro.umontreal.ca>
2007-09-16 22:15:22 +0000

    (ediff-verbose-p): This var is not a constant.

--- lisp/ediff-init.el
+++ lisp/ediff-init.el
@@ -1406,7 +1406,7 @@ This property can be toggled interactively."
 ;;; Misc
 
 ;; if nil, this silences some messages
-(defconst ediff-verbose-p t)
+(defvar ediff-verbose-p t)
 
 (defcustom ediff-autostore-merges  'group-jobs-only
   "*Save the results of merge jobs automatically.
