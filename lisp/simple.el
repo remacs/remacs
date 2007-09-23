b66eb11bece1d0922972fb4dcf45b834ee354293
Richard M. Stallman <rms@gnu.org>
2007-09-23 15:22:47 +0000

    (copy-region-as-kill): Doc fix.

--- lisp/simple.el
+++ lisp/simple.el
@@ -2745,7 +2745,9 @@ text.  See `insert-for-yank'."
   "Save the region as if killed, but don't kill it.
 In Transient Mark mode, deactivate the mark.
 If `interprogram-cut-function' is non-nil, also save the text for a window
-system cut and paste."
+system cut and paste.
+
+This command's old key binding has been given to `kill-ring-save'."
   (interactive "r")
   (if (eq last-command 'kill-region)
       (kill-append (filter-buffer-substring beg end) (< end beg))
