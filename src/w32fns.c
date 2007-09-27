60860eb36d45e00509fd97c167ee4e9fbb5b7ac6
Jason Rumney <jasonr@gnu.org>
2007-09-27 22:10:53 +0000

    (globals_of_w32fns): Init common controls.

--- src/w32fns.c
+++ src/w32fns.c
@@ -48,6 +48,7 @@ Boston, MA 02110-1301, USA.  */
 
 #include "bitmaps/gray.xbm"
 
+#include <commctrl.h>
 #include <commdlg.h>
 #include <shellapi.h>
 #include <ctype.h>
@@ -9073,6 +9074,9 @@ void globals_of_w32fns ()
 	      &w32_ansi_code_page,
 	      doc: /* The ANSI code page used by the system.  */);
   w32_ansi_code_page = GetACP ();
+
+  /* MessageBox does not work without this when linked to comctl32.dll 6.0.  */
+  InitCommonControls ();
 }
 
 #undef abort
