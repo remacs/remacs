88852d458258f2ccdbc2e4b2733329d5dc916335
Juanma Barranquero <lekktu@gmail.com>
2007-10-01 09:57:19 +0000

    (Fload): Fix typo in docstring.

--- src/lread.c
+++ src/lread.c
@@ -502,7 +502,7 @@ read_filtered_event (no_switch_frame, ascii_required, error_nonascii,
 
 /* Read until we get an acceptable event.  */
  retry:
-  do 
+  do
     val = read_char (0, 0, 0, (input_method ? Qnil : Qt), 0,
 		     NUMBERP (seconds) ? &end_time : NULL);
   while (INTEGERP (val) && XINT (val) == -2); /* wrong_kboard_jmpbuf */
@@ -645,7 +645,7 @@ DEFUN ("get-file-char", Fget_file_char, Sget_file_char, 0, 0, 0,
 
 
 
-/* Value is non-zero if the file asswociated with file descriptor FD
+/* Value is non-zero if the file associated with file descriptor FD
    is a compiled Lisp file that's safe to load.  Only files compiled
    with Emacs are safe to load.  Files compiled with XEmacs can lead
    to a crash in Fbyte_code because of an incompatible change in the
@@ -741,7 +741,7 @@ DEFUN ("load", Fload, Sload, 1, 5, 0,
        doc: /* Execute a file of Lisp code named FILE.
 First try FILE with `.elc' appended, then try with `.el',
 then try FILE unmodified (the exact suffixes in the exact order are
-determined by  `load-suffixes').  Environment variable references in
+determined by `load-suffixes').  Environment variable references in
 FILE are replaced with their values by calling `substitute-in-file-name'.
 This function searches the directories in `load-path'.
 
@@ -912,7 +912,7 @@ Return t if the file exists and loads successfully.  */)
                                    tmp))
                     : found) ;
 
-  /* Check fore the presence of old-style quotes and warn about them.  */
+  /* Check for the presence of old-style quotes and warn about them.  */
   specbind (Qold_style_backquotes, Qnil);
   record_unwind_protect (load_warn_old_style_backquotes, file);
 
@@ -2718,7 +2718,7 @@ read1 (readcharfun, pch, first_in_list)
       }
     default:
     default_label:
-      if (c <= 040) goto retry;	
+      if (c <= 040) goto retry;
       if (c == 0x8a0) /* NBSP */
 	goto retry;
       {
@@ -3873,7 +3873,7 @@ init_lread ()
 		    Vload_path = Fcons (tem, Vload_path);
 		}
 
-	      /* Add site-list under the installation dir, if it exists.  */
+	      /* Add site-lisp under the installation dir, if it exists.  */
 	      tem = Fexpand_file_name (build_string ("site-lisp"),
 				       Vinstallation_directory);
 	      tem1 = Ffile_exists_p (tem);
@@ -3933,7 +3933,7 @@ init_lread ()
       /* NORMAL refers to the lisp dir in the source directory.  */
       /* We used to add ../lisp at the front here, but
 	 that caused trouble because it was copied from dump_path
-	 into Vload_path, aboe, when Vinstallation_directory was non-nil.
+	 into Vload_path, above, when Vinstallation_directory was non-nil.
 	 It should be unnecessary.  */
       Vload_path = decode_env_path (0, normal);
       dump_path = Vload_path;
@@ -3992,7 +3992,7 @@ init_lread ()
 }
 
 /* Print a warning, using format string FORMAT, that directory DIRNAME
-   does not exist.  Print it on stderr and put it in *Message*.  */
+   does not exist.  Print it on stderr and put it in *Messages*.  */
 
 void
 dir_warning (format, dirname)
