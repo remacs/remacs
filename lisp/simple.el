72d595b5f100c2b6522cc17398868cf5333e608c
Glenn Morris <rgm@gnu.org>
2007-10-07 00:31:20 +0000

    (bad-packages-alist): Clarify Semantic and CEDET version numbers.

--- lisp/simple.el
+++ lisp/simple.el
@@ -5810,7 +5810,8 @@ works by saving the value of `buffer-invisibility-spec' and setting it to nil."
   ;; Definitely 2.0pre3, probably all 2.0pre's before this.
   '((semantic semantic-version "2\\.0pre[1-3]"
               "The version of `semantic' loaded does not work in Emacs 22.
-It can cause constant high CPU load.  Upgrade to at least 2.0pre4.")
+It can cause constant high CPU load.
+Upgrade to at least Semantic 2.0pre4 (distributed with CEDET 1.0pre4).")
     ;; CUA-mode does not work with GNU Emacs version 22.1 and newer.
     ;; Except for version 1.2, all of the 1.x and 2.x version of cua-mode
     ;; provided the `CUA-mode' feature.  Since this is no longer true,
