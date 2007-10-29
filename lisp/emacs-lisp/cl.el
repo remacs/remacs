877873196957b89fba4daa1e3ec1ab310b0e4e5d
Glenn Morris <rgm@gnu.org>
2007-10-29 00:22:36 +0000

    Move local variables to end of file, and set byte-compile-warnings to `(not cl-functions)'.

--- lisp/emacs-lisp/cl.el
+++ lisp/emacs-lisp/cl.el
@@ -1,4 +1,4 @@
-;;; cl.el --- Common Lisp extensions for Emacs -*-byte-compile-dynamic: t;-*-
+;;; cl.el --- Common Lisp extensions for Emacs
 
 ;; Copyright (C) 1993, 2001, 2002, 2003, 2004, 2005,
 ;;   2006, 2007 Free Software Foundation, Inc.
@@ -657,6 +657,11 @@ If ALIST is non-nil, the new pairs are prepended to it."
 (provide 'cl)
 
 (run-hooks 'cl-load-hook)
+
+;; Local variables:
+;; byte-compile-dynamic: t
+;; byte-compile-warnings: (not cl-functions)
+;; End:
 
 ;; arch-tag: 5f07fa74-f153-4524-9303-21f5be125851
 ;;; cl.el ends here
