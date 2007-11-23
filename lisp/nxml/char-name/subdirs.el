43b997800269937e4bae25693b7b588f06c4456e
Stefan Monnier <monnier@iro.umontreal.ca>
2007-11-23 18:49:02 +0000

    Remove.

--- lisp/nxml/char-name/subdirs.el
+++
@@ -1,8 +0,0 @@
-;; -*- no-byte-compile: t -*-
-;; In load-path, after this directory should come
-;; certain of its subdirectories.  Here we specify them.
-(normal-top-level-add-to-load-path '("unicode" ))
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; End:
--- lisp/nxml/subdirs.el
+++
@@ -1,8 +0,0 @@
-;; -*- no-byte-compile: t -*-
-;; In load-path, after this directory should come
-;; certain of its subdirectories.  Here we specify them.
-(normal-top-level-add-to-load-path '("char-name" ))
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; End:
