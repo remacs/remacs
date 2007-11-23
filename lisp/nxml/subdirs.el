8cd39fb3c4cf47d2464f00eaa69c587e17dd11cc
Mark A. Hershberger <mah@everybody.org>
2007-11-23 06:58:00 +0000

    Initial merge of nxml

---
+++ lisp/nxml/subdirs.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t -*-
+;; In load-path, after this directory should come
+;; certain of its subdirectories.  Here we specify them.
+(normal-top-level-add-to-load-path '("char-name" ))
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; End:
