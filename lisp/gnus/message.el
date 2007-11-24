77fb0e182adbdf0c2b121ff0fed4fc81b889f7ed
Glenn Morris <rgm@gnu.org>
2007-11-24 03:19:54 +0000

    (message-tool-bar-retro): Update for rename mail_send.xpm->mail-send.xpm.

--- lisp/gnus/message.el
+++ lisp/gnus/message.el
@@ -7381,7 +7381,7 @@ See `gmm-tool-bar-from-list' for details on the format of the list."
 
 (defcustom message-tool-bar-retro
   '(;; Old Emacs 21 icon for consistency.
-    (message-send-and-exit "gnus/mail_send")
+    (message-send-and-exit "gnus/mail-send")
     (message-kill-buffer "close")
     (message-dont-send "cancel")
     (mml-attach-file "attach" mml-mode-map)
