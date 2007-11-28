e81916de845d7958cf2a69a24146ddaaaeb4f0cf
Glenn Morris <rgm@gnu.org>
2007-11-28 05:12:01 +0000

    Move to etc/nxml

--- lisp/nxml/char-name/unicode/0FE20-0FE2F.el
+++
@@ -1,8 +0,0 @@
-(nxml-define-char-name-set 'combining-half-marks
-  '(("COMBINING LIGATURE LEFT HALF" #xFE20)
-    ("COMBINING LIGATURE RIGHT HALF" #xFE21)
-    ("COMBINING DOUBLE TILDE LEFT HALF" #xFE22)
-    ("COMBINING DOUBLE TILDE RIGHT HALF" #xFE23)
-    ))
-
-;; arch-tag: de291a34-737a-4bbb-8d97-94f878921d05
