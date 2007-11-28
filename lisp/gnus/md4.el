2ff9cf9cf4debb3bb5c415ee76668086d5592319
Glenn Morris <rgm@gnu.org>
2007-11-28 07:48:36 +0000

    Remove copyright from author with assignment and merge into FSF years. Add 2007 to copyright years.

--- lisp/gnus/md4.el
+++ lisp/gnus/md4.el
@@ -1,7 +1,7 @@
 ;;; md4.el --- MD4 Message Digest Algorithm.
 
-;; Copyright (C) 2004 Free Software Foundation, Inc.
-;; Copyright (C) 2001 Taro Kawagishi
+;; Copyright (C) 2001, 2004, 2007  Free Software Foundation, Inc.
+
 ;; Author: Taro Kawagishi <tarok@transpulse.org>
 ;; Keywords: MD4
 ;; Version: 1.00
