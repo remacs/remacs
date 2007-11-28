8a4290cdd84102205da699ba5ad9c46fb42a5194
Glenn Morris <rgm@gnu.org>
2007-11-28 04:06:40 +0000

    Add 2007 to copyright years. Switch license to standard GPLv3+ text.
    (xmltok-get-declared-encoding-position): Declare as a function.

--- lisp/nxml/nxml-enc.el
+++ lisp/nxml/nxml-enc.el
@@ -1,24 +1,26 @@
 ;;; nxml-enc.el --- XML encoding auto-detection
 
-;; Copyright (C) 2003 Free Software Foundation, Inc.
+;; Copyright (C) 2003, 2007 Free Software Foundation, Inc.
 
 ;; Author: James Clark
 ;; Keywords: XML
 
-;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License as
-;; published by the Free Software Foundation; either version 2 of
-;; the License, or (at your option) any later version.
+;; This file is part of GNU Emacs.
 
-;; This program is distributed in the hope that it will be
-;; useful, but WITHOUT ANY WARRANTY; without even the implied
-;; warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-;; PURPOSE.  See the GNU General Public License for more details.
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
 
-;; You should have received a copy of the GNU General Public
-;; License along with this program; if not, write to the Free
-;; Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-;; MA 02111-1307 USA
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
 
 ;;; Commentary:
 
@@ -71,6 +73,9 @@
 (defun nxml-set-xml-coding (file-name size)
   "Function to use as `set-auto-coding-function' when file is known to be XML."
   (nxml-detect-coding-system (+ (point) (min size 1024))))
+
+(declare-function xmltok-get-declared-encoding-position "xmltok"
+                  (&optional limit))    ; autoloaded
 
 (defun nxml-detect-coding-system (limit)
   (if (< limit (+ (point) 2))
