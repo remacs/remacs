e542e76e4ceef54f46a157c4f26054d74b3c590a
Kenichi Handa <handa@m17n.org>
2007-12-01 04:20:54 +0000

    (tamil-composition-function): Use font-shape-text if auto-compose-current-font is non-nil.

--- lisp/language/tml-util.el
+++ lisp/language/tml-util.el
@@ -360,12 +360,17 @@
 If STRING is not nil, it is a string, and POS is an index to the string.
 In this case, compose characters after POS of the string."
   (if string
-      ;; Not yet implemented.
-      nil
+      (if (eq (string-match tamil-composable-pattern pos) pos)
+	  (if auto-compose-current-font
+	      (or (font-shape-text 0 (match-end 0) auto-compose-current-font
+				   string)
+		  pos)))
     (goto-char pos)
     (if (looking-at tamil-composable-pattern)
-	(prog1 (match-end 0)
-	  (tamil-compose-syllable-region pos (match-end 0))))))
+	(if auto-compose-current-font
+	    (or (font-shape-text pos (match-end 0) auto-compose-current-font))
+	  (prog1 (match-end 0)
+	    (tamil-compose-syllable-region pos (match-end 0)))))))
 
 (provide 'tml-util)
 
