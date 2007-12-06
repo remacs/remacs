fd19b75d0425b774d8560576ce5c783dbf3218e4
Kenichi Handa <handa@m17n.org>
2007-12-06 07:33:02 +0000

    (malayalam-composition-function): Use font-shape-text if auto-compose-current-font is non-nil.

--- lisp/language/mlm-util.el
+++ lisp/language/mlm-util.el
@@ -106,12 +106,19 @@
 If STRING is not nil, it is a string, and POS is an index to the string.
 In this case, compose characters after POS of the string."
   (if string
-      ;; Not yet implemented.
-      nil
+      (if auto-compose-current-font
+	  (if (eq (string-match "[$,1@ (B-$,1A?(B]+" pos) pos)
+	      (or (font-shape-text 0 (match-end 0) auto-compose-current-font
+				   string)
+		  pos)))
     (goto-char pos)
-    (if (looking-at malayalam-composable-pattern)
-	(prog1 (match-end 0)
-	  (malayalam-compose-syllable-region pos (match-end 0))))))
+    (if auto-compose-current-font
+	(if (looking-at  "[$,1@ (B-$,1A?(B]+")
+	    (or (font-shape-text pos (match-end 0) auto-compose-current-font)
+		pos)
+	  (if (looking-at malayalam-composable-pattern)
+	      (prog1 (match-end 0)
+		(malayalam-compose-syllable-region pos (match-end 0))))))))
 
 ;; Notes on conversion steps.
 
