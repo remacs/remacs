0c757b8d8fc7a7a8ac5f0def29ffcef40eb16ffb
Andreas Schwab <schwab@suse.de>
2008-01-03 17:51:06 +0000

    Update.

--- lisp/emacs-lisp/cl-loaddefs.el
+++ lisp/emacs-lisp/cl-loaddefs.el
@@ -283,7 +283,7 @@ Not documented
 ;;;;;;  do* do loop return-from return block etypecase typecase ecase
 ;;;;;;  case load-time-value eval-when destructuring-bind function*
 ;;;;;;  defmacro* defun* gentemp gensym cl-compile-time-init) "cl-macs"
-;;;;;;  "cl-macs.el" "6ebe05f6ba95a14ea35d74ef52c17882")
+;;;;;;  "cl-macs.el" "24c5d69dcac117dea68e32a91fec9f67")
 ;;; Generated autoloads from cl-macs.el
 
 (autoload 'cl-compile-time-init "cl-macs" "\
