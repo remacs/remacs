f36e650b8b6ab81fd92831bb81f7264d726b7b00
Romain Francoise <romain@orebokech.com>
2008-01-05 18:11:25 +0000

    2008-01-05  Sven Joachim  <svenjoac@gmx.de>
    	* vc-bzr.el: Fix typo in header.

--- lisp/vc-bzr.el
+++ lisp/vc-bzr.el
@@ -15,7 +15,7 @@
 
 ;; This file is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-LC;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
