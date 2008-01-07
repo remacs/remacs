4e643dd23c96da2fb2f1491786ead6edd89a1210
Glenn Morris <rgm@gnu.org>
2008-01-07 02:11:00 +0000

    Add 2008 to copyright years.

--- lisp/progmodes/octave-mod.el
+++ lisp/progmodes/octave-mod.el
@@ -1,6 +1,6 @@
 ;;; octave-mod.el --- editing Octave source files under Emacs
 
-;; Copyright (C) 1997, 2001, 2002, 2003, 2004, 2005, 2006, 2007
+;; Copyright (C) 1997, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008
 ;; Free Software Foundation, Inc.
 
 ;; Author: Kurt Hornik <Kurt.Hornik@wu-wien.ac.at>
