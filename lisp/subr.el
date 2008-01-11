cf1917060aa30a20ef40aa296497c4a4ebeeb9f1
Richard M. Stallman <rms@gnu.org>
2008-01-11 14:44:15 +0000

    (atomic-change-group): Prevent undo list truncation.

--- lisp/subr.el
+++ lisp/subr.el
@@ -1802,6 +1802,10 @@ user can undo the change normally."
   (let ((handle (make-symbol "--change-group-handle--"))
 	(success (make-symbol "--change-group-success--")))
     `(let ((,handle (prepare-change-group))
+	   ;; Don't truncate any undo data in the middle of this.
+	   (undo-outer-limit nil)
+	   (undo-limit most-positive-fixnum)
+	   (undo-strong-limit most-positive-fixnum)
 	   (,success nil))
        (unwind-protect
 	   (progn
