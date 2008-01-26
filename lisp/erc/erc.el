15db4a13e5ae62e10bda61ed78dd5024353e6499
Michael Olson <mwolson@gnu.org>
2008-01-26 21:49:41 +0000

    Sync ERC 5.3 release.

--- lisp/erc/erc.el
+++ lisp/erc/erc.el
@@ -66,7 +66,7 @@
 
 ;;; Code:
 
-(defconst erc-version-string "Version 5.3 (RC 1)"
+(defconst erc-version-string "Version 5.3"
   "ERC version.  This is used by function `erc-version'.")
 
 (eval-when-compile (require 'cl))
