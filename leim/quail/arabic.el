f3c8f8d13894207218ae9c6068fc444b6e9b9d5a
Glenn Morris <rgm@gnu.org>
2008-02-02 04:13:38 +0000

    Update copyright years and GPL version.

--- leim/quail/arabic.el
+++ leim/quail/arabic.el
@@ -1,14 +1,15 @@
 ;;; arabic.el --- Quail package for inputting Arabic	-*- coding: utf-8;-*-
 
-;; Copyright (C) 2007 Free Software Foundation, Inc.
-;; Written by James Cloos <cloos@jhcloos.com>
+;; Copyright (C) 2007, 2008 Free Software Foundation, Inc.
+
+;; Author: James Cloos <cloos@jhcloos.com>
 ;; Keywords: mule, input method, Arabic
 
 ;; This file is part of GNU Emacs.
 
 ;; GNU Emacs is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 2, or (at your option)
+;; the Free Software Foundation; either version 3, or (at your option)
 ;; any later version.
 
 ;; GNU Emacs is distributed in the hope that it will be useful,
