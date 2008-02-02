7e14d905634fb54fd5f26e6c51d3427e44e77fb8
Glenn Morris <rgm@gnu.org>
2008-02-02 04:35:05 +0000

    Update copyright years and GPL version.

--- src/ftfont.h
+++ src/ftfont.h
@@ -1,5 +1,5 @@
 /* ftfont.h -- Interface definition for Freetype font backend.
-   Copyright (C) 2007
+   Copyright (C) 2007, 2008
      National Institute of Advanced Industrial Science and Technology (AIST)
      Registration Number H13PRO009
 
@@ -7,7 +7,7 @@ This file is part of GNU Emacs.
 
 GNU Emacs is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2, or (at your option)
+the Free Software Foundation; either version 3, or (at your option)
 any later version.
 
 GNU Emacs is distributed in the hope that it will be useful,
--- src/w32font.h
+++ src/w32font.h
@@ -1,11 +1,11 @@
 /* Shared GDI and Uniscribe Font backend declarations for the W32 API.
-   Copyright (C) 2007 Free Software Foundation, Inc.
+   Copyright (C) 2007, 2008 Free Software Foundation, Inc.
 
 This file is part of GNU Emacs.
 
 GNU Emacs is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2, or (at your option)
+the Free Software Foundation; either version 3, or (at your option)
 any later version.
 
 GNU Emacs is distributed in the hope that it will be useful,
