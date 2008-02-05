f01e03d36870b8f7044c91d20b3c55f2ebd1f02f
Juanma Barranquero <lekktu@gmail.com>
2008-02-05 16:02:55 +0000

    In help, use generic names for the image libraries.

--- nt/configure.bat
+++ nt/configure.bat
@@ -120,11 +120,11 @@ echo.   --no-opt                disable optimization
 echo.   --no-cygwin             use -mno-cygwin option with GCC
 echo.   --cflags FLAG           pass FLAG to compiler
 echo.   --ldflags FLAG          pass FLAG to compiler when linking
-echo.   --without-png           do not use libpng even if it is installed
-echo.   --without-jpeg          do not use jpeg-6b even if it is installed
-echo.   --without-gif           do not use libungif even if it is installed
-echo.   --without-tiff          do not use libtiff even if it is installed
-echo.   --without-xpm           do not use libXpm even if it is installed
+echo.   --without-png           do not use PNG library even if it is installed
+echo.   --without-jpeg          do not use JPEG library even if it is installed
+echo.   --without-gif           do not use GIF library even if it is installed
+echo.   --without-tiff          do not use TIFF library even if it is installed
+echo.   --without-xpm           do not use XPM library even if it is installed
 goto end
 rem ----------------------------------------------------------------------
 :setprefix
