f51995fa922a78a20a2418556c4c99851a67560c
Kenichi Handa <handa@m17n.org>
2008-02-11 05:25:24 +0000

    (boyer_moore): Fix incorrect synching of the trunk and emacs-unicode-2.

--- src/search.c
+++ src/search.c
@@ -1785,7 +1785,7 @@ boyer_moore (n, base_pat, len, len_byte, trt, inverse_trt,
 		ch = -1;
 	    }
 
-	  if (ch >= 0400)
+	  if (ch >= 0200)
 	    j = (ch & 0x3F) | 0200;
 	  else
 	    j = *ptr;
@@ -1804,7 +1804,7 @@ boyer_moore (n, base_pat, len, len_byte, trt, inverse_trt,
 	      while (1)
 		{
 		  TRANSLATE (ch, inverse_trt, ch);
-		  if (ch >= 0400)
+		  if (ch >= 0200)
 		    j = (ch & 0x3F) | 0200;
 		  else
 		    j = ch;
