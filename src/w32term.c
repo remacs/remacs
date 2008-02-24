2face66975f3da09dbc3e032c0cfeb67047c53f0
Jason Rumney <jasonr@gnu.org>
2008-02-24 15:45:28 +0000

    (x_draw_glyph_string_background): Clear the background manually when cleartype is in use. (x_draw_glyph_string_foreground): Draw text transparently when cleartype is in use.

--- src/w32term.c
+++ src/w32term.c
@@ -1846,6 +1846,7 @@ x_draw_glyph_string_background (s, force_p)
 	       || s->font_not_found_p
 	       || s->extends_to_end_of_line_p
                || s->font->bdf
+	       || cleartype_active
 	       || force_p)
 	{
 	  x_clear_glyph_string_rect (s, s->x, s->y + box_line_width,
@@ -1874,7 +1875,8 @@ x_draw_glyph_string_foreground (s)
   else
     x = s->x;
 
-  if (s->for_overlaps || (s->background_filled_p && s->hl != DRAW_CURSOR))
+  if (s->for_overlaps || (s->background_filled_p && s->hl != DRAW_CURSOR)
+      || cleartype_active)
     SetBkMode (s->hdc, TRANSPARENT);
   else
     SetBkMode (s->hdc, OPAQUE);
