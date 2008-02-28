af8b50d18f529b5b2ca665e81ee6b612d60a38dc
Glenn Morris <rgm@gnu.org>
2008-02-28 03:44:36 +0000

    (auto-insert-alist): Update to FDL 1.2.

--- lisp/autoinsert.el
+++ lisp/autoinsert.el
@@ -228,10 +228,11 @@ If this contains a %s, that will be replaced by the matching rule."
 
 @quotation
 Permission is granted to copy, distribute and/or modify this document
-under the terms of the GNU Free Documentation License, Version 1.1 or
-any later version published by the Free Software Foundation; with no
-Invariant Sections, and no Cover Texts.  A copy of the license is
-included in the section entitled ``GNU Free Documentation License.''
+under the terms of the GNU Free Documentation License, Version 1.2
+or any later version published by the Free Software Foundation;
+with no Invariant Sections, no Front-Cover Texts, and no Back-Cover
+Texts.  A copy of the license is included in the section entitled ``GNU
+Free Documentation License''.
 
 A copy of the license is also available from the Free Software
 Foundation Web site at @url{http://www.gnu.org/licenses/fdl.html}.
