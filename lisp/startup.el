1344a1f99e110e040d69325f27a6d2afd26a7535
Juri Linkov <juri@jurta.org>
2008-03-02 16:18:21 +0000

    Revert 2008-02-28 change that adds initial message to *scratch* buffer regardless of the value of `inhibit-startup-screen'.

--- lisp/startup.el
+++ lisp/startup.el
@@ -42,6 +42,7 @@
 
 (defcustom inhibit-startup-screen nil
   "Non-nil inhibits the startup screen.
+It also inhibits display of the initial message in the `*scratch*' buffer.
 
 This is for use in your personal init file (but NOT site-start.el), once
 you are familiar with the contents of the startup screen."
@@ -1122,7 +1123,9 @@ or `CVS', and any subdirectory that contains a file named `.nosearch'."
 
 ")
   "Initial message displayed in *scratch* buffer at startup.
-If this is nil, no message will be displayed."
+If this is nil, no message will be displayed.
+If `inhibit-startup-screen' is non-nil, then no message is displayed,
+regardless of the value of this variable."
   :type '(choice (text :tag "Message")
 		 (const :tag "none" nil))
   :group 'initialization)
@@ -2121,14 +2124,6 @@ A fancy display is used on graphic displays, normal otherwise."
 	    ;; abort later.
 	    (unless (frame-live-p (selected-frame)) (kill-emacs nil))))))
 
-    ;; If *scratch* exists and is empty, insert initial-scratch-message.
-    (and initial-scratch-message
-	 (get-buffer "*scratch*")
-	 (with-current-buffer "*scratch*"
-	   (when (zerop (buffer-size))
-	     (insert initial-scratch-message)
-	     (set-buffer-modified-p nil))))
-
     (if (or inhibit-startup-screen
 	    noninteractive
 	    emacs-quick-startup)
@@ -2172,6 +2167,14 @@ A fancy display is used on graphic displays, normal otherwise."
       ;; 	(precompute-menubar-bindings))
       ;; (with-no-warnings
       ;; 	(setq menubar-bindings-done t))
+
+      ;; If *scratch* exists and is empty, insert initial-scratch-message.
+      (and initial-scratch-message
+	   (get-buffer "*scratch*")
+	   (with-current-buffer "*scratch*"
+	     (when (zerop (buffer-size))
+	       (insert initial-scratch-message)
+	       (set-buffer-modified-p nil))))
 
       (if (> file-count 0)
 	  (display-startup-screen t)
