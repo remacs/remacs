6dc48f1415b2c5756b937648bd32bab4003cb42a
Chong Yidong <cyd@stupidchicken.com>
2008-03-07 16:46:49 +0000

    Bump to version 22.1.92

--- lisp/version.el
+++ lisp/version.el
@@ -30,7 +30,7 @@
 (defconst emacs-copyright "Copyright (C) 2008 Free Software Foundation, Inc."
   "Short copyright string for this version of Emacs.")
 
-(defconst emacs-version "22.1.91" "\
+(defconst emacs-version "22.1.92" "\
 Version numbers of this version of Emacs.")
 
 (defconst emacs-major-version
