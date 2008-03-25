d4919479f915dd0d98878cb63d2848b12fe8da15
Juri Linkov <juri@jurta.org>
2008-03-25 21:50:45 +0000

    Add file cookie coding:iso-8859-1 for the degree sign in battery-mode-line-format.

--- lisp/battery.el
+++ lisp/battery.el
@@ -1,4 +1,4 @@
-;;; battery.el --- display battery status information
+;;; battery.el --- display battery status information  -*- coding: iso-8859-1 -*-
 
 ;; Copyright (C) 1997, 1998, 2000, 2001, 2002, 2003, 2004,
 ;;   2005, 2006, 2007, 2008 Free Software Foundation, Inc.
