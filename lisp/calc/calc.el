e835d95623c3c38a19f70068149819c592cd5df4
Jay Belanger <jay.p.belanger@gmail.com>
2008-04-01 02:30:31 +0000

    Autoload `calc-yank'. (calc-mode-map): Add keybindings for `calc-yank'.

--- lisp/calc/calc.el
+++ lisp/calc/calc.el
@@ -948,6 +948,8 @@ If nil, selections displayed but ignored.")
     (define-key map "\C-m" 'calc-enter)
     (define-key map "\M-\C-m" 'calc-last-args-stub)
     (define-key map "\C-j" 'calc-over)
+    (define-key map "\C-y" 'calc-yank)
+    (define-key map [mouse-2] 'calc-yank)
 
     (mapc (lambda (x) (define-key map (char-to-string x) 'undefined))
 	  "lOW")
@@ -1092,7 +1094,9 @@ If nil, selections displayed but ignored.")
     calc-missing-key calc-mod calc-other-window calc-over calc-percent
     calc-pop-above calc-power calc-roll-down calc-roll-up
     calc-shift-Y-prefix-help calc-tutorial calcDigit-letter
-    report-calc-bug)))
+    report-calc-bug)
+
+ ("calc-yank" calc-yank)))
 
 
 ;;;###autoload (define-key ctl-x-map "*" 'calc-dispatch)
