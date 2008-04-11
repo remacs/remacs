87c24bb7401ddbcfdc177015e252107f47d13c80
Eli Zaretskii <eliz@gnu.org>
2008-04-11 19:24:04 +0000

    (__MINGW_NOTHROW): If not defined, define to nothing. Add copyright notice.

--- nt/inc/sys/stat.h
+++ nt/inc/sys/stat.h
@@ -1,14 +1,34 @@
-/*
- * sys/stat.h supplied with MSVCRT uses too narrow data types for
- * inode and user/group id, so we replace them with our own.
- */
+/* sys/stat.h supplied with MSVCRT uses too narrow data types for
+   inode and user/group id, so we replace them with our own.
+
+   Copyright (C) 2008  Free Software Foundation, Inc.
+
+This file is part of GNU Emacs.
+
+GNU Emacs is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+GNU Emacs is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU Emacs; see the file COPYING.  If not, write to
+the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+Boston, MA 02110-1301, USA.  */
 
 #ifndef INC_SYS_STAT_H_
 #define INC_SYS_STAT_H_
 
 #ifdef __MINGW32__
 # include <_mingw.h>
-#else
+#endif
+
+/* Only MinGW 3.13 and later has __MINGW_NOTHROW.  */
+#ifndef __MINGW_NOTHROW
 # define __MINGW_NOTHROW
 #endif
 
