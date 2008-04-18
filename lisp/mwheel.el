89ee01639d7f9fbbe7edbb422e40723ecb6ef103
Juanma Barranquero <lekktu@gmail.com>
2008-04-18 10:30:38 +0000

    (mouse-wheel-down-button, mouse-wheel-up-button, mouse-wheel-click-button): Add WHEN to obsolescence declaration.

--- lisp/mwheel.el
+++ lisp/mwheel.el
@@ -56,7 +56,8 @@
 
 (defvar mouse-wheel-down-button 4)
 (make-obsolete-variable 'mouse-wheel-down-button
-                        'mouse-wheel-down-event)
+                        'mouse-wheel-down-event
+			"22.1")
 (defcustom mouse-wheel-down-event
   ;; In the latest versions of XEmacs, we could just use mouse-%s as well.
   (if (memq window-system '(w32 mac))
@@ -70,7 +71,8 @@
 
 (defvar mouse-wheel-up-button 5)
 (make-obsolete-variable 'mouse-wheel-up-button
-                        'mouse-wheel-up-event)
+                        'mouse-wheel-up-event
+			"22.1")
 (defcustom mouse-wheel-up-event
   ;; In the latest versions of XEmacs, we could just use mouse-%s as well.
   (if (memq window-system '(w32 mac))
@@ -84,7 +86,8 @@
 
 (defvar mouse-wheel-click-button 2)
 (make-obsolete-variable 'mouse-wheel-click-button
-                        'mouse-wheel-click-event)
+                        'mouse-wheel-click-event
+			"22.1")
 (defcustom mouse-wheel-click-event
   ;; In the latest versions of XEmacs, we could just use mouse-%s as well.
   (intern (format (if (featurep 'xemacs) "button%s" "mouse-%s")
