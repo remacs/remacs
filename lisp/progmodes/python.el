0c8a412cfd9ec585b1ca00c85492b10f633fc886
Stefan Monnier <monnier@iro.umontreal.ca>
2008-05-05 07:10:25 +0000

    (python-block-pairs): Align finally with except.

--- lisp/progmodes/python.el
+++ lisp/progmodes/python.el
@@ -747,7 +747,7 @@ Set `python-indent' locally to the value guessed."
   '(("else" "if" "elif" "while" "for" "try" "except")
     ("elif" "if" "elif")
     ("except" "try" "except")
-    ("finally" "try"))
+    ("finally" "try" "except"))
   "Alist of keyword matches.
 The car of an element is a keyword introducing a statement which
 can close a block opened by a keyword in the cdr.")
