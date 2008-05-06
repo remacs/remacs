4ee57b2a2d197452b39217907075951008cf5589
Glenn Morris <rgm@gnu.org>
2008-05-06 03:36:21 +0000

    Switch to recommended form of GPLv3 permissions notice.

--- lisp/erc/erc-capab.el
+++ lisp/erc/erc-capab.el
@@ -2,10 +2,10 @@
 
 ;; Copyright (C) 2006, 2007, 2008 Free Software Foundation, Inc.
 
-;; GNU Emacs is free software; you can redistribute it and/or modify
+;; GNU Emacs is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 3, or (at your option)
-;; any later version.
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
 
 ;; GNU Emacs is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -13,9 +13,7 @@
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs; see the file COPYING.  If not, write to the
-;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-;; Boston, MA 02110-1301, USA.
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
