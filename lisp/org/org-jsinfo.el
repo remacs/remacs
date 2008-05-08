77fa600f201d9c349512cf152a5bcae195f495ea
Eli Zaretskii <eliz@gnu.org>
2008-05-08 10:55:33 +0000

    Renamed org-infojs.el to org-jsinfo.el.

--- lisp/org/org-jsinfo.el
+++ lisp/org/org-jsinfo.el
@@ -1,4 +1,4 @@
-;;; org-infojs.el --- Support for org-info.js Javascript in Org HTML export
+;;; org-jsinfo.el --- Support for org-info.js Javascript in Org HTML export
 
 ;; Copyright (C) 2004, 2005, 2006, 2007, 2008 Free Software Foundation, Inc.
 
