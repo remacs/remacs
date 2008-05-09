3187ea63b1b6d95d7c6c68ae1e72d152b5b0a45b
Glenn Morris <rgm@gnu.org>
2008-05-09 04:38:17 +0000

    Switch to recommended form of GPLv3 permissions notice.

--- mac/src/chdir.c
+++ mac/src/chdir.c
@@ -2,12 +2,14 @@
    Copyright (C) 1999, 2000, 2001, 2002, 2003, 2004,
       2005, 2006, 2007, 2008  Free Software Foundation, Inc.
 
+Contributed by Andrew Choi (akochoi@mac.com).
+
 This file is part of GNU Emacs.
 
-GNU Emacs is free software; you can redistribute it and/or modify
+GNU Emacs is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3, or (at your option)
-any later version.
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
 GNU Emacs is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,11 +17,8 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with GNU Emacs; see the file COPYING.  If not, write to
-the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-Boston, MA 02110-1301, USA.  */
+along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.  */
 
-/* Contributed by Andrew Choi (akochoi@mac.com).  */
 
 #include <string.h>
 #include <Files.h>
