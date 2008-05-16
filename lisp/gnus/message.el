5833346788468ca47efb5c833528a12b4235a511
Miles Bader <miles@gnu.org>
2008-05-16 02:05:17 +0000

    Merge from gnus--devo--0
    Revision: emacs@sv.gnu.org/emacs--devo--0--patch-1165

--- lisp/gnus/message.el
+++ lisp/gnus/message.el
@@ -1016,7 +1016,6 @@ Used by `message-yank-original' via `message-yank-cite'."
   :link '(custom-manual "(message)Insertion Variables")
   :type 'integer)
 
-;;;###autoload
 (defcustom message-cite-function 'message-cite-original-without-signature
   "*Function for citing an original message.
 Predefined functions include `message-cite-original' and
