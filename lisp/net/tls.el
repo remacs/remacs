3c5fa30ac92a7146ea31ce3c78ee8a03432a452f
Glenn Morris <rgm@gnu.org>
2008-06-07 02:38:59 +0000

    Remove unnecessary eval-and-compile.

--- lisp/net/tls.el
+++ lisp/net/tls.el
@@ -1,7 +1,7 @@
 ;;; tls.el --- TLS/SSL support via wrapper around GnuTLS
 
-;; Copyright (C) 1996, 1997, 1998, 1999, 2002, 2003, 2004,
-;;   2005, 2006, 2007, 2008 Free Software Foundation, Inc.
+;; Copyright (C) 1996, 1997, 1998, 1999, 2002, 2003, 2004, 2005, 2006,
+;;   2007, 2008  Free Software Foundation, Inc.
 
 ;; Author: Simon Josefsson <simon@josefsson.org>
 ;; Keywords: comm, tls, gnutls, ssl
@@ -45,9 +45,8 @@
 
 ;;; Code:
 
-(eval-and-compile
-  (autoload 'format-spec "format-spec")
-  (autoload 'format-spec-make "format-spec"))
+(autoload 'format-spec "format-spec")
+(autoload 'format-spec-make "format-spec")
 
 (defgroup tls nil
   "Transport Layer Security (TLS) parameters."
