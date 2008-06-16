5de85e832505cd25f47069b51e51d20289e0ad55
Glenn Morris <rgm@gnu.org>
2008-06-16 02:23:10 +0000

    (set-version): Add doc/lispref/book-spine.texinfo.

--- admin/admin.el
+++ admin/admin.el
@@ -82,6 +82,9 @@ Root must be the root of an Emacs source tree."
   (set-version-in-file root "doc/lispref/vol2.texi" version
 		       (rx (and "EMACSVER" (1+ space)
 				(submatch (1+ (in "0-9."))))))
+  (set-version-in-file root "doc/lispref/book-spine.texinfo" version
+		       (rx (and "Emacs Version" (1+ space)
+				(submatch (1+ (in "0-9."))))))
   (set-version-in-file root "doc/man/emacs.1" version
 		       (rx (and ".TH EMACS" (1+ not-newline)
                                 "GNU Emacs" (1+ space)
