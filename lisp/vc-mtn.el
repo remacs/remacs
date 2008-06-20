469ca4030a351d865a4412a284ace9d2e11cf9e7
Dan Nicolaescu <dann@ics.uci.edu>
2008-06-20 17:16:41 +0000

    (vc-mtn-log-view-mode): Set log-view-per-file-logs and log-view-file-re.

--- lisp/vc-mtn.el
+++ lisp/vc-mtn.el
@@ -24,6 +24,11 @@
 
 ;; 
 
+;;; TODO:
+
+;; - The `previous-version' VC method needs to be supported, 'D' in
+;;   log-view-mode uses it.
+
 ;;; Code:
 
 (eval-when-compile (require 'cl) (require 'vc))
@@ -158,10 +163,12 @@
 (defvar log-view-message-re)
 (defvar log-view-file-re)
 (defvar log-view-font-lock-keywords)
+(defvar log-view-per-file-logs)
 
 (define-derived-mode vc-mtn-log-view-mode log-view-mode "Mtn-Log-View"
-  ;; TODO: Not sure what to do about file markers for now.
-  (set (make-local-variable 'log-view-file-re) "\\'\\`")
+  ;; Don't match anything.
+  (set (make-local-variable 'log-view-file-re) "\\`a\\`")
+  (set (make-local-variable 'log-view-per-file-logs) nil)
   ;; TODO: Use a more precise regexp than "[ |/]+" to avoid false positives
   ;; in the ChangeLog text.
   (set (make-local-variable 'log-view-message-re)
