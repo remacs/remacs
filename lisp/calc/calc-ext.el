7a3e1fb5a87552da8ae9ec98b56b177c3b5094c6
Jay Belanger <jay.p.belanger@gmail.com>
2008-06-24 05:22:08 +0000

    (calc-init-extensions): Add keybinding for `kill-ring-save'.

--- lisp/calc/calc-ext.el
+++ lisp/calc/calc-ext.el
@@ -134,6 +134,7 @@
   (define-key calc-mode-map "\M-k" 'calc-copy-as-kill)
   (define-key calc-mode-map "\C-w" 'calc-kill-region)
   (define-key calc-mode-map "\M-w" 'calc-copy-region-as-kill)
+  (define-key calc-mode-map "\M-\C-w" 'kill-ring-save)
   (define-key calc-mode-map "\C-_" 'calc-undo)
   (define-key calc-mode-map "\C-xu" 'calc-undo)
   (define-key calc-mode-map "\M-\C-m" 'calc-last-args)
