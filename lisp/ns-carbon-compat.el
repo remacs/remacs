35eaf383238ee9a2cf29d864d3913bd6b6bd3741
Glenn Morris <rgm@gnu.org>
2008-07-16 02:22:51 +0000

    Remove permissions notice from trivial file.

--- lisp/ns-carbon-compat.el
+++ lisp/ns-carbon-compat.el
@@ -1,25 +1,9 @@
-;;; ns-carbon-compat.el ---
-;;;     Carbon compatibility layer for Mac users of NS (Cocoa) GUI.
-;;; Copyright (C) 2008 Free Software Foundation, Inc.
+;;; ns-carbon-compat.el --- Carbon compatibility layer for Mac users of NS (Cocoa) GUI.
 
-;;; Author: Adrian Robert
-;;; Keywords: Carbon, MacOSX
+;; Author: Adrian Robert
+;; Keywords: Carbon, MacOSX
 
-;;; This file is part of GNU Emacs.
-;;;
-;;; GNU Emacs is free software; you can redistribute it and/or modify
-;;; it under the terms of the GNU General Public License as published by
-;;; the Free Software Foundation; either version 2, or (at your option)
-;;; any later version.
-;;;
-;;; GNU Emacs is distributed in the hope that it will be useful,
-;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;; GNU General Public License for more details.
-;;;
-;;; You should have received a copy of the GNU General Public License
-;;; along with GNU Emacs; see the file COPYING.  If not, write to
-;;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+;; Add a license if this becomes non-trivial (first year 2008).
 
 ;;; Commentary:
 
