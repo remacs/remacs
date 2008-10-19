8787f54c2f00e6cb488b9d4bc1f8dc47d9f85387
Eli Zaretskii <eliz@gnu.org>
2008-10-19 10:10:00 +0000

    (kkc-init-file-name): Run file names that begin with a period thru `convert-standard-filename'.

--- lisp/international/kkc.el
+++ lisp/international/kkc.el
@@ -41,7 +41,7 @@
   "String denoting KKC input method.
 This string is shown at mode line when users are in KKC mode.")
 
-(defvar kkc-init-file-name "~/.kkcrc"
+(defvar kkc-init-file-name (convert-standard-filename "~/.kkcrc")
   "Name of a file which contains user's initial setup code for KKC.")
 
 ;; A flag to control a file specified by `kkc-init-file-name'.
