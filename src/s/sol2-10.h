9b3362b8a14e134f2f72bb621d76b9cfaf44bedb
Dan Nicolaescu <dann@ics.uci.edu>
2008-10-23 19:32:19 +0000

    * configure (*-sunos5*, *-solaris* ): Use the new file sol2-10.h. Use sol2-6.h for Solaris 7-9. * s/sol2-10.h: New file.

---
+++ src/s/sol2-10.h
@@ -0,0 +1,22 @@
+/* Handle Solaris 2.10.  */
+
+#include "sol2-6.h"
+
+#define SYSTEM_MALLOC
+
+/*
+ * Use the Solaris dldump() function, called from unexsol.c, to dump
+ * emacs, instead of the generic ELF dump code found in unexelf.c.
+ * The resulting binary has a complete symbol table, and is better
+ * for debugging and other observabilty tools (debuggers, pstack, etc).
+ *
+ * If you encounter a problem using dldump(), please consider sending
+ * a message to the OpenSolaris tools-linking mailing list:
+ *      http://mail.opensolaris.org/mailman/listinfo/tools-linking
+ *
+ * It is likely that dldump() works with older Solaris too,
+ * but this has not been tested, and so, this change is for
+ * Solaris 10 and newer only at this time.
+ */
+#undef UNEXEC
+#define UNEXEC unexsol.o
