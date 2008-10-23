bd07cecc1002004cbd3566ee1a96d994bf8f09f6
Juanma Barranquero <lekktu@gmail.com>
2008-10-23 13:46:17 +0000

    Update.

--- lisp/emacs-lisp/cl-loaddefs.el
+++ lisp/emacs-lisp/cl-loaddefs.el
@@ -282,7 +282,7 @@ Not documented
 ;;;;;;  do-all-symbols do-symbols dotimes dolist do* do loop return-from
 ;;;;;;  return block etypecase typecase ecase case load-time-value
 ;;;;;;  eval-when destructuring-bind function* defmacro* defun* gentemp
-;;;;;;  gensym) "cl-macs" "cl-macs.el" "db690711cf205074d21590cc64a26d89")
+;;;;;;  gensym) "cl-macs" "cl-macs.el" "2ca2812501162dcf751301a122f83a4a")
 ;;; Generated autoloads from cl-macs.el
 
 (autoload 'gensym "cl-macs" "\
