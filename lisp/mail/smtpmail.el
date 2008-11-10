373f031296fcda27a2346f743134c9d71dd22daf
Teodor Zlatanov <tzz@lifelogs.com>
2008-11-10 20:10:51 +0000

    (smtpmail-open-stream): Use `starttls-any-program-available'.

--- lisp/mail/smtpmail.el
+++ lisp/mail/smtpmail.el
@@ -503,13 +503,7 @@ This is relative to `smtpmail-queue-dir'."
 (defun smtpmail-open-stream (process-buffer host port)
   (let ((cred (smtpmail-find-credentials
 	       smtpmail-starttls-credentials host port)))
-    (if (null (and cred (condition-case ()
-			    (with-no-warnings
-			      (require 'starttls)
-			      (call-process (if starttls-use-gnutls
-						starttls-gnutls-program
-					      starttls-program)))
-			  (error nil))))
+    (if (null (and cred (starttls-any-program-available)))
 	;; The normal case.
 	(open-network-stream "SMTP" process-buffer host port)
       (let* ((cred-key (smtpmail-cred-key cred))
