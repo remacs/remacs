b7749a8184a7863b108693dfbcbc9025436f8c3e
Michael Kifer <kifer@cs.stonybrook.edu>
2008-11-23 03:30:55 +0000

    2008-11-22  Michael Kifer  <kifer@cs.stonybrook.edu>
    	* ediff-hook.el: added tooltips to Emacs menus.
    
    	* ediff.el: changed date of last update.

--- lisp/ediff-hook.el
+++ lisp/ediff-hook.el
@@ -142,87 +142,116 @@
   (defvar menu-bar-ediff-menu (make-sparse-keymap "Compare"))
   (fset 'menu-bar-ediff-menu (symbol-value 'menu-bar-ediff-menu))
   
-  ;; define ediff-menu
+  ;; define ediff compare menu
   (define-key menu-bar-ediff-menu [window]
-    '("This Window and Next Window" . compare-windows))
+    '(menu-item "This Window and Next Window" compare-windows
+		:help "Compare the current window and the next window"))
   (define-key menu-bar-ediff-menu [ediff-windows-linewise]
-    '("Windows Line-by-line..." . ediff-windows-linewise))
+    '(menu-item "Windows Line-by-line..." ediff-windows-linewise
+		:help "Compare windows line-wise"))
   (define-key menu-bar-ediff-menu [ediff-windows-wordwise]
-    '("Windows Word-by-word..." . ediff-windows-wordwise))
+    '(menu-item "Windows Word-by-word..." ediff-windows-wordwise
+		:help "Compare windows word-wise"))
   (define-key menu-bar-ediff-menu [separator-ediff-windows] '("--"))
   (define-key menu-bar-ediff-menu [ediff-regions-linewise]
-    '("Regions Line-by-line..." . ediff-regions-linewise))
+    '(menu-item "Regions Line-by-line..." ediff-regions-linewise
+		:help "Compare regions line-wise"))
   (define-key menu-bar-ediff-menu [ediff-regions-wordwise]
-    '("Regions Word-by-word..." . ediff-regions-wordwise))
+    '(menu-item "Regions Word-by-word..." ediff-regions-wordwise
+		:help "Compare regions word-wise"))
   (define-key menu-bar-ediff-menu [separator-ediff-regions] '("--"))
   (define-key menu-bar-ediff-menu [ediff-dir-revision]
-    '("Directory Revisions..." . ediff-directory-revisions))
+    '(menu-item "Directory Revisions..." ediff-directory-revisions
+		:help "Compare directory files with their older versions"))
   (define-key menu-bar-ediff-menu [ediff-revision]
-    '("File with Revision..." . ediff-revision))
+    '(menu-item "File with Revision..." ediff-revision
+		:help "Compare file with its older versions"))
   (define-key menu-bar-ediff-menu [separator-ediff-directories] '("--"))
   (define-key menu-bar-ediff-menu [ediff-directories3]
-    '("Three Directories..." . ediff-directories3))
+    '(menu-item "Three Directories..." ediff-directories3
+		:help "Compare files common to three directories simultaneously"))
   (define-key menu-bar-ediff-menu [ediff-directories]
-    '("Two Directories..." . ediff-directories))
+    '(menu-item "Two Directories..." ediff-directories
+		:help "Compare files common to two directories simultaneously"))
   (define-key menu-bar-ediff-menu [separator-ediff-files] '("--"))
   (define-key menu-bar-ediff-menu [ediff-buffers3]
-    '("Three Buffers..." . ediff-buffers3))
+    '(menu-item "Three Buffers..." ediff-buffers3
+		:help "Compare three buffers simultaneously"))
   (define-key menu-bar-ediff-menu [ediff-files3]
-    '("Three Files..." . ediff-files3))
+    '(menu-item "Three Files..." ediff-files3
+		:help "Compare three files simultaneously"))
   (define-key menu-bar-ediff-menu [ediff-buffers]
-    '("Two Buffers..." . ediff-buffers))
+    '(menu-item "Two Buffers..." ediff-buffers
+		:help "Compare two buffers simultaneously"))
   (define-key menu-bar-ediff-menu [ediff-files]
-    '("Two Files..." . ediff-files))
+    '(menu-item "Two Files..." ediff-files
+		:help "Compare two files simultaneously"))
 
-  ;; define merge menu
+  ;; define ediff merge menu
   (define-key
     menu-bar-ediff-merge-menu [ediff-merge-dir-revisions-with-ancestor]
-    '("Directory Revisions with Ancestor..."
-      . ediff-merge-directory-revisions-with-ancestor))
+    '(menu-item "Directory Revisions with Ancestor..."
+      ediff-merge-directory-revisions-with-ancestor
+      :help "Merge versions of the files in the same directory by comparing the files with common ancestors"))
   (define-key
     menu-bar-ediff-merge-menu [ediff-merge-dir-revisions]
-    '("Directory Revisions..." . ediff-merge-directory-revisions))
+    '(menu-item "Directory Revisions..." ediff-merge-directory-revisions
+      :help "Merge versions of the files in the same directory (without using ancestor information)"))
   (define-key
     menu-bar-ediff-merge-menu [ediff-merge-revisions-with-ancestor]
-    '("Revisions with Ancestor..."
-      . ediff-merge-revisions-with-ancestor))
+    '(menu-item "Revisions with Ancestor..."
+      ediff-merge-revisions-with-ancestor
+      :help "Merge versions of the same file by comparing them with a common ancestor"))
   (define-key menu-bar-ediff-merge-menu [ediff-merge-revisions]
-    '("Revisions..." . ediff-merge-revisions))
+    '(menu-item "Revisions..." ediff-merge-revisions
+      :help "Merge versions of the same file (without using ancestor information)"))
   (define-key menu-bar-ediff-merge-menu [separator-ediff-merge] '("--"))
   (define-key
     menu-bar-ediff-merge-menu [ediff-merge-directories-with-ancestor]
-    '("Directories with Ancestor..."
-      . ediff-merge-directories-with-ancestor))
+    '(menu-item "Directories with Ancestor..."
+      ediff-merge-directories-with-ancestor
+      :help "Merge files common to a pair of directories by comparing the files with common ancestors"))
   (define-key menu-bar-ediff-merge-menu [ediff-merge-directories]
-    '("Directories..." . ediff-merge-directories))
+    '(menu-item "Directories..." ediff-merge-directories
+		:help "Merge files common to a pair of directories"))
   (define-key
     menu-bar-ediff-merge-menu [separator-ediff-merge-dirs] '("--"))
   (define-key
     menu-bar-ediff-merge-menu [ediff-merge-buffers-with-ancestor]
-    '("Buffers with Ancestor..." . ediff-merge-buffers-with-ancestor))
+    '(menu-item "Buffers with Ancestor..." ediff-merge-buffers-with-ancestor
+      :help "Merge buffers by comparing their contents with a common ancestor"))
   (define-key menu-bar-ediff-merge-menu [ediff-merge-buffers]
-    '("Buffers..." . ediff-merge-buffers))
+    '(menu-item "Buffers..." ediff-merge-buffers
+      :help "Merge buffers (without using ancestor information)"))
   (define-key menu-bar-ediff-merge-menu [ediff-merge-files-with-ancestor]
-    '("Files with Ancestor..." . ediff-merge-files-with-ancestor))
+    '(menu-item "Files with Ancestor..." ediff-merge-files-with-ancestor
+      :help "Merge files by comparing them with a common ancestor"))
   (define-key menu-bar-ediff-merge-menu [ediff-merge-files]
-    '("Files..." . ediff-merge-files))
+    '(menu-item "Files..." ediff-merge-files
+      :help "Merge files (without using ancestor information)"))
 
   ;; define epatch menu
   (define-key menu-bar-epatch-menu [ediff-patch-buffer]
-    '("To a Buffer..." . ediff-patch-buffer))
+    '(menu-item "To a Buffer..." ediff-patch-buffer
+      :help "Apply a patch to the contents of a buffer"))
   (define-key menu-bar-epatch-menu [ediff-patch-file]
-    '("To a File..." . ediff-patch-file))
+    '(menu-item "To a File..." ediff-patch-file
+      :help "Apply a patch to a file"))
 
   ;; define ediff miscellanea
   (define-key menu-bar-ediff-misc-menu [emultiframe]
-    '("Toggle use of separate control buffer frame"
-      . ediff-toggle-multiframe))
+    '(menu-item "Toggle use of separate control buffer frame"
+      ediff-toggle-multiframe
+      :help "Switch between the single-frame presentation mode and the multi-frame mode"))
   (define-key menu-bar-ediff-misc-menu [eregistry]
-    '("List Ediff Sessions" . ediff-show-registry))
+    '(menu-item "List Ediff Sessions" ediff-show-registry
+		:help "List all active Ediff sessions; it is a convenient way to find and resume such a session"))
   (define-key menu-bar-ediff-misc-menu [ediff-cust]
-    '("Customize Ediff" . ediff-customize))
+    '(menu-item "Customize Ediff" ediff-customize
+		:help "Change some of the parameters that govern the behavior of Ediff"))
   (define-key menu-bar-ediff-misc-menu [ediff-doc]
-    '("Ediff Manual" . ediff-documentation)))
+    '(menu-item "Ediff Manual" ediff-documentation
+		:help "Bring up the Ediff manual")))
 
 (provide 'ediff-hook)
 
