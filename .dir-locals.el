b08ddfb407a2017fd781fadf6cc2573909a2b664
Dan Nicolaescu <dann@ics.uci.edu>
2008-12-24 00:45:51 +0000

    * make-dist (tempdir): Distribute .dir-locals.el.
    * .dir-locals.el: New file.

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+((nil . ((indent-tabs-mode . t)
+         (tab-width . 8)
+         (fill-column . 80)))
+ (c-mode . ((c-file-style . "GNU")))
+ (change-log-mode . ((add-log-time-zone-rule . t)))
+ ("lisp/mail/pmail*"
+  . ((nil . ((change-log-default-name . "ChangeLog.pmail"))))))
