b5c3fb2111792133b2c5a788145efaa8101602bf
Miles Bader <miles@gnu.org>
2008-12-26 02:31:57 +0000

    Changes from arch/CVS synchronization

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,3 +5,5 @@
  (change-log-mode . ((add-log-time-zone-rule . t)))
  ("lisp/mail/pmail*"
   . ((nil . ((change-log-default-name . "ChangeLog.pmail"))))))
+
+;; arch-tag: fb93c160-e9fe-4184-aad1-e4f5daa11cbd
