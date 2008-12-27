bda28f63f85748466238fe70590ec607366d8014
Dan Nicolaescu <dann@ics.uci.edu>
2008-12-27 17:10:24 +0000

    Remove non-working entry for pmail. (fill-column): Fix typo. (change-log-mode): Add fill column.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,9 +1,8 @@
 ((nil . ((indent-tabs-mode . t)
          (tab-width . 8)
-         (fill-column . 80)))
+         (fill-column . 70)))
  (c-mode . ((c-file-style . "GNU")))
- (change-log-mode . ((add-log-time-zone-rule . t)))
- ("lisp/mail/pmail*"
-  . ((nil . ((change-log-default-name . "ChangeLog.pmail"))))))
+ (change-log-mode . ((add-log-time-zone-rule . t)
+		     (fill-column . 74))))
 
 ;; arch-tag: fb93c160-e9fe-4184-aad1-e4f5daa11cbd
