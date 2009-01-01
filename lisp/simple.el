263bc3fa9417fa1f7a89534ff1572b040801f696
Martin Rudalics <rudalics@gmx.at>
2009-01-01 09:14:42 +0000

    (minibuffer-default-add-shell-commands): Fix doc-string typo.

--- lisp/simple.el
+++ lisp/simple.el
@@ -1995,7 +1995,7 @@ stdout will be intermixed in the output stream.")
 (declare-function mailcap-file-default-commands "mailcap" (files))
 
 (defun minibuffer-default-add-shell-commands ()
-  "Return a list of all commands associted with the current file.
+  "Return a list of all commands associated with the current file.
 This function is used to add all related commands retrieved by `mailcap'
 to the end of the list of defaults just after the default value."
   (interactive)
