3799447ca4e052cc97a6daafe1149d56b6def002
Eli Zaretskii <eliz@gnu.org>
2009-01-03 15:09:53 +0000

    Fix copyright years.

--- src/vm-limit.c
+++ src/vm-limit.c
@@ -1,6 +1,6 @@
 /* Functions for memory limit warnings.
    Copyright (C) 1990, 1992, 2001, 2002, 2003, 2004, 2005, 2006, 2007,
-                 2008  Free Software Foundation, Inc.
+                 2008, 2009  Free Software Foundation, Inc.
 
 This file is part of GNU Emacs.
 
