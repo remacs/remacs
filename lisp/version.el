1746e0c3fe423a17683f63743b037529d163ade3
Glenn Morris <rgm@gnu.org>
2009-01-05 00:49:36 +0000

    Fix typo in previous.

--- lisp/version.el
+++ lisp/version.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 1985, 1992, 1994, 1995, 1999, 2000, 2001, 2002,
 ;;   2003, 2004, 2005, 2006, 2007, 2008, 2009
-;    Free Software Foundation, Inc.
+;;   Free Software Foundation, Inc.
 
 ;; Maintainer: FSF
 ;; Keywords: internal
