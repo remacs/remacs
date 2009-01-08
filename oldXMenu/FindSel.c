29b3066a1d5e94c83ba99a159e033931c9f416f4
Glenn Morris <rgm@gnu.org>
2009-01-08 03:20:26 +0000

    Add 2009 to copyright years. Add permissions notice.

--- oldXMenu/FindSel.c
+++ oldXMenu/FindSel.c
@@ -1,8 +1,24 @@
+/* Copyright    Massachusetts Institute of Technology    1985	*/
+
 #include "copyright.h"
 
-/* Copyright    Massachusetts Institute of Technology    1985	*/
-/* Copyright (C) 2001, 2002, 2003, 2004, 2005,
-                 2006, 2007, 2008 Free Software Foundation, Inc.  */
+/*
+Copyright (C) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008,
+  2009  Free Software Foundation, Inc.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 /*
  * XMenu:	MIT Project Athena, X Window system menu package
