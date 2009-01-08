bb892cdec70b2f937e6b7f7597c5f1c7f467c9ea
Glenn Morris <rgm@gnu.org>
2009-01-08 03:20:38 +0000

    Add 2009 to copyright years.

--- oldXMenu/insque.c
+++ oldXMenu/insque.c
@@ -1,5 +1,6 @@
-/* Copyright (C) 1993, 1994, 1995, 1996, 1997, 1998, 2001, 2002, 2003,
-                 2004, 2005, 2006, 2007, 2008 Free Software Foundation, Inc.
+/*
+Copyright (C) 1993, 1994, 1995, 1996, 1997, 1998, 2001, 2002, 2003,
+  2004, 2005, 2006, 2007, 2008, 2009  Free Software Foundation, Inc.
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
