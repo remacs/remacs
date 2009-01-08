0a64641e5b37c07371ef261a2a142775c7c30808
Glenn Morris <rgm@gnu.org>
2009-01-08 03:52:24 +0000

    Revert copyright year changes for files that are not part of Emacs.

--- src/tparam.c
+++ src/tparam.c
@@ -1,6 +1,6 @@
 /* Merge parameters into a termcap entry string.
    Copyright (C) 1985, 1987, 1993, 1995, 2000, 2001, 2002, 2003, 2004,
-                 2005, 2006, 2007, 2008, 2009  Free Software Foundation, Inc.
+                 2005, 2006, 2007, 2008  Free Software Foundation, Inc.
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
