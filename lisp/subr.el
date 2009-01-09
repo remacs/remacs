d293848d9d01f87cb1d827dbb32d911508960b5a
Glenn Morris <rgm@gnu.org>
2009-01-09 03:28:56 +0000

    (last-input-char, last-command-char): Move here from src/keyboard.c.

--- lisp/subr.el
+++ lisp/subr.el
@@ -1059,6 +1059,16 @@ to reread, so it now uses nil to mean `no event', instead of -1."
 (make-obsolete-variable 'translation-table-for-input nil "23.1")
 
 (defvaralias 'messages-buffer-max-lines 'message-log-max)
+
+;; These aliases exist in Emacs 19.34, and probably before, but were
+;; only marked as obsolete in 23.1.
+;; The lisp manual (since at least Emacs 21) descrribes them as
+;; existing "for compatibility with Emacs version 18".
+(define-obsolete-variable-alias 'last-input-char 'last-input-event
+  "at least 19.34")
+(define-obsolete-variable-alias 'last-command-char 'last-command-event
+  "at least 19.34")
+
 
 ;;;; Alternate names for functions - these are not being phased out.
 
