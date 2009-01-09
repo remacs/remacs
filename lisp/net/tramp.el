c28f19e5ac0b55c620116cc403b1c25d97ca2132
Michael Albinus <michael.albinus@gmx.de>
2009-01-09 08:15:27 +0000

    * net/tramp.el: Revert previous patch.  `last-command-char' is used in code used only for XEmacs, where it is different from `last-command-event'.

--- lisp/net/tramp.el
+++ lisp/net/tramp.el
@@ -3738,10 +3738,10 @@ beginning of local filename are not substituted."
 	      ;; We don't need to handle `last-input-event', because
 	      ;; due to the key map we know it must be ?/ or ?~.
 	      (let ((s (concat (buffer-substring (point-min) (point))
-			       (string last-command-event))))
+			       (string last-command-char))))
 		(delete-region (point-min) (point))
 		(insert (substitute-in-file-name s))
-		(setq ad-return-value last-command-event))
+		(setq ad-return-value last-command-char))
 	    ad-do-it))))
 
    '(minibuffer-electric-separator
