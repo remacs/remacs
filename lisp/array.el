d53fa5920ce28b1c1255dcec69a9d1a36e89fba9
Glenn Morris <rgm@gnu.org>
2009-01-10 22:53:56 +0000

    Comment (minor header format fixes).

--- lisp/array.el
+++ lisp/array.el
@@ -1,9 +1,9 @@
 ;;; array.el --- array editing commands for GNU Emacs
 
-;; Copyright (C) 1987, 2000, 2001, 2002, 2003, 2004,
-;;   2005, 2006, 2007, 2008, 2009 Free Software Foundation, Inc.
+;; Copyright (C) 1987, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007,
+;;   2008, 2009 Free Software Foundation, Inc.
 
-;; Author David M. Brown
+;; Author: David M. Brown
 ;; Maintainer: FSF
 ;; Keywords: extensions
 
