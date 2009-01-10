9032435905078afe805465ae42522f2af22bf1e3
Glenn Morris <rgm@gnu.org>
2009-01-10 22:53:08 +0000

    Comment (add Author:, based on authors.el).

--- lisp/diff.el
+++ lisp/diff.el
@@ -1,8 +1,10 @@
 ;;; diff.el --- run `diff' in compilation-mode
 
-;; Copyright (C) 1992, 1994, 1996, 2001, 2002, 2003, 2004,
-;;   2005, 2006, 2007, 2008, 2009 Free Software Foundation, Inc.
+;; Copyright (C) 1992, 1994, 1996, 2001, 2002, 2003, 2004, 2005, 2006,
+;;   2007, 2008, 2009 Free Software Foundation, Inc.
 
+;; Author: Frank Bresz
+;; (according to authors.el)
 ;; Maintainer: FSF
 ;; Keywords: unix, tools
 
--- src/xrdb.c
+++ src/xrdb.c
@@ -2,6 +2,9 @@
    Copyright (C) 1990, 1993, 1994, 2000, 2001, 2002, 2003, 2004,
                  2005, 2006, 2007, 2008, 2009 Free Software Foundation, Inc.
 
+Author: Joseph Arceneaux
+Created: 4/90
+
 This file is part of GNU Emacs.
 
 GNU Emacs is free software: you can redistribute it and/or modify
@@ -16,8 +19,6 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.  */
-
-/* Written by jla, 4/90 */
 
 #ifdef emacs
 #include <config.h>
