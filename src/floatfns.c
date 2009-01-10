0a9dd3a7545f2527f1098b981d1a23ec2ebf468d
Glenn Morris <rgm@gnu.org>
2009-01-10 23:42:02 +0000

    Comment (add Author:, based on ack.texi).

--- src/floatfns.c
+++ src/floatfns.c
@@ -2,6 +2,9 @@
    Copyright (C) 1988, 1993, 1994, 1999, 2001, 2002, 2003, 2004,
                  2005, 2006, 2007, 2008, 2009  Free Software Foundation, Inc.
 
+Author: Wolfgang Rupprecht
+(according to ack.texi)
+
 This file is part of GNU Emacs.
 
 GNU Emacs is free software: you can redistribute it and/or modify
