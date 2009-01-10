76ced33856e0f39c9afe81d503749782deaac0f5
Glenn Morris <rgm@gnu.org>
2009-01-10 22:03:12 +0000

    Comment (add an author based on ack.texi).

--- lisp/textmodes/page-ext.el
+++ lisp/textmodes/page-ext.el
@@ -3,7 +3,8 @@
 ;; Copyright (C) 1990, 1991, 1993, 1994, 2001, 2002, 2003, 2004, 2005, 2006,
 ;;   2007, 2008, 2009  Free Software Foundation, Inc.
 
-;; Maintainer: Robert J. Chassell <bob@gnu.org>
+;; Author: Robert J. Chassell <bob@gnu.org>
+;; (according to ack.texi)
 ;; Keywords: wp data
 
 ;; This file is part of GNU Emacs.
