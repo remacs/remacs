eedcb417fa241eab63fc1cc9d9a94233050e8779
Glenn Morris <rgm@gnu.org>
2009-01-10 23:01:48 +0000

    Comment (add Author:, based on authors.el).

--- lib-src/hexl.c
+++ lib-src/hexl.c
@@ -1,6 +1,9 @@
 /* Convert files for Emacs Hexl mode.
-   Copyright (C) 1989, 2001, 2002, 2003, 2004, 2005, 2006, 2007,
-                 2008, 2009  Free Software Foundation, Inc.
+   Copyright (C) 1989, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008,
+                 2009  Free Software Foundation, Inc.
+
+Author: Keith Gabryelski
+(according to authors.el)
 
 This file is not considered part of GNU Emacs.
 
