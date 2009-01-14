c2ad0dc9a8f6ac5aba84d4629042c653cf0e1cea
Juri Linkov <juri@jurta.org>
2009-01-14 23:16:35 +0000

    (change-log-mode): Remove bug-reference-mode.

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,7 +4,6 @@
  (change-log-mode . ((add-log-time-zone-rule . t)
 		     (fill-column . 74)
 		     (bug-reference-url-format
-                      . "http://emacsbugs.donarmstrong.com/cgi-bin/bugreport.cgi?bug=%s")
-		     (mode . bug-reference))))
+                      . "http://emacsbugs.donarmstrong.com/cgi-bin/bugreport.cgi?bug=%s"))))
 
 ;; arch-tag: fb93c160-e9fe-4184-aad1-e4f5daa11cbd
