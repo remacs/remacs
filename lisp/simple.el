337c561c774d3940249be381625cdf4294471099
Chong Yidong <cyd@stupidchicken.com>
2009-04-06 01:21:48 +0000

    * simple.el (handle-shift-selection): Clarify docstring.

--- lisp/simple.el
+++ lisp/simple.el
@@ -3760,20 +3760,20 @@ shift-translation."
   :group 'editing-basics)
 
 (defun handle-shift-selection ()
-  "Activate/deactivate mark depending on invocation thru ``shift translation.''
+  "Activate/deactivate mark depending on invocation thru shift translation.
+This function is called by `call-interactively' when a command
+with a `^' character in its `interactive' spec is invoked, before
+running the command itself.
 
-\(See `this-command-keys-shift-translated' for the meaning of
-shift translation.)
+If `shift-select-mode' is enabled and the command was invoked
+through shift translation, set the mark and activate the region
+temporarily, unless it was already set in this way.  See
+`this-command-keys-shift-translated' for the meaning of shift
+translation.
 
-This is called whenever a command with a `^' character in its
-`interactive' spec is invoked.
-Its behavior is controlled by `shift-select-mode'.
-
-If the command was invoked through shift translation, set the
-mark and activate the region temporarily, unless it was already
-set in this way.  If the command was invoked without shift
-translation, or if the region was activated by the mouse,
-deactivate the mark if the region is temporarily active."
+Otherwise, if the region has been activated temporarily,
+deactivate it, and restore the variable `transient-mark-mode' to
+its earlier value."
   (cond ((and shift-select-mode this-command-keys-shift-translated)
          (unless (and mark-active
                       (eq (car-safe transient-mark-mode) 'only))
