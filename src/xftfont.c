99061dfcf68b52602e134fdb1a0bd3417546f7a8
Chong Yidong <cyd@stupidchicken.com>
2009-06-30 22:07:44 +0000

    * xftfont.c (xftfont_open): Avoid passing NULL argument to XftLockFace.

--- src/xftfont.c
+++ src/xftfont.c
@@ -287,14 +287,15 @@ xftfont_open (f, entity, pixel_size)
   match = XftFontMatch (display, FRAME_X_SCREEN_NUMBER (f), pat, &result);
   FcPatternDestroy (pat);
   xftfont = XftFontOpenPattern (display, match);
-  ft_face = XftLockFace (xftfont);
-  UNBLOCK_INPUT;
-
-  if (! xftfont)
+  if (!xftfont)
     {
+      UNBLOCK_INPUT;
       XftPatternDestroy (match);
       return Qnil;
     }
+  ft_face = XftLockFace (xftfont);
+  UNBLOCK_INPUT;
+
   /* We should not destroy PAT here because it is kept in XFTFONT and
      destroyed automatically when XFTFONT is closed.  */
   font_object = font_make_object (VECSIZE (struct xftfont_info), entity, size);
