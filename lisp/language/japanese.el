40149292c2bd97339d7849ad5d28f1e4419c2f33
Kenichi Handa <handa@m17n.org>
2009-07-08 02:19:24 +0000

    (japanese-shift-jis-2004): Fix typo in the docstring.

--- lisp/language/japanese.el
+++ lisp/language/japanese.el
@@ -183,7 +183,7 @@ eucJP-ms is defined in <http://www.opengroup.or.jp/jvc/cde/appendix.html>."
 (define-coding-system-alias 'euc-jisx0213 'euc-jis-2004)
 
 (define-coding-system 'japanese-shift-jis-2004
-  "Shift_JIS 8-bit encodinf for Japanese (MIME:SHIFT_JIS-2004)"
+  "Shift_JIS 8-bit encoding for Japanese (MIME:SHIFT_JIS-2004)"
   :coding-type 'shift-jis
   :mnemonic ?S
   :charset-list '(ascii katakana-jisx0201 
