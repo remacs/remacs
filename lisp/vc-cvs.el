53dab179d324b7f92daa1b1b4b2776ac073eaf21
Sam Steingold <sds@gnu.org>
2009-08-14 04:24:01 +0000

    (vc-cvs-merge-news): Yet annother fix of message parsing.

--- lisp/vc-cvs.el
+++ lisp/vc-cvs.el
@@ -458,7 +458,8 @@ The changes are between FIRST-REVISION and SECOND-REVISION."
           (if (re-search-forward
                (concat "^\\([CMUP] \\)?"
                        (regexp-quote
-                        (substring file (length default-directory)))
+                        (substring file (length (expand-file-name
+                                                 "." default-directory))))
                        "\\( already contains the differences between \\)?")
                nil t)
               (cond
