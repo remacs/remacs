729eaddaca2d5f683dcd6d4a587f4db84636c486
Eli Zaretskii <eliz@gnu.org>
2009-08-22 10:26:14 +0000

    (encode_coding_string): Don't encode unibyte strings.  (Bug#4047)

--- src/coding.h
+++ src/coding.h
@@ -738,9 +738,10 @@ extern void encode_coding_object P_ ((struct coding_system *,
 			SBYTES (string), Qt)
 
 #define encode_coding_string(coding, string, nocopy)			\
-  (encode_coding_object (coding, string, 0, 0, SCHARS (string),		\
-			 SBYTES (string), Qt),				\
-   (coding)->dst_object)
+  (STRING_MULTIBYTE(string) ?						\
+    (encode_coding_object (coding, string, 0, 0, SCHARS (string),	\
+			   SBYTES (string), Qt),			\
+     (coding)->dst_object) : (string))
 
 
 #define decode_coding_c_string(coding, src, bytes, dst_object)		\
