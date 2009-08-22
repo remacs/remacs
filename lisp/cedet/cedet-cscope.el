4c0cf01cb4912e6396aa0797fa92e640b0638bdc
Chong Yidong <cyd@stupidchicken.com>
2009-08-22 21:34:12 +0000

    cedet-cscope.el: Fix copyright header.

--- lisp/cedet/cedet-cscope.el
+++ lisp/cedet/cedet-cscope.el
@@ -1,6 +1,6 @@
 ;;; cedet-cscope.el --- CScope support for CEDET
 
-;;; Copyright (C) Free Software Foundation, Inc.
+;;; Copyright (C) 2009 Free Software Foundation, Inc.
 
 ;; Author: Eric M. Ludlam <zappo@gnu.org>
 
