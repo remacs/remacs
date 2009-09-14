cc1cb3da4a836ab02f0506ef31b8dc01c9aafe65
Dan Nicolaescu <dann@ics.uci.edu>
2009-09-14 23:24:56 +0000

    (change-log-mode): Restore bug-reference-mode.

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,6 +4,7 @@
  (change-log-mode . ((add-log-time-zone-rule . t)
 		     (fill-column . 74)
 		     (bug-reference-url-format
-                      . "http://emacsbugs.donarmstrong.com/cgi-bin/bugreport.cgi?bug=%s"))))
+                      . "http://emacsbugs.donarmstrong.com/cgi-bin/bugreport.cgi?bug=%s")
+		     (mode . bug-reference))))
 
 ;; arch-tag: fb93c160-e9fe-4184-aad1-e4f5daa11cbd
