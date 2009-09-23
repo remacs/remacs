f456006161eb12d8ae6e080ef6f1535ef20c9c03
Adrian Robert <Adrian.B.Robert@gmail.com>
2009-09-23 02:35:54 +0000

    * font.c (DEFAULT_ENCODING) [HAVE_NS]: Remove special case.

--- src/font.c
+++ src/font.c
@@ -56,11 +56,7 @@ Lisp_Object Qopentype;
 /* Important character set strings.  */
 Lisp_Object Qascii_0, Qiso8859_1, Qiso10646_1, Qunicode_bmp, Qunicode_sip;
 
-#ifdef HAVE_NS
-#define DEFAULT_ENCODING Qiso10646_1
-#else
 #define DEFAULT_ENCODING Qiso8859_1
-#endif
 
 /* Unicode category `Cf'.  */
 static Lisp_Object QCf;
