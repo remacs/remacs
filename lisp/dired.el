3b77302a4da5576bb4bf746527a135a4dd132e19
Juanma Barranquero <lekktu@gmail.com>
2009-10-11 03:48:10 +0000

    Update.

--- lisp/dired.el
+++ lisp/dired.el
@@ -3461,7 +3461,7 @@ Ask means pop up a menu for the user to select one of copy, move or link."
 ;;;;;;  dired-run-shell-command dired-do-shell-command dired-do-async-shell-command
 ;;;;;;  dired-clean-directory dired-do-print dired-do-touch dired-do-chown
 ;;;;;;  dired-do-chgrp dired-do-chmod dired-compare-directories dired-backup-diff
-;;;;;;  dired-diff) "dired-aux" "dired-aux.el" "19cd0d559197e5587fe27e1a81fb2644")
+;;;;;;  dired-diff) "dired-aux" "dired-aux.el" "6c7ccd455c2cd50d48164ebd5c52e216")
 ;;; Generated autoloads from dired-aux.el
 
 (autoload 'dired-diff "dired-aux" "\
