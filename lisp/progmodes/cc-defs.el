0fe0a1cab0b299045879a5471ecc912373be7b57
Alan Mackenzie <acm@muc.de>
2009-11-18 07:07:32 +0000

    (c-version): Bump to 5.31.8.

--- lisp/progmodes/cc-defs.el
+++ lisp/progmodes/cc-defs.el
@@ -95,7 +95,7 @@
 
 ;;; Variables also used at compile time.
 
-(defconst c-version "5.31.7"
+(defconst c-version "5.31.8"
   "CC Mode version number.")
 
 (defconst c-version-sym (intern c-version))
