cb190d7d5f3a63086e3f28981ca175945305a3c0
Stefan Monnier <monnier@iro.umontreal.ca>
2009-11-25 03:59:19 +0000

    (string-prefix-p): New function.

--- lisp/subr.el
+++ lisp/subr.el
@@ -3193,6 +3193,13 @@ and replace a sub-expression, e.g.
       (setq matches (cons (substring string start l) matches)) ; leftover
       (apply #'concat (nreverse matches)))))
 
+(defun string-prefix-p (str1 str2 &optional ignore-case)
+  "Return non-nil if STR1 is a prefix of STR2.
+If IGNORE-CASE is non-nil, the comparison is done without paying attention
+to case differences."
+  (eq t (compare-strings str1 nil nil
+                         str2 0 (length str1) ignore-case)))
+
 ;;;; invisibility specs
 
 (defun add-to-invisibility-spec (element)
