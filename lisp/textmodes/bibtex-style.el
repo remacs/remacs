f0fd77be4f10df24db76c1dc170593ffa8af9c47
Kevin Ryde <user42@zip.com.au>
2009-11-26 00:01:20 +0000

    * textmodes/bibtex-style.el: Add "Keywords: tex".

--- lisp/textmodes/bibtex-style.el
+++ lisp/textmodes/bibtex-style.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2005, 2007, 2008, 2009  Free Software Foundation, Inc.
 
 ;; Author: Stefan Monnier <monnier@iro.umontreal.ca>
-;; Keywords: 
+;; Keywords: tex
 
 ;; This file is part of GNU Emacs.
 
