2f00e2992cd9ff3118c2aea40e2f878292cd321a
Dan Nicolaescu <dann@ics.uci.edu>
2009-11-26 03:24:41 +0000

    (power_letter): Remove duplicate const.

--- src/xdisp.c
+++ src/xdisp.c
@@ -18076,7 +18076,7 @@ pint2str (buf, width, d)
    readable" representation of the nonnegative integer D to BUF using
    a minimal field width WIDTH.  D should be smaller than 999.5e24. */
 
-static const const char power_letter[] =
+static const char power_letter[] =
   {
     0,	 /* not used */
     'k', /* kilo */
