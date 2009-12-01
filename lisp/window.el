3689984f0081b32ceae6668974a1e04231f6d728
Glenn Morris <rgm@gnu.org>
2009-12-01 03:15:01 +0000

    (window-to-use): Define for compiler.

--- lisp/window.el
+++ lisp/window.el
@@ -1,7 +1,8 @@
 ;;; window.el --- GNU Emacs window commands aside from those written in C
 
 ;; Copyright (C) 1985, 1989, 1992, 1993, 1994, 2000, 2001, 2002,
-;;   2003, 2004, 2005, 2006, 2007, 2008, 2009 Free Software Foundation, Inc.
+;;   2003, 2004, 2005, 2006, 2007, 2008, 2009
+;;   Free Software Foundation, Inc.
 
 ;; Maintainer: FSF
 ;; Keywords: internal
@@ -1034,6 +1035,8 @@ Do not raise the selected frame.  Return WINDOW."
 		     (eq frame (window-frame (minibuffer-selected-window)))))
       (raise-frame frame))
     window))
+
+(defvar window-to-use)           ; dynamically bound in display-buffer
 
 (defun window--display-buffer-2 (buffer window &optional dedicated)
   "Display BUFFER in WINDOW and make its frame visible.
