c3520efda3e255fd23c827b82d183b9a3a9b2487
Glenn Morris <rgm@gnu.org>
2009-12-17 04:12:40 +0000

    (bug-reference-url-format): Change to debbugs.gnu.org.

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,8 +3,7 @@
  (c-mode . ((c-file-style . "GNU")))
  (change-log-mode . ((add-log-time-zone-rule . t)
 		     (fill-column . 74)
-		     (bug-reference-url-format
-                      . "http://emacsbugs.donarmstrong.com/cgi-bin/bugreport.cgi?bug=%s")
+		     (bug-reference-url-format . "http://debbugs.gnu.org/%s")
 		     (mode . bug-reference))))
 
 ;; arch-tag: fb93c160-e9fe-4184-aad1-e4f5daa11cbd
