ad13a3ee9b9f4ee81b19c6c02da8cc338901304e
YAMAMOTO Mitsuharu <mituharu@math.s.chiba-u.ac.jp>
2010-03-20 12:37:39 +0900

    xfns.c (Fx_create_frame) [USE_LUCID]: Add BLOCK_INPUT around XLoadQueryFont.

--- src/xfns.c
+++ src/xfns.c
@@ -3347,7 +3347,9 @@ This function is an internal primitive--use `make-frame' instead.  */)
 #ifdef USE_LUCID
   /* Prevent lwlib/xlwmenu.c from crashing because of a bug
      whereby it fails to get any font.  */
+  BLOCK_INPUT;
   xlwmenu_default_font = XLoadQueryFont (FRAME_X_DISPLAY (f), "fixed");
+  UNBLOCK_INPUT;
 #endif
 
   /* Frame contents get displaced if an embedded X window has a border.  */
