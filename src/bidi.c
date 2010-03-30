b118e65f9d1c4ed14ad8918bf8a299c2a632ab55
Eli Zaretskii <eliz@gnu.org>
2010-03-30 12:55:42 +0300

    bidi.c: Update Copyright years.

--- src/bidi.c
+++ src/bidi.c
@@ -1,5 +1,6 @@
 /* Low-level bidirectional buffer-scanning functions for GNU Emacs.
-   Copyright (C) 2000, 2001, 2004, 2005, 2009	Free Software Foundation, Inc.
+   Copyright (C) 2000, 2001, 2004, 2005, 2009, 2010
+   Free Software Foundation, Inc.
 
 This file is part of GNU Emacs.
 
