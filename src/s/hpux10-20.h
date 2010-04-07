5e5a3b92e4fb79ad986d37c2fe8e58b9ab0518f6
Dan Nicolaescu <dann@ics.uci.edu>
2010-04-06 23:11:45 -0700

    Simplify code for HP machines. * m/hp800.h (LOAD_AVE_TYPE, LOAD_AVE_CVT, NO_REMAP): Do not define for GNU_LINUX, not needed. (UNEXEC, NEED_BSDTTY): Move definitions... * s/hpux10-20.h (UNEXEC, NEED_BSDTTY): ... here.

--- src/s/hpux10-20.h
+++ src/s/hpux10-20.h
@@ -89,6 +89,11 @@ along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #define HAVE_PERROR  /* Delete this line for version 6.  */
 
+#define UNEXEC unexhp9k800.o
+
+/* Include the file bsdtty.h, since this machine has job control.  */
+#define NEED_BSDTTY
+
 /* This is how to get the device name of the tty end of a pty.  */
 #define PTY_TTY_NAME_SPRINTF \
             sprintf (pty_name, "/dev/pty/tty%c%x", c, i);
