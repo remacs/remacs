00030710427af3bb3a1798690985d83e1bcba67c
Dan Nicolaescu <dann@ics.uci.edu>
2010-04-15 14:52:27 -0700

    * frame.h (FRAME_SAMPLE_VISIBILITY): Undo previous change.

--- src/frame.h
+++ src/frame.h
@@ -793,7 +793,7 @@ typedef struct frame *FRAME_PTR;
 
 #define FRAME_SAMPLE_VISIBILITY(f) \
   (((f)->async_visible && (f)->visible != (f)->async_visible) ? \
-   SET_FRAME_GARBAGED (f) : \
+   SET_FRAME_GARBAGED (f) : 0, \
    (f)->visible = (f)->async_visible, \
    (f)->iconified = (f)->async_iconified)
 
