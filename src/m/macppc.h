8e324eb6e0ef1b560391bdf3d83e8937aef231c8
Dan Nicolaescu <dann@ics.uci.edu>
2010-04-23 02:06:43 -0700

    (LD_SWITCH_SYSTEM_TEMACS): Remove #undef. (LD_SWITCH_MACHINE_TEMACS): Remove, configure sets nocombreloc.

--- src/m/macppc.h
+++ src/m/macppc.h
@@ -44,10 +44,6 @@ along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #ifdef GNU_LINUX
 #define LINKER $(CC) -nostdlib
-/* s/gnu-linux.h defines this to `-z nocombreloc' which does not work here
-   because prefix-args is not used.  */
-#undef LD_SWITCH_SYSTEM_TEMACS
-#define LD_SWITCH_MACHINE_TEMACS -Xlinker -znocombreloc
 #ifdef _ARCH_PPC64
 #undef START_FILES
 #define START_FILES pre-crt0.o /usr/lib64/crt1.o /usr/lib64/crti.o
