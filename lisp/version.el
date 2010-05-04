8838172bf0c35831f6b0f0a9763af0d04edf5468
Chong Yidong <cyd@stupidchicken.com>
2010-05-03 21:31:59 -0400

    Bump version to 23.1.97.

--- lisp/version.el
+++ lisp/version.el
@@ -32,7 +32,7 @@
 (defconst emacs-copyright "Copyright (C) 2010 Free Software Foundation, Inc." "\
 Short copyright string for this version of Emacs.")
 
-(defconst emacs-version "23.1.96" "\
+(defconst emacs-version "23.1.97" "\
 Version numbers of this version of Emacs.")
 
 (defconst emacs-major-version (progn (string-match "^[0-9]+" emacs-version) (string-to-number (match-string 0 emacs-version))) "\
