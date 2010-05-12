706deb23dce31d2af3e672c3f30d0a578c023c12
Katsumi Yamaoka <yamaoka@jpl.org>
2010-05-12 08:24:25 +0000

    Synch with Gnus trunk. (message-forward-make-body-plain, message-forward-make-body-mml):  Use mm-multibyte-string-p instead of multibyte-string-p.

--- lisp/gnus/message.el
+++ lisp/gnus/message.el
@@ -7165,7 +7165,7 @@ Optional DIGEST will use digest to forward."
 	(contents (with-current-buffer forward-buffer (buffer-string)))
 	e)
     (unless (featurep 'xemacs)
-      (unless (multibyte-string-p contents)
+      (unless (mm-multibyte-string-p contents)
 	(error "Attempt to insert unibyte string from the buffer \"%s\"\
  to the multibyte buffer \"%s\""
 	       (if (bufferp forward-buffer)
@@ -7220,7 +7220,7 @@ Optional DIGEST will use digest to forward."
     (if (not message-forward-decoded-p)
 	(let ((contents (with-current-buffer forward-buffer (buffer-string))))
 	  (unless (featurep 'xemacs)
-	    (unless (multibyte-string-p contents)
+	    (unless (mm-multibyte-string-p contents)
 	      (error "Attempt to insert unibyte string from the buffer \"%s\"\
  to the multibyte buffer \"%s\""
 		     (if (bufferp forward-buffer)
