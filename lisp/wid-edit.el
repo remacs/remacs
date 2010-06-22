6095a05b11f7457b3dad3db638bf941e76a030db
Glenn Morris <rgm@gnu.org>
2010-06-21 20:24:22 -0700

    * lisp/wid-edit.el (widget-complete): Doc fix.

--- lisp/wid-edit.el
+++ lisp/wid-edit.el
@@ -1156,9 +1156,12 @@ the field."
     (if field
 	(narrow-to-region (line-beginning-position) (line-end-position)))))
 
+;; This used to say:
+;; "When not inside a field, move to the previous button or field."
+;; but AFAICS, it has always just thrown an error.
 (defun widget-complete ()
   "Complete content of editable field from point.
-When not inside a field, move to the previous button or field."
+When not inside a field, signal an error."
   (interactive)
   (let ((field (widget-field-find (point))))
     (if field
