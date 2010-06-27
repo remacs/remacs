3726248fdce1550b9dfa9a2836f5d419a0259461
Chong Yidong <cyd@stupidchicken.com>
2010-06-27 14:03:26 -0400

    Remove unnecessary let-bindings in bookmark.el (Bug#6515)
    * bookmark.el (bookmark-bmenu-2-window)
    (bookmark-bmenu-other-window)
    (bookmark-bmenu-other-window-with-mouse): Remove unnecessary
    bindings of bookmark-automatically-show-annotations (Bug#6515).

--- lisp/bookmark.el
+++ lisp/bookmark.el
@@ -1854,8 +1854,7 @@ With a prefix arg, prompts for a file to save them in."
         (pop-up-windows t))
     (delete-other-windows)
     (switch-to-buffer (other-buffer))
-    (let ((bookmark-automatically-show-annotations nil)) ;FIXME: needed?
-      (bookmark--jump-via bmrk 'pop-to-buffer))
+    (bookmark--jump-via bmrk 'pop-to-buffer)
     (bury-buffer menu)))
 
 
@@ -1869,8 +1868,7 @@ With a prefix arg, prompts for a file to save them in."
   "Select this line's bookmark in other window, leaving bookmark menu visible."
   (interactive)
   (let ((bookmark (bookmark-bmenu-bookmark)))
-    (let ((bookmark-automatically-show-annotations t)) ;FIXME: needed?
-      (bookmark--jump-via bookmark 'switch-to-buffer-other-window))))
+    (bookmark--jump-via bookmark 'switch-to-buffer-other-window)))
 
 
 (defun bookmark-bmenu-switch-other-window ()
@@ -1881,8 +1879,7 @@ The current window remains selected."
         (pop-up-windows t)
         same-window-buffer-names
         same-window-regexps)
-    (let ((bookmark-automatically-show-annotations t)) ;FIXME: needed?
-      (bookmark--jump-via bookmark 'display-buffer))))
+    (bookmark--jump-via bookmark 'display-buffer)))
 
 (defun bookmark-bmenu-other-window-with-mouse (event)
   "Select bookmark at the mouse pointer in other window, leaving bookmark menu visible."
