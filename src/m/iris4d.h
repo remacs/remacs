6ba577cbed5eddcf89c6707a5be838a4f598fbf6
Dan Nicolaescu <dann@ics.uci.edu>
2010-07-26 21:03:14 -0700

    * src/m/iris4d.h (XUINT, XSET): Remove, not needed.

--- src/m/iris4d.h
+++ src/m/iris4d.h
@@ -36,17 +36,5 @@ along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.  */
 #define DATA_START 0x10000000
 #define DATA_SEG_BITS	0x10000000
 
-
-#if _MIPS_SZLONG != 64
-/* fixme: should there be 64-bit definitions?  (The ones below aren't OK.)  */
-
-/* The standard definitions of these macros would work ok,
-   but these are faster because the constants are short.  */
-#define XUINT(a) (((unsigned)(a) << BITS_PER_INT-VALBITS) >> BITS_PER_INT-VALBITS)
-
-#define XSET(var, type, ptr) \
-   ((var) = ((int)(type) << VALBITS) + (((unsigned) (ptr) << BITS_PER_INT-VALBITS) >> BITS_PER_INT-VALBITS))
-#endif /* _LP64 */
-
 /* arch-tag: fff5e139-9ae0-465d-afec-837c41ea0aa6
    (do not change this comment) */
