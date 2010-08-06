32bc67090a4a4a25cc299644b94d726c4949c7d2
Dan Nicolaescu <dann@ics.uci.edu>
2010-08-06 13:08:26 -0700

    * src/process.c: Simplify include logic.

--- src/process.c
+++ src/process.c
@@ -64,19 +64,13 @@ along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.  */
 #if !defined (O_NDELAY) && defined (HAVE_PTYS) && !defined(USG5)
 #include <fcntl.h>
 #endif /* HAVE_PTYS and no O_NDELAY */
+#if defined(HAVE_NET_IF_H)
+#include <net/if.h>
+#endif /* HAVE_NET_IF_H */
 #endif /* HAVE_SYS_IOCTL_H */
 
 #ifdef NEED_BSDTTY
 #include <bsdtty.h>
-#endif
-
-/* Can we use SIOCGIFCONF and/or SIOCGIFADDR */
-#if defined(HAVE_SYS_IOCTL_H) && defined(HAVE_NET_IF_H)
-/* sys/ioctl.h may have been included already */
-#ifndef SIOCGIFADDR
-#include <sys/ioctl.h>
-#endif
-#include <net/if.h>
 #endif
 
 #ifdef HAVE_SYS_WAIT
