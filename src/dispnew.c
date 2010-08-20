b43c883c71bc3cf494b17dde69d275c73d10942e
Eli Zaretskii <eliz@gnu.org>
2010-08-20 23:55:09 +0300

    Fix last change in buffer_posn_from_coords for text terminals.
     dispnew.c (buffer_posn_from_coords): Add one-character offset for R2L lines.

--- src/dispnew.c
+++ src/dispnew.c
@@ -5402,7 +5402,10 @@ buffer_posn_from_coords (struct window *w, int *x, int *y, struct display_pos *p
        iterator doesn't know about that.)  The following line adjusts
        the pixel position to the iterator geometry, which is what
        move_it_* routines use.  */
-    to_x = window_box_width (w, TEXT_AREA) - to_x;
+    to_x = window_box_width (w, TEXT_AREA) - to_x
+	   /* Text terminals need a one-character offset to get it right.  */
+	   - (FRAME_MSDOS_P (WINDOW_XFRAME (w))
+	      || FRAME_TERMCAP_P (WINDOW_XFRAME (w)));
 
   /* Now move horizontally in the row to the glyph under *X. */
   move_it_in_display_line (&it, ZV, to_x, MOVE_TO_X);
