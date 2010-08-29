96ae4c8fa704b0385d6f2cf10b69bf289e2fb7ef
Chong Yidong <cyd@stupidchicken.com>
2010-08-29 18:15:09 -0400

    Merge Finder and package-menu functionality.
    * lisp/finder.el: Require `package'.
    (finder-known-keywords): Tweak descriptions.  Retire `oop' keyword.
    (finder-package-info): Var deleted.
    (finder-keywords-hash, finder--builtins-alist): New vars.
    (finder-compile-keywords): Compute package--builtins and
    finder-keywords-hash instead of finder-keywords-hash, respecting
    the "Package" header.
    (finder-unknown-keywords, finder-list-matches): Use
    finder-keywords-hash and package--list-packages.
    (finder-mode): Don't set font-lock-defaults.
    (finder-exit): We don't use "*Finder-package*" and "*Finder
    Category*" buffers anymore.
    
    * lisp/info.el (Info-finder-find-node): Search package-alist instead of
    finder-package-info.
    
    * lisp/emacs-lisp/package.el (package--builtins-base): Var deleted.
    (package--builtins): Set default value to nil.
    (package-initialize): Load precomputed value of package--builtins
    from finder-inf.el.
    (package-alist, package-compute-transaction)
    (package-download-transaction): Improve docstring.
    (package-read-all-archive-contents): Do not change
    package--builtins here.
    (list-packages): Make package-list-packages an alias for this.
    Sort by status by default.
    (package--list-packages): Add optional PACKAGES arg.
    (describe-package-1): Use font-lock-face property.  For built-in
    packages, insert file commentary.
    (package--generate-package-list): Rename from
    package-list-packages-internal; all callers changed.  Add optional
    PACKAGES arg.  Add alphabetical sort fallbacks.
    (package-menu--version-predicate, package-menu--status-predicate)
    (package-menu--description-predicate)
    (package-menu--name-predicate): New functions.

--- lisp/ibuffer.el
+++ lisp/ibuffer.el
@@ -2641,7 +2641,7 @@ will be inserted before the group at point."
 ;;;;;;  ibuffer-backward-filter-group ibuffer-forward-filter-group
 ;;;;;;  ibuffer-toggle-filter-group ibuffer-mouse-toggle-filter-group
 ;;;;;;  ibuffer-interactive-filter-by-mode ibuffer-mouse-filter-by-mode
-;;;;;;  ibuffer-auto-mode) "ibuf-ext" "ibuf-ext.el" "e1272bfdc7c3b6e926b2a68155217303")
+;;;;;;  ibuffer-auto-mode) "ibuf-ext" "ibuf-ext.el" "fa9822b5ef905f06d8a03dc9ce3a2894")
 ;;; Generated autoloads from ibuf-ext.el
 
 (autoload 'ibuffer-auto-mode "ibuf-ext" "\
