ba96ddf511d4c853127d3432d17adb32626c8419
Katsumi Yamaoka <yamaoka@jpl.org>
2010-09-14 06:01:14 +0000

    time-date.el (format-seconds): Comment fix.

--- lisp/calendar/time-date.el
+++ lisp/calendar/time-date.el
@@ -317,10 +317,10 @@ This function does not work for SECONDS greater than `most-positive-fixnum'."
       (setq start (match-end 0)
             spec (match-string 1 string))
       (unless (string-equal spec "%")
-	;; `assoc-string' is not available in Emacs 21.  So when compiling
-	;; Gnus (`time-date.el' is part of Gnus) with Emacs 21, we get a
-	;; warning here.  But `format-seconds' is not used anywhere in Gnus so
-	;; it's not a real problem. --rsteib
+	;; `assoc-string' is not available in XEmacs or Emacs 21.  So when
+	;; compiling Gnus (`time-date.el' is part of Gnus) with XEmacs or
+	;; Emacs 21, we get a warning here.  But `format-seconds' is not
+	;; used anywhere in Gnus so it's not a real problem. --rsteib
         (or (setq match (assoc-string spec units t))
             (error "Bad format specifier: `%s'" spec))
         (if (assoc-string spec usedunits t)
