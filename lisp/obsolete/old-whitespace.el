269c197ee9d1c3c0b8c03c278d76a570740dd624
Stefan Monnier <monnier@iro.umontreal.ca>
2010-09-18 00:28:10 +0200

    * lisp/obsolete/old-whitespace.el (whitespace-rescan-files-in-buffers): Use with-current-buffer.

--- lisp/obsolete/old-whitespace.el
+++ lisp/obsolete/old-whitespace.el
@@ -725,9 +725,8 @@ If timer is not set, then set it to scan the files in
 	(setq bufname (cadr thiselt))
 	(setq buf (get-buffer bufname))
 	(if (buffer-live-p buf)
-	    (save-excursion
+	    (with-current-buffer bufname
 	      ;;(message "buffer %s live" bufname)
-	      (set-buffer bufname)
 	      (if whitespace-mode
 		  (progn
 		    ;;(message "checking for whitespace in %s" bufname)
