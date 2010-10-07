3a3cbf0ad3973f3cf1f67cabdc01c96a8f34f586
Stefan Monnier <monnier@iro.umontreal.ca>
2010-10-07 20:22:07 +0200

    * lisp/gnus/gnus-sum.el (gnus-number): Rename from `number'. (gnus-article-marked-p, gnus-summary-limit-to-display-predicate) (gnus-summary-limit-children): Update uses correspondingly.

--- lisp/gnus/gnus-sum.el
+++ lisp/gnus/gnus-sum.el
@@ -5682,17 +5682,17 @@ If SELECT-ARTICLES, only select those articles from GROUP."
 			    (unseen . unseen))
 			  gnus-article-mark-lists))
       (push (cons (cdr elem)
-		  (gnus-byte-compile
+		  (gnus-byte-compile    ;Why bother?
 		   `(lambda () (gnus-article-marked-p ',(cdr elem)))))
 	    gnus-summary-display-cache)))
   (let ((gnus-category-predicate-alist gnus-summary-display-cache)
 	(gnus-category-predicate-cache gnus-summary-display-cache))
     (gnus-get-predicate display)))
 
-;; Uses the dynamically bound `number' variable.
-(defvar number)
+;; Uses the dynamically bound `gnus-number' variable.
+(defvar gnus-number)
 (defun gnus-article-marked-p (type &optional article)
-  (let ((article (or article number)))
+  (let ((article (or article gnus-number)))
     (cond
      ((eq type 'tick)
       (memq article gnus-newsgroup-marked))
@@ -8283,9 +8283,9 @@ articles that are younger than AGE days."
   (unless gnus-newsgroup-display
     (error "There is no `display' group parameter"))
   (let (articles)
-    (dolist (number gnus-newsgroup-articles)
+    (dolist (gnus-number gnus-newsgroup-articles)
       (when (funcall gnus-newsgroup-display)
-	(push number articles)))
+	(push gnus-number articles)))
     (gnus-summary-limit articles))
   (gnus-summary-position-point))
 
@@ -8686,7 +8686,7 @@ fetch-old-headers verbiage, and so on."
 	       (apply '+ (mapcar 'gnus-summary-limit-children
 				 (cdr thread)))
 	     0))
-	  (number (mail-header-number (car thread)))
+           (number (mail-header-number (car thread)))
 	  score)
       (if (and
 	   (not (memq number gnus-newsgroup-marked))
@@ -8732,7 +8732,8 @@ fetch-old-headers verbiage, and so on."
 	      t)
 	    ;; Do the `display' group parameter.
 	    (and gnus-newsgroup-display
-		 (not (funcall gnus-newsgroup-display)))))
+                 (let ((gnus-number number))
+                   (not (funcall gnus-newsgroup-display))))))
 	  ;; Nope, invisible article.
 	  0
 	;; Ok, this article is to be visible, so we add it to the limit
