7dfc15df2f3caa07932cf78ba9dcd93c7c5c5040
Glenn Morris <rgm@gnu.org>
2010-10-23 14:58:18 -0700

    * lisp/textmodes/flyspell.el (flyspell-mode): If there was an error, say what it was.
    Otherwise someone is going to report the same meaningless bug for the
    rest of time.
    
    http://debbugs.gnu.org/cgi/bugreport.cgi?bug=5574
    http://debbugs.gnu.org/cgi/bugreport.cgi?bug=5576
    http://debbugs.gnu.org/cgi/bugreport.cgi?bug=7272

--- lisp/textmodes/flyspell.el
+++ lisp/textmodes/flyspell.el
@@ -494,9 +494,9 @@ in your .emacs file.
   :keymap flyspell-mode-map
   :group 'flyspell
   (if flyspell-mode
-      (condition-case ()
+      (condition-case err
 	  (flyspell-mode-on)
-	(error (message "Enabling Flyspell mode gave an error")
+	(error (message "Error enabling Flyspell mode:\n%s" (cdr err))
 	       (flyspell-mode -1)))
     (flyspell-mode-off)))
 
@@ -2354,5 +2354,4 @@ This function is meant to be added to `flyspell-incorrect-hook'."
 
 (provide 'flyspell)
 
-;; arch-tag: 05d915b9-e9cf-44fb-9137-fc28f5eaab2a
 ;;; flyspell.el ends here
