77b143ebc1100462cd6b9be6b92db91c35eb5f5d
Jes Bodi Klinke <jes@bodi-klinke.dk>
2010-10-27 21:10:22 -0400

    * lisp/progmodes/compile.el (compilation-mode-font-lock-keywords): Don't confuse -omega as "-o mega".

--- lisp/progmodes/compile.el
+++ lisp/progmodes/compile.el
@@ -543,7 +543,7 @@ you may also want to change `compilation-page-delimiter'.")
      ;; Command output lines.  Recognize `make[n]:' lines too.
      ("^\\([[:alnum:]_/.+-]+\\)\\(\\[\\([0-9]+\\)\\]\\)?[ \t]*:"
       (1 font-lock-function-name-face) (3 compilation-line-face nil t))
-     (" --?o\\(?:utfile\\|utput\\)?[= ]?\\(\\S +\\)" . 1)
+     (" -\\(?:o[= ]?\\|-\\(?:outfile\\|output\\)[= ]\\)\\(\\S +\\)" . 1)
      ("^Compilation \\(finished\\).*"
       (0 '(face nil message nil help-echo nil mouse-face nil) t)
       (1 compilation-info-face))
