de4708cbd8ec2185efcad0b20f3e2766f4b95e23
Glenn Morris <rgm@gnu.org>
2010-11-09 20:50:31 -0800

    Comments related to `bzr commit --fixes'.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,8 @@
 ((nil . ((tab-width . 8)
          (fill-column . 70)))
  (c-mode . ((c-file-style . "GNU")))
+ ;; You must set bugtracker_debbugs_url in your bazaar.conf for this to work.
+ ;; See admin/notes/bugtracker.
  (log-edit-mode . ((log-edit-rewrite-fixes
                     " (bug#\\([0-9]+\\))" . "debbugs:\\1")))
  (change-log-mode . ((add-log-time-zone-rule . t)
@@ -8,4 +10,3 @@
 		     (bug-reference-url-format . "http://debbugs.gnu.org/%s")
 		     (mode . bug-reference))))
 
-;; arch-tag: fb93c160-e9fe-4184-aad1-e4f5daa11cbd
