f2f6365c7453e5a568fd4028bb87031f691df8b7
Andrew Cohen <cohen@andy.bu.edu>
2010-12-07 14:10:11 +0000

    nnir.el (nnir-run-gmane): Restore sub-optimal test for gmane server.  (nnir-request-article): Improve article retrieval.

--- lisp/gnus/nnir.el
+++ lisp/gnus/nnir.el
@@ -656,17 +656,14 @@ Add an entry here when adding a new search engine.")
        article)
     (save-excursion
       (let ((artfullgroup (nnir-article-group article))
-	    (artno (nnir-article-number article))
-	    ;; Bug?
-	    ;; Why must we bind nntp-server-buffer here?  It won't
-	    ;; work if `buf' is used, say.  (Of course, the set-buffer
-	    ;; line below must then be updated, too.)
-	    (nntp-server-buffer (or to-buffer nntp-server-buffer)))
-	(set-buffer nntp-server-buffer)
-	(erase-buffer)
+	    (artno (nnir-article-number article)))
 	(message "Requesting article %d from group %s"
 		 artno artfullgroup)
-	(gnus-request-article artno artfullgroup nntp-server-buffer)
+	(if to-buffer
+	    (with-current-buffer to-buffer
+	      (let ((gnus-article-decode-hook nil))
+		(gnus-request-article-this-buffer artno artfullgroup)))
+	  (gnus-request-article artno artfullgroup))
 	(cons artfullgroup artno)))))
 
 (deffoo nnir-request-move-article (article group server accept-form
@@ -1378,10 +1375,7 @@ Tested with Namazu 2.0.6 on a GNU/Linux system."
 ;; gmane interface
 (defun nnir-run-gmane (query srv &optional groups)
   "Run a search against a gmane back-end server."
-  (if (gnus-string-match-p
-       "gmane.org$"
-       (or (cadr (assoc 'nntp-address  (cddr (gnus-server-to-method srv))))
-	   ""))
+  (if (gnus-string-match-p  "gmane" srv)
       (let* ((case-fold-search t)
 	     (qstring (cdr (assq 'query query)))
 	     (server (cadr (gnus-server-to-method srv)))
