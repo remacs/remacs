287d4c2ca317cf6427ddd9202c104c5be2f8a9af
Glenn Morris <rgm@gnu.org>
2011-01-02 16:35:01 -0800

    * admin/admin.el (set-copyright): Also handle \year in refcards/*.tex.

--- admin/admin.el
+++ admin/admin.el
@@ -1,7 +1,7 @@
 ;;; admin.el --- utilities for Emacs administration
 
-;; Copyright (C) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011
-;;   Free Software Foundation, Inc.
+;; Copyright (C) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009,
+;;   2010, 2011  Free Software Foundation, Inc.
 
 ;; This file is part of GNU Emacs.
 
@@ -172,7 +172,6 @@ Root must be the root of an Emacs source tree."
    version (rx (and "Version=" (submatch (1+ (in "0-9.")))))))
 
 ;; Note this makes some assumptions about form of short copyright.
-;; FIXME add the \year in the refcards/*.tex files.
 (defun set-copyright (root copyright)
   "Set Emacs short copyright to COPYRIGHT in relevant files under ROOT.
 Root must be the root of an Emacs source tree."
@@ -185,17 +184,17 @@ Root must be the root of an Emacs source tree."
   (unless (file-exists-p (expand-file-name "src/emacs.c" root))
     (error "%s doesn't seem to be the root of an Emacs source tree" root))
   (set-version-in-file root "lisp/version.el" copyright
-		       (rx (and "emacs-copyright" (0+ space)
-				?\" (submatch (1+ (not (in ?\")))) ?\")))
+        	       (rx (and "emacs-copyright" (0+ space)
+        			?\" (submatch (1+ (not (in ?\")))) ?\")))
   (set-version-in-file root "lib-src/ebrowse.c" copyright
                        (rx (and "emacs_copyright" (0+ (not (in ?\")))
-				?\" (submatch (1+ (not (in ?\")))) ?\")))
+        			?\" (submatch (1+ (not (in ?\")))) ?\")))
   (set-version-in-file root "lib-src/etags.c" copyright
                        (rx (and "emacs_copyright" (0+ (not (in ?\")))
-				?\" (submatch (1+ (not (in ?\")))) ?\")))
+        			?\" (submatch (1+ (not (in ?\")))) ?\")))
   (set-version-in-file root "lib-src/rcs2log" copyright
-		       (rx (and "Copyright" (0+ space) ?= (0+ space)
-				?\' (submatch (1+ nonl)))))
+        	       (rx (and "Copyright" (0+ space) ?= (0+ space)
+        			?\' (submatch (1+ nonl)))))
   ;; This one is a nuisance, as it needs to be split over two lines.
   (string-match "\\(.*[0-9]\\{4\\} *\\)\\(.*\\)" copyright)
   ;; nextstep.
@@ -211,9 +210,19 @@ Root must be the root of an Emacs source tree."
   (set-version-in-file
    root "nextstep/GNUstep/Emacs.base/Resources/Info-gnustep.plist"
    copyright (rx (and "Copyright" (0+ space) ?\= (0+ space)
-                      ?\" (submatch (1+ (not (in ?\"))))))))
+                      ?\" (submatch (1+ (not (in ?\")))))))
+  (when (string-match "\\([0-9]\\{4\\}\\)" copyright)
+    (setq copyright (match-string 1 copyright))
+    (dolist (file (directory-files (expand-file-name "etc/refcards" root)
+                                   t "\\.tex\\'"))
+      (unless (string-match "gnus-refcard\\.tex" file)
+        (set-version-in-file
+         root file copyright
+         (concat (if (string-match "ru-refcard\\.tex" file)
+                     "\\\\newcommand{\\\\cyear}\\[0\\]{"
+                   "\\\\def\\\\year{")
+                 "\\([0-9]\\{4\\}\\)}.+%.+copyright year"))))))
 
 (provide 'admin)
 
-;; arch-tag: 4ea83636-2293-408b-884e-ad64f22a3bf5
 ;;; admin.el ends here
