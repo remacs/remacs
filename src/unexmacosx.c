e885315dd05a005596d4f0585748d25dadff8901
Jan Djärv <jan.h.d@swipnet.se>
2011-01-20 07:43:04 +0100

    * src/unexmacosx.c: Add comment about include order.

--- src/unexmacosx.c
+++ src/unexmacosx.c
@@ -86,15 +86,20 @@ along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.  */
    be changed accordingly.
 */
 
-#include <stdio.h>
+/* config.h #define:s malloc/realloc/free and then includes stdlib.h.
+   We want the undefined versions, but if config.h includes stdlib.h
+   with the #define:s in place, the prototypes will be wrong and we get
+   warnings.  To prevent that, include stdlib.h before config.h.  */
+
 #include <stdlib.h>
-#include <fcntl.h>
-#include <stdarg.h>
-#include <sys/types.h>
 #include <config.h>
 #undef malloc
 #undef realloc
 #undef free
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdarg.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <mach/mach.h>
 #include <mach-o/loader.h>
