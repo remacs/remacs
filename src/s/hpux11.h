6de1218f3cbfabdb60446cffc5013962aa434de0
Peter O'Gorman <bug-gnu-emacs@mlists.thewrittenword.com>
2011-01-23 17:00:22 -0500

    Two more hunks from the HP-UX patch at Bug#6811.
    * configure.in: Add HP-UX on IA64 (Bug#6811).
    * src/s/hpux11.h: Set CANNOT_DUMP on IA64 (Bug#6811).

--- src/s/hpux11.h
+++ src/s/hpux11.h
@@ -12,5 +12,11 @@
    then close and reopen it in the child.  */
 #define USG_SUBTTY_WORKS
 
+/* FIXME.  Peter O'Gorman reports that dumping using unexelf.o doesn't
+   work either: http://debbugs.gnu.org/cgi/bugreport.cgi?bug=6811 */
+#if __ia64
+#define CANNOT_DUMP 1
+#endif
+
 /* arch-tag: f5a3d780-82cd-4a9a-832e-a4031aab788b
    (do not change this comment) */
