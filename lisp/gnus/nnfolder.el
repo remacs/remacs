b9c82730f91a85fbeeb68edd0310d33d8f8f5ff7
Chong Yidong <cyd@stupidchicken.com>
2011-02-16 20:46:55 -0500

    Don't let-bind copyright-update in nnfolder-save-buffer.
    See:
    http://lists.gnu.org/archive/html/emacs-devel/2011-02/msg00284.html
    http://lists.gnu.org/archive/html/emacs-devel/2011-02/msg00651.html
    
    * nnfolder.el (nnfolder-save-buffer): Don't let-bind copyright-update,
    in case it's not yet loaded.

--- lisp/gnus/nnfolder.el
+++ lisp/gnus/nnfolder.el
@@ -1099,8 +1099,8 @@ This command does not work if you use short group names."
     (gnus-make-directory (file-name-directory (buffer-file-name)))
     (let ((coding-system-for-write
 	   (or nnfolder-file-coding-system-for-write
-	       nnfolder-file-coding-system))
-	  (copyright-update nil))
+	       nnfolder-file-coding-system)))
+      (set (make-local-variable 'copyright-update) nil)
       (save-buffer)))
   (unless (or gnus-nov-is-evil nnfolder-nov-is-evil)
     (nnfolder-save-nov)))
