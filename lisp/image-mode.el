61aa93ed0d083435c6fef9ab85d5776f233376e1
Eli Zaretskii <eliz@gnu.org>
2011-02-18 19:51:15 +0200

    Fix bug #8047 with require-final-newline and image file display.
     image-mode.el (image-toggle-display-image): Disable
     require-final-newline in buffers visiting binary image files.

--- lisp/image-mode.el
+++ lisp/image-mode.el
@@ -507,6 +507,11 @@ was inserted."
     ;; This just makes the arrow displayed in the right fringe
     ;; area look correct when the image is wider than the window.
     (setq truncate-lines t)
+    ;; Disable adding a newline at the end of the image file when it
+    ;; is written with, e.g., C-x C-w.
+    (if (coding-system-equal (coding-system-base buffer-file-coding-system)
+			     'no-conversion)
+	(set (make-local-variable 'require-final-newline) nil))
     ;; Allow navigation of large images
     (set (make-local-variable 'auto-hscroll-mode) nil)
     (setq image-type type)
