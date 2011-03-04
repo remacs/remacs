7fd0923ba444df89f916446af3f79ddb3fe1056c
Daiki Ueno <ueno@unixuser.org>
2011-03-04 13:59:21 +0000

    message.el (message-options): Revert the change that's a workaround for XEmacs buffer-local issue.

--- lisp/gnus/message.el
+++ lisp/gnus/message.el
@@ -1814,10 +1814,7 @@ You must have the \"hashcash\" binary installed, see `hashcash-path'."
 
 (defvar	message-options nil
   "Some saved answers when sending message.")
-
-(if (featurep 'xemacs)
-    (make-local-variable 'message-options)
-  (make-variable-buffer-local 'message-options))
+(make-variable-buffer-local 'message-options)
 
 (defvar message-send-mail-real-function nil
   "Internal send mail function.")
