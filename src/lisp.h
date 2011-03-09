e6ca6543685fded0d1b3322dd06d0fa70d3e2a44
Paul Eggert <eggert@cs.ucla.edu>
2011-03-08 21:40:59 -0800

    * alloc.c (mark_ttys): Move decl from here ...
    * lisp.h (mark_ttys): ... to here, so that it's checked against defn.

--- src/lisp.h
+++ src/lisp.h
@@ -3378,6 +3378,7 @@ extern Lisp_Object directory_files_internal (Lisp_Object, Lisp_Object,
 
 /* Defined in term.c */
 extern int *char_ins_del_vector;
+extern void mark_ttys (void);
 extern void syms_of_term (void);
 extern void fatal (const char *msgid, ...) NO_RETURN;
 
