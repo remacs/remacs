c82b2579f0d0abc7cfe75f9f29bc2c7010473878
Stefan Monnier <monnier@iro.umontreal.ca>
2011-03-31 11:17:06 -0400

    * lisp/loadup.el: Load minibuffer after loaddefs, to use define-minor-mode.

--- lisp/loadup.el
+++ lisp/loadup.el
@@ -106,7 +106,6 @@
 
 (load "cus-face")
 (load "faces")  ; after here, `defface' may be used.
-(load "minibuffer")
 
 (load "button")
 (load "startup")
@@ -117,6 +116,7 @@
   ;; In case loaddefs hasn't been generated yet.
   (file-error (load "ldefs-boot.el")))
 
+(load "minibuffer")
 (load "abbrev")         ;lisp-mode.el and simple.el use define-abbrev-table.
 (load "simple")
 
