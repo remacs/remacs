e6eb4e9e0471c66eddaece34a90ce0e882a7e747
Paul Eggert <eggert@cs.ucla.edu>
2011-04-04 00:48:36 -0700

    * lread.c (safe_to_load_p): Make the end-of-loop test the inverse of the in-loop.

--- src/lread.c
+++ src/lread.c
@@ -907,7 +907,7 @@ safe_to_load_p (int fd)
 	if (i == 4)
 	  version = buf[i];
 
-      if (i == nbytes
+      if (i >= nbytes
 	  || fast_c_string_match_ignore_case (Vbytecomp_version_regexp,
 					      buf + i) < 0)
 	safe_p = 0;
