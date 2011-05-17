e4157b9c9208b87b827b879d3876651311d97e0b
Glenn Morris <rgm@gnu.org>
2011-05-16 19:39:23 -0700

    * idlw-shell.el (idlwave-shell-complete-filename): Replace obsolete function.

--- lisp/progmodes/idlw-shell.el
+++ lisp/progmodes/idlw-shell.el
@@ -2187,7 +2187,7 @@ args of an executive .run, .rnew or .compile."
   ;; CWD might have changed, resync, to set default directory
   (idlwave-shell-resync-dirs)
   (let ((comint-file-name-chars idlwave-shell-file-name-chars))
-    (comint-dynamic-complete-as-filename)))
+    (comint-filename-completion)))
 
 (defun idlwave-shell-executive-command ()
   "Return the name of the current executive command, if any."
