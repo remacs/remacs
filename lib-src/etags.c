8d366f40eb6de6853a4e3c7a698f471e04e34461
Glenn Morris <rgm@gnu.org>
2011-05-20 19:27:00 -0700

    * lib-src/etags.c: Fix typo in previous comment change.

--- lib-src/etags.c
+++ lib-src/etags.c
@@ -93,7 +93,7 @@ char pot_etags_version[] = "@(#) pot revision number is 17.38.1.4";
 
 #ifdef HAVE_CONFIG_H
 # include <config.h>
-  /* This is probably not necessary any more.  On some systms, config.h
+  /* This is probably not necessary any more.  On some systems, config.h
      used to define static as nothing for the sake of unexec.  We don't
      want that here since we don't use unexec.  None of these systems
      are supported any more, but the idea is still mentioned in
