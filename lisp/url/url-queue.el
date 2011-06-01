3b7d5980c94cdf3188b2a5bb002e00d887312957
Glenn Morris <rgm@gnu.org>
2011-06-01 10:34:24 -0700

    Add :version tags for url-queue.el.
    * lisp/url/url-queue.el (url-queue-parallel-processes, url-queue-timeout):
    Add :version tag for options that will be new in 24.1.

--- lisp/url/url-queue.el
+++ lisp/url/url-queue.el
@@ -33,11 +33,13 @@
 
 (defcustom url-queue-parallel-processes 6
   "The number of concurrent processes."
+  :version "24.1"
   :type 'integer
   :group 'url)
 
 (defcustom url-queue-timeout 5
   "How long to let a job live once it's started (in seconds)."
+  :version "24.1"
   :type 'integer
   :group 'url)
 
