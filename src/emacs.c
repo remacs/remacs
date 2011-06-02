8b3115e7025b5091b2a7cd68f6ac3442964b59e5
Dan Nicolaescu <dann@ics.uci.edu>
2011-06-02 09:56:04 -0700

    * src/emacs.c (main): Warn when starting a GTK emacs in daemon mode.

--- src/emacs.c
+++ src/emacs.c
@@ -1120,6 +1120,11 @@ main (int argc, char **argv)
 	}
 
 #ifndef NS_IMPL_COCOA
+#ifdef USE_GTK
+      fprintf (stderr, "\nWarning: due to a long standing Gtk+ bug\nhttp://bugzilla.gnome.org/show_bug.cgi?id=85715\n\
+Emacs might crash when run in daemon mode and the X11 connection is unexpectedly lost.\n\
+Using an Emacs configured with --with-x-toolkit=lucid does not have this problem.\n");
+#endif
       f = fork ();
 #else /* NS_IMPL_COCOA */
       /* Under Cocoa we must do fork+exec as CoreFoundation lib fails in
