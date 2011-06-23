437b2cb453cedf1a9033b2082879f1964c67ed23
Paul Eggert <eggert@cs.ucla.edu>
2011-06-22 22:35:51 -0700

    * lisp.h (DEFVAR_KBOARD): Use offsetof instead of char * finagling.

--- src/lisp.h
+++ src/lisp.h
@@ -1980,10 +1980,7 @@ extern void defvar_kboard (struct Lisp_Kboard_Objfwd *, const char *, int);
 #define DEFVAR_KBOARD(lname, vname, doc)			\
   do {								\
     static struct Lisp_Kboard_Objfwd ko_fwd;			\
-    defvar_kboard (&ko_fwd,					\
-		   lname,					\
-		   (int)((char *)(&current_kboard->vname ## _)	\
-			 - (char *)current_kboard));		\
+    defvar_kboard (&ko_fwd, lname, offsetof (KBOARD, vname ## _)); \
   } while (0)
 
 
