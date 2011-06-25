a150502ad89cc06930319f3bdc0ab0a8db429e73
Glenn Morris <rgm@gnu.org>
2011-06-25 12:52:34 -0700

    * lib-src/emacsclient.c (print_help_and_exit): More fixing of previous.

--- lib-src/emacsclient.c
+++ lib-src/emacsclient.c
@@ -674,7 +674,7 @@ The following OPTIONS are accepted:\n\
 -nw, -t, --tty 		Open a new Emacs frame on the current terminal\n\
 -c, --create-frame    	Create a new frame instead of trying to\n\
 			use the current Emacs frame\n\
--F, --frame-parameters=ALIST\n\
+-F ALIST, --frame-parameters=ALIST\n\
 			Set the parameters of a new frame\n\
 -e, --eval    		Evaluate the FILE arguments as ELisp expressions\n\
 -n, --no-wait		Don't wait for the server to return\n\
