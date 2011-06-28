ad85fe1f1b0719e4e33596a0eb60aa3f0fca74fe
Deniz Dogan <deniz@dogan.se>
2011-06-28 13:23:01 +0200

    * textmodes/css-mode.el (css-font-lock-keywords): Fix grouped selectors.
    Fixes: debbugs:5732

--- lisp/textmodes/css-mode.el
+++ lisp/textmodes/css-mode.el
@@ -240,7 +240,7 @@
     ;; thus prevent this highlighting from being applied (actually now that
     ;; I use `append' this should work better).  But really the part of hte
     ;; selector between [...] should simply not be highlighted.
-    (,(concat "^\\([ \t]*[^@:{\n][^:{\n]+\\(?::" (regexp-opt css-pseudo-ids t)
+    (,(concat "^\\([ \t]*[^@:{}\n][^:{}]+\\(?::" (regexp-opt css-pseudo-ids t)
               "\\(?:([^)]+)\\)?[^:{\n]*\\)*\\)\\(?:\n[ \t]*\\)*{")
      (1 'css-selector append))
     ;; In the above rule, we allow the open-brace to be on some subsequent
