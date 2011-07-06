123403e42f1a1d556fb33cfd60cbec124d740837
Paul Eggert <eggert@cs.ucla.edu>
2011-07-06 11:04:23 -0700

    Use pthread_sigmask, not sigprocmask.
    * callproc.c (Fcall_process):
    * sysdep.c (sys_sigblock, sys_sigunblock, sys_sigsetmask):
    * process.c (create_process):
    sigprocmask is portable only for single-threaded applications, and
    Emacs can be multi-threaded when it uses GTK.

--- src/sysdep.c
+++ src/sysdep.c
@@ -1534,7 +1534,7 @@ sigset_t
 sys_sigblock (sigset_t new_mask)
 {
   sigset_t old_mask;
-  sigprocmask (SIG_BLOCK, &new_mask, &old_mask);
+  pthread_sigmask (SIG_BLOCK, &new_mask, &old_mask);
   return (old_mask);
 }
 
@@ -1542,7 +1542,7 @@ sigset_t
 sys_sigunblock (sigset_t new_mask)
 {
   sigset_t old_mask;
-  sigprocmask (SIG_UNBLOCK, &new_mask, &old_mask);
+  pthread_sigmask (SIG_UNBLOCK, &new_mask, &old_mask);
   return (old_mask);
 }
 
@@ -1550,7 +1550,7 @@ sigset_t
 sys_sigsetmask (sigset_t new_mask)
 {
   sigset_t old_mask;
-  sigprocmask (SIG_SETMASK, &new_mask, &old_mask);
+  pthread_sigmask (SIG_SETMASK, &new_mask, &old_mask);
   return (old_mask);
 }
 
