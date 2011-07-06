1f2b92cb8733f0aae18088d91e0f440174d3ed83
Lars Magne Ingebrigtsen <larsi@gnus.org>
2011-07-06 17:09:11 +0200

    * net/network-stream.el (network-stream-open-starttls): Try using a plain connection even if the server offered STARTTLS, and we kinda wanted to use it, if Emacs doesn't have any STARTTLS capability.  This should make smtpmail.el work in slightly more configurations.

--- lisp/net/network-stream.el
+++ lisp/net/network-stream.el
@@ -281,18 +281,14 @@ functionality.
 	      (network-stream-command stream capability-command eo-capa))))
 
     ;; If TLS is mandatory, close the connection if it's unencrypted.
-    (when (and (or require-tls
-		   ;; The server said it was possible to do STARTTLS,
-		   ;; and we wanted to use it...
-		   (and starttls-command
-			(plist-get parameters :use-starttls-if-possible)))
+    (when (and require-tls
 	       ;; ... but Emacs wasn't able to -- either no built-in
 	       ;; support, or no gnutls-cli installed.
 	       (eq resulting-type 'plain))
-	  (setq error
-		(if require-tls
-		    "Server does not support TLS"
-		  "Server supports STARTTLS, but Emacs does not have support for it"))
+      (setq error
+	    (if require-tls
+		"Server does not support TLS"
+	      "Server supports STARTTLS, but Emacs does not have support for it"))
       (delete-process stream)
       (setq stream nil))
     ;; Return value:
