adbb5a4018335f47edb7e682f76caed3c023f7da
Lars Magne Ingebrigtsen <larsi@gnus.org>
2011-07-11 15:36:54 +0200

    Fix syntax error in last checkin.

--- lisp/dired-x.el
+++ lisp/dired-x.el
@@ -1103,10 +1103,9 @@ See `dired-guess-shell-alist-user'."
 
     ;; Return commands or nil if flist is still non-nil.
     ;; Evaluate the commands in order that any logical testing will be done.
-    (remove-duplicates
-     (if (cdr cmds)
-	 (mapcar #'eval cmds)
-       (eval (car cmds))))))		; single command
+    (if (cdr cmds)
+	(remove-duplicates (mapcar #'eval cmds))
+      (eval (car cmds)))))		; single command
 
 (defun dired-guess-shell-command (prompt files)
   "Ask user with PROMPT for a shell command, guessing a default from FILES."
