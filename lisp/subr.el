a28e4607f0c5c5812a87dd816559d1601ed146ba
Lars Magne Ingebrigtsen <larsi@gnus.org>
2011-07-14 01:40:11 +0200

    Doc fix for with-silent-modifications
    * subr.el (with-silent-modifications): Clarify somewhat what the
    macro inhibits.
    
    Fixes: debbugs:6525

--- lisp/subr.el
+++ lisp/subr.el
@@ -3072,8 +3072,15 @@ See also `with-temp-file' and `with-output-to-string'."
   "Execute BODY, pretending it does not modify the buffer.
 If BODY performs real modifications to the buffer's text, other
 than cosmetic ones, undo data may become corrupted.
-Typically used around modifications of text-properties which do not really
-affect the buffer's content."
+
+This macro will run BODY normally, but doesn't count its buffer
+modifications as being buffer modifications.  This affects things
+like buffer-modified-p, checking whether the file is locked by
+someone else, running buffer modification hooks, and other things
+of that nature.
+
+Typically used around modifications of text-properties which do
+not really affect the buffer's content."
   (declare (debug t) (indent 0))
   (let ((modified (make-symbol "modified")))
     `(let* ((,modified (buffer-modified-p))
