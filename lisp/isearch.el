3e8cd5ce9547c4482aca2a46a30a6d1a19e137c4
Juri Linkov <juri@jurta.org>
2011-08-25 23:48:45 +0300

    * lisp/isearch.el (isearch-occur): Let-bind `search-spaces-regexp' to `search-whitespace-regexp'.
    Fixes: debbugs:9364

--- lisp/isearch.el
+++ lisp/isearch.el
@@ -1445,9 +1445,10 @@ string.  NLINES has the same meaning as in `occur'."
      (t (regexp-quote isearch-string)))
     (if current-prefix-arg (prefix-numeric-value current-prefix-arg))))
   (let ((case-fold-search isearch-case-fold-search)
-	;; set `search-upper-case' to nil to not call
-	;; `isearch-no-upper-case-p' in `occur-1'
-	(search-upper-case nil))
+	;; Set `search-upper-case' to nil to not call
+	;; `isearch-no-upper-case-p' in `occur-1'.
+	(search-upper-case nil)
+	(search-spaces-regexp search-whitespace-regexp))
     (occur regexp nlines)))
 
 (declare-function hi-lock-read-face-name "hi-lock" ())
