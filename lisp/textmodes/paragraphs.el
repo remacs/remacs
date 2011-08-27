3fc9b2184582296ffe4a602d87534f35ba4b4919
Alan Mackenzie <acm@muc.de>
2011-08-27 08:41:23 +0000

    progmodes/cc-menus.el (cc-imenu-c++-generic-expression): Make it handle function pointer parameters properly.

--- lisp/textmodes/paragraphs.el
+++ lisp/textmodes/paragraphs.el
@@ -456,21 +456,25 @@ sentences.  Also, every paragraph boundary terminates sentences as well."
         (sentence-end (sentence-end)))
     (while (< arg 0)
       (let ((pos (point))
-	    ;; We used to use (start-of-paragraph-text), but this can
-	    ;; prevent sentence-end from matching if it is anchored at
-	    ;; BOL and the paragraph starts indented.
-	    (par-beg (save-excursion (backward-paragraph) (point))))
-       (if (and (re-search-backward sentence-end par-beg t)
-		(or (< (match-end 0) pos)
-		    (re-search-backward sentence-end par-beg t)))
-	   (goto-char (match-end 0))
-	 (goto-char par-beg)))
+	    (par-beg
+	     (save-excursion
+	       (start-of-paragraph-text)
+	       ;; Move PAR-BEG back over indentation
+	       ;; to allow s1entence-end to match if it is anchored at
+	       ;; BOL and the paragraph starts indented.
+	       (beginning-of-line)
+	       (point))))
+	(if (and (re-search-backward sentence-end par-beg t)
+		 (or (< (match-end 0) pos)
+		     (re-search-backward sentence-end par-beg t)))
+	    (goto-char (match-end 0))
+	  (goto-char par-beg)))
       (setq arg (1+ arg)))
     (while (> arg 0)
       (let ((par-end (save-excursion (end-of-paragraph-text) (point))))
-       (if (re-search-forward sentence-end par-end t)
-	   (skip-chars-backward " \t\n")
-	 (goto-char par-end)))
+	(if (re-search-forward sentence-end par-end t)
+	    (skip-chars-backward " \t\n")
+	  (goto-char par-end)))
       (setq arg (1- arg)))
     (constrain-to-field nil opoint t)))
 
