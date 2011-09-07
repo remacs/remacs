edb7b4dc8b089385d8b0d12081227fc416fae23e
Andreas Schwab <schwab@linux-m68k.org>
2011-09-07 18:48:37 +0200

    * src/search.c (boyer_moore): Take unibyte characters from pattern literally.  (Bug#9458)

--- src/search.c
+++ src/search.c
@@ -1837,7 +1837,7 @@ boyer_moore (n, base_pat, len, len_byte, trt, inverse_trt,
 		ch = -1;
 	    }
 
-	  if (ch >= 0200)
+	  if (ch >= 0200 && multibyte)
 	    j = (ch & 0x3F) | 0200;
 	  else
 	    j = *ptr;
@@ -1856,7 +1856,7 @@ boyer_moore (n, base_pat, len, len_byte, trt, inverse_trt,
 	      while (1)
 		{
 		  TRANSLATE (ch, inverse_trt, ch);
-		  if (ch >= 0200)
+		  if (ch >= 0200 && multibyte)
 		    j = (ch & 0x3F) | 0200;
 		  else
 		    j = ch;
