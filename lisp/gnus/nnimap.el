943399bce86a0141daacdd5d1e2f62f2362cbfe9
Lars Magne Ingebrigtsen <larsi@gnus.org>
2011-09-26 22:52:31 +0000

    nnimap.el (nnimap-insert-partial-structure): The charset parameter isn't case-sensitive.

--- lisp/gnus/nnimap.el
+++ lisp/gnus/nnimap.el
@@ -672,12 +672,13 @@ textual parts.")
       (if (consp (caar structure))
 	  (nnimap-insert-partial-structure (pop structure) parts t)
 	(let ((bit (pop structure)))
-	  (insert (format  "Content-type: %s/%s"
-			   (downcase (nth 0 bit))
-			   (downcase (nth 1 bit))))
-	  (if (member "CHARSET" (nth 2 bit))
+	  (insert (format "Content-type: %s/%s"
+			  (downcase (nth 0 bit))
+			  (downcase (nth 1 bit))))
+	  (if (member-ignore-case "CHARSET" (nth 2 bit))
 	      (insert (format
-		       "; charset=%S\n" (cadr (member "CHARSET" (nth 2 bit)))))
+		       "; charset=%S\n"
+		       (cadr (member-ignore-case "CHARSET" (nth 2 bit)))))
 	    (insert "\n"))
 	  (insert (format "Content-transfer-encoding: %s\n"
 			  (nth 5 bit)))
