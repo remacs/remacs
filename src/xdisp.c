d3d0842fe219892079cac179ab0843792f29cf1a
Chong Yidong <cyd@gnu.org>
2011-10-28 23:17:09 +0800

    * src/xdisp.c (note_mouse_highlight): Fix use of uninitialized var.
    See:
    http://lists.gnu.org/archive/html/emacs-devel/2011-10/msg01202.html

--- src/xdisp.c
+++ src/xdisp.c
@@ -26835,11 +26835,14 @@ note_mouse_highlight (struct frame *f, int x, int y)
   /* Which window is that in?  */
   window = window_from_coordinates (f, x, y, &part, 1);
 
-  /* If we were displaying active text in another window, clear that.
-     Also clear if we move out of text area in same window.  */
+  /* If displaying active text in another window, clear that.  */
   if (! EQ (window, hlinfo->mouse_face_window)
-      || (part != ON_TEXT && part != ON_MODE_LINE && part != ON_HEADER_LINE
-	  && !NILP (hlinfo->mouse_face_window)))
+      /* Also clear if we move out of text area in same window.  */
+      || (!NILP (hlinfo->mouse_face_window)
+	  && !NILP (window)
+	  && part != ON_TEXT
+	  && part != ON_MODE_LINE
+	  && part != ON_HEADER_LINE))
     clear_mouse_face (hlinfo);
 
   /* Not on a window -> return.  */
