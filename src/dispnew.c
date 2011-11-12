1e5b2111b6ebfa6f8e244add939de453e9feafe6
Eli Zaretskii <eliz@gnu.org>
2011-11-12 12:09:38 +0200

    dispnew.c (scrolling_window): Fix index usage.
     src/dispnew.c (scrolling_window): Fix incorrect indices in accessing
     current_matrix and desired_matrix.  (Bug#9990)

--- src/dispnew.c
+++ src/dispnew.c
@@ -4334,10 +4334,10 @@ scrolling_window (struct window *w, int header_line_p)
   j = last_old;
   while (i - 1 > first_new
          && j - 1 > first_old
-         && MATRIX_ROW (current_matrix, i - 1)->enabled_p
-	 && (MATRIX_ROW (current_matrix, i - 1)->y
-	     == MATRIX_ROW (desired_matrix, j - 1)->y)
-	 && !MATRIX_ROW (desired_matrix, j - 1)->redraw_fringe_bitmaps_p
+         && MATRIX_ROW (current_matrix, j - 1)->enabled_p
+	 && (MATRIX_ROW (current_matrix, j - 1)->y
+	     == MATRIX_ROW (desired_matrix, i - 1)->y)
+	 && !MATRIX_ROW (desired_matrix, i - 1)->redraw_fringe_bitmaps_p
          && row_equal_p (MATRIX_ROW (desired_matrix, i - 1),
                          MATRIX_ROW (current_matrix, j - 1), 1))
     --i, --j;
