55dc42fcf374b0168d3fda7edca0f24a7bfd4b7b
Glenn Morris <rgm@gnu.org>
2011-12-04 11:31:41 -0800

    * appt.el: Restore significant whitespace deleted by "spelling fix".

--- lisp/calendar/appt.el
+++ lisp/calendar/appt.el
@@ -508,7 +508,9 @@ The time should be in either 24 hour format or am/pm format.
 Optional argument WARNTIME is an integer (or string) giving the number
 of minutes before the appointment at which to start warning.
 The default is `appt-message-warning-time'."
-  (interactive "sTime (hh:mm[am/pm]): \nsMessage:
+  ;; People who like to delete trailing whitespace: READ THIS.
+  ;; This whitespace is supposed to be here.
+  (interactive "sTime (hh:mm[am/pm]): \nsMessage: 
 sMinutes before the appointment to start warning: ")
   (unless (string-match appt-time-regexp time)
     (error "Unacceptable time-string"))
