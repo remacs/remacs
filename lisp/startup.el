04877ddb324700e5fbd97fdd52516ed6a81582ea
Chong Yidong <cyd@gnu.org>
2012-01-15 00:09:55 +0800

    * startup.el (command-line): Fix values recognized by cursorBlink resource.

--- lisp/startup.el
+++ lisp/startup.el
@@ -929,7 +929,7 @@ opening the first frame (e.g. open a connection to an X server).")
 	      emacs-basic-display
 	      (and (memq window-system '(x w32 ns))
 		   (not (member (x-get-resource "cursorBlink" "CursorBlink")
-				'("off" "false")))))
+				'("no" "off" "false" "0")))))
     (setq no-blinking-cursor t))
 
   ;; Re-evaluate predefined variables whose initial value depends on
