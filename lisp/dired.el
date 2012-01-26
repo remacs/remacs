8681f11d3b4c9e9d9b85cc997db7e091c19e6c13
Glenn Morris <rgm@gnu.org>
2012-01-26 06:18:28 -0500

    Auto-commit of loaddefs files.

--- lisp/dired.el
+++ lisp/dired.el
@@ -4196,7 +4196,7 @@ instead.
 ;;;***
 
 ;;;### (autoloads (dired-do-relsymlink dired-jump-other-window dired-jump)
-;;;;;;  "dired-x" "dired-x.el" "85900e333d980b376bf820108ae1a1fc")
+;;;;;;  "dired-x" "dired-x.el" "8d995933a8d82be3a8662d7eff7543cc")
 ;;; Generated autoloads from dired-x.el
 
 (autoload 'dired-jump "dired-x" "\
