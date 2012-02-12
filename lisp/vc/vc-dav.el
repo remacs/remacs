5de0e3c3d0d68487cfe5b455bc9d7df4fb734c18
Glenn Morris <rgm@gnu.org>
2012-02-11 17:49:51 -0800

    Header fix

--- lisp/vc/vc-dav.el
+++ lisp/vc/vc-dav.el
@@ -7,6 +7,8 @@
 ;; Keywords: url, vc
 ;; Package: vc
 
+;; This file is part of GNU Emacs.
+
 ;; GNU Emacs is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
@@ -19,7 +21,6 @@
 
 ;; You should have received a copy of the GNU General Public License
 ;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
-
 
 ;;; Commentary:
 
