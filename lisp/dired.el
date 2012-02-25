3c9dfce6da30c2b4483b0e95d33af3dd0bd9b26f
Chong Yidong <cyd@gnu.org>
2012-02-25 11:01:11 +0800

    Reduce x-selection-timeout to 5s, and add a message for clipboard manager saving.
    * lisp/term/x-win.el (x-initialize-window-system): Reduce default for
    x-selection-timeout to 5 seconds.
    
    * src/xselect.c (Fx_selection_exists_p): Doc fix.
    (x_clipboard_manager_save_all): Print an informative message
    before saving to clipboard manager.
    
    Fixes: debbugs:8869

--- lisp/dired.el
+++ lisp/dired.el
@@ -3732,7 +3732,7 @@ Ask means pop up a menu for the user to select one of copy, move or link."
 ;;;;;;  dired-run-shell-command dired-do-shell-command dired-do-async-shell-command
 ;;;;;;  dired-clean-directory dired-do-print dired-do-touch dired-do-chown
 ;;;;;;  dired-do-chgrp dired-do-chmod dired-compare-directories dired-backup-diff
-;;;;;;  dired-diff) "dired-aux" "dired-aux.el" "e77c506a0dd793230c5856a67e408fc6")
+;;;;;;  dired-diff) "dired-aux" "dired-aux.el" "cab9b84177ac3555c24cf8e870a64095")
 ;;; Generated autoloads from dired-aux.el
 
 (autoload 'dired-diff "dired-aux" "\
