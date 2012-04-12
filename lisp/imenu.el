d333dc4c9619d594654427180776a59c766ff853
Drew Adams <drew.adams@oracle.com>
2012-04-12 22:00:24 +0200

    * imenu.el (imenu--generic-function): Ignore invisible definitions.
    Fixes: debbugs:10123

--- lisp/imenu.el
+++ lisp/imenu.el
@@ -800,7 +800,17 @@ depending on PATTERNS."
 	      (goto-char (point-max))
 	      (while (and (if (functionp regexp)
 			      (funcall regexp)
-			    (re-search-backward regexp nil t))
+			    (and
+			     (re-search-backward regexp nil t)
+			     ;; Do not count invisible definitions.
+			     (let ((invis (invisible-p (point))))
+			       (or (not invis)
+				   (progn
+				     (while (and invis
+						 (not (bobp)))
+				       (setq invis (not (re-search-backward
+							 regexp nil 'move))))
+				     (not invis))))))
 			  ;; Exit the loop if we get an empty match,
 			  ;; because it means a bad regexp was specified.
 			  (not (= (match-beginning 0) (match-end 0))))
