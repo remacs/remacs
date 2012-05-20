1b170bc63c2f3a3fbe6ba6996d5a015e82634909
Ken Brown <kbrown@cornell.edu>
2012-05-20 08:19:13 -0400

    Get rid of compiler warnings on Cygwin.
    * src/gmalloc.c (_free_internal_nolock, _realloc_internal_nolock)
    [CYGWIN]:  Cast ptr to (char *) before comparing to _heapbase.

--- src/gmalloc.c
+++ src/gmalloc.c
@@ -1004,7 +1004,7 @@ _free_internal_nolock (void *ptr)
     return;
 
 #ifdef CYGWIN
-  if (ptr < _heapbase)
+  if ((char *) ptr < _heapbase)
     /* We're being asked to free something in the static heap. */
     return;
 #endif
@@ -1341,7 +1341,7 @@ _realloc_internal_nolock (void *ptr, size_t size)
     return _malloc_internal_nolock (size);
 
 #ifdef CYGWIN
-  if (ptr < _heapbase)
+  if ((char *) ptr < _heapbase)
     /* ptr points into the static heap */
     return special_realloc (ptr, size);
 #endif
