24b0cff0ba42f6600db603cc2a4a6736fa5e7e92
Martin Rudalics <rudalics@gmx.at>
2012-06-18 08:22:22 +0200

    Completely remove stringp check in special-display-p.
    * window.el (special-display-p): Completely remove stringp
    check.  Suggested by Andreas Schwab <schwab@linux-m68k.org>.

--- lisp/window.el
+++ lisp/window.el
@@ -4192,8 +4192,6 @@ or matches BUFFER-NAME, the return value is the cdr of that
 entry."
   (let (tmp)
     (cond
-     ((not (stringp buffer-name))
-      (error "Invalid buffer name %s" buffer-name))
      ((member buffer-name special-display-buffer-names)
       t)
      ((setq tmp (assoc buffer-name special-display-buffer-names))
