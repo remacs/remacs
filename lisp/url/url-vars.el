938beca46ed9565271005366175429febd66ea89
Glenn Morris <rgm@gnu.org>
2012-06-30 14:13:05 -0700

    * url-vars.el (mm-mime-mule-charset-alist, mm-coding-system-p): Declare.

--- lisp/url/url-vars.el
+++ lisp/url/url-vars.el
@@ -212,6 +212,9 @@ Should be an assoc list of headers/contents.")
 (defvar url-mime-encoding-string nil
   "String to send in the Accept-encoding: field in HTTP requests.")
 
+(defvar mm-mime-mule-charset-alist)
+(declare-function mm-coding-system-p "mm-util" (cs))
+
 ;; Perhaps the first few should actually be given decreasing `q's and
 ;; the list should be trimmed significantly.
 ;; Fixme: do something sane if we don't have `sort-coding-systems'
