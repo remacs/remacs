95bdccb782fbeb97cc2cc12a64635d6d5f85475f
Stefan Monnier <monnier@iro.umontreal.ca>
2012-07-18 05:33:09 -0400

    * lisp/progmodes/cperl-mode.el (cperl-unwind-to-safe): Don't inf-loop at end of narrowed buffer.
    Fixes: debbugs:11966

--- lisp/progmodes/cperl-mode.el
+++ lisp/progmodes/cperl-mode.el
@@ -3498,7 +3498,8 @@ Works before syntax recognition is done."
     (if end
 	;; Do the same for end, going small steps
 	(save-excursion
-	  (while (and end (get-text-property end 'syntax-type))
+	  (while (and end (< end (point-max))
+		      (get-text-property end 'syntax-type))
 	    (setq pos end
 		  end (next-single-property-change end 'syntax-type nil (point-max)))
 	    (if end (progn (goto-char end)
