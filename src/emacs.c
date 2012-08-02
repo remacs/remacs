6edd2f3de3c3ce416bb9ce672feba68000387d20
Glenn Morris <rgm@gnu.org>
2012-08-01 23:43:13 -0700

    Comment

--- src/emacs.c
+++ src/emacs.c
@@ -2375,7 +2375,7 @@ Special values:
 Anything else (in Emacs 24.1, the possibilities are: aix, berkeley-unix,
 hpux, irix, usg-unix-v) indicates some sort of Unix system.  */);
   Vsystem_type = intern_c_string (SYSTEM_TYPE);
-  /* The above values are from SYSTEM_TYPE in include files under src/s.  */
+  /* See configure.ac (and config.nt) for the possible SYSTEM_TYPEs.  */
 
   DEFVAR_LISP ("system-configuration", Vsystem_configuration,
 	       doc: /* Value is string indicating configuration Emacs was built for.
