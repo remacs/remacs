3fbc4b542cd022e5f66cbdd04860f4b84be08e80
Juanma Barranquero <lekktu@gmail.com>
2012-08-03 14:16:40 +0200

    * nt/config.nt: Sync with autogen/config.in. (DOS_NT, MSDOS): New macros. (WRETCODE, wait3): Remove.
    * nt/inc/ms-w32.h (DOS_NT): Remove; defined in config.nt.
    
    * lib-src/makefile.w32-in (LOCAL_FLAGS): Remove WINDOWSNT and DOS_NT,
    they are always defined in config.h.

--- nt/inc/ms-w32.h
+++ nt/inc/ms-w32.h
@@ -23,9 +23,6 @@ along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.  */
 #ifndef WINDOWSNT
 #define WINDOWSNT
 #endif
-#ifndef DOS_NT
-#define DOS_NT 	/* MSDOS or WINDOWSNT */
-#endif
 
 /* #undef const */
 
