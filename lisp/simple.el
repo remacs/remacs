d67d3afd3a4ebe1c879ac7f3f8287b274b18d3ad
Glenn Morris <rgm@gnu.org>
2012-09-01 17:56:03 -0700

    * lisp/simple.el (undo): Tweak message in undo-only case.
    Fixes: debbugs:12283

--- lisp/simple.el
+++ lisp/simple.el
@@ -1886,9 +1886,10 @@ as an argument limits undo to changes within the current region."
     ;; so, ask the user whether she wants to skip the redo/undo pair.
     (let ((equiv (gethash pending-undo-list undo-equiv-table)))
       (or (eq (selected-window) (minibuffer-window))
-	  (setq message (if undo-in-region
-			    (if equiv "Redo in region!" "Undo in region!")
-			  (if equiv "Redo!" "Undo!"))))
+	  (setq message (format "%s%s!"
+                                (if (or undo-no-redo (not equiv))
+                                    "Undo" "Redo")
+                                (if undo-in-region " in region" ""))))
       (when (and (consp equiv) undo-no-redo)
 	;; The equiv entry might point to another redo record if we have done
 	;; undo-redo-undo-redo-... so skip to the very last equiv.
