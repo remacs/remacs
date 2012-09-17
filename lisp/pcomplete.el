580bd8683bd69caaf131137d977dc189ed822134
Chong Yidong <cyd@gnu.org>
2012-09-17 22:45:51 +0800

    Make pcomplete less eager to add an extra space.
    * comint.el (comint--complete-file-name-data): Don't add a space
    if the status is `sole'; that adds a gratuitous space in the
    completion-cycling case.
    
    * pcomplete.el (pcomplete-completions-at-point): Likewise.
    
    Fixes: debbugs:12092

--- lisp/pcomplete.el
+++ lisp/pcomplete.el
@@ -451,9 +451,12 @@ Same as `pcomplete' but using the standard completion UI."
           (list beg (point) table
                 :predicate pred
                 :exit-function
+		;; If completion is finished, add a terminating space.
+		;; We used to also do this if STATUS is `sole', but
+		;; that does not work right when completion cycling.
                 (unless (zerop (length pcomplete-termination-string))
-                  (lambda (_s finished)
-                    (when (memq finished '(sole finished))
+                  (lambda (_s status)
+                    (when (eq status 'finished)
                       (if (looking-at
                            (regexp-quote pcomplete-termination-string))
                           (goto-char (match-end 0))
