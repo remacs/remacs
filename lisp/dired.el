cd99601878e97578ecd8e2209feeda275a3a13f5
Chong Yidong <cyd@gnu.org>
2012-10-24 22:38:49 +0800

    Document some Emacs 24.3 changes.
    * doc/emacs/dired.texi (Dired Deletion, Marks vs Flags): Document Emacs 24.3
    changes to the mark and unmark commands.
    (Comparison in Dired): Document chages to dired-diff.  Remove M-=,
    which is no longer bound to dired-backup-diff.
    
    * doc/emacs/mule.texi (Text Coding): set-buffer-file-coding-system can now
    be invoked from the mode line.
    
    * lisp/dired.el (dired-mark, dired-unmark, dired-flag-file-deletion):
    Doc fix.

--- lisp/dired.el
+++ lisp/dired.el
@@ -3107,10 +3107,11 @@ argument or confirmation)."
       (forward-line 1))))
 
 (defun dired-mark (arg)
-  "Mark the current (or next ARG) files.
+  "Mark the file at point in the Dired buffer.
+If the region is active, mark all files in the region.
+Otherwise, with a prefix arg, mark files on the next ARG lines.
+
 If on a subdir headerline, mark all its files except `.' and `..'.
-If the region is active in Transient Mark mode, mark all files
-in the active region.
 
 Use \\[dired-unmark-all-files] to remove all marks
 and \\[dired-unmark] on a subdir to remove the marks in
@@ -3136,7 +3137,10 @@ this subdir."
        (function (lambda () (delete-char 1) (insert dired-marker-char))))))))
 
 (defun dired-unmark (arg)
-  "Unmark the current (or next ARG) files.
+  "Unmark the file at point in the Dired buffer.
+If the region is active, unmark all files in the region.
+Otherwise, with a prefix arg, unmark files on the next ARG lines.
+
 If looking at a subdir, unmark all its files except `.' and `..'.
 If the region is active in Transient Mark mode, unmark all files
 in the active region."
@@ -3146,7 +3150,9 @@ in the active region."
 
 (defun dired-flag-file-deletion (arg)
   "In Dired, flag the current line's file for deletion.
-With prefix arg, repeat over several lines.
+If the region is active, flag all files in the region.
+Otherwise, with a prefix arg, flag files on the next ARG lines.
+
 If on a subdir headerline, flag all its files except `.' and `..'.
 If the region is active in Transient Mark mode, flag all files
 in the active region."
