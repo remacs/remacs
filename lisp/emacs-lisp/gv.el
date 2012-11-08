41626722272734aa3a7e4fdfafd3b085133db665
Stefan Monnier <monnier@iro.umontreal.ca>
2012-11-07 20:24:14 -0500

    * lisp/emacs-lisp/gv.el (gv-define-simple-setter): Fix last change.
    Fixes: debbugs:12812

--- lisp/emacs-lisp/gv.el
+++ lisp/emacs-lisp/gv.el
@@ -220,7 +220,8 @@ so as to preserve the semantics of `setf'."
   `(gv-define-setter ,name (val &rest args)
      ,(if fix-return
           `(macroexp-let2 nil v val
-             (cons ',setter (append args (list v))))
+             (cons ',setter (append args (list v)))
+             v)
         `(cons ',setter (append args (list val))))))
 
 ;;; Typical operations on generalized variables.
