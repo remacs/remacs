535efd4a430b6595a30efaa2919e565ad21209b9
Paul Eggert <eggert@cs.ucla.edu>
2012-11-18 09:10:35 -0800

    More minor time fixes.
    * calendar/time-date.el: Commentary fix.
    * net/tramp-sh.el (tramp-do-file-attributes-with-ls): Undo last change;
    too much other code depends on (0 0) time stamps.
    * net/tramp.el (tramp-time-less-p, tramp-time-subtract):
    Add a couple of FIXME comments.

--- lisp/net/tramp-sh.el
+++ lisp/net/tramp-sh.el
@@ -1270,11 +1270,12 @@ target of the symlink differ."
          res-uid
          ;; 3. File gid.
          res-gid
-         ;; 4. Last access time, as a list of integers in the same format
-         ;; as `current-time'.
+         ;; 4. Last access time, as a list of integers.  Normally this would be
+         ;; in the same format as `current-time', but the subseconds part is not
+         ;; currently implemented, and (0 0) denotes an unknown time.
          ;; 5. Last modification time, likewise.
          ;; 6. Last status change time, likewise.
-         '(0 0 0 0) '(0 0 0 0) '(0 0 0 0)	;CCC how to find out?
+         '(0 0) '(0 0) '(0 0)		;CCC how to find out?
          ;; 7. Size in bytes (-1, if number is out of range).
          res-size
          ;; 8. File modes, as a string of ten letters or dashes as in ls -l.
