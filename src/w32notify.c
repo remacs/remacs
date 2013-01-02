09b8afb6c8b235279dac6cdc7ffee68b782712ac
Paul Eggert <eggert@cs.ucla.edu>
2013-01-02 08:30:50 -0800

    Update copyright dates to 2013.

--- src/w32notify.c
+++ src/w32notify.c
@@ -1,5 +1,5 @@
 /* Filesystem notifications support for GNU Emacs on the Microsoft Windows API.
-   Copyright (C) 2012  Free Software Foundation, Inc.
+   Copyright (C) 2012-2013 Free Software Foundation, Inc.
 
 This file is part of GNU Emacs.
 
--- test/automated/advice-tests.el
+++ test/automated/advice-tests.el
@@ -1,6 +1,6 @@
 ;;; advice-tests.el --- Test suite for the new advice thingy.
 
-;; Copyright (C) 2012 Free Software Foundation, Inc.
+;; Copyright (C) 2012-2013 Free Software Foundation, Inc.
 
 ;; This file is part of GNU Emacs.
 
