e11dacb57703fb8044332d8a3933b815547911ec
Stefan Monnier <monnier@iro.umontreal.ca>
2013-02-20 16:02:37 -0500

    * src/sheap.c (report_sheap_usage): Prefer message1_nolog.

--- src/sheap.c
+++ src/sheap.c
@@ -91,8 +91,7 @@ report_sheap_usage (int die_if_pure_storage_exceeded)
   char buf[200];
   sprintf (buf, "Static heap usage: %d of %d bytes",
 	   bss_sbrk_ptr - bss_sbrk_buffer, STATIC_HEAP_SIZE);
-  /* Don't change this call to message1! message1 can log
-     messages, and at this point, we're not allowed to create
+  /* Don't log messages, cause at this point, we're not allowed to create
      buffers.  */
-  message ("%s", buf);
+  message1_nolog ("%s", buf);
 }
