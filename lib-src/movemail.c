f58afc72bd0fd54189c2ca66b9907ca0d338de75
Paul Eggert <eggert@cs.ucla.edu>
2013-03-12 17:54:47 -0700

    * movemail.c (main): Call umask on all systems.
    This is OK since Emacs already assumes umask elsewhere.

--- lib-src/movemail.c
+++ lib-src/movemail.c
@@ -380,13 +380,9 @@ main (int argc, char **argv)
       if (indesc < 0)
 	pfatal_with_name (inname);
 
-#ifdef BSD_SYSTEM
       /* In case movemail is setuid to root, make sure the user can
 	 read the output file.  */
-      /* This is desirable for all systems
-	 but I don't want to assume all have the umask system call */
       umask (umask (0) & 0333);
-#endif /* BSD_SYSTEM */
       outdesc = open (outname, O_WRONLY | O_CREAT | O_EXCL, 0666);
       if (outdesc < 0)
 	pfatal_with_name (outname);
