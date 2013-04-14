5e00cfa51043b42d59061525fecd461d0b2d44f9
Eli Zaretskii <eliz@gnu.org>
2013-04-14 18:05:42 +0300

    Fixed autoloads and byte compilation due to MSYS filename-butchering and xargs limitations.

--- lisp/cus-dep.el
+++ lisp/cus-dep.el
@@ -38,6 +38,18 @@ ldefs-boot\\|cus-load\\|finder-inf\\|esh-groups\\|subdirs\\)\\.el$\\)"
 
 (autoload 'autoload-rubric "autoload")
 
+(defun set-generated-custom-dependencies-file (file)
+  "Set value of `generated-custom-dependencies-file' from FILE.
+
+On systems other than MS-Windows, just sets the value
+of `generated-custom-dependencies-file'.  On MS-Windows, converts
+/d/foo/bar form passed by MSYS Make into d:/foo/bar that Emacs can
+grok.  This function is called from lisp/Makefile."
+  (when (and (eq system-type 'windows-nt)
+	     (string-match "\\`/[a-zA-Z]/" file))
+    (setq file (concat (substring file 1 2) ":" (substring file 2))))
+  (setq generated-custom-dependencies-file file))
+
 (defun custom-make-dependencies ()
   "Batch function to extract custom dependencies from .el files.
 Usage: emacs -batch -l ./cus-dep.el -f custom-make-dependencies DIRS"
