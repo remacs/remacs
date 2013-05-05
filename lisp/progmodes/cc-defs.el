5356e1a3d98cf48b7350ebfe583325a491a37b1c
Alan Mackenzie <acm@muc.de>
2013-05-05 12:17:12 +0000

    * progmodes/cc-defs.el (c-version): Increment to 5.32.5.

--- lisp/progmodes/cc-defs.el
+++ lisp/progmodes/cc-defs.el
@@ -93,7 +93,7 @@
 
 ;;; Variables also used at compile time.
 
-(defconst c-version "5.32.4"
+(defconst c-version "5.32.5"
   "CC Mode version number.")
 
 (defconst c-version-sym (intern c-version))
