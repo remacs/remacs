a1c700dec5a6b0db88d57087c5a7e43e30b1fc00
Kelly Dean <kellydeanch@yahoo.com>
2013-05-06 23:11:40 +0300

    * lisp/simple.el (shell-command-on-region): Doc fix.
    Fixes: debbugs:14279

--- lisp/simple.el
+++ lisp/simple.el
@@ -2868,7 +2868,7 @@ Command Output*' is deleted.
 
 Optional fourth arg OUTPUT-BUFFER specifies where to put the
 command's output.  If the value is a buffer or buffer name, put
-the output there.  Any other value, including nil, means to
+the output there.  Any other value, excluding nil, means to
 insert the output in the current buffer.  In either case, the
 output is inserted after point (leaving mark after it).
 
