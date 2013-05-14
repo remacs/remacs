eda9c7d7070741708c8e3a9e2bf927582a96c32a
Eli Zaretskii <eliz@gnu.org>
2013-05-14 17:09:43 +0300

    Another attempt to solve bug #14062 with assertion violations on MS-Windows.
     src/w32fns.c (w32_wnd_proc): Don't call WINDOW_HEADER_LINE_HEIGHT
     unless we know that the window w is a leaf window.  Another
     attempt at solving bug#14062.

--- src/w32fns.c
+++ src/w32fns.c
@@ -3183,8 +3183,9 @@ w32_wnd_proc (HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	  form.ptCurrentPos.y = w32_system_caret_y;
 
 	  form.rcArea.left = WINDOW_TEXT_TO_FRAME_PIXEL_X (w, 0);
-	  form.rcArea.top = (WINDOW_TOP_EDGE_Y (w)
-			     + WINDOW_HEADER_LINE_HEIGHT (w));
+	  form.rcArea.top = WINDOW_TOP_EDGE_Y (w);
+	  if (BUFFERP (w->contents))
+	    form.rcArea.top += WINDOW_HEADER_LINE_HEIGHT (w);
 	  form.rcArea.right = (WINDOW_BOX_RIGHT_EDGE_X (w)
 			       - WINDOW_RIGHT_MARGIN_WIDTH (w)
 			       - WINDOW_RIGHT_FRINGE_WIDTH (w));
