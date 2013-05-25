df76dacbd1f6830c96cf63053efe776d40c0e84d
Stefan Monnier <monnier@iro.umontreal.ca>
2013-05-24 23:08:04 -0400

    * lisp/emacs-lisp/lisp.el (lisp-completion-at-point): Don't burp at EOB.
    Fixes: debbugs:14446

--- lisp/emacs-lisp/lisp.el
+++ lisp/emacs-lisp/lisp.el
@@ -726,7 +726,7 @@ considered."
         (let ((tail (if (null (car table-etc))
                         (cdr table-etc)
                       (cons
-                       (if (memq (char-syntax (char-after end))
+                       (if (memq (char-syntax (or (char-after end) ?\s))
                                  '(?\s ?>))
                            (cadr table-etc)
                          (apply-partially 'completion-table-with-terminator
