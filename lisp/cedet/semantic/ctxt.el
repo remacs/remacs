c6bc7508a99546eac0ad2f926eaece09b2086d16
Paul Eggert <eggert@cs.ucla.edu>
2013-06-10 16:02:33 -0700

    Spelling fixes.

--- lisp/cedet/semantic/ctxt.el
+++ lisp/cedet/semantic/ctxt.el
@@ -366,7 +366,7 @@ For example, in the C statement:
 If the cursor is on 'this', will move point to the ; after entry.")
 
 (defun semantic-ctxt-end-of-symbol-default (&optional point)
-  "Move poin to the end of the current symbol under POINT.
+  "Move point to the end of the current symbol under POINT.
 This will move past type/field names when applicable.
 Depends on `semantic-type-relation-separator-character', and will
 work on C like languages."
@@ -422,18 +422,18 @@ work on C like languages."
 
 		;; Skip the separator and the symbol.
 		(goto-char (match-end 0))
-		
+
 		(if (looking-at "\\w\\|\\s_")
 		    ;; Skip symbols
 		    (forward-sexp 1)
 		  ;; No symbol, exit the search...
 		  (setq continuesearch nil))
-		  
+
 		(setq end (point)))
-		
+
 	      ;; Cont...
 	      )
-	  
+
 	  ;; Restore position if we go to far....
 	  (error (goto-char end)) )
 
