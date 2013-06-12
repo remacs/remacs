65e704b9ab10582945b4342ce5f7478f7109a873
Lars Magne Ingebrigtsen <larsi@gnus.org>
2013-06-12 22:32:33 +0000

    lisp/gnus/shr.el (shr-tag-svg): Ignore <svg>

--- lisp/gnus/shr.el
+++ lisp/gnus/shr.el
@@ -891,6 +891,9 @@ ones, in case fg and bg are nil."
 (defun shr-tag-comment (cont)
   )
 
+(defun shr-tag-svg (cont)
+  )
+
 (defun shr-tag-sup (cont)
   (let ((start (point)))
     (shr-generic cont)
