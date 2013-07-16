5a422d95aa63713b29d93ffbfd3595d235a026ba
Grégoire Jadi <gregoire.jadi@gmail.com>
2013-07-16 19:39:21 +0200

    * src/xwidget.c (Fmake_xwidget): Set the initial value of `query-on-exit' flag to t (kill_without_query = !query-on-exit).

--- src/xwidget.c
+++ src/xwidget.c
@@ -209,7 +209,7 @@ TYPE is a symbol which can take one of the following values:
   
   xw->height = XFASTINT(height);
   xw->width = XFASTINT(width);
-  xw->kill_without_query = 1;
+  xw->kill_without_query = 0;
   XSETXWIDGET (val, xw); // set the vectorlike_header of VAL with the correct value
   Vxwidget_list = Fcons (val, Vxwidget_list);
   xw->widgetwindow_osr = NULL;
