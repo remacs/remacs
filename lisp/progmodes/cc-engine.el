6758b6a8db00bc9bf9748d3d8c542cd269a9bffc
Glenn Morris <rgm@gnu.org>
2013-08-29 13:26:48 -0400

    * lisp/progmodes/cc-engine.el (c-pull-open-brace): Move definition before use. Ref: http://lists.gnu.org/archive/html/emacs-devel/2013-08/msg00773.html

--- lisp/progmodes/cc-engine.el
+++ lisp/progmodes/cc-engine.el
@@ -6479,6 +6479,15 @@ comment at the start of cc-engine.el for more info."
 	   (c-go-list-forward)
          t)))
 
+(defmacro c-pull-open-brace (ps)
+  ;; Pull the next open brace from PS (which has the form of paren-state),
+  ;; skipping over any brace pairs.  Returns NIL when PS is exhausted.
+  `(progn
+     (while (consp (car ,ps))
+       (setq ,ps (cdr ,ps)))
+     (prog1 (car ,ps)
+       (setq ,ps (cdr ,ps)))))
+
 (defun c-back-over-member-initializers ()
   ;; Test whether we are in a C++ member initializer list, and if so, go back
   ;; to the introducing ":", returning the position of the opening paren of
@@ -8402,15 +8411,6 @@ comment at the start of cc-engine.el for more info."
 		    nil))
 	  (back-to-indentation)
 	  (vector (point) open-paren-pos))))))
-
-(defmacro c-pull-open-brace (ps)
-  ;; Pull the next open brace from PS (which has the form of paren-state),
-  ;; skipping over any brace pairs.  Returns NIL when PS is exhausted.
-  `(progn
-     (while (consp (car ,ps))
-       (setq ,ps (cdr ,ps)))
-     (prog1 (car ,ps)
-       (setq ,ps (cdr ,ps)))))
 
 (defun c-most-enclosing-decl-block (paren-state)
   ;; Return the buffer position of the most enclosing decl-block brace (in the
