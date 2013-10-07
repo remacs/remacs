2e7ba2c231f813115bd1e4a1adbf520710c0e0b9
Stefan Monnier <monnier@iro.umontreal.ca>
2013-10-07 01:10:06 -0400

    * lisp/emacs-lisp/tq.el (tq-create): Use a closure instead of `(lambda...).

--- lisp/emacs-lisp/tq.el
+++ lisp/emacs-lisp/tq.el
@@ -1,4 +1,4 @@
-;;; tq.el --- utility to maintain a transaction queue
+;;; tq.el --- utility to maintain a transaction queue  -*- lexical-binding:t -*-
 
 ;; Copyright (C) 1985-1987, 1992, 2001-2013 Free Software Foundation,
 ;; Inc.
@@ -87,8 +87,7 @@ to a tcp server on another machine."
 				     (process-name process)))))))
     (buffer-disable-undo (tq-buffer tq))
     (set-process-filter process
-			`(lambda (proc string)
-			   (tq-filter ',tq string)))
+			(lambda (_proc string) (tq-filter tq string)))
     tq))
 
 (defun tq-queue-add (tq question re closure fn)
