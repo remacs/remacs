1139b0be8b1bc0d63420c69aaec0e2b0cb19d71d
Xue Fuqiao <xfq.free@gmail.com>
2013-12-24 16:33:02 +0800

    * doc/emacs/trouble.texi (Checklist): Fix a cross-reference.

--- lisp/emacs-lisp/rx.el
+++ lisp/emacs-lisp/rx.el
@@ -871,7 +871,7 @@ NO-GROUP non-nil means don't put shy groups around the result."
 REGEXPS is a non-empty sequence of forms of the sort listed below.
 
 Note that `rx' is a Lisp macro; when used in a Lisp program being
- compiled, the translation is performed by the compiler.
+compiled, the translation is performed by the compiler.
 See `rx-to-string' for how to do such a translation at run-time.
 
 The following are valid subforms of regular expressions in sexp
