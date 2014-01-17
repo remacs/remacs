d13bb153bf26a4425f70e6ec143105f06d57747c
Glenn Morris <rgm@gnu.org>
2014-01-16 22:21:57 -0500

    * lisp/emacs-lisp/authors.el (authors-aliases): Remove more unused entries
    * lisp/leim/quail/tibetan.el, lisp/menu-bar.el: Fix author header comment.

--- lisp/leim/quail/tibetan.el
+++ lisp/leim/quail/tibetan.el
@@ -23,7 +23,7 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 
-;; Author: Toru TOMABECHI, <Toru.Tomabechi@orient.unil.ch>
+;; Author: Toru TOMABECHI <Toru.Tomabechi@orient.unil.ch>
 
 ;; Created: Feb. 17. 1997
 
