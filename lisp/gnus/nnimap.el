ec80e689331728b22f34a797ec06354ac4595b4a
Lars Ingebrigtsen <larsi@gnus.org>
2014-01-31 10:13:28 +0000

    lisp/gnus/nnimap.el (nnimap-transform-headers): Partially revert mistakenly checked-in test code from last checkin

--- lisp/gnus/nnimap.el
+++ lisp/gnus/nnimap.el
@@ -216,7 +216,7 @@ textual parts.")
 		  ;; Start of the header section.
 		  (or (re-search-forward "] {[0-9]+}\r?\n" nil t)
 		      ;; Start of the next FETCH.
-		      (re-search-forward "\\* [0-9]+ \\(UID \\)? FETCH" nil t)
+		      (re-search-forward "\\* [0-9]+ FETCH" nil t)
 		      (point-max)))
 		t)
 	  (setq size (string-to-number (match-string 1)))
