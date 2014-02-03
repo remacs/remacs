be445cf2abe016f00c59964268d6d46c3b5da421
Glenn Morris <rgm@gnu.org>
2014-02-02 19:12:29 -0800

    Some doc for desktop-auto-save-timeout
    * doc/emacs/misc.texi (Saving Emacs Sessions):
    Mention desktop-auto-save-timeout.
    
    * lisp/desktop.el (desktop-save-mode): Doc fix.
    
    * etc/NEWS: Related edit.

--- lisp/desktop.el
+++ lisp/desktop.el
@@ -160,7 +160,9 @@ the mode if ARG is omitted or nil.
 
 If Desktop Save mode is enabled, the state of Emacs is saved from
 one session to another.  See variable `desktop-save' and function
-`desktop-read' for details."
+`desktop-read' for details.
+
+For options you can set, browse the `desktop' customization group."
   :global t
   :group 'desktop)
 
