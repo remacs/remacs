41a4a82fa19abb3f615a41215d56165a3138102b
Glenn Morris <rgm@gnu.org>
2014-02-07 20:26:13 -0500

    * lisp/abbrev.el (edit-abbrevs-mode): Derive from fundamental-mode.
    Fixes: debbugs:16682

--- lisp/abbrev.el
+++ lisp/abbrev.el
@@ -1007,7 +1007,7 @@ SORTFUN is passed to `sort' to change the default ordering."
 
 ;; Keep it after define-abbrev-table, since define-derived-mode uses
 ;; define-abbrev-table.
-(define-derived-mode edit-abbrevs-mode special-mode "Edit-Abbrevs"
+(define-derived-mode edit-abbrevs-mode fundamental-mode "Edit-Abbrevs"
   "Major mode for editing the list of abbrev definitions.")
 
 (provide 'abbrev)
