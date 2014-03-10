f38145ba81691d60f3a8e3198e4754359c8b8a55
Martin Rudalics <rudalics@gmx.at>
2014-03-10 19:53:39 +0100

    Revert last change to w32_read_socket.

--- src/w32term.c
+++ src/w32term.c
@@ -4684,7 +4684,8 @@ w32_read_socket (struct terminal *terminal,
 		  {
 		    bool iconified = FRAME_ICONIFIED_P (f);
 
-		    SET_FRAME_VISIBLE (f, 1);
+		    if (iconified)
+		      SET_FRAME_VISIBLE (f, 1);
 		    SET_FRAME_ICONIFIED (f, 0);
 
 		    /* wait_reading_process_output will notice this
