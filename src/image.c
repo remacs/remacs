61febcb6e32860575316b8a539a2e9f13f70e3c2
Paul Eggert <eggert@cs.ucla.edu>
2014-05-04 11:51:32 -0700

    Revert recent libpng changes.
    They fix only bugs that aren't being reported, so it may be better
    to do them on the trunk.
    * configure.ac (PNG_CFLAGS): Remove.  All uses removed.
    (LIBPNG): Don't be consistent about -lpng16 versus -lpng.
    Ignore libpng-config's exit status.  Always append -lz -lm.
    * src/Makefile.in (PNG_CFLAGS): Remove; all uses removed.
    * src/image.c [HAVE_LIBPNG_PNG_H]: Include <libpng/png.h>, not <png.h>.
    
    Fixes: debbugs:17339

--- src/image.c
+++ src/image.c
@@ -22,7 +22,11 @@ along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.  */
 #include <unistd.h>
 
 #ifdef HAVE_PNG
+#if defined HAVE_LIBPNG_PNG_H
+# include <libpng/png.h>
+#else
 # include <png.h>
+#endif
 #endif
 
 #include <setjmp.h>
