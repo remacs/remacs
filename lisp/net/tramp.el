7973d8d5facf11b6408f8e17c9ad11efc7ff6eba
Michael Albinus <michael.albinus@gmx.de>
2014-06-02 17:17:39 +0200

    * net/tramp.el (tramp-handle-unhandled-file-name-directory): Return "/".

--- lisp/net/tramp.el
+++ lisp/net/tramp.el
@@ -3340,8 +3340,9 @@ User is always nil."
 (defun tramp-handle-unhandled-file-name-directory (_filename)
   "Like `unhandled-file-name-directory' for Tramp files."
   ;; With Emacs 23, we could simply return `nil'.  But we must keep it
-  ;; for backward compatibility.
-  (expand-file-name "~/"))
+  ;; for backward compatibility.  "~/" cannot be returned, because
+  ;; there might be machines without a HOME directory (like hydra).
+  "/")
 
 (defun tramp-handle-set-visited-file-modtime (&optional time-list)
   "Like `set-visited-file-modtime' for Tramp files."
