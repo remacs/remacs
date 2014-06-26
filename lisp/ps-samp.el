18b345686d6a6f00eadb866b541b4f458fcfdb02
Glenn Morris <rgm@gnu.org>
2014-06-25 23:35:38 -0700

    ps-samp.el: Make it slightly less awful
    * lisp/ps-samp.el (ps-rmail-mode-hook, ps-gnus-article-prepare-hook)
    (ps-vm-mode-hook, ps-gnus-summary-setup, ps-info-mode-hook):
    Use [print] key.  Only set local values.
    (ps-article-subject, ps-article-author): Use standard functions
    like mail-fetch-field.
    (ps-info-file, ps-info-node): Use match-string.
    (ps-jts-ps-setup, ps-jack-setup): Remove, merging into...
    (ps-samp-ps-setup): ... new function.

--- lisp/ps-samp.el
+++ lisp/ps-samp.el
@@ -29,18 +29,7 @@
 
 ;;; Commentary:
 
-;; See ps-print.el for documentation.
-
-;;; Code:
-
-
-(require 'ps-print)
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Sample Setup Code:
-
-
+;; Some example hacks for ps-print.el.
 ;; This stuff is for anybody that's brave enough to look this far,
 ;; and able to figure out how to use it.  It isn't really part of
 ;; ps-print, but I'll leave it here in hopes it might be useful:
@@ -48,20 +37,23 @@
 ;; WARNING!!! The following code is *sample* code only.
 ;; Don't use it unless you understand what it does!
 
-;; The key `f22' should probably be replaced by `print'.  --Stef
+;;; Code:
 
-;; A hook to bind to `rmail-mode-hook' to locally bind prsc and set the
-;; `ps-left-headers' specially for mail messages.
+(require 'ps-print)
+
+
+
+;; A hook to bind to `rmail-mode-hook' to locally bind prsc and set
+;; `ps-left-header' specially for mail messages.
 (defun ps-rmail-mode-hook ()
-  (local-set-key [(f22)] 'ps-rmail-print-message-from-summary)
-  (setq ps-header-lines 3
-	ps-left-header
-	;; The left headers will display the message's subject, its
-	;; author, and the name of the folder it was in.
-	'(ps-article-subject ps-article-author buffer-name)))
+  (local-set-key [print] 'ps-rmail-print-message-from-summary)
+  (setq-local ps-header-lines 3)
+  ;; The left header will display the message's subject, its
+  ;; author, and the name of the folder it was in.
+  (setq-local ps-left-header
+	      '(ps-article-subject ps-article-author buffer-name)))
 
-;; See `ps-gnus-print-article-from-summary'.  This function does the
-;; same thing for rmail.
+;; Like `ps-gnus-print-article-from-summary', but for rmail.
 (defun ps-rmail-print-message-from-summary ()
   (interactive)
   (ps-print-message-from-summary 'rmail-summary-buffer "RMAIL"))
@@ -76,61 +68,57 @@
 	 (with-current-buffer ps-buf
 	   (ps-spool-buffer-with-faces)))))
 
-;; Look in an article or mail message for the Subject: line.  To be
-;; placed in `ps-left-headers'.
+;; Look in an article or mail message for the Subject: line.
 (defun ps-article-subject ()
   (save-excursion
-    (goto-char (point-min))
-    (if (re-search-forward "^Subject:[ \t]+\\(.*\\)$" nil t)
-	(buffer-substring (match-beginning 1) (match-end 1))
-      "Subject ???")))
+    (save-restriction
+      (narrow-to-region (point-min) (progn (rfc822-goto-eoh) (point)))
+      (concat "Subject: " (or (mail-fetch-field "Subject") "???")))))
 
 ;; Look in an article or mail message for the From: line.  Sorta-kinda
 ;; understands RFC-822 addresses and can pull the real name out where
-;; it's provided.  To be placed in `ps-left-headers'.
+;; it's provided.
 (defun ps-article-author ()
   (save-excursion
-    (goto-char (point-min))
-    (if (re-search-forward "^From:[ \t]+\\(.*\\)$" nil t)
-	(let ((fromstring (buffer-substring (match-beginning 1) (match-end 1))))
-	  (cond
+    (save-restriction
+      (narrow-to-region (point-min) (progn (rfc822-goto-eoh) (point)))
+      (let ((fromstring (mail-fetch-field "From")))
+	(cond
+	 ;; Try first to match addresses that look like
+	 ;; thompson@wg2.waii.com (Jim Thompson)
+	 ((and fromstring (string-match ".*[ \t]+(\\(.*\\))" fromstring))
+	  (match-string 1 fromstring))
+	 ;; Next try to match addresses that look like
+	 ;; Jim Thompson <thompson@wg2.waii.com> or
+	 ;; "Jim Thompson" <thompson@wg2.waii.com>
+	 ((and fromstring
+	       (string-match "\\(\"?\\)\\(.*\\)\\1[ \t]+<.*>" fromstring))
+	  (match-string 2 fromstring))
+	 ;; Couldn't find a real name -- show the address instead.
+	 (fromstring)
+	 (t "From ???"))))))
 
-	   ;; Try first to match addresses that look like
-	   ;; thompson@wg2.waii.com (Jim Thompson)
-	   ((string-match ".*[ \t]+(\\(.*\\))" fromstring)
-	    (substring fromstring (match-beginning 1) (match-end 1)))
-
-	   ;; Next try to match addresses that look like
-	   ;; Jim Thompson <thompson@wg2.waii.com> or
-	   ;; "Jim Thompson" <thompson@wg2.waii.com>
-	   ((string-match "\\(\"?\\)\\(.*\\)\\1[ \t]+<.*>" fromstring)
-	    (substring fromstring (match-beginning 2) (match-end 2)))
-
-	   ;; Couldn't find a real name -- show the address instead.
-	   (t fromstring)))
-      "From ???")))
-
-;; A hook to bind to `gnus-article-prepare-hook'.  This will set the
-;; `ps-left-headers' specially for gnus articles.  Unfortunately,
+;; A hook to bind to `gnus-article-prepare-hook'.  This will set
+;; `ps-left-header' specially for gnus articles.  Unfortunately,
 ;; `gnus-article-mode-hook' is called only once, the first time the *Article*
 ;; buffer enters that mode, so it would only work for the first time
 ;; we ran gnus.  The second time, this hook wouldn't get set up.  The
 ;; only alternative is `gnus-article-prepare-hook'.
 (defun ps-gnus-article-prepare-hook ()
-  (setq ps-header-lines 3
-	ps-left-header
-	;; The left headers will display the article's subject, its
-	;; author, and the newsgroup it was in.
-	'(ps-article-subject ps-article-author gnus-newsgroup-name)))
+  (setq-local ps-header-lines 3)
+  ;; The left headers will display the article's subject, its
+  ;; author, and the newsgroup it was in.
+  (setq-local ps-left-header
+	      '(ps-article-subject ps-article-author gnus-newsgroup-name)))
 
-;; A hook to bind to `vm-mode-hook' to locally bind prsc and set the
-;; `ps-left-headers' specially for mail messages.
+;; A hook to bind to `vm-mode-hook' to locally bind prsc and set
+;; `ps-left-header' specially for mail messages.
 (defun ps-vm-mode-hook ()
-  (local-set-key [(f22)] 'ps-vm-print-message-from-summary)
-  (setq ps-header-lines 3
-	ps-left-header
-	;; The left headers will display the message's subject, its
-	;; author, and the name of the folder it was in.
+  (local-set-key [print] 'ps-vm-print-message-from-summary)
+  (setq-local ps-header-lines 3)
+  ;; The left headers will display the message's subject, its
+  ;; author, and the name of the folder it was in.
+  (setq-local ps-left-header
 	'(ps-article-subject ps-article-author buffer-name)))
 
 ;; Every now and then I forget to switch from the *Summary* buffer to
@@ -138,55 +126,43 @@
 ;; article subjects shows up at the printer.  This function, bound to
 ;; prsc for the gnus *Summary* buffer means I don't have to switch
 ;; buffers first.
-;; sb:  Updated for Gnus 5.
 (defun ps-gnus-print-article-from-summary ()
   (interactive)
   (ps-print-message-from-summary 'gnus-article-buffer "*Article*"))
 
-;; See `ps-gnus-print-article-from-summary'.  This function does the
-;; same thing for vm.
+;; Like `ps-gnus-print-article-from-summary', but for vm.
 (defun ps-vm-print-message-from-summary ()
   (interactive)
   (ps-print-message-from-summary 'vm-mail-buffer ""))
 
-;; A hook to bind to bind to `gnus-summary-setup-buffer' to locally bind
-;; prsc.
+;; A hook to bind to `gnus-summary-setup-buffer' to locally bind prsc.
 (defun ps-gnus-summary-setup ()
-  (local-set-key [(f22)] 'ps-gnus-print-article-from-summary))
+  (local-set-key [print] 'ps-gnus-print-article-from-summary))
 
-;; Look in an article or mail message for the Subject: line.  To be
-;; placed in `ps-left-headers'.
 (defun ps-info-file ()
   (save-excursion
     (goto-char (point-min))
     (if (re-search-forward "File:[ \t]+\\([^, \t\n]*\\)" nil t)
-	(buffer-substring (match-beginning 1) (match-end 1))
+	(match-string 1)
       "File ???")))
 
-;; Look in an article or mail message for the Subject: line.  To be
-;; placed in `ps-left-headers'.
 (defun ps-info-node ()
   (save-excursion
     (goto-char (point-min))
     (if (re-search-forward "Node:[ \t]+\\([^,\t\n]*\\)" nil t)
-	(buffer-substring (match-beginning 1) (match-end 1))
+	(match-string 1)
       "Node ???")))
 
 (defun ps-info-mode-hook ()
-  (setq ps-left-header
-	;; The left headers will display the node name and file name.
-	'(ps-info-node ps-info-file)))
+  ;; The left headers will display the node name and file name.
+  (setq-local ps-left-header '(ps-info-node ps-info-file)))
 
-;; WARNING! The following function is a *sample* only, and is *not*
-;; meant to be used as a whole unless you understand what the effects
-;; will be!  (In fact, this is a copy of Jim's setup for ps-print --
-;; I'd be very surprised if it was useful to *anybody*, without
-;; modification.)
-
-(defun ps-jts-ps-setup ()
-  (global-set-key [(f22)] 'ps-spool-buffer-with-faces) ;f22 is prsc
-  (global-set-key [(shift f22)] 'ps-spool-region-with-faces)
-  (global-set-key [(control f22)] 'ps-despool)
+;; WARNING! The following function is a *sample* only, and is *not* meant
+;; to be used as a whole unless you understand what the effects will be!
+(defun ps-samp-ps-setup ()
+  (global-set-key [print] 'ps-spool-buffer-with-faces)
+  (global-set-key [S-print] 'ps-spool-region-with-faces)
+  (global-set-key [C-print] 'ps-despool)
   (add-hook 'gnus-article-prepare-hook 'ps-gnus-article-prepare-hook)
   (add-hook 'gnus-summary-mode-hook 'ps-gnus-summary-setup)
   (add-hook 'vm-mode-hook 'ps-vm-mode-hook)
@@ -195,24 +171,10 @@
   (setq ps-spool-duplex t
 	ps-print-color-p nil
 	ps-lpr-command "lpr"
-	ps-lpr-switches '("-Jjct,duplex_long"))
-  'ps-jts-ps-setup)
-
-;; WARNING! The following function is a *sample* only, and is *not*
-;; meant to be used as a whole unless it corresponds to your needs.
-;; (In fact, this is a copy of Jack's setup for ps-print --
-;; I would not be that surprised if it was useful to *anybody*,
-;; without modification.)
-
-(defun ps-jack-setup ()
-  (setq ps-print-color-p  nil
-	ps-lpr-command    "lpr"
-	ps-lpr-switches   nil
-
+	ps-lpr-switches '("-Jjct,duplex_long")
 	ps-paper-type        'a4
 	ps-landscape-mode    t
 	ps-number-of-columns 2
-
 	ps-left-margin   (/ (* 72  1.0) 2.54) ;  1.0 cm
 	ps-right-margin  (/ (* 72  1.0) 2.54) ;  1.0 cm
 	ps-inter-column  (/ (* 72  1.0) 2.54) ;  1.0 cm
@@ -225,13 +187,11 @@
 	ps-header-lines       2
 	ps-show-n-of-n        t
 	ps-spool-duplex       nil
-
 	ps-font-family             'Courier
 	ps-font-size               5.5
 	ps-header-font-family      'Helvetica
 	ps-header-font-size        6
-	ps-header-title-font-size  8)
-  'ps-jack-setup)
+	ps-header-title-font-size  8))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
