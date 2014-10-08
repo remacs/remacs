c35e776245915b96e9e80dec5354d087fcdc18ff
Eli Zaretskii <eliz@gnu.org>
2014-10-08 15:32:38 +0300

    Fix bug #18649 with handling C-g on MS-Windows in -nw sessions.
     src/w32inevt.c (maybe_generate_resize_event): Pass non-zero as the
     DELAY argument to change_frame_size, so that the frame size
     changes, if any are needed, are delayed until the next redisplay.
     This is to avoid a too early QUIT inside change_frame_size, when
     it calls Lisp in frame_windows_min_size, in case one of the events
     we've read sets the quit-flag.

--- src/w32inevt.c
+++ src/w32inevt.c
@@ -605,7 +605,7 @@ maybe_generate_resize_event (void)
   change_frame_size (f,
 		     1 + info.srWindow.Right - info.srWindow.Left,
 		     1 + info.srWindow.Bottom - info.srWindow.Top
-		     - FRAME_MENU_BAR_LINES (f), 0, 0, 0, 0);
+		     - FRAME_MENU_BAR_LINES (f), 0, 1, 0, 0);
 }
 
 #if HAVE_W32NOTIFY
