c1ec59da4905015d841ffeba9bb53a674b3a44f4
Jan Djärv <jan.h.d@swipnet.se>
2014-10-11 22:00:59 +0200

    Fix ns-use-fullscreen-animation again...  add boolean.
    * cus-start.el (all): Add missing ns and boolean to
    ns-use-fullscreen-animation.

--- lisp/cus-start.el
+++ lisp/cus-start.el
@@ -404,7 +404,7 @@ Leaving \"Default\" unchecked is equivalent with specifying a default of
 	     (ns-antialias-text ns boolean "23.1")
 	     (ns-auto-hide-menu-bar ns boolean "24.1")
 	     (ns-use-native-fullscreen ns boolean "24.4")
-             (ns-use-fullscreen-animation ns "25.1")
+             (ns-use-fullscreen-animation ns boolean "25.1")
              (ns-use-srgb-colorspace ns boolean "24.4")
 	     ;; process.c
 	     (delete-exited-processes processes-basics boolean)
