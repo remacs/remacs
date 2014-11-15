b8c9a4e36dc3525fa03e0e0d862693dc26137b5f
Jan D <jan.h.d@swipnet.se>
2014-11-15 14:35:15 +0100

    Fix getting frame size wrong when restoring desktop.
    * nsmenu.m (update_frame_tool_bar): If tool bar changes height,
    call updateFrameSize.

--- src/nsmenu.m
+++ src/nsmenu.m
@@ -1072,9 +1072,12 @@ update_frame_tool_bar (struct frame *f)
   EmacsView *view = FRAME_NS_VIEW (f);
   NSWindow *window = [view window];
   EmacsToolbar *toolbar = [view toolbar];
+  int oldh;
 
   if (view == nil || toolbar == nil) return;
   block_input ();
+
+  oldh = FRAME_TOOLBAR_HEIGHT (f);
 
 #ifdef NS_IMPL_COCOA
   [toolbar clearActive];
@@ -1182,6 +1185,8 @@ update_frame_tool_bar (struct frame *f)
   if (FRAME_TOOLBAR_HEIGHT (f) < 0) // happens if frame is fullscreen.
     FRAME_TOOLBAR_HEIGHT (f) = 0;
 
+  if (oldh != FRAME_TOOLBAR_HEIGHT (f))
+    [view updateFrameSize:YES];
   if (view->wait_for_tool_bar && FRAME_TOOLBAR_HEIGHT (f) > 0)
       [view setNeedsDisplay: YES];
 
