c39443c1d651bab2eb023f4c38db418c3dc04160
Ulf Jasper <ulf.jasper@web.de>
2014-11-21 16:31:30 +0100

    'libxml-parse(html|xml)-region': new optional param 'discard-comments'.
    * doc/lispref/text.texi (Parsing HTML/XML): Document new optional parameter
    'discard-comments' of 'libxml-parse(html|xml)-region'.
    
    * src/xml.c (parse_region): Take care of new optional parameter
    'discard-comments' of 'libxml-parse(html|xml)-region'.
    (Flibxml_parse_html_region, Flibxml_parse_xml_region): New
    optional parameter 'discard-comments'.
    
    * test/automated/libxml-tests.el
    (libxml-tests--data-comments-preserved): Renamed from
    'libxml-tests--data'.
    (libxml-tests--data-comments-discarded): New.
    (libxml-tests): Check whether 'libxml-parse-xml-region' is
    discarding comments correctly.

--- test/automated/libxml-tests.el
+++ test/automated/libxml-tests.el
@@ -27,7 +27,7 @@
 
 (require 'ert)
 
-(defvar libxml-tests--data
+(defvar libxml-tests--data-comments-preserved
   `(;; simple case
     ("<?xml version=\"1.0\"?><foo baz=\"true\">bar</foo>"
      . (foo ((baz . "true")) "bar"))
@@ -40,17 +40,35 @@
 	      "<bar>blub</bar></foo><!--comment-b--><!--comment-c-->")
      . (top nil (comment nil "comment-a") (foo ((a . "b")) (bar nil "blub"))
             (comment nil "comment-b") (comment nil "comment-c"))))
-  "Alist of XML strings and their expected parse trees.")
+  "Alist of XML strings and their expected parse trees for preserved comments.")
+
+(defvar libxml-tests--data-comments-discarded
+  `(;; simple case
+    ("<?xml version=\"1.0\"?><foo baz=\"true\">bar</foo>"
+     . (foo ((baz . "true")) "bar"))
+    ;; toplevel comments -- first document child must not get lost
+    (,(concat "<?xml version=\"1.0\"?><foo>bar</foo><!--comment-1-->"
+	      "<!--comment-2-->")
+     . (foo nil "bar"))
+    (,(concat "<?xml version=\"1.0\"?><!--comment-a--><foo a=\"b\">"
+	      "<bar>blub</bar></foo><!--comment-b--><!--comment-c-->")
+     . (foo ((a . "b")) (bar nil "blub"))))
+  "Alist of XML strings and their expected parse trees for discarded comments.")
 
 
 (ert-deftest libxml-tests ()
   "Test libxml."
   (when (fboundp 'libxml-parse-xml-region)
     (with-temp-buffer
-      (dolist (test libxml-tests--data)
+      (dolist (test libxml-tests--data-comments-preserved)
         (erase-buffer)
         (insert (car test))
         (should (equal (cdr test)
-                       (libxml-parse-xml-region (point-min) (point-max))))))))
+                       (libxml-parse-xml-region (point-min) (point-max)))))
+      (dolist (test libxml-tests--data-comments-discarded)
+        (erase-buffer)
+        (insert (car test))
+        (should (equal (cdr test)
+                       (libxml-parse-xml-region (point-min) (point-max) nil t)))))))
 
 ;;; libxml-tests.el ends here
