52a4e87c630ea397408efe4f8486be55e1199905
Paul Eggert <eggert@cs.ucla.edu>
2015-01-01 14:27:34 -0800

    Fix copyright years by hand
    These are dates that admin/update-copyright did not update, or
    updated incorrectly.  Also, back out the copyright-date change to
    doc/misc/texinfo.tex, as upstream hasn't updated that date yet.

--- lisp/net/tramp-sh.el
+++ lisp/net/tramp-sh.el
@@ -736,7 +736,7 @@ on the remote host.")
 (defconst tramp-perl-encode
   "%s -e '
 # This script contributed by Juanma Barranquero <lektu@terra.es>.
-# Copyright (C) 2002-2014 Free Software Foundation, Inc.
+# Copyright (C) 2002-2015 Free Software Foundation, Inc.
 use strict;
 
 my %%trans = do {
@@ -774,7 +774,7 @@ This string is passed to `format', so percent characters need to be doubled.")
 (defconst tramp-perl-decode
   "%s -e '
 # This script contributed by Juanma Barranquero <lektu@terra.es>.
-# Copyright (C) 2002-2014 Free Software Foundation, Inc.
+# Copyright (C) 2002-2015 Free Software Foundation, Inc.
 use strict;
 
 my %%trans = do {
