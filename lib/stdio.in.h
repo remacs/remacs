035f5aaa91ebd40598e0d89881bbc3cf532440a0
Paul Eggert <eggert@cs.ucla.edu>
2015-01-01 11:05:24 -0800

    Merge from gnulib, incorporating:
    2015-01-01 version-etc: new year

--- lib/stdio.in.h
+++ lib/stdio.in.h
@@ -1,6 +1,6 @@
 /* A GNU-like <stdio.h>.
 
-   Copyright (C) 2004, 2007-2014 Free Software Foundation, Inc.
+   Copyright (C) 2004, 2007-2015 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
