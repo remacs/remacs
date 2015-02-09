5c20aa7486f9cb8871f40bca9ac5fee09caefca3
Paul Eggert <eggert@cs.ucla.edu>
2015-02-08 16:00:17 -0800

    Port to platforms lacking test -a and -o
    * configure.ac (HAVE_LIBXML2):
    * lisp/Makefile.in (compile-clean):
    * lisp/net/tramp-sh.el (tramp-find-executable):
    Prefer '&&' and '||' to 'test -a' and 'test -o'.

--- lisp/net/tramp-sh.el
+++ lisp/net/tramp-sh.el
@@ -3796,7 +3796,7 @@ This function expects to be in the right *tramp* buffer."
 	(tramp-send-command
 	 vec
 	 (format (concat "while read d; "
-			 "do if test -x $d/%s -a -f $d/%s; "
+			 "do if test -x $d/%s && test -f $d/%s; "
 			 "then echo tramp_executable $d/%s; "
 			 "break; fi; done <<'%s'\n"
 			 "%s\n%s")
