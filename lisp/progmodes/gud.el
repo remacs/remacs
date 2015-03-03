7133f262bbd818509825a3317c91e91e62bd56fb
Stefan Monnier <monnier@iro.umontreal.ca>
2015-03-03 14:23:49 -0500

    * lisp/progmodes/gud.el: Use lexical-binding.
    Fixes: debbugs:19966
    
    * lisp/emacs-lisp/gv.el (gv-ref): Warn about likely problematic cases.

--- lisp/progmodes/gud.el
+++ lisp/progmodes/gud.el
@@ -1,4 +1,4 @@
-;;; gud.el --- Grand Unified Debugger mode for running GDB and other debuggers
+;;; gud.el --- Grand Unified Debugger mode for running GDB and other debuggers  -*- lexical-binding:t -*-
 
 ;; Copyright (C) 1992-1996, 1998, 2000-2015 Free Software Foundation,
 ;; Inc.
