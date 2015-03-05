17ecfea3b904c4676a52dd9d2ea33c8c3e13b870
Artur Malabarba <am12548@it055607.users.bris.ac.uk>
2015-03-05 13:25:53 +0000

    package.el (package-refresh-contents): Update doc.

--- lisp/emacs-lisp/package.el
+++ lisp/emacs-lisp/package.el
@@ -1695,9 +1695,10 @@ similar to an entry in `package-alist'.  Save the cached copy to
 
 ;;;###autoload
 (defun package-refresh-contents ()
-  "Download the ELPA archive description if needed.
-This informs Emacs about the latest versions of all packages, and
-makes them available for download."
+  "Download the description of all configured ELPAs.
+For each archive configured in the variable `package-archives',
+this informs Emacs about the latest versions of all packages it
+offers, and makes them available for download."
   (interactive)
   ;; FIXME: Do it asynchronously.
   (unless (file-exists-p package-user-dir)
