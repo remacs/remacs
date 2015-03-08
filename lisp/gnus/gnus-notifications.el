6b134bcba9de5605086ee9382c0be13174480cac
Adam Sjøgren <asjo@koldfront.dk>
2015-03-08 22:45:06 +0000

    lisp/gnus/gnus-notifications.el (gnus-notifications-action): Replace x-focus-frame kludge by calling gnus-select-frame-set-input-focus

--- lisp/gnus/gnus-notifications.el
+++ lisp/gnus/gnus-notifications.el
@@ -81,7 +81,7 @@ not get notifications."
             (article (nth 2 group-article)))
         (cond ((string= key "read")
                (gnus-fetch-group group (list article))
-               (when (and window-system (fboundp 'x-focus-frame)) (x-focus-frame (selected-frame))))
+               (gnus-select-frame-set-input-focus (selected-frame)))
               ((string= key "mark-read")
                (gnus-update-read-articles
                 group
