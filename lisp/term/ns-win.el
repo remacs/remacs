d4d66f4d5409a5d0ab2c821f66c9c9485d7ea9d0
Jan D <jan.h.d@swipnet.se>
2015-05-01 10:22:29 +0200

    (ns-get-cut-buffer-internal): Remove this alias.
    * lisp/term/ns-win.el (ns-get-cut-buffer-internal): Remove this alias.

--- lisp/term/ns-win.el
+++ lisp/term/ns-win.el
@@ -717,8 +717,6 @@ See the documentation of `create-fontset-from-fontset-spec' for the format.")
 
 ;;;; Pasteboard support.
 
-(define-obsolete-function-alias 'ns-get-cut-buffer-internal
-  'ns-get-selection-internal "24.1")
 (define-obsolete-function-alias 'ns-store-cut-buffer-internal
   'gui-set-selection "24.1")
 
