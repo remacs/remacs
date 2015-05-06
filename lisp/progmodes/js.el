754fdb1280dfe56a0b94b20c1d7ab0fb5b5615d1
Glenn Morris <rgm@gnu.org>
2015-05-05 20:11:22 -0700

    * lisp/progmodes/js.el (js--optimize-arglist): Remove declaration.

--- lisp/progmodes/js.el
+++ lisp/progmodes/js.el
@@ -2852,10 +2852,6 @@ with `js--js-encode-value'."
 (defsubst js--js-true (value)
   (not (js--js-not value)))
 
-;; The somewhat complex code layout confuses the byte-compiler into
-;; thinking this function "might not be defined at runtime".
-(declare-function js--optimize-arglist "js" (arglist))
-
 (eval-and-compile
   (defun js--optimize-arglist (arglist)
     "Convert immediate js< and js! references to deferred ones."
