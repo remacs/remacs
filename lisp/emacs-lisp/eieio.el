5ed5a04963aa5372cd07d72e0330045e99609964
Glenn Morris <rgm@gnu.org>
2015-05-08 06:22:25 -0400

    ; Auto-commit of loaddefs files.

--- lisp/emacs-lisp/eieio.el
+++ lisp/emacs-lisp/eieio.el
@@ -978,7 +978,7 @@ Optional argument GROUP is the sub-group of slots to display.
 
 ;;;***
 
-;;;### (autoloads nil "eieio-opt" "eieio-opt.el" "3005b815c6b30eccbf0642170b3f82a5")
+;;;### (autoloads nil "eieio-opt" "eieio-opt.el" "59cee62a4829ace9bb4a6526442d2b3c")
 ;;; Generated autoloads from eieio-opt.el
 
 (autoload 'eieio-browse "eieio-opt" "\
