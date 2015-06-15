e58a815339fe77f184da8e71e3b932a46b2c0433
Paul Eggert <eggert@cs.ucla.edu>
2015-06-15 12:40:32 -0700

    Fix quoting when making derived mode docstring
    * lisp/emacs-lisp/derived.el (derived-mode-make-docstring):
    Nest regexp-quote inside format, not the reverse.
    Problem reported by Artur Malabarba in:
    http://lists.gnu.org/archive/html/emacs-devel/2015-06/msg00206.html

--- lisp/emacs-lisp/derived.el
+++ lisp/emacs-lisp/derived.el
@@ -331,9 +331,10 @@ which more-or-less shadow%s %s's corresponding table%s."
 			"\n\nThis mode "
 		      (concat
 		       "\n\nIn addition to any hooks its parent mode "
-		       (if (string-match (regexp-quote (format "[`‘]%s['’]"
-                                                               parent))
-					 docstring) nil
+		       (if (string-match (format "[`‘]%s['’]"
+                                                 (regexp-quote parent))
+					 docstring)
+                           nil
 			 (format "`%s' " parent))
 		       "might have run,\nthis mode "))
 		    (format "runs the hook `%s'" hook)
