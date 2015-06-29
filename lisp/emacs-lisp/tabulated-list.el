23d00d4cddc4526483248f8886ab26310a280ed3
Artur Malabarba <bruce.connor.am@gmail.com>
2015-06-29 10:07:24 +0100

    * lisp/emacs-lisp/tabulated-list.el (tabulated-list-print):
    Don't block remember-pos if buffer isn't displayed.  (Bug#20921)

--- lisp/emacs-lisp/tabulated-list.el
+++ lisp/emacs-lisp/tabulated-list.el
@@ -321,11 +321,11 @@ changing `tabulated-list-sort-key'."
         (sorter (tabulated-list--get-sorter))
 	entry-id saved-pt saved-col window-line)
     (and remember-pos
+	 (setq entry-id (tabulated-list-get-id))
+	 (setq saved-col (current-column))
          (when (eq (window-buffer) (current-buffer))
            (setq window-line
-                 (count-screen-lines (window-start) (point))))
-	 (setq entry-id (tabulated-list-get-id))
-	 (setq saved-col (current-column)))
+                 (count-screen-lines (window-start) (point)))))
     ;; Sort the entries, if necessary.
     (when sorter
       (setq entries (sort entries sorter)))
