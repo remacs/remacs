41cb0162c5bcf440dca36afcd493db585e8c4901
Paul Eggert <eggert@cs.ucla.edu>
2015-08-23 13:50:59 -0700

    Fix minor glitches from ‘format’ reversion
    * doc/lispref/strings.texi (Formatting Strings):
    After reversion, ‘text-quoting-style’ is documented in ‘Keys in
    Documentation’, not below.
    * src/syntax.c (Finternal_describe_syntax_value):
    Prefer AUTO_STRING to build_string where either will do, as
    AUTO_STRING is a bit faster.

--- src/syntax.c
+++ src/syntax.c
@@ -1333,9 +1333,11 @@ DEFUN ("internal-describe-syntax-value", Finternal_describe_syntax_value,
     insert_string (" (nestable)");
 
   if (prefix)
-    insert1 (Fsubstitute_command_keys
-	     (build_string
-	      (",\n\t  is a prefix character for `backward-prefix-chars'")));
+    {
+      AUTO_STRING (prefixdoc,
+		   ",\n\t  is a prefix character for `backward-prefix-chars'");
+      insert1 (Fsubstitute_command_keys (prefixdoc));
+    }
 
   return syntax;
 }
