bf86385345e37e69d03d65ac3e7ce3fc30d9269d
Stefan Monnier <monnier@iro.umontreal.ca>
2015-09-09 16:31:58 -0400

    (time-to-seconds, time-less-p): Mark unused vars with underscore
    * lisp/calendar/time-date.el (time-to-seconds, time-less-p):
    Mark unused vars with underscore.

--- lisp/calendar/time-date.el
+++ lisp/calendar/time-date.el
@@ -179,7 +179,7 @@ If DATE lacks timezone information, GMT is assumed."
       (defun time-to-seconds (&optional time)
         "Convert optional value TIME to a floating point number.
 TIME defaults to the current time."
-        (with-decoded-time-value ((high low micro pico type
+        (with-decoded-time-value ((high low micro pico _type
 				   (or time (current-time))))
           (+ (* high 65536.0)
              low
@@ -262,8 +262,8 @@ Return the difference in the format of a time value."
 
     (defun time-less-p (t1 t2)
       "Return non-nil if time value T1 is earlier than time value T2."
-      (with-decoded-time-value ((high1 low1 micro1 pico1 type1 t1)
-				(high2 low2 micro2 pico2 type2 t2))
+      (with-decoded-time-value ((high1 low1 micro1 pico1 _type1 t1)
+				(high2 low2 micro2 pico2 _type2 t2))
 	(or (< high1 high2)
 	    (and (= high1 high2)
 		 (or (< low1 low2)
