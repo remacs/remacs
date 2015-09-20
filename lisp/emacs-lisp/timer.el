c7e69c404be9d7611ffb00ea027fed90c0667fec
Paul Eggert <eggert@cs.ucla.edu>
2015-09-20 09:45:04 -0700

    '.' -> `.' in doc string
    * lisp/emacs-lisp/timer.el (run-at-time): In doc string, quote
    individual chars with grave quotes instead of straight quotes, as
    this works better when they are translated to curved quotes.

--- lisp/emacs-lisp/timer.el
+++ lisp/emacs-lisp/timer.el
@@ -350,7 +350,7 @@ TIME should be one of:
 - a string giving today's time like \"11:23pm\"
   (the acceptable formats are HHMM, H:MM, HH:MM, HHam, HHAM,
   HHpm, HHPM, HH:MMam, HH:MMAM, HH:MMpm, or HH:MMPM;
-  a period '.' can be used instead of a colon ':' to separate
+  a period `.' can be used instead of a colon `:' to separate
   the hour and minute parts);
 - a string giving a relative time like \"90\" or \"2 hours 35 minutes\"
   (the acceptable forms are a number of seconds without units
