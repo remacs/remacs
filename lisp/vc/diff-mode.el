138ad3d93b7abe08ac399f582aa6c8aac869e17e
Dmitry Gutov <dgutov@yandex.ru>
2015-11-14 03:03:58 +0200

    ; Fix warnings
    * lisp/vc/diff-mode.el (diff-kill-applied-hunks):
    Fix unused variable warnings.

--- lisp/vc/diff-mode.el
+++ lisp/vc/diff-mode.el
@@ -1821,7 +1821,7 @@ With a prefix argument, try to REVERSE the hunk."
   "Kill all hunks that have already been applied starting at point."
   (interactive)
   (while (not (eobp))
-    (pcase-let ((`(,buf ,line-offset ,pos ,src ,_dst ,switched)
+    (pcase-let ((`(,_buf ,line-offset ,_pos ,_src ,_dst ,switched)
                  (diff-find-source-location nil nil)))
       (if (and line-offset switched)
           (diff-hunk-kill)
