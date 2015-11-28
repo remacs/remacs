40b794bf004032e6f9a01047562c6504bcf7cf4e
Eli Zaretskii <eliz@gnu.org>
2015-11-28 13:11:05 +0200

    Add etags tests for the recent Lua-related bugfix
    * test/etags/lua-src/test.lua: New file, tests the issues raised
    by bug#21934.
    * test/etags/Makefile (LUASRC): Add test.lua.
    * test/etags/ETAGS.good_1:
    * test/etags/ETAGS.good_2:
    * test/etags/ETAGS.good_3:
    * test/etags/ETAGS.good_4:
    * test/etags/ETAGS.good_5:
    * test/etags/ETAGS.good_6:
    * test/etags/CTAGS.good: Adapt to the new Lua test.  Also, an old
    regression fix, resolved around 25 May 2015, required changes to
    the "good" ETAGS files.

---
+++ test/etags/lua-src/test.lua
@@ -0,0 +1,15 @@
+Rectangle = {}
+function Rectangle.getPos ()
+end
+
+Circle = {}
+function Circle.getPos ()
+end
+
+Cube = {}
+function Cube.data.getFoo ()
+end
+
+Square = {}
+function Square.something:Bar ()
+end
