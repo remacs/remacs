d686802510c3902f6be4a4c4e82b1134d9dfb8a0
Lars Ingebrigtsen <larsi@gnus.org>
2015-12-28 20:52:41 +0100

    Reconnect erc even on server errors
    * lisp/erc/erc-backend.el (erc-server-reconnect-p): Try to
    reconnect even if a server error has occurred (bug#18527).

--- lisp/erc/erc-backend.el
+++ lisp/erc/erc-backend.el
@@ -609,7 +609,6 @@ EVENT is the message received from the closed connection process."
            ;; otherwise go through the full spectrum of checks:
            (and erc-server-auto-reconnect
                 (not erc-server-banned)
-                (not erc-server-error-occurred)
                 ;; make sure we don't infinitely try to reconnect, unless the
                 ;; user wants that
                 (or (eq erc-server-reconnect-attempts t)
