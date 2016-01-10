bd3f53dd2e14b886ec46667a10611c549d4f09be
Stefan Monnier <monnier@iro.umontreal.ca>
2016-01-10 00:31:29 -0500

    * sh-script.el (sh-smie-sh-rules): Improve indentation inside $(...)
    * lisp/progmodes/sh-script.el (sh-smie-sh-rules): Improve indentation
    within $(...).
    * test/indent/shell.sh: Add corresponding test.

--- lisp/progmodes/sh-script.el
+++ lisp/progmodes/sh-script.el
@@ -2115,7 +2115,11 @@ May return nil if the line should not be treated as continued."
     ;; sh-indent-after-done: aligned completely differently.
     (`(:after . "in") (sh-var-value 'sh-indent-for-case-label))
     ;; sh-indent-for-continuation: Line continuations are handled differently.
-    (`(:after . ,(or `"(" `"{" `"[")) (sh-var-value 'sh-indent-after-open))
+    (`(:after . ,(or `"(" `"{" `"["))
+     (if (not (looking-at ".[ \t]*[^\n \t#]"))
+         (sh-var-value 'sh-indent-after-open)
+       (goto-char (1- (match-end 0)))
+       `(column . ,(current-column))))
     ;; sh-indent-after-function: we don't handle it differently.
     ))
 
