72e3713dc10dfacaabb23bbc905e7a45e0960307
Paul Eggert <eggert@cs.ucla.edu>
2016-01-30 18:59:49 -0800

    Port recent my_edata change to MS-Windows
    * src/lastfile.c (my_edata): Also define if WINDOWSNT.

--- src/lastfile.c
+++ src/lastfile.c
@@ -38,8 +38,8 @@ along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #include "lisp.h"
 
-#if ((!defined SYSTEM_MALLOC && !defined HYBRID_MALLOC && !defined WINDOWSNT) \
-     || defined CYGWIN || defined DARWIN_OS)
+#if ((!defined SYSTEM_MALLOC && !defined HYBRID_MALLOC) \
+     || defined WINDOWSNT || defined CYGWIN || defined DARWIN_OS)
 char my_edata[] = "End of Emacs initialized data";
 #endif
 
