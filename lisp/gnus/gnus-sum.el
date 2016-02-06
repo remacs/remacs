3ed423bc352b423960c643f297ec0f0fa3b7d2e1
OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>
2016-02-06 15:41:05 +1100

    Display the decoded Gnus group name
    * lisp/gnus/gnus-sum.el (gnus-summary-read-group-1): Use the
    decoded group name in the message.
    
    Copyright-paperwork-exempt: yes

--- lisp/gnus/gnus-sum.el
+++ lisp/gnus/gnus-sum.el
@@ -4004,7 +4004,8 @@ If SELECT-ARTICLES, only select those articles from GROUP."
 	      (gnus-group-next-unread-group 1))
 	  (gnus-handle-ephemeral-exit quit-config)))
       (if (null (gnus-list-of-unread-articles group))
-	  (gnus-message 3 "Group %s contains no messages" group)
+	  (gnus-message 3 "Group %s contains no messages"
+			(gnus-group-decoded-name group))
 	(gnus-message 3 "Can't select group"))
       nil)
      ;; The user did a `C-g' while prompting for number of articles,
