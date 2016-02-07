7149cc54e7e8bb2266f44221d7f4b4bed70579f3
Alan Mackenzie <acm@muc.de>
2016-02-07 15:06:43 +0000

    On leaving CC Mode, clean up by removing character properties.
    * lisp/progmodes/cc-mode.el (c-leave-cc-mode-mode): Remove from the buffer
    all instances of the text properties/extents category, syntax-table,
    c-is-sws, c-in-sws, c-type, and c-awk-NL-prop.

--- lisp/progmodes/cc-mode.el
+++ lisp/progmodes/cc-mode.el
@@ -141,7 +141,18 @@
 ;; derived-mode-ex.el>.
 
 (defun c-leave-cc-mode-mode ()
-  (setq c-buffer-is-cc-mode nil))
+  (when c-buffer-is-cc-mode
+    (save-restriction
+      (widen)
+      (c-save-buffer-state ()
+	(c-clear-char-properties (point-min) (point-max) 'category)
+	(c-clear-char-properties (point-min) (point-max) 'syntax-table)
+	(c-clear-char-properties (point-min) (point-max) 'c-is-sws)
+	(c-clear-char-properties (point-min) (point-max) 'c-in-sws)
+	(c-clear-char-properties (point-min) (point-max) 'c-type)
+	(if (c-major-mode-is 'awk-mode)
+	    (c-clear-char-properties (point-min) (point-max) 'c-awk-NL-prop))))
+    (setq c-buffer-is-cc-mode nil)))
 
 (defun c-init-language-vars-for (mode)
   "Initialize the language variables for one of the language modes
