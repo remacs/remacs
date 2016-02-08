22e239cb406cd5008d3942e66ebb153d8a2d20a4
David Edmondson <dme@dme.org>
2016-02-09 10:26:52 +1100

    Compare recipient and keys case-insensitively
    * lisp/gnus/mml2015.el: (mml-secure-check-user-id): When comparing a
    recipient address with that from a key, do so in a case insensitive
    manner (bug#22603).

--- lisp/gnus/mml-sec.el
+++ lisp/gnus/mml-sec.el
@@ -655,10 +655,10 @@ The passphrase is read and cached."
     (catch 'break
       (dolist (uid uids nil)
 	(if (and (stringp (epg-user-id-string uid))
-		 (equal (car (mail-header-parse-address
-			      (epg-user-id-string uid)))
-			(car (mail-header-parse-address
-			      recipient)))
+		 (equal (downcase (car (mail-header-parse-address
+					(epg-user-id-string uid))))
+			(downcase (car (mail-header-parse-address
+					recipient))))
 		 (not (memq (epg-user-id-validity uid)
 			    '(revoked expired))))
 	    (throw 'break t))))))
