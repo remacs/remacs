5825f42e7a2ed27610f83234038fcf91ec44222e
Glenn Morris <rgm@gnu.org>
2016-02-08 08:43:52 -0800

    ; Some standardization of copyright and license notices in recent new files

--- test/lisp/net/network-stream-tests.el
+++ test/lisp/net/network-stream-tests.el
@@ -4,6 +4,8 @@
 
 ;; Author: Lars Ingebrigtsen <larsi@gnus.org>
 
+;; This file is part of GNU Emacs.
+
 ;; GNU Emacs is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
