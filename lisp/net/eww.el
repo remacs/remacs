9935e7cc8ca61bc0852eefe1142e4c8c62416c73
Lars Ingebrigtsen <larsi@gnus.org>
2016-02-09 13:51:45 +1100

    Make the `R' command get the correct relative <img>s
    * lisp/net/eww.el (eww-readable): Preserve the base URL so
    that image expansions are fetched from the right place (bug#22605).
    
    (cherry picked from commit 8722e849f75ceafb82a1c17105e8ab76077a8ebc)
    
    Backport:

--- lisp/net/eww.el
+++ lisp/net/eww.el
@@ -615,11 +615,13 @@ the like."
 		(condition-case nil
 		    (decode-coding-region (point-min) (point-max) 'utf-8)
 		  (coding-system-error nil))
-		(libxml-parse-html-region (point-min) (point-max)))))
+		(libxml-parse-html-region (point-min) (point-max))))
+         (base (plist-get eww-data :url)))
     (eww-score-readability dom)
     (eww-save-history)
     (eww-display-html nil nil
-		      (eww-highest-readability dom)
+                      (list 'base (list (cons 'href base))
+                            (eww-highest-readability dom))
 		      nil (current-buffer))
     (dolist (elem '(:source :url :title :next :previous :up))
       (plist-put eww-data elem (plist-get old-data elem)))
