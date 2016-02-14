374c21d59a3e2b8a49c7e4ecc466edb5313dbb98
Lars Ingebrigtsen <larsi@gnus.org>
2016-02-14 14:50:53 +1100

    Remove compat function from pop3
    * lisp/gnus/pop3.el (pop3-set-process-query-on-exit-flag): Remove.

--- lisp/gnus/pop3.el
+++ lisp/gnus/pop3.el
@@ -533,13 +533,6 @@ this is nil, `ssl' is assumed for connections to port
 		 (const :tag "SSL/TLS" ssl)
 		 (const starttls)))
 
-(eval-and-compile
-  (if (fboundp 'set-process-query-on-exit-flag)
-      (defalias 'pop3-set-process-query-on-exit-flag
-	'set-process-query-on-exit-flag)
-    (defalias 'pop3-set-process-query-on-exit-flag
-      'process-kill-without-query)))
-
 (defun pop3-open-server (mailhost port)
   "Open TCP connection to MAILHOST on PORT.
 Returns the process associated with the connection."
@@ -576,7 +569,7 @@ Returns the process associated with the connection."
 	  (setq pop3-timestamp
 		(substring response (or (string-match "<" response) 0)
 			   (+ 1 (or (string-match ">" response) -1)))))
-	(pop3-set-process-query-on-exit-flag (car result) nil)
+	(set-process-query-on-exit-flag (car result) nil)
 	(erase-buffer)
 	(car result)))))
 
