92da003692b894e6dc514e3a56a1861849dcad8c
Lars Ingebrigtsen <larsi@gnus.org>
2016-02-23 16:09:06 +1100

    Require that the buffer that we're trying to patch exist
    * lisp/vc/ediff.el (ediff-patch-buffer): Require that the
    buffer that we're trying to patch exist (bug#21852).

--- lisp/vc/ediff.el
+++ lisp/vc/ediff.el
@@ -1367,7 +1367,8 @@ buffer. If odd -- assume it is in a file."
     (require 'ediff-ptch)
     (setq patch-buf
 	  (ediff-get-patch-buffer
-	   (if arg (prefix-numeric-value arg)) patch-buf))
+	   (if arg (prefix-numeric-value arg))
+           (get-buffer patch-buf)))
     (setq source-dir (cond (ediff-use-last-dir ediff-last-dir-patch)
 			   ((and (not ediff-patch-default-directory)
 				 (buffer-file-name patch-buf))
