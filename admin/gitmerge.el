4fab26ba5ac381ce836eff521fc56d8d28570221
Paul Eggert <eggert@cs.ucla.edu>
2016-03-22 15:46:30 -0700

    * admin/gitmerge.el (gitmerge-skip-regexp): Add "no need to merge".

--- admin/gitmerge.el
+++ admin/gitmerge.el
@@ -50,7 +50,7 @@
 (defvar gitmerge-skip-regexp
   ;; We used to include "sync" in there, but in my experience it only
   ;; caused false positives.  --Stef
-  "back[- ]?port\\|do not merge\\|re-?generate\\|bump version\\|from trunk\\|\
+  "back[- ]?port\\|\\(do not\\|no need to\\) merge\\|re-?generate\\|bump version\\|from trunk\\|\
 Auto-commit"
   "Regexp matching logs of revisions that might be skipped.
 `gitmerge-missing' will ask you if it should skip any matches.")
