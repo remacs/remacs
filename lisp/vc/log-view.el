b8d5a8fd1a16a30ac42883bfea3f25ecef1e7fde
Dmitry Gutov <dgutov@yandex.ru>
2016-04-19 01:20:59 +0300

    Remove the Meta-CVS VC backend
    * lisp/obsolete/vc-mcvs.el: Remove the file (bug#20475).
    
    * lisp/vc/log-view.el (log-view-extract-comment): Remove the MCVS
    reference.
    
    * doc/misc/efaq-w32.texi (Version control): Same.

--- lisp/vc/log-view.el
+++ lisp/vc/log-view.el
@@ -542,7 +542,7 @@ If called interactively, visit the version at point."
       (setq en (point))
       (or (log-view-current-entry nil t)
           (throw 'beginning-of-buffer nil))
-      (cond ((memq backend '(SCCS RCS CVS MCVS SVN))
+      (cond ((memq backend '(SCCS RCS CVS SVN))
 	     (forward-line 2))
 	    ((eq backend 'Hg)
 	     (forward-line 4)
