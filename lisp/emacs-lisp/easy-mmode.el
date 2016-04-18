d11abb25428e9a468a5525c462489eb5a854798f
Philipp Stephani <p.stephani2@gmail.com>
2016-04-18 12:32:35 -0400

    * lisp/emacs-lisp/easy-mmode.el (define-minor-mode): Fix docstring
    so that it matches the actual implementation.
    See https://lists.gnu.org/archive/html/help-gnu-emacs/2016-04/msg00071.html

--- lisp/emacs-lisp/easy-mmode.el
+++ lisp/emacs-lisp/easy-mmode.el
@@ -108,9 +108,10 @@ Optional LIGHTER is displayed in the mode line when the mode is on.
 Optional KEYMAP is the default keymap bound to the mode keymap.
   If non-nil, it should be a variable name (whose value is a keymap),
   or an expression that returns either a keymap or a list of
-  arguments for `easy-mmode-define-keymap'.  If you supply a KEYMAP
-  argument that is not a symbol, this macro defines the variable
-  MODE-map and gives it the value that KEYMAP specifies.
+  (KEY . BINDING) pairs where KEY and BINDING are suitable for
+  `define-key'.  If you supply a KEYMAP argument that is not a
+  symbol, this macro defines the variable MODE-map and gives it
+  the value that KEYMAP specifies.
 
 BODY contains code to execute each time the mode is enabled or disabled.
   It is executed after toggling the mode, and before running MODE-hook.
