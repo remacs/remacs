02c1aa8fe52959df140dd1c0ab8467f00c946a5c
Lars Ingebrigtsen <larsi@gnus.org>
2016-04-29 22:14:05 +0200

    Doc fix for align-newline-and-indent
    * lisp/align.el (align-newline-and-indent): Mention that
    alignment is done by `align' (bug#17707).
    
    (cherry picked from commit 340a224ec3e01706112a07164da9a9f3f369a5aa)

--- lisp/align.el
+++ lisp/align.el
@@ -1051,7 +1051,9 @@ to be colored."
 
 ;;;###autoload
 (defun align-newline-and-indent ()
-  "A replacement function for `newline-and-indent', aligning as it goes."
+  "A replacement function for `newline-and-indent', aligning as it goes.
+The alignment is done by calling `align' on the region that was
+indented."
   (interactive)
   (let ((separate (or (if (and (symbolp align-region-separate)
 			       (boundp align-region-separate))
