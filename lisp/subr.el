9722ae18c3aa7243185fb005241f337712ac0ac5
Lars Ingebrigtsen <larsi@gnus.org>
2016-04-30 23:34:29 +0200

    with-silent-modifications doc clarification
    * lisp/subr.el (with-silent-modifications): Rearrange the doc
    string a bit so that the most pertinent information is at the
    top (bug#21171).
    
    (cherry picked from commit e0e70f030e69d9696a963a86f5f7caaff4df06eb)

--- lisp/subr.el
+++ lisp/subr.el
@@ -3291,6 +3291,8 @@ See also `with-temp-file' and `with-output-to-string'."
 
 (defmacro with-silent-modifications (&rest body)
   "Execute BODY, pretending it does not modify the buffer.
+This macro is Typically used around modifications of
+text-properties which do not really affect the buffer's content.
 If BODY performs real modifications to the buffer's text, other
 than cosmetic ones, undo data may become corrupted.
 
@@ -3298,10 +3300,7 @@ This macro will run BODY normally, but doesn't count its buffer
 modifications as being buffer modifications.  This affects things
 like `buffer-modified-p', checking whether the file is locked by
 someone else, running buffer modification hooks, and other things
-of that nature.
-
-Typically used around modifications of text-properties which do
-not really affect the buffer's content."
+of that nature."
   (declare (debug t) (indent 0))
   (let ((modified (make-symbol "modified")))
     `(let* ((,modified (buffer-modified-p))
