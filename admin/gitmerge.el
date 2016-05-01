f648e4866981b142fca788372e1fd9013a0bb16a
David Engster <deng@randomsample.de>
2016-05-01 21:05:18 +0200

    gitmerge: Add cherry pick to gitmerge-skip-regexp
    * gitmerge.el (gitmerge-skip-regexp): Add "cherry picked from commit",
      which is the string appended by 'git cherry-pick -x'.

--- admin/gitmerge.el
+++ admin/gitmerge.el
@@ -50,8 +50,8 @@
 (defvar gitmerge-skip-regexp
   ;; We used to include "sync" in there, but in my experience it only
   ;; caused false positives.  --Stef
-  "back[- ]?port\\|\\(do not\\|no need to\\) merge\\|re-?generate\\|bump version\\|from trunk\\|\
-Auto-commit"
+  "back[- ]?port\\|cherry picked from commit\\|\\(do not\\|no need to\\) merge\\|\
+re-?generate\\|bump version\\|from trunk\\|Auto-commit"
   "Regexp matching logs of revisions that might be skipped.
 `gitmerge-missing' will ask you if it should skip any matches.")
 
