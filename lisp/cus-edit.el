f2536958ec711b50a0cf8714defb921193ea8ae4
Noam Postavsky <npostavs@gmail.com>
2016-06-25 13:12:11 -0400

    Update docs for `customize-mode'
    * lisp/cus-edit.el (customize-mode): This function works with both major
    and minor modes, make docstring and prompt reflect that (Bug #23649).
    
    * doc/lispref/modes.texi (Derived Modes): Remove note about
    customize-mode being experimental; it has been around since Emacs
    22 (Bug #11299).
    
    Co-authored-by: Drew Adams <drew.adams@oracle.com>

--- lisp/cus-edit.el
+++ lisp/cus-edit.el
@@ -1072,9 +1072,10 @@ are shown; the contents of those subgroups are initially hidden."
 
 ;;;###autoload
 (defun customize-mode (mode)
-  "Customize options related to the current major mode.
-If a prefix \\[universal-argument] was given (or if the current major mode has no known group),
-then prompt for the MODE to customize."
+  "Customize options related to a major or minor mode.
+By default the current major mode is used.  With a prefix
+argument or if the current major mode has no known group, prompt
+for the MODE to customize."
   (interactive
    (list
     (let ((completion-regexp-list '("-mode\\'"))
@@ -1083,8 +1084,8 @@ then prompt for the MODE to customize."
 	  major-mode
 	(intern
 	 (completing-read (if group
-			      (format "Major mode (default %s): " major-mode)
-			    "Major mode: ")
+			      (format "Mode (default %s): " major-mode)
+			    "Mode: ")
 			  obarray
 			  'custom-group-of-mode
 			  t nil nil (if group (symbol-name major-mode))))))))
