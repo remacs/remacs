248d5dd13cd2ae0b1319a52b331e451fa7dd99c8
Stefan Monnier <monnier@iro.umontreal.ca>
2016-07-14 14:56:38 -0400

    Include cl-generic in package--builtin-versions (bug#22817)
    * lisp/emacs-lisp/cl-generic.el (package--builtin-versions):
    Add ourselves manually.  Don't merge since there's a better fix on master.

--- lisp/emacs-lisp/cl-generic.el
+++ lisp/emacs-lisp/cl-generic.el
@@ -86,6 +86,11 @@
 
 ;;; Code:
 
+;; The autoloads.el mechanism which adds package--builtin-versions
+;; maintenance to loaddefs.el doesn't work for preloaded packages (such
+;; as this one), so we have to do it by hand!
+(push (purecopy '(cl-generic 1 0)) package--builtin-versions)
+
 ;; Note: For generic functions that dispatch on several arguments (i.e. those
 ;; which use the multiple-dispatch feature), we always use the same "tagcodes"
 ;; and the same set of arguments on which to dispatch.  This works, but is
