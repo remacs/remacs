9a41cd1215e60415777bb139c0669100e347375b
Mark Oteiza <mvoteiza@udel.edu>
2016-07-20 18:46:15 -0400

    ; Fix typo
    Thanks to Robert Weiner.

--- lisp/rect.el
+++ lisp/rect.el
@@ -403,7 +403,7 @@ With a prefix (or a FILL) argument, also fill too short lines."
   :version "25.1")
 
 (defcustom rectangle-preview t
-  "If non-nil, `string-rectangle' will show an-the-fly preview."
+  "If non-nil, `string-rectangle' will show an on-the-fly preview."
   :version "25.1"
   :type 'boolean)
 
