edcdf64960a2ab4e8d9ce4419874e43b6d3ccee4
Alan Mackenzie <acm@muc.de>
2016-07-23 10:43:34 +0000

    Java Mode: Handle strings as case labels correctly.
    This fixes debbugs #23901.
    
    * lisp/progmodes/cc-langs.el (c-nonlabel-token-key): Remove "\"" from the
    Java value.

--- lisp/progmodes/cc-langs.el
+++ lisp/progmodes/cc-langs.el
@@ -3253,8 +3253,8 @@ i.e. before \":\".  Only used if `c-recognize-colon-labels' is set."
 			  (append (c-lang-const c-label-kwds)
 				  (c-lang-const c-protection-kwds))
 			  :test 'string-equal)))
-  ;; Don't allow string literals, except in AWK.  Character constants are OK.
-  (c objc java pike idl) (concat "\"\\|"
+  ;; Don't allow string literals, except in AWK and Java.  Character constants are OK.
+  (c objc pike idl) (concat "\"\\|"
 				 (c-lang-const c-nonlabel-token-key))
   ;; Also check for open parens in C++, to catch member init lists in
   ;; constructors.  We normally allow it so that macros with arguments
