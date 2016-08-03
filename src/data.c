d841a03c5ee3e9a59a133474601f9bda190ee8fa
Paul Eggert <eggert@cs.ucla.edu>
2016-08-03 14:58:30 -0700

    ; Spelling fix

--- src/data.c
+++ src/data.c
@@ -630,8 +630,8 @@ global value outside of any lexical scope.  */)
 }
 
 /* FIXME: It has been previously suggested to make this function an
-   alias for symbol-function, but upon discussion at Debbug#23957,
-   there is a risk breaking backward compatiblity, as some users of
+   alias for symbol-function, but upon discussion at Bug#23957,
+   there is a risk breaking backward compatibility, as some users of
    fboundp may expect `t' in particular, rather than any true
    value.  An alias is still welcome so long as the compatibility
    issues are addressed.  */
