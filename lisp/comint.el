80082d00d8ab8803c792751c0019c2f88614f48e
Michael Albinus <michael.albinus@gmx.de>
2016-08-17 10:10:47 +0200

    Fix Bug#24203
    * lisp/comint.el (comint-password-prompt-regexp): Relax regexp.  (Bug#24203)

--- lisp/comint.el
+++ lisp/comint.el
@@ -350,8 +350,8 @@ This variable is buffer-local."
     t)
    " +\\)"
    "\\(?:" (regexp-opt password-word-equivalents) "\\|Response\\)"
-   "\\(?:\\(?:, try\\)? *again\\| (empty for no passphrase)\\| (again)\\)?\
-\\(?: for [^:：៖]+\\)?[:：៖]\\s *\\'")
+   "\\(?:\\(?:, try\\)? *again\\| (empty for no passphrase)\\| (again)\\)?"
+   "\\(?: for .+\\)?[:：៖]\\s *\\'")
   "Regexp matching prompts for passwords in the inferior process.
 This is used by `comint-watch-for-password-prompt'."
   :version "25.2"
