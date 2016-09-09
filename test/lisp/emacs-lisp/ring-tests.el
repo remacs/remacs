367f8568bc9e759ebdfb423648891efa0346456b
Simen Heggestøyl <simenheg@gmail.com>
2016-09-09 18:46:55 +0200

    * lisp/emacs-lisp/ring.el: Use lexical-binding
    * lisp/emacs-lisp/ring.el (ring-elements): Don't use the RESULT
    argument of `dotimes' when the iteration variable isn't referred by
    it.
    (ring-member): Don't pass nil as the RESULT argument of `dotimes'
    since it's the default.

--- test/lisp/emacs-lisp/ring-tests.el
+++ test/lisp/emacs-lisp/ring-tests.el
@@ -5,6 +5,8 @@
 ;; Author: Simen Heggestøyl <simenheg@gmail.com>
 ;; Keywords:
 
+;; This file is part of GNU Emacs.
+
 ;; GNU Emacs is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
