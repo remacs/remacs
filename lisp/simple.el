2e2469bbfeb6c1865012f34a180c9beda9e56862
Eli Zaretskii <eliz@gnu.org>
2016-09-16 12:31:41 +0300

    Improve detectability of 'next-logical-line' and 'previous-logical-line'
    * lisp/simple.el (next-line, previous-line): Mention
    'next-logical-line' and 'previous-logical-line' in the doc
    strings.  (Bug#24443)
    
    This was backported from master
    (cherry picked from commit e95d0d15bb1534803b0adb3c3a927c8beb7d4e79)

--- lisp/simple.el
+++ lisp/simple.el
@@ -5641,6 +5641,7 @@ cursor to the end of the buffer.
 If the variable `line-move-visual' is non-nil, this command moves
 by display lines.  Otherwise, it moves by buffer lines, without
 taking variable-width characters or continued lines into account.
+See \\[next-logical-line] for a command that always moves by buffer lines.
 
 The command \\[set-goal-column] can be used to create
 a semipermanent goal column for this command.
@@ -5684,6 +5685,7 @@ column, or at the end of the line if it is not long enough.
 If the variable `line-move-visual' is non-nil, this command moves
 by display lines.  Otherwise, it moves by buffer lines, without
 taking variable-width characters or continued lines into account.
+See \\[previous-logical-line] for a command that always moves by buffer lines.
 
 The command \\[set-goal-column] can be used to create
 a semipermanent goal column for this command.
