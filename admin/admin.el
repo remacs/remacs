31d93aa1d533cf9542f2136b96bad95d0997465d
Eli Zaretskii <eliz@gnu.org>
2016-11-06 18:58:03 +0200

    Add Emacs version number to nt/README.W32
    * nt/README.W32: Include the version number of the latest Emacs
    release.
    
    * admin/admin.el (set-version): Update the version in nt/README.W32.

--- admin/admin.el
+++ admin/admin.el
@@ -109,6 +109,9 @@ Root must be the root of an Emacs source tree."
 		       (rx (and "AC_INIT" (1+ (not (in ?,)))
                                 ?, (0+ space)
                                 (submatch (1+ (in "0-9."))))))
+  (set-version-in-file root "nt/README.W32" version
+		       (rx (and "version" (1+ space)
+				(submatch (1+ (in "0-9."))))))
   ;; TODO: msdos could easily extract the version number from
   ;; configure.ac with sed, rather than duplicating the information.
   (set-version-in-file root "msdos/sed2v2.inp" version
