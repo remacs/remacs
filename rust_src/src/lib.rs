5d6a6700bce2fd55bbc4943724295b82e9df4fc0
Wilfred Hughes <me@wilfred.me.uk>
2016-11-23 19:30:51 +0000

    Remove skeleton tests

--- rust_src/src/lib.rs
+++ rust_src/src/lib.rs
@@ -20,10 +20,3 @@ const fn builtin_lisp_symbol(index: EmacsInt) -> EmacsInt {
 // First, we need a reference to Qt, the t symbol.
 // TODO: what generates globals.h, and where does the number 926 come from?
 static Qt: i64 = builtin_lisp_symbol(926);
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
-}
