be80a1a06ab5167df6c9494700739d2d0ee510f1
Wilfred Hughes <me@wilfred.me.uk>
2016-11-23 08:49:21 +0000

    Define a static variable

--- rust_src/src/lib.rs
+++ rust_src/src/lib.rs
@@ -1,6 +1,18 @@
 #![feature(libc)]
 
+// TODO: move to libc from crates.io
 extern crate libc;
+
+// Use Emacs naming conventions.
+#[allow(non_upper_case_globals)]
+
+// TODO: typedef EMACS_INT to long int
+//
+// note this is dependent on platform and compiler flags passed when
+// compiling emacs.
+
+// First, we need a reference to Qt, the t symbol.
+static Qt: i64 = 1;
 
 #[cfg(test)]
 mod tests {
