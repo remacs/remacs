9c27ca20a66c9d8a680aad3d8b82817740a3be2d
Wilfred Hughes <me@wilfred.me.uk>
2016-11-24 08:26:30 +0000

    Define VectorLikeHeader and LispSubr structs

--- rust_src/src/lib.rs
+++ rust_src/src/lib.rs
@@ -2,6 +2,8 @@
 
 extern crate libc;
 
+use std::os::raw::c_char;
+
 
 // EMACS_INT is defined as 'long int' in lisp.h.
 type EmacsInt = libc::c_longlong;
@@ -9,6 +11,23 @@ type EmacsInt = libc::c_longlong;
 // This is dependent on CHECK_LISP_OBJECT_TYPE, a compile time flag,
 // but it's usually false.
 type LispObject = EmacsInt;
+
+#[repr(C)]
+struct VectorLikeHeader {
+    size: libc::ptrdiff_t
+}
+
+#[repr(C)]
+struct LispSubr {
+    header: VectorLikeHeader,
+    // TODO: lisp.h has an elaborate union here.
+    function: *mut libc::c_void,
+    min_args: libc::c_short,
+    max_args: libc::c_short,
+    symbol_name: *const c_char,
+    intspec: *const c_char,
+    doc: *const c_char,
+}
 
 // Use Emacs naming conventions.
 #[allow(non_upper_case_globals)]
