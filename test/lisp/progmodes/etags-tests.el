2f68cb3e0502a9dc69613e97a5a5079ebf9249fb
Eli Zaretskii <eliz@gnu.org>
2016-12-01 18:49:51 +0200

    Fix bugs with buffer-local tags tables
    * lisp/progmodes/etags.el (visit-tags-table): After
    'visit-tags-table-buffer' returns, retrieve the value of
    'tags-file-name' from the buffer we started in.  Force
    recomputation of 'tags-completion-table' next time it is used,
    since the list of tags table has changed.
    (visit-tags-table-buffer): Accept an additional optional argument
    CBUF, the buffer in which to start processing, and switch to that
    buffer if CBUF is non-nil.  All callers changed to supply a
    non-nil CBUF when they call 'visit-tags-table-buffer' in a loop.
    Doc fix.
    (tags-completion-table): Accept an optional argument, the buffer
    for which to build 'tags-completion-table', and build that
    buffer's completion table.
    (tags-lazy-completion-table): Pass the current buffer to
    'tags-completion-table'.
    (tags-file-name): Don't say in the doc string that setting this
    variable directly is enough; say that 'visit-tags-table' should be
    used for that.  (Bug#158)  (Bug#17326)  (Bug#23164)
    
    * doc/emacs/maintaining.texi (Select Tags Table): Delete the
    advice to set 'tags-file-name' directly.
    
    * test/lisp/progmodes/etags-tests.el: New tests.

---
+++ test/lisp/progmodes/etags-tests.el
@@ -0,0 +1,83 @@
+;;; etags-tests.el --- Test suite for etags.el.
+
+;; Copyright (C) 2016 Free Software Foundation, Inc.
+
+;; Author: Eli Zaretskii <eliz@gnu.org>
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
+(require 'ert)
+(require 'etags)
+
+(defvar his-masters-voice t)
+
+(defun y-or-n-p (_prompt)
+  "Replacement for `y-or-n-p' that returns what we tell it to."
+  his-masters-voice)
+
+(ert-deftest etags-bug-158 ()
+  "Test finding tags with local and global tags tables."
+  (let ((buf-with-global-tags (get-buffer-create "*buf-global*"))
+        (buf-with-local-tags (get-buffer-create "*buf-local*"))
+        xref-buf)
+    (set-buffer buf-with-global-tags)
+    (setq default-directory (expand-file-name "."))
+    (visit-tags-table "./manual/etags/ETAGS.good_1")
+    ;; Check that tags in ETAGS.good_1 are recognized.
+    (setq xref-buf (xref-find-definitions "LL_Task_Procedure_Access/t"))
+    (should (bufferp xref-buf))
+    (kill-buffer xref-buf)
+    (setq xref-buf (xref-find-definitions "PrintAdd"))
+    (should (bufferp xref-buf))
+    (kill-buffer xref-buf)
+    ;; Check that tags not in ETAGS.good_1, but in ETAGS.good_3, are
+    ;; NOT recognized.
+    (should-error (xref-find-definitions "intNumber") :type 'user-error)
+    (kill-buffer xref-buf)
+    (set-buffer buf-with-local-tags)
+    (setq default-directory (expand-file-name "."))
+    (let (his-masters-voice)
+      (visit-tags-table "./manual/etags/ETAGS.good_3" t))
+    ;; Check that tags in ETAGS.good_1 are recognized.
+    (setq xref-buf (xref-find-definitions "LL_Task_Procedure_Access/t"))
+    (should (bufferp xref-buf))
+    (kill-buffer xref-buf)
+    (setq xref-buf (xref-find-definitions "PrintAdd"))
+    (should (bufferp xref-buf))
+    (kill-buffer xref-buf)
+    ;; Check that tags in ETAGS.good_3 are recognized.  This is a test
+    ;; for bug#158.
+    (setq xref-buf (xref-find-definitions "intNumber"))
+    (should (or (null xref-buf)
+                (bufferp xref-buf)))
+    ;; Bug #17326
+    (should (string= (file-name-nondirectory
+                      (buffer-local-value 'tags-file-name buf-with-local-tags))
+                     "ETAGS.good_3"))
+    (should (string= (file-name-nondirectory
+                      (default-value 'tags-file-name))
+                     "ETAGS.good_1"))
+    (if (bufferp xref-buf) (kill-buffer xref-buf))))
+
+(ert-deftest etags-bug-23164 ()
+  "Test that setting a local value of tags table doesn't signal errors."
+  (set-buffer (get-buffer-create "*foobar*"))
+  (fundamental-mode)
+  (visit-tags-table "./manual/etags/ETAGS.good_3" t)
+  (should (equal (should-error (xref-find-definitions "foobar123"))
+                 '(user-error "No definitions found for: foobar123"))))
