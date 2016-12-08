a7523ba955005d546d7b3d0351dfefc1347b4b47
Noam Postavsky <npostavs@gmail.com>
2016-12-07 23:17:37 -0500

    Fix rx-any with range with ?\] and ?-
    * lisp/emacs-lisp/rx.el: Make sure not to produce a circular
    list (Bug#25123).
    * test/lisp/emacs-lisp/rx-tests.el (rx-char-any): New test.

--- lisp/emacs-lisp/rx.el
+++ lisp/emacs-lisp/rx.el
@@ -521,7 +521,7 @@ ARG is optional."
 	     (setq args (nconc (delq ?- args) (list ?-))))
 	    ((setq m (assq ?- args))
 	     ;; next to the bracket's range, make the second range
-	     (setcdr args (cons m (delq m args))))))
+	     (setcdr args (cons m (delq m (cdr args)))))))
      ;; bracket in the end range
      ;;	 => "[]...-]"
      ((setq m (rassq ?\] args))
