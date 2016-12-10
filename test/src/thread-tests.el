25a52ca6b2521623334e5768ae26e46595a1b36b
Glenn Morris <rgm@gnu.org>
2016-12-10 10:19:06 -0800

    ; Fix copyright years in new files

--- test/src/thread-tests.el
+++ test/src/thread-tests.el
@@ -1,6 +1,6 @@
 ;;; threads.el --- tests for threads.
 
-;; Copyright (C) 2012, 2013  Free Software Foundation, Inc.
+;; Copyright (C) 2012-2016 Free Software Foundation, Inc.
 
 ;; This file is part of GNU Emacs.
 
