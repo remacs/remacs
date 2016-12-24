e2767bd010d5c30df97789b8b56a42eff4234e5b
Paul Eggert <eggert@cs.ucla.edu>
2016-12-23 18:28:44 -0800

    Use max_align_t instead of void *
    * src/thread.c (run_thread): Don’t assume void * is aligned enough.

--- src/thread.c
+++ src/thread.c
@@ -645,17 +645,12 @@ run_thread (void *state)
 {
   /* Make sure stack_top and m_stack_bottom are properly aligned as GC
      expects.  */
-  union
-  {
-    void *p;
-    char c;
-  } stack_pos;
+  max_align_t stack_pos;
 
   struct thread_state *self = state;
   struct thread_state **iter;
 
-  self->m_stack_bottom = &stack_pos.c;
-  self->stack_top = &stack_pos.c;
+  self->m_stack_bottom = self->stack_top = (char *) &stack_pos;
   self->thread_id = sys_thread_self ();
 
   acquire_global_lock (self);
