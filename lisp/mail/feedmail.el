8f0376309ee37e4f1da21d78971c4df2df5fd7b6
Stefan Monnier <monnier@iro.umontreal.ca>
2017-01-05 21:02:48 -0500

    (feedmail-deduce-address-list): Avoid add-to-list on local variables.
    Author:
    
    * lisp/mail/feedmail.el (feedmail-deduce-address-list):
    Avoid add-to-list on local variables.

--- lisp/mail/feedmail.el
+++ lisp/mail/feedmail.el
@@ -3147,7 +3147,7 @@ been weeded out."
 		(setq simple-address (substring address-blob (match-beginning 2) (match-end 2)))
 		(setq address-blob (replace-match "" t t address-blob))
 		(if (not (member simple-address address-list))
-		    (add-to-list 'address-list simple-address)))
+		    (push simple-address address-list)))
 	      ))
 	  (kill-buffer nil)))
     (identity address-list)))
