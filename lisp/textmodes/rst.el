3c69f2c5bb1ec7775b0ac1b18550affbc249586a
Glenn Morris <rgm@gnu.org>
2017-01-11 18:23:17 -0500

    * lisp/textmodes/rst.el (rst-package-emacs-version-alist): Fix entry.

--- lisp/textmodes/rst.el
+++ lisp/textmodes/rst.el
@@ -267,7 +267,7 @@ in parentheses follows the development revision and the time stamp.")
     ("1.3.0" . "24.3")
     ("1.3.1" . "24.3")
     ("1.4.0" . "24.3")
-    ("1.4.1" . "24.5")
+    ("1.4.1" . "25.1")
     ))
 
 (unless (assoc rst-official-version rst-package-emacs-version-alist)
