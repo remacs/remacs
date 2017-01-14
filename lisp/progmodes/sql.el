69b50f56ad1c684cd4690eef97ad44979b6ad3ca
Eli Zaretskii <eliz@gnu.org>
2017-01-14 10:40:01 +0200

    * lisp/progmodes/sql.el (sql-product-alist): Doc fix.  (Bug#25440)

--- lisp/progmodes/sql.el
+++ lisp/progmodes/sql.el
@@ -551,14 +551,17 @@ may be any one of the following:
                         database and server) needed to connect to
                         the database.
 
- :sqli-comint-func      name of a function which accepts no
-                        parameters that will use the values of
-                        `sql-user', `sql-password',
-                        `sql-database', `sql-server' and
-                        `sql-port' to open a comint buffer and
-                        connect to the database.  Do product
+ :sqli-comint-func      name of a function of two arguments, PRODUCT
+                        and OPTIONS, that will open a comint buffer
+                        and connect to the database.  PRODUCT is the
+                        first argument to be passed to `sql-comint',
+                        and OPTIONS should be included in its second
+                        argument.  The function should use the values
+                        of `sql-user', `sql-password', `sql-database',
+                        `sql-server' and `sql-port' to .  Do product
                         specific configuration of comint in this
-                        function.
+                        function.  See `sql-comint-oracle' for an
+                        example of such a function.
 
  :list-all              Command string or function which produces
                         a listing of all objects in the database.
