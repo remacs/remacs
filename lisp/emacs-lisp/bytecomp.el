fbe6b90b0ced594cf74d5fd0dc9a32666f0d7d38
Vibhav Pant <vibhavp@gmail.com>
2017-01-19 23:13:53 +0530

    * lisp/emacs-lisp/bytecomp.el: Fix errors with matching quoted forms
    * lisp/emacs-lisp/bytecomp.el: (byte-compile-cond-jump-table-info)
      eval obj2 to avoid quoted forms being stored as is.

--- lisp/emacs-lisp/bytecomp.el
+++ lisp/emacs-lisp/bytecomp.el
@@ -4005,7 +4005,7 @@ that suppresses all warnings during execution of BODY."
                         (eq obj1 prev-var)
                         ;; discard duplicate clauses
                         (not (assq obj2 cases)))
-                   (push (list obj2 body) cases)
+                   (push (list (eval obj2) body) cases)
                  (if (eq condition t)
                      (progn (push (list 'default body) cases)
                             (throw 'break t))
