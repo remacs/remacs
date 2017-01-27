17278f25c95f5c45f136b71ca82a0b3f6514623c
David DeSimone <djd231@scarletmail.rutgers.edu>
2017-01-27 00:22:01 -0800

    Writing rust-layer tests for temporary file generation

--- rust_src/src/files.rs
+++ rust_src/src/files.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::Error;
+use std::io::{Write, Read, Seek, SeekFrom, Error};
 
 use std::os::unix::io::IntoRawFd;
 
@@ -19,4 +19,16 @@ pub extern "C" fn mkrstemp(_: *mut libc::c_char,
 
 pub fn make_temporary_file() -> Result<File, Error> {
     tempfile::tempfile()
+}
+
+#[test]
+fn test_make_temporary_file() {
+    let mut file = make_temporary_file().unwrap();
+    file.write_all(b"Attempting to write temporary data").unwrap();
+
+    file.seek(SeekFrom::Start(0)).unwrap();
+    let mut string = String::new();
+    file.read_to_string(&mut string).unwrap();
+    assert_eq!(string
+               ,"Attempting to write temporary data");
 }
