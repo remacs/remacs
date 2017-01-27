29b8283a99893363c02053e76abba9001bcdcc4a
David DeSimone <djd231@scarletmail.rutgers.edu>
2017-01-26 23:24:19 -0800

    Removing 'gen_filename' and 'mkostemp' related macros in the .m4 files.
    Updating rust code to use _ syntax to avoid unused variable warnings.

--- rust_src/src/files.rs
+++ rust_src/src/files.rs
@@ -6,14 +6,14 @@ use std::os::unix::io::IntoRawFd;
 extern crate tempfile;
 extern crate libc;
 
-#[allow(unused_variables)]
 #[no_mangle]
-pub extern "C" fn mkrstemp(template: *mut libc::c_char,
-                          flags: libc::c_int) -> libc::c_int {
+pub extern "C" fn mkrstemp(_: *mut libc::c_char,
+                           _: libc::c_int)
+                           -> libc::c_int {
 
     match make_temporary_file() {
         Ok(tempFile) => tempFile.into_raw_fd(),
-        Err(err) => -1,
+        Err(_) => -1,
     }
 }
 
