7d2d954d6a4554ee1d17b6a04a9f3aead72b6ece
Simen Heggestøyl <simenheg@gmail.com>
2017-03-29 19:50:18 +0200

    Add one more CSS pseudo-class
    * lisp/textmodes/css-mode.el (css-pseudo-class-ids): Add
    `focus-within'.

--- lisp/textmodes/css-mode.el
+++ lisp/textmodes/css-mode.el
@@ -44,9 +44,9 @@
 
 (defconst css-pseudo-class-ids
   '("active" "checked" "default" "disabled" "empty" "enabled" "first"
-    "first-child" "first-of-type" "focus" "hover" "in-range"
-    "indeterminate" "invalid" "lang" "last-child" "last-of-type"
-    "left" "link" "not" "nth-child" "nth-last-child"
+    "first-child" "first-of-type" "focus" "focus-within" "hover"
+    "in-range" "indeterminate" "invalid" "lang" "last-child"
+    "last-of-type" "left" "link" "not" "nth-child" "nth-last-child"
     "nth-last-of-type" "nth-of-type" "only-child" "only-of-type"
     "optional" "out-of-range" "read-only" "read-write" "required"
     "right" "root" "scope" "target" "valid" "visited")
