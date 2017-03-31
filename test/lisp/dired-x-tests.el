1da9a207669a3cf5d27ac1dd61543c1492e05360
Tino Calancha <tino.calancha@gmail.com>
2017-03-31 17:27:08 +0900

    dired-mark-suffix: New command
    Now dired-mark-extension prepends '.' to extension when not present.
    Add command dired-mark-suffix to preserve the previous
    behaviour (Bug#25942).
    * lisp/dired-x.el (dired-mark-suffix): New command;
    mark files ending in a given suffix.
    (dired--mark-suffix-interactive-spec): New defun.
    (dired-mark-extension, dired-mark-suffix): Use it.
    * doc/misc/dired-x.texi (Advanced Mark Commands): Update manual.
    * test/lisp/dired-x-tests.el: New test suite; add test for these features.
    ; * etc/NEWS (Incompatible Lisp Changes in Emacs 26.1):
    ; Mention these changes.

---
+++ test/lisp/dired-x-tests.el
@@ -0,0 +1,53 @@
+;;; dired-x-tests.el --- Test suite for dired-x. -*- lexical-binding: t -*-
+
+;; Copyright (C) 2017 Free Software Foundation, Inc.
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+(require 'ert)
+(require 'dired-x)
+
+
+(ert-deftest dired-test-bug25942 ()
+  "Test for http://debbugs.gnu.org/25942 ."
+  (let* ((dirs (list "Public" "Music"))
+         (files (list ".bashrc" "bar.c" "foo.c" "c" ".c"))
+         (all-but-c
+          (sort
+           (append (copy-sequence dirs)
+                   (delete "c" (copy-sequence files)))
+           #'string<))
+         (dir (make-temp-file "Bug25942" 'dir))
+         (extension "c"))
+    (unwind-protect
+        (progn
+          (dolist (d dirs)
+            (make-directory (expand-file-name d dir)))
+          (dolist (f files)
+            (write-region nil nil (expand-file-name f dir)))
+          (dired dir)
+          (dired-mark-extension extension)
+          (should (equal '("bar.c" "foo.c")
+                         (sort (dired-get-marked-files 'local) #'string<)))
+          (dired-unmark-all-marks)
+          (dired-mark-suffix extension)
+          (should (equal all-but-c
+                         (sort (dired-get-marked-files 'local) #'string<))))
+      (delete-directory dir 'recursive))))
+
+(provide 'dired-x-tests)
+;; dired-x-tests.el ends here
