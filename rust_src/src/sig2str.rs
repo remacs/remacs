a1883ba33ac4d4d98b06160a3e14ac7053d4f997
Michael Walker <mike@barrucadu.co.uk>
2017-04-03 23:03:11 +0100

    Implement str2sig.
    This does not copy the RTMIN/RTMAX behaviour from the old C versions.
    The libc crate does not define those values, which means that the
    default behaviour where RTMIN=0 and RTMAX=-1 applies.
    
    In the default case, those code paths are never executed:
    
        if (0 < rtmin && ...
        if (0 < rtmax && ...

--- rust_src/src/sig2str.rs
+++ rust_src/src/sig2str.rs
@@ -1,6 +1,8 @@
 /// Convert between signal names and numbers.
 
 use libc::{self, c_char, c_int};
+use std::ffi::CStr;
+use std::str::FromStr;
 
 #[cfg(not(unix))]
 const numname: [(&'static str, c_int); 0] = [];
@@ -69,7 +71,22 @@ const numname: [(&'static str, c_int); 27] = [("HUP", libc::SIGHUP),
 /// Convert the signal name SIGNAME to the signal number
 /// *SIGNUM. Return 0 if successful, -1 otherwise.
 #[no_mangle]
-pub extern "C" fn str2sig(signame: *const c_char, signum: *mut c_int) -> c_int {
+pub unsafe extern "C" fn str2sig(signame: *const c_char, signum: *mut c_int) -> c_int {
+    let s = CStr::from_ptr(signame).to_string_lossy();
+    match FromStr::from_str(s.as_ref()) {
+        Ok(i) => {
+            *signum = i;
+            return 0;
+        }
+        Err(_) => {
+            for &(name, num) in numname.iter() {
+                if name == s {
+                    *signum = num;
+                    return 0;
+                }
+            }
+        }
+    }
     return -1;
 }
 
