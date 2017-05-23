ea19dd64e28a05e3b8d10f1683862bdfad43b1a8
Stefan Monnier <monnier@iro.umontreal.ca>
2017-05-23 08:16:59 -0400

    * lisp/vc/vc-hg.el (compilation-directory): Silence byte-compiler.

--- lisp/vc/vc-hg.el
+++ lisp/vc/vc-hg.el
@@ -1302,6 +1302,7 @@ REV is the revision to check out into WORKFILE."
 
 (autoload 'vc-do-async-command "vc-dispatcher")
 (autoload 'log-view-get-marked "log-view")
+(defvar compilation-directory)
 
 (defun vc-hg--pushpull (command prompt &optional obsolete)
   "Run COMMAND (a string; either push or pull) on the current Hg branch.
