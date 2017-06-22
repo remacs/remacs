386a3da920482b8cb3e962fb944d135c8a770e26
Martin Rudalics <rudalics@gmx.at>
2017-06-22 09:24:06 +0200

    Fix make_hash_table calls in lread.c
    * src/lread.c (readevalloop, read_internal_start): Fix
    make_hash_table calls to make build succeed.

--- src/lread.c
+++ src/lread.c
@@ -1942,13 +1942,13 @@ readevalloop (Lisp_Object readcharfun,
 	read_objects_map
 	  = make_hash_table (hashtest_eq, DEFAULT_HASH_SIZE,
 			     DEFAULT_REHASH_SIZE, DEFAULT_REHASH_THRESHOLD,
-			     Qnil, Qnil);
+			     Qnil, false);
       if (! HASH_TABLE_P (read_objects_completed)
 	  || XHASH_TABLE (read_objects_completed)->count)
 	read_objects_completed
 	  = make_hash_table (hashtest_eq, DEFAULT_HASH_SIZE,
 			     DEFAULT_REHASH_SIZE, DEFAULT_REHASH_THRESHOLD,
-			     Qnil, Qnil);
+			     Qnil, false);
       if (!NILP (Vpurify_flag) && c == '(')
 	{
 	  val = read_list (0, readcharfun);
@@ -2159,12 +2159,12 @@ read_internal_start (Lisp_Object stream, Lisp_Object start, Lisp_Object end)
       || XHASH_TABLE (read_objects_map)->count)
     read_objects_map
       = make_hash_table (hashtest_eq, DEFAULT_HASH_SIZE, DEFAULT_REHASH_SIZE,
-			 DEFAULT_REHASH_THRESHOLD, Qnil, Qnil);
+			 DEFAULT_REHASH_THRESHOLD, Qnil, false);
   if (! HASH_TABLE_P (read_objects_completed)
       || XHASH_TABLE (read_objects_completed)->count)
     read_objects_completed
       = make_hash_table (hashtest_eq, DEFAULT_HASH_SIZE, DEFAULT_REHASH_SIZE,
-			 DEFAULT_REHASH_THRESHOLD, Qnil, Qnil);
+			 DEFAULT_REHASH_THRESHOLD, Qnil, false);
   if (EQ (Vread_with_symbol_positions, Qt)
       || EQ (Vread_with_symbol_positions, stream))
     Vread_symbol_positions_list = Qnil;
