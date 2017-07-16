7eefbfd2f0720fda5edb0b834d383c72a3bd3cf4
brotzeit <brotzeitmacher@gmail.com>
2017-07-16 21:01:30 +0200

    Update windows.rs

--- rust_src/src/windows.rs
+++ rust_src/src/windows.rs
@@ -4,7 +4,7 @@ use lisp::LispObject;
 use remacs_macros::lisp_fn;
 
 
-/// Return t if OBJECT is a window.
+/// Return t if OBJECT is a window and nil otherwise.
 #[lisp_fn]
 fn windowp(object: LispObject) -> LispObject {
     LispObject::from_bool(object.is_window())
