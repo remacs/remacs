27a7fa124174a63a54f56ccc1d470aaca0092c2d
Daniel Brooks <db48x@db48x.net>
2017-07-25 15:57:12 -0700

    try using macports to install the xorg-server-devel package

---
+++ .osx-deps.sh
@@ -0,0 +1,6 @@
+export COLUMNS=80
+wget https://raw.githubusercontent.com/GiovanniBussi/macports-ci/master/macports-ci
+chmod +x ./macports-ci
+./macports-ci install
+PATH="/opt/local/bin:$PATH"
+sudo port -v install xorg-server-devel
