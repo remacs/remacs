b4bf8670953d68db16bf194618447daa9b1376ae
brotzeit <brotzeitmacher@gmail.com>
2017-07-25 21:29:51 +0200

    port point

---
+++ rust_src/src/editfns.rs
@@ -0,0 +1,13 @@
+//! Lisp functions pertaining to editing.
+
+use remacs_macros::lisp_fn;
+use lisp::{LispObject};
+use remacs_sys::{Fcurrent_buffer, EmacsInt};
+
+
+#[lisp_fn]
+pub fn point() -> LispObject {
+    let buffer = LispObject::from_raw(unsafe { Fcurrent_buffer() });
+    let b = buffer.as_vectorlike().unwrap().as_buffer().unwrap();
+    LispObject::from_natnum(b.pt as EmacsInt)
+}
--- rust_src/src/lib.rs
+++ rust_src/src/lib.rs
@@ -44,6 +44,7 @@ mod windows;
 mod interactive;
 mod process;
 mod fonts;
+mod editfns;
 
 #[cfg(all(not(test), target_os = "macos"))]
 use alloc_unexecmacosx::OsxUnexecAlloc;
@@ -115,6 +116,7 @@ pub use vectors::Fsort;
 pub use lists::merge;
 pub use buffers::Fget_buffer;
 pub use buffers::Fcurrent_buffer;
+pub use editfns::Fpoint;
 
 // Used in process.c
 pub use str2sig::str2sig;
@@ -274,5 +276,6 @@ pub extern "C" fn rust_init_syms() {
         defsubr(&*floatfns::Sfloor);
         defsubr(&*floatfns::Sround);
         defsubr(&*floatfns::Struncate);
+        defsubr(&*editfns::Spoint);
     }
 }
