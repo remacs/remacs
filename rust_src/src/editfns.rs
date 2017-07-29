6597b5049e4b3af46e206ccc1b21c3227ee6d59b
brotzeit <brotzeitmacher@gmail.com>
2017-07-29 17:29:06 +0200

    fix point

--- rust_src/src/editfns.rs
+++ rust_src/src/editfns.rs
@@ -1,13 +1,15 @@
 //! Lisp functions pertaining to editing.
 
 use remacs_macros::lisp_fn;
-use lisp::{LispObject};
-use remacs_sys::{Fcurrent_buffer, EmacsInt};
+use lisp::LispObject;
+use remacs_sys::EmacsInt;
+use threads::ThreadState;
 
 
+/// Return value of point, as an integer.
+/// Beginning of buffer is position (point-min).
 #[lisp_fn]
 pub fn point() -> LispObject {
-    let buffer = LispObject::from_raw(unsafe { Fcurrent_buffer() });
-    let b = buffer.as_vectorlike().unwrap().as_buffer().unwrap();
-    LispObject::from_natnum(b.pt as EmacsInt)
+    let buffer_ref = ThreadState::current_buffer();
+    LispObject::from_natnum(buffer_ref.pt as EmacsInt)
 }
