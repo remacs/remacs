5840399b7610544bbc4eb006a6cd79c0f7c71612
Eli Zaretskii <eliz@gnu.org>
2017-08-05 16:28:09 +0300

    Avoid segfaults while producing Punct.el
    * lisp/international/mule-conf.el: Undo unification of
    cns11643-15, as that causes segfaults during bootstrap.
    (Bug#27964)

--- lisp/international/mule-conf.el
+++ lisp/international/mule-conf.el
@@ -1186,7 +1186,8 @@
 (unify-charset 'chinese-cns11643-5)
 (unify-charset 'chinese-cns11643-6)
 (unify-charset 'chinese-cns11643-7)
-(unify-charset 'chinese-cns11643-15)
+;; Doing the below causes Emacs to segfault during Punct.el production.
+;; (unify-charset 'chinese-cns11643-15)
 (unify-charset 'big5)
 (unify-charset 'chinese-big5-1)
 (unify-charset 'chinese-big5-2)
