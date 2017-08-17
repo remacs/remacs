8118c173136acb51c4b6f1f997c18157c79248d3
brotzeit <fritz.stelzer@cancom.de>
2017-08-17 14:00:11 +0200

    port marker-buffer

--- rust_src/src/marker.rs
+++ rust_src/src/marker.rs
@@ -1,8 +1,12 @@
-use libc::ptrdiff_t;
+use libc::{c_void, ptrdiff_t};
+use std::mem;
 
+use remacs_sys::make_lisp_ptr;
 use lisp::{LispObject, ExternalPtr};
-use remacs_sys::{Lisp_Marker, EmacsInt};
+use remacs_sys::{Lisp_Marker, EmacsInt, Lisp_Type};
 use remacs_macros::lisp_fn;
+
+use buffers::LispBufferRef;
 
 pub type LispMarkerRef = ExternalPtr<Lisp_Marker>;
 
@@ -25,6 +29,16 @@ impl LispMarkerRef {
         // TODO: add assertions that marker_position in marker.c has.
         self.charpos
     }
+
+    pub fn buffer(self) -> Option<LispBufferRef> {
+        let buf = self.buffer;
+        if buf.is_null() {
+            None
+        } else {
+            Some(unsafe { mem::transmute(self.buffer) })
+        }
+
+    }
 }
 
 /// Return t if OBJECT is a marker (editor pointer).
@@ -38,7 +52,23 @@ fn markerp(object: LispObject) -> LispObject {
 fn marker_position(object: LispObject) -> LispObject {
     let pos = object.as_marker_or_error().charpos();
     match pos {
+        Some(p) => LispObject::from_natnum(p as EmacsInt),
         None => LispObject::constant_nil(),
-        _ => LispObject::from_natnum(pos.unwrap() as EmacsInt),
+    }
+}
+
+/// Return the buffer that MARKER points into, or nil if none.
+/// Returns nil if MARKER points into a dead buffer.
+#[lisp_fn]
+fn marker_buffer(object: LispObject) -> LispObject {
+    let buf = object.as_marker_or_error().buffer();
+    match buf {
+        Some(b) => unsafe {
+            LispObject::from_raw(make_lisp_ptr(
+                b.as_ptr() as *mut c_void,
+                Lisp_Type::Lisp_Vectorlike,
+            ))
+        },
+        None => LispObject::constant_nil(),
     }
 }
