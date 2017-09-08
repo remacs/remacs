a5fec62b519ae8c0a6528366ac8b71cd0c7ac52e
Alan Third <alan@idiocy.org>
2017-09-08 19:26:47 +0100

    Provide native touchpad scrolling on macOS
    * etc/NEWS: Describe changes.
    * lisp/term/ns-win.el (mouse-wheel-scroll-amount,
    mouse-wheel-progressive-speed): Set to smarter values for macOS
    touchpads.
    * src/nsterm.m (emacsView::mouseDown): Use precise scrolling deltas to
    calculate scrolling for touchpads and mouse wheels.
    (syms_of_nsterm): Add variables 'ns-use-system-mwheel-acceleration',
    'ns-touchpad-scroll-line-height' and 'ns-touchpad-use-momentum'.
    * src/keyboard.c (make_lispy_event): Pass on .arg when relevant.
    * src/termhooks.h (event_kind): Update comments re. WHEEL_EVENT.
    * lisp/mwheel.el (mwheel-scroll): Use line count.
    * lisp/subr.el (event-line-count): New function.

--- lisp/term/ns-win.el
+++ lisp/term/ns-win.el
@@ -736,6 +736,25 @@ See the documentation of `create-fontset-from-fontset-spec' for the format.")
 (global-unset-key [horizontal-scroll-bar drag-mouse-1])
 
 
+;;;; macOS-like defaults for trackpad and mouse wheel scrolling on
+;;;; macOS 10.7+.
+
+;; FIXME: This doesn't look right.  Is there a better way to do this
+;; that keeps customize happy?
+(let ((appkit-version (progn
+                        (string-match "^appkit-\\([^\s-]*\\)" ns-version-string)
+                        (string-to-number (match-string 1 ns-version-string)))))
+  ;; Appkit 1138 ~= macOS 10.7.
+  (when (and (featurep 'cocoa) (>= appkit-version 1138))
+    (setq mouse-wheel-scroll-amount '(1 ((shift) . 5) ((control))))
+    (put 'mouse-wheel-scroll-amount 'customized-value
+         (list (custom-quote (symbol-value 'mouse-wheel-scroll-amount))))
+
+    (setq mouse-wheel-progressive-speed nil)
+    (put 'mouse-wheel-progressive-speed 'customized-value
+         (list (custom-quote (symbol-value 'mouse-wheel-progressive-speed))))))
+
+
 ;;;; Color support.
 
 ;; Functions for color panel + drag
