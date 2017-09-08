9b9c93053a6691303b2ad73f107576e07cd69db4
Alfred M. Szmidt <ams@gnu.org>
2017-09-08 12:19:24 +0300

    Support SVN files with svn:externals property
    * lisp/vc/vc-svn.el (vc-svn-parse-status): Don't ignore files
    marked with the svn:externals property.
    
    Copyright-paperwork-exempt: yes

--- lisp/vc/vc-svn.el
+++ lisp/vc/vc-svn.el
@@ -701,8 +701,7 @@ Set file properties accordingly.  If FILENAME is non-nil, return its status."
   (let (multifile file status propstat)
     (goto-char (point-min))
     (while (re-search-forward
-            ;; Ignore the files with status X.
-	    "^\\(?:\\?\\|[ ACDGIMR!~][ MC][ L][ +][ S]..\\([ *]\\) +\\([-0-9]+\\) +\\([0-9?]+\\) +\\([^ ]+\\)\\) +" nil t)
+            "^\\(?:\\?\\|[ ACDGIMR!~][ MC][ L][ +][ SX]..\\([ *]\\) +\\([-0-9]+\\) +\\([0-9?]+\\) +\\([^ ]+\\)\\) +" nil t)
       ;; If the username contains spaces, the output format is ambiguous,
       ;; so don't trust the output's filename unless we have to.
       (setq file (or (unless multifile filename)
