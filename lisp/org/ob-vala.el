b23504c5eaeaeacefecfec30d0984332f40be2c0
Glenn Morris <rgm@gnu.org>
2017-09-29 12:49:19 -0400

    ; Standardize license notices

--- lisp/org/ob-vala.el
+++ lisp/org/ob-vala.el
@@ -8,6 +8,8 @@
 
 ;;; License:
 
+;; This file is part of GNU Emacs.
+
 ;; GNU Emacs is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
