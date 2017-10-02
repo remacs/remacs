d09ac15005f36b2788406da6c573487fc323129d
Eli Zaretskii <eliz@gnu.org>
2017-10-02 21:35:51 +0300

    Fix the --without-x build
    * src/frame.c (Ficonify_frame) [HAVE_WINDOW_SYSTEM]: Use
    frame_parent only in GUI builds to avoid compilation errors in
    --without-x builds.  (Bug#28611)

--- src/frame.c
+++ src/frame.c
@@ -2542,6 +2542,7 @@ for how to proceed.  */)
   (Lisp_Object frame)
 {
   struct frame *f = decode_live_frame (frame);
+#ifdef HAVE_WINDOW_SYSTEM
   Lisp_Object parent = f->parent_frame;
 
   if (!NILP (parent))
@@ -2562,6 +2563,7 @@ for how to proceed.  */)
 	  return Qnil;
 	}
     }
+#endif	/* HAVE_WINDOW_SYSTEM */
 
   /* Don't allow minibuf_window to remain on an iconified frame.  */
   check_minibuf_window (frame, EQ (minibuf_window, selected_window));
