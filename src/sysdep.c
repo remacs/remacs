f352d0257ce5ca1d424b55fc179f18b81bd465ab
Paul Eggert <eggert@cs.ucla.edu>
2017-10-09 10:47:56 -0700

    Fix PWD check on DOS_NT
    * src/sysdep.c (get_current_dir_name_or_unreachable):
    Do not consider a file name like "a:b" to be absolute on DOS_NT.

--- src/sysdep.c
+++ src/sysdep.c
@@ -274,8 +274,8 @@ get_current_dir_name_or_unreachable (void)
      sometimes a nicer name, and using it may avoid a fatal error if a
      parent directory is searchable but not readable.  */
   if (pwd
-      && (IS_DIRECTORY_SEP (*pwd) || (*pwd && IS_DEVICE_SEP (pwd[1])))
       && (pwdlen = strlen (pwd)) < bufsize_max
+      && IS_DIRECTORY_SEP (pwd[pwdlen && IS_DEVICE_SEP (pwd[1]) ? 2 : 0])
       && stat (pwd, &pwdstat) == 0
       && stat (".", &dotstat) == 0
       && dotstat.st_ino == pwdstat.st_ino
