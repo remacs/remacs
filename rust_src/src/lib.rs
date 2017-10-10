82fd7105883789b7bfcb723a0d1b2ac61e913346
Yaroslav Heriatovych <Yarikx@gmail.com>
2017-10-10 22:17:51 +0300

    ported minibuffer-selected-window

--- rust_src/src/lib.rs
+++ rust_src/src/lib.rs
@@ -241,6 +241,7 @@ pub extern "C" fn rust_init_syms() {
         defsubr(&*windows::Swindow_buffer);
         defsubr(&*windows::Swindow_valid_p);
         defsubr(&*windows::Swindow_start);
+        defsubr(&*windows::Sminibuffer_selected_window);
         defsubr(&*process::Sget_process);
         defsubr(&*process::Sprocessp);
         defsubr(&*process::Sprocess_name);
--- rust_src/src/windows.rs
+++ rust_src/src/windows.rs
@@ -2,7 +2,8 @@
 
 use lisp::{LispObject, ExternalPtr};
 use remacs_macros::lisp_fn;
-use remacs_sys::{Lisp_Window, selected_window as current_window};
+use remacs_sys::{Lisp_Window, selected_window as current_window,
+                 minibuf_selected_window as current_minibuf_window, minibuf_level};
 use marker::marker_position;
 use editfns::point;
 
@@ -134,4 +135,19 @@ pub fn window_minibuffer_p(window: LispObject) -> LispObject {
         window
     };
     LispObject::from_bool(win.as_window_or_error().is_minibuffer())
+}
+
+/// Return the window which was selected when entering the minibuffer.
+/// Returns nil, if selected window is not a minibuffer window.
+#[lisp_fn]
+pub fn minibuffer_selected_window() -> LispObject {
+    let level = unsafe { minibuf_level };
+    let current_minibuf = unsafe { LispObject::from_raw(current_minibuf_window) };
+    if level > 0 && selected_window().as_window_or_error().is_minibuffer() &&
+        current_minibuf.as_window_or_error().is_live()
+    {
+        current_minibuf
+    } else {
+        LispObject::constant_nil()
+    }
 }
