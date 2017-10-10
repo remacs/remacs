c89f001de1c36652a94366bf1aaeec40288d8305
Mark Oteiza <mvoteiza@udel.edu>
2017-10-10 10:24:45 -0400

    Add mode map to Flymake diagnostic button
    * lisp/progmodes/flymake.el (flymake--diagnostics-buffer-entries): Add
    keymap propery.

--- lisp/progmodes/flymake.el
+++ lisp/progmodes/flymake.el
@@ -1132,6 +1132,7 @@ POS can be a buffer position or a button"
                       mouse-face highlight
                       help-echo "mouse-2: visit this diagnostic"
                       face nil
+                      keymap flymake-diagnostics-buffer-mode-map
                       mouse-action flymake-goto-diagnostic)]))))
 
 (define-derived-mode flymake-diagnostics-buffer-mode tabulated-list-mode
