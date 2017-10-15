9e4265ef91bd659a5fac49fbe7dc34b91f89d4ed
Noam Postavsky <npostavs@gmail.com>
2017-10-15 16:41:17 -0400

    Ignore string properties when saving eshell history (Bug#28700)
    * lisp/eshell/em-hist.el (eshell-write-history): Remove properties
    before inserting history strings.
    (eshell-read-history): Remove obsolete comment.
    * test/lisp/eshell/em-hist-tests.el (eshell-write-readonly-history):
    New test.

---
+++ test/lisp/eshell/em-hist-tests.el
@@ -0,0 +1,39 @@
+;;; tests/em-hist-tests.el --- em-hist test suite
+
+;; Copyright (C) 2017 Free Software Foundation, Inc.
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Code:
+
+(require 'ert)
+(require 'em-hist)
+
+(ert-deftest eshell-write-readonly-history ()
+  "Test that having read-only strings in history is okay."
+  (let ((histfile (make-temp-file "eshell-history"))
+        (eshell-history-ring (make-ring 2)))
+    (ring-insert eshell-history-ring
+                 (propertize "echo foo" 'read-only t))
+    (ring-insert eshell-history-ring
+                 (propertize "echo bar" 'read-only t))
+    (unwind-protect
+        (eshell-write-history histfile)
+      (delete-file histfile))))
+
+(provide 'em-hist-test)
+
+;;; em-hist-tests.el ends here
