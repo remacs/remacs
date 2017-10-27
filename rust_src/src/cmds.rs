51456a8e5b0697d21e146f6b49fa9c685b9eb412
galczo5 <kamil.galek@gmail.com>
2017-10-27 19:54:06 +0200

    formatted cmds.rs

--- rust_src/src/cmds.rs
+++ rust_src/src/cmds.rs
@@ -1,6 +1,6 @@
 use lisp::LispObject;
 use remacs_macros::lisp_fn;
-use remacs_sys::{EmacsInt, Fline_beginning_position, set_point};
+use remacs_sys::{set_point, EmacsInt, Fline_beginning_position};
 use threads::ThreadState;
 
 /// Return buffer position N characters after (before if N negative) point.
@@ -10,18 +10,26 @@ pub fn forward_point(n: LispObject) -> LispObject {
     LispObject::from_fixnum(n.as_fixnum_or_error() + pt as EmacsInt)
 }
 
-//Move point to beginning of current line (in the logical order).
-//With argument N not nil or 1, move forward N - 1 lines first.
-//If point reaches the beginning or end of buffer, it stops there.
+/// Move point to beginning of current line (in the logical order).
+/// With argument N not nil or 1, move forward N - 1 lines first.
+/// If point reaches the beginning or end of buffer, it stops there.
+/// This function constrains point to the current field unless this moves
+/// point to a different line than the original, unconstrained result.
+/// If N is nil or 1, and a front-sticky field starts at point, the point
+/// does not move.  To ignore field boundaries bind
+/// `inhibit-field-text-motion' to t, or use the `forward-line' function
+/// instead.  For instance, `(forward-line 0)' does the same thing as
+/// `(beginning-of-line)', except that it ignores field boundaries.
 #[lisp_fn(min = "0", intspec = "^p")]
 pub fn beginning_of_line(mut n: LispObject) -> LispObject {
-
-    if n.is_nil()
+    if n.is_nil() {
         n = LispObject::from_fixnum(1);
+    }
 
-    let pos = unsafe {
-        LispObject::from_raw(Fline_beginning_position(n.to_raw()))
+    unsafe {
+        let pos = LispObject::from(Fline_beginning_position(n.to_raw()));
+        set_point(pos.as_fixnum_or_error() as isize);
     };
-    unsafe { set_point(pos.as_fixnum_or_error() as isize) };
+
     LispObject::constant_nil()
 }
