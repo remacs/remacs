cc8f72ca221e772bd925d9ba41bd4202eb7e5c04
Eli Zaretskii <eliz@gnu.org>
2017-10-28 12:06:30 +0300

    Clarify obsolescence message for 'whitespace-tab'
    * lisp/whitespace.el (whitespace-tab): Make the obsolescence
    message more clear about the replacement.

--- lisp/whitespace.el
+++ lisp/whitespace.el
@@ -495,7 +495,8 @@ Used when `whitespace-style' includes the value `spaces'.")
 (defvar whitespace-tab 'whitespace-tab
   "Symbol face used to visualize TAB.
 Used when `whitespace-style' includes the value `tabs'.")
-(make-obsolete-variable 'whitespace-tab "use the face instead." "24.4")
+(make-obsolete-variable 'whitespace-tab
+                        "customize the face `whitespace-tab' instead." "24.4")
 
 (defface whitespace-tab
   '((((class color) (background dark))
