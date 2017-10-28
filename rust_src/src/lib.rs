c930b6f59058a6a67f71c6ffed1e723f36caf04c
Robbert van der Helm <mail@robbertvanderhelm.nl>
2017-10-28 17:27:06 +0200

    Port the following-char function

--- rust_src/src/lib.rs
+++ rust_src/src/lib.rs
@@ -113,6 +113,7 @@ pub use math::Fsub1;
 pub use math::arithcompare;
 pub use editfns::Feobp;
 pub use editfns::Fbobp;
+pub use editfns::Ffollowing_char;
 
 // Widely used in the C codebase.
 pub use lists::Fsetcar;
@@ -436,6 +437,7 @@ pub extern "C" fn rust_init_syms() {
         defsubr(&*editfns::Sgoto_char);
         defsubr(&*editfns::Sposition_bytes);
         defsubr(&*editfns::Sinsert_byte);
+        defsubr(&*editfns::Sfollowing_char);
         defsubr(&*editfns::Schar_after);
         defsubr(&*editfns::Spropertize);
         defsubr(&*fns::Sfeaturep);
