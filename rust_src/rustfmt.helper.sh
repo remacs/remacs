b1da48677c0a0d137b56ae6d6f4987033824424d
Sean 'Shaleh' Perry <shaleh@speakeasy.net>
2017-10-29 16:27:13 -0700

    Doh! submitted the wrong version.

--- rust_src/rustfmt.helper.sh
+++ rust_src/rustfmt.helper.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# Add this to your Emacs config and it will format the file whenever you save.
+#
+#     (setq rust-format-on-save t)
+#
+# Then make sure this script is in your path before ~/.cargo/bin
+
 LD_LIBRARY_PATH=$(rustc --print sysroot)/lib:$LD_LIBRARY_PATH ~/.cargo/bin/rustfmt "$@"
