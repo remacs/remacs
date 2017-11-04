8e295d1024b20ac202ae2d9aeacd5e4a83b60505
galczo5 <kamil.galek@gmail.com>
2017-11-04 19:38:26 +0100

    ported beginning-of-line (#382)
    Port beginning of line
    
    Closes #382

--- rust_src/src/cmds.rs
+++ rust_src/src/cmds.rs
@@ -1,7 +1,8 @@
 //! Commands
 
 use remacs_macros::lisp_fn;
-use remacs_sys::EmacsInt;
+use remacs_sys::{EmacsInt, Fline_beginning_position};
+use remacs_sys::set_point;
 
 use lisp::LispObject;
 use lisp::defsubr;
@@ -13,6 +14,30 @@ use threads::ThreadState;
 pub fn forward_point(n: LispObject) -> LispObject {
     let pt = ThreadState::current_buffer().pt();
     LispObject::from_fixnum(n.as_fixnum_or_error() + pt as EmacsInt)
+}
+
+/// Move point to beginning of current line (in the logical order).
+/// With argument N not nil or 1, move forward N - 1 lines first.
+/// If point reaches the beginning or end of buffer, it stops there.
+/// This function constrains point to the current field unless this moves
+/// point to a different line than the original, unconstrained result.
+/// If N is nil or 1, and a front-sticky field starts at point, the point
+/// does not move.  To ignore field boundaries bind
+/// `inhibit-field-text-motion' to t, or use the `forward-line' function
+/// instead.  For instance, `(forward-line 0)' does the same thing as
+/// `(beginning-of-line)', except that it ignores field boundaries.
+#[lisp_fn(min = "0", intspec = "^p")]
+pub fn beginning_of_line(mut n: LispObject) -> LispObject {
+    if n.is_nil() {
+        n = LispObject::from_fixnum(1);
+    }
+
+    unsafe {
+        let pos = LispObject::from(Fline_beginning_position(n.to_raw()));
+        set_point(pos.as_fixnum_or_error() as isize);
+    };
+
+    LispObject::constant_nil()
 }
 
 include!(concat!(env!("OUT_DIR"), "/cmds_exports.rs"));
