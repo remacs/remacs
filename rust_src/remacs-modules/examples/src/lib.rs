6a16f29a938ecb9ee3272ea17299de3d7dd6dd3f
David DeSimone <djd231@scarletmail.rutgers.edu>
2017-12-02 21:45:06 -0500

    Commiting examples for using Rust based modules

---
+++ rust_src/remacs-modules/examples/src/lib.rs
@@ -0,0 +1,26 @@
+extern crate remacs_modules;
+
+use remacs_modules::{EmacsRuntime};
+use std::ptr;
+
+/// The entry point for your module.
+/// For now this need to be #[no_mangle] and extern "C"
+/// Remacs will call this when your module is required.
+#[no_mangle]
+pub extern "C" fn module_init(runtime: EmacsRuntime) -> i32 {
+    let mut env = runtime.get_env();
+    let fun = env.make_function(0, // min args
+                                0, // max args
+                                // You callback, as a boxed closure
+                                Box::new(|env, _args, _user_data| { 
+                                    env.make_integer(43)
+                                }),
+                                "make a number", // docstring
+                                ptr::null_mut() // User data
+    );
+    
+    env.bind("mymod-func", fun); // Bind 'mymod-func' to our created function
+    env.provide("mymod"); // provide our library so we can 'require' it
+
+    0 // Success! 
+}
