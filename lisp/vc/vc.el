9f1c613b07e4a745671af5818366660e3765984d
Charles A. Roelli <charles@aurox.ch>
2017-12-20 20:52:45 +0100

    Fix vc-region-history when narrowed
    * lisp/vc/vc.el (vc-region-history): Fix behavior when narrowed, by using
    line numbers starting from (point-min).

--- lisp/vc/vc.el
+++ lisp/vc/vc.el
@@ -2417,8 +2417,8 @@ When called interactively with a prefix argument, prompt for REMOTE-LOCATION."
 (defun vc-region-history (from to)
   "Show the history of the region FROM..TO."
   (interactive "r")
-  (let* ((lfrom (line-number-at-pos from))
-         (lto   (line-number-at-pos (1- to)))
+  (let* ((lfrom (line-number-at-pos from t))
+         (lto   (line-number-at-pos (1- to) t))
          (file buffer-file-name)
          (backend (vc-backend file))
          (buf (get-buffer-create "*VC-history*")))
