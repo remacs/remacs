c10ca355cdbb655a0c10f986b6e9c9f9cbc5b30a
Sean Perry <shaleh@speakeasy.net>
2017-12-21 16:25:37 -0800

    Update icons with proper transparency (#548)
    * Update icons with proper transparency
    
    Switch to rsvg-convert
    
    Better looking output. Smaller files.

--- etc/images/icons/hicolor/generate_from_svg.sh
+++ etc/images/icons/hicolor/generate_from_svg.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
+# This uses the rsvg-convert tool from the rsvg package
+# On Debian systems this is in librsvg2-bin
+# On OSX use homebrew and install the librsvg formula
+
+APP=emacs
+
 for i in *; do
-    if [ "$i" != "scalable" ]; then
+    if [ -d "$i" -a "$i" != "scalable" ]; then
         mkdir -p $i/apps
-        convert -resize $i scalable/apps/emacs.svg $i/apps/emacs.png
+
+        width=`echo $i|cut -f1 -dx`
+        height=`echo $i|cut -f2 -dx`
+
+        rsvg-convert --format png --background-color none \
+                     --width $width --height $height \
+                     --output $i/apps/${APP}.png scalable/apps/${APP}.svg
     fi
 done
