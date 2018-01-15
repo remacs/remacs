98e5fb9bf3b49bea82a4d3e38be20415bd7f7684
Charles A. Roelli <charles@aurox.ch>
2018-01-15 21:14:05 +0100

    Remove incorrect documentation in comint.el
    * lisp/comint.el (comint-interrupt-subjob, comint-kill-subjob)
    (comint-quit-subjob, comint-stop-subjob): Remove incorrect
    documentation.  (Bug#30079)

--- lisp/comint.el
+++ lisp/comint.el
@@ -2477,9 +2477,7 @@ Sets mark to the value of point when this command is run."
     (comint-truncate-buffer)))
 
 (defun comint-interrupt-subjob ()
-  "Interrupt the current subjob.
-This command also kills the pending input
-between the process mark and point."
+  "Interrupt the current subjob."
   (interactive)
   (comint-skip-input)
   (interrupt-process nil comint-ptyp)
@@ -2487,25 +2485,19 @@ between the process mark and point."
   )
 
 (defun comint-kill-subjob ()
-  "Send kill signal to the current subjob.
-This command also kills the pending input
-between the process mark and point."
+  "Send kill signal to the current subjob."
   (interactive)
   (comint-skip-input)
   (kill-process nil comint-ptyp))
 
 (defun comint-quit-subjob ()
-  "Send quit signal to the current subjob.
-This command also kills the pending input
-between the process mark and point."
+  "Send quit signal to the current subjob."
   (interactive)
   (comint-skip-input)
   (quit-process nil comint-ptyp))
 
 (defun comint-stop-subjob ()
   "Stop the current subjob.
-This command also kills the pending input
-between the process mark and point.
 
 WARNING: if there is no current subjob, you can end up suspending
 the top-level process running in the buffer.  If you accidentally do
