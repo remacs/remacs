2ce56c51a83f6658cc6c79e4cc3716cfddbb03ff
Glenn Morris <rgm@gnu.org>
2018-01-26 19:53:20 -0800

    Workaround a libotf crash with Kannada font (bug#30193)
    * configure.ac (HAVE_OTF_KANNADA_BUG): New define.
    * src/xfaces.c (syms_of_xfaces) <Vface_ignored_fonts>:
    Add problematic Kannada font if necessary.

--- src/xfaces.c
+++ src/xfaces.c
@@ -6526,7 +6526,12 @@ other font of the appropriate family and registry is available.  */);
 	       doc: /* List of ignored fonts.
 Each element is a regular expression that matches names of fonts to
 ignore.  */);
+#ifdef HAVE_OTF_KANNADA_BUG
+  /* https://debbugs.gnu.org/30193  */
+  Vface_ignored_fonts = list1 (build_string ("Noto Serif Kannada"));
+#else
   Vface_ignored_fonts = Qnil;
+#endif
 
   DEFVAR_LISP ("face-remapping-alist", Vface_remapping_alist,
 	       doc: /* Alist of face remappings.
