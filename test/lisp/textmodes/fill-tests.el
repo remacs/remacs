e3b46f0b9ea75d80d6cdd85e9f801304b285ee6d
Glenn Morris <rgm@gnu.org>
2018-01-26 19:18:23 -0800

    ; * test/lisp/textmodes/fill-tests.el: Fix copyright years.

--- test/lisp/textmodes/fill-tests.el
+++ test/lisp/textmodes/fill-tests.el
@@ -1,6 +1,6 @@
 ;;; fill-test.el --- ERT tests for fill.el -*- lexical-binding: t -*-
 
-;; Copyright (C) 2017 Free Software Foundation, Inc.
+;; Copyright (C) 2017-2018 Free Software Foundation, Inc.
 
 ;; Author:     Marcin Borkowski <mbork@mbork.pl>
 ;; Keywords:   text, wp
