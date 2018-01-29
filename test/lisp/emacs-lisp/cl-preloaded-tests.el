17bf85938c2835f371dd44b4d9f6c9f741c2b054
Glenn Morris <rgm@gnu.org>
2018-01-28 18:07:03 -0800

    ; * test/lisp/emacs-lisp/cl-preloaded-tests.el: Fix copyright notice.

--- test/lisp/emacs-lisp/cl-preloaded-tests.el
+++ test/lisp/emacs-lisp/cl-preloaded-tests.el
@@ -1,16 +1,16 @@
 ;;; cl-preloaded-tests.el --- unit tests for cl-preloaded.el  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2017 Free Software Foundation, Inc.
+;; Copyright (C) 2017-2018 Free Software Foundation, Inc.
 ;; Author: Philipp Stephani <phst@google.com>
 
 ;; This file is part of GNU Emacs.
 
-;; This program is free software; you can redistribute it and/or modify
+;; GNU Emacs is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
 
-;; This program is distributed in the hope that it will be useful,
+;; GNU Emacs is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
