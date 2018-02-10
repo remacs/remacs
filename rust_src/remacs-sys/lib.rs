3516ca93d515ab5868e9e914300d49830c362f44
Sean 'Shaleh' Perry <shaleh@speakeasy.net>
2018-02-09 22:07:00 -0800

    Missed this one.

--- rust_src/remacs-sys/lib.rs
+++ rust_src/remacs-sys/lib.rs
@@ -1,5 +1,5 @@
-#![cfg_attr(feature="clippy", feature(plugin))]
-#![cfg_attr(feature="clippy", plugin(clippy))]
+#![cfg_attr(feature = "clippy", feature(plugin))]
+#![cfg_attr(feature = "clippy", plugin(clippy))]
 #![feature(const_size_of)]
 #![allow(non_camel_case_types, non_snake_case, non_upper_case_globals)]
 
