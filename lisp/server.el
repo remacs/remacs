3a718ffca097b35218c3e041a94adff937f3052f
Paul Eggert <eggert@cs.ucla.edu>
2018-02-12 12:52:44 -0800

    Finish renaming to internal--daemon-sockname
    * doc/lispref/processes.texi (Misc Network): Omit discussion of
    internal--daemon-sockname, as non-Emacs code shouldn't rely on it.
    * src/process.c (syms_of_process): Rename internal-daemon-sockname
    to internal--daemon-sockname.  All uses changed.

--- lisp/server.el
+++ lisp/server.el
@@ -258,8 +258,8 @@ prevents multiple initializations when an external socket has
 been consumed.")
 
 (defcustom server-name
-  (if internal-daemon-sockname
-      (file-name-nondirectory internal-daemon-sockname)
+  (if internal--daemon-sockname
+      (file-name-nondirectory internal--daemon-sockname)
     "server")
   "The name of the Emacs server, if this Emacs process creates one.
 The command `server-start' makes use of this.  It should not be
@@ -271,8 +271,8 @@ changed while a server is running."
 ;; We do not use `temporary-file-directory' here, because emacsclient
 ;; does not read the init file.
 (defvar server-socket-dir
-  (if internal-daemon-sockname
-      (file-name-directory internal-daemon-sockname)
+  (if internal--daemon-sockname
+      (file-name-directory internal--daemon-sockname)
     (and (featurep 'make-network-process '(:family local))
          (format "%s/emacs%d" (or (getenv "TMPDIR") "/tmp") (user-uid))))
   "The directory in which to place the server socket.
@@ -631,7 +631,7 @@ To force-start a server, do \\[server-force-delete] and then
       ;; Check to see if an uninitialized external socket has been
       ;; passed in, if that is the case, skip checking
       ;; `server-running-p' as this will return the wrong result.
-      (if (and internal-daemon-sockname
+      (if (and internal--daemon-sockname
                (not server--external-socket-initialized))
           (setq server--external-socket-initialized t)
         ;; Delete the socket files made by previous server invocations.
