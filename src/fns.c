c87d04e05ebeaf9e72eaa81cbd98fb4910772667
Eli Zaretskii <eliz@gnu.org>
2018-02-27 18:44:15 +0200

    Avoid aborts in 'md5'
    * src/fns.c (extract_data_from_object): Don't crash if called with
    an invalid object.  (Bug#30627)

--- src/fns.c
+++ src/fns.c
@@ -4952,6 +4952,9 @@ extract_data_from_object (Lisp_Object spec,
 #endif
     }
 
+  if (!STRINGP (object))
+    signal_error ("Invalid object argument",
+		  NILP (object) ? build_string ("nil") : object);
   return SSDATA (object);
 }
 
