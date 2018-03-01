321e1a61e12bfb24554e0795a57fe77b49706635
Glenn Morris <rgm@gnu.org>
2018-03-01 13:10:37 -0500

    Quieten mh-compat compilation
    * lisp/mh-e/mh-compat.el (mh-assoc-string)
    (mh-replace-regexp-in-string): Silence compiler warnings.

--- lisp/mh-e/mh-compat.el
+++ lisp/mh-e/mh-compat.el
@@ -65,7 +65,8 @@ Simulate NOERROR argument in XEmacs which lacks it."
 Case is ignored if CASE-FOLD is non-nil.
 This function is used by Emacs versions that lack `assoc-string',
 introduced in Emacs 22."
-  (if case-fold
+  ;; Test for fboundp is solely to silence compiler for Emacs >= 22.1.
+  (if (and case-fold (fboundp 'assoc-ignore-case))
       (assoc-ignore-case key list)
     (assoc key list)))
 
@@ -307,7 +308,8 @@ This function is used by XEmacs that lacks `replace-regexp-in-string'.
 The function `replace-in-string' is used instead.
 The arguments FIXEDCASE, SUBEXP, and START, used by
 `replace-in-string' are ignored."
-  (replace-in-string string regexp rep literal))
+  (if (featurep 'xemacs)                ; silence Emacs compiler
+      (replace-in-string string regexp rep literal)))
 
 (defun-mh mh-test-completion
   test-completion (string collection &optional predicate)
