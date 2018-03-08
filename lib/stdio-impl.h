bb1309f1eb7651b41f2b6b7fb97ddb86bb82160c
Paul Eggert <eggert@cs.ucla.edu>
2018-03-07 17:55:44 -0800

    Update from Gnulib
    This includes:
    2018-03-07 maint: write-file-hooks -> before-save-hook
    2018-03-05 binary-io: pacify gcc -Wunused-parameter
    2018-03-05 fflush: adjust to glibc 2.28 libio.h removal
    * build-aux/config.guess, build-aux/config.sub:
    * build-aux/gitlog-to-changelog, build-aux/install-sh:
    * build-aux/move-if-change, build-aux/update-copyright:
    * doc/misc/texinfo.tex, lib/binary-io.h, lib/fpending.c:
    * lib/stdio-impl.h: Copy from Gnulib.

--- lib/stdio-impl.h
+++ lib/stdio-impl.h
@@ -18,6 +18,12 @@
    the same implementation of stdio extension API, except that some fields
    have different naming conventions, or their access requires some casts.  */
 
+/* Glibc 2.28 made _IO_IN_BACKUP private.  For now, work around this
+   problem by defining it ourselves.  FIXME: Do not rely on glibc
+   internals.  */
+#if !defined _IO_IN_BACKUP && defined _IO_EOF_SEEN
+# define _IO_IN_BACKUP 0x100
+#endif
 
 /* BSD stdio derived implementations.  */
 
