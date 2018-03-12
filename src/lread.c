981cfbfef18073aefd5e224993c7a272fd653c35
Paul Eggert <eggert@cs.ucla.edu>
2018-03-12 10:35:25 -0700

    Revert overenthusiastic procfs fixup
    Also, be more systematic in calls to string_to_number.
    * src/sysdep.c (list_system_processes) [HAVE_PROCFS]: Allow pids
    to be floating-point if they exceed fixnum range.  This partially
    reverts my patch 2018-03-09T20:06:05Z!eggert@cs.ucla.edu, which
    went too far in fixing string-to-number mishandling.

--- src/lread.c
+++ src/lread.c
@@ -2603,7 +2603,7 @@ read_integer (Lisp_Object readcharfun, EMACS_INT radix)
       invalid_syntax (buf);
     }
 
-  return string_to_number (buf, radix, 0);
+  return string_to_number (buf, radix, false);
 }
 
 
@@ -3412,7 +3412,7 @@ read1 (Lisp_Object readcharfun, int *pch, bool first_in_list)
 
 	if (!quoted && !uninterned_symbol)
 	  {
-	    Lisp_Object result = string_to_number (read_buffer, 10, 0);
+	    Lisp_Object result = string_to_number (read_buffer, 10, false);
 	    if (! NILP (result))
 	      return unbind_to (count, result);
 	  }
