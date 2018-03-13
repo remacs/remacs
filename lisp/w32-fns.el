a6437924f796c07ec15a13c7d1885e83d1b0b0cc
Glenn Morris <rgm@gnu.org>
2018-03-13 13:49:05 -0400

    Doc fixes re obsolete items
    * doc/emacs/maintaining.texi (VC Undo):
    * doc/misc/efaq-w32.texi (Font names): Replace obsolete aliases.
    * doc/misc/url.texi (Customization):
    Don't mention url-temporary-directory, it essentially does nothing.
    * lisp/hilit-chg.el
    (highlight-changes-global-changes-existing-buffers): Doc fix.
    ; * lisp/w32-fns.el: Comment.
    ; * etc/TODO: Remove obsolete ffap mention.

--- lisp/w32-fns.el
+++ lisp/w32-fns.el
@@ -260,7 +260,7 @@ bit output with no translation."
                (cons xlfd-charset (cons windows-charset codepage))))
 
 ;; The last charset we add becomes the "preferred" charset for the return
-;; value from w32-select-font etc, so list the most important charsets last.
+;; value from x-select-font etc, so list the most important charsets last.
 (w32-add-charset-info "iso8859-14" 'w32-charset-ansi  28604)
 (w32-add-charset-info "iso8859-15" 'w32-charset-ansi  28605)
 ;; The following two are included for pattern matching.
