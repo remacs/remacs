3c2962f57ec0dc51c3d169600bd388e6b754dec6
Miles Yucht <miles@databricks.com>
2018-03-20 07:50:05 -0700

    Work on windows

--- test/rust_src/src/process-tests.el
+++ test/rust_src/src/process-tests.el
@@ -5,7 +5,8 @@
 (require 'ert)
 
 (ert-deftest process-tests--process-inherit-coding-system-flag ()
-  (let ((proc (start-process "test" nil "ls")))
+  (let ((cmd) (if (eq system-type 'windows-nt) "dir" "ls")
+        (proc (start-process "test" nil command)))
     (should (not (process-inherit-coding-system-flag proc)))
     (set-process-inherit-coding-system-flag proc t)
     (should (equal t (process-inherit-coding-system-flag proc)))))
