8a55d7ceb117548f8b9df43db105dab1365ef417
Karlo Licudine <accidentalrebel@gmail.com>
2018-03-23 01:03:55 +0800

    Ported map-keymap-internal and map_keymap_internal functions with tests. (#686)
    Closes #684.

--- test/rust_src/src/keymap-tests.el
+++ test/rust_src/src/keymap-tests.el
@@ -63,6 +63,65 @@
     (should-error (map-keymap test-function nil))
     (should-error (map-keymap test-function '(test)))))
 
+(ert-deftest keymap-tests--map-keymap-internal ()
+  (let* ((sample-keymap '(keymap
+                         (27 keymap
+                             (24 . lisp-send-defun)
+                             (28 . forward-line))
+                         (3 keymap
+                            (25 . run-lisp))))
+        (sample-keymap-with-parent '(keymap
+                                     (3 keymap
+                                        (25 . run-lisp))
+                                     (27 keymap
+                                         (24 . lisp-send-defun))
+                                     keymap
+                                     (127 . backward-delete-char-untabify)
+                                     (26 keymap
+                                         (17 . indent-sexp))))
+        (keys nil)
+        (values nil)
+        (test-function '(lambda (key value)
+                          (push key keys)
+                          (push value values))))
+
+    ;; Test simple keymap with children
+    (should-not (map-keymap-internal test-function sample-keymap))
+    (should (equal keys '(3 27)))
+    (should (equal values '((keymap (25 . run-lisp))
+                            (keymap (24 . lisp-send-defun) (28 . forward-line)))))
+
+    ;; Test keymap with parent keymap
+    (setq keys nil)
+    (setq values nil)
+    (should (equal (map-keymap-internal test-function sample-keymap-with-parent) '(keymap
+                                                                                   (127 . backward-delete-char-untabify)
+                                                                                   (26 keymap
+                                                                                       (17 . indent-sexp)))))
+    (should (equal keys '(27 3)))
+    (should (equal values '((keymap (24 . lisp-send-defun))
+                            (keymap (25 . run-lisp)))))
+
+    ;; If one of the elements is a char-table
+    (setq keys nil)
+    (setq values nil)
+    (should-not (map-keymap-internal test-function `(keymap (24 . lisp-send-defun) ,(make-char-table 'test 0))))
+    (should (equal keys '((0 . 4194303) 24)))
+    (should (equal values '(0 lisp-send-defun)))
+
+    ;; If one of the elements is a vector
+    (setq keys nil)
+    (setq values nil)
+    (should-not (map-keymap-internal test-function '(keymap (24 . lisp-send-defun) [0 0 0 0 0 0])))
+    (should (equal keys '(5 4 3 2 1 0 24)))
+    (should (equal values '(0 0 0 0 0 0 lisp-send-defun)))
+
+    ;; Test invalid inputs
+    (should-error (map-keymap-internal nil nil))
+    (should-error (map-keymap-internal "test" nil))
+    (should-error (map-keymap-internal  test-function nil))
+    (should-error (map-keymap-internal test-function '(test)))))
+  
 (ert-deftest keymap-tests--set-keymap-parent ()
   (let ((sample-keymap '(keymap
                          (3 keymap
