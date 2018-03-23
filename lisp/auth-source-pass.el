a6cbdd4e6f75d40977719892af53fee5488a2c0e
Damien Cassou <damien@cassou.me>
2018-03-23 09:18:54 +0100

    * lisp/auth-source-pass.el: Update version to 4.0.0

--- lisp/auth-source-pass.el
+++ lisp/auth-source-pass.el
@@ -4,7 +4,7 @@
 
 ;; Author: Damien Cassou <damien@cassou.me>,
 ;;         Nicolas Petton <nicolas@petton.fr>
-;; Version: 3.0.0
+;; Version: 4.0.0
 ;; Package-Requires: ((emacs "25"))
 ;; Url: https://github.com/DamienCassou/auth-password-store
 ;; Created: 07 Jun 2015
