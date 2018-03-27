29006a6fd8e989013098b0281aa0b9efe13feb1d
Damien Cassou <damien@cassou.me>
2018-03-27 16:57:51 +0200

    Detect if a message can be encrypted and add an MML tag
    * lisp/gnus/message.el (message-all-recipients): Return a list of
    pairs, one for each recipient in To, Cc, Bcc.
    (message-all-epg-keys-available-p): Check that there is a public key
    in epg for each recipient of the current message.
    (message-sign-encrypt-if-all-keys-available): Add MML tag to sign and
    encrypt current message if there is a public key for every recipient
    in current message.
    
    * test/lisp/gnus/message-tests.el (message-all-recipients): Test for
    message-all-recipients.

--- lisp/gnus/message.el
+++ lisp/gnus/message.el
@@ -2582,6 +2582,36 @@ PGG manual, depending on the value of `mml2015-use'."
 		      (t
 		       'message)))))
 
+(defun message-all-recipients ()
+  "Return a list of all recipients in the message, looking at TO, CC and BCC.
+
+Each recipient is in the format of `mail-extract-address-components'."
+  (mapcan (lambda (header)
+            (let ((header-value (message-fetch-field header)))
+              (and
+               header-value
+               (mail-extract-address-components header-value t))))
+          '("To" "Cc" "Bcc")))
+
+(defun message-all-epg-keys-available-p ()
+  "Return non-nil if the pgp keyring has a public key for each recipient."
+  (require 'epa)
+  (let ((context (epg-make-context epa-protocol)))
+    (catch 'break
+      (dolist (recipient (message-all-recipients))
+        (let ((recipient-email (cadr recipient)))
+          (when (and recipient-email (not (epg-list-keys context recipient-email)))
+            (throw 'break nil))))
+      t)))
+
+(defun message-sign-encrypt-if-all-keys-available ()
+  "Add MML tag to encrypt message when there is a key for each recipient.
+
+Consider adding this function to `message-send-hook' to
+systematically send encrypted emails when possible."
+  (when (message-all-epg-keys-available-p)
+    (mml-secure-message-sign-encrypt)))
+
 
 
 ;;;
