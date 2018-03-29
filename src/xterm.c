00c1f771f2a51ffa675ec5a07ea330f2605cd302
Noam Postavsky <npostavs@gmail.com>
2018-03-29 19:17:34 -0400

    * src/xterm.c (x_make_frame_visible): Fix typo in previous change.

--- src/xterm.c
+++ src/xterm.c
@@ -11551,7 +11551,7 @@ x_make_frame_visible (struct frame *f)
     poll_for_input_1 ();
     poll_suppress_count = old_poll_suppress_count;
 #endif
-    if (FRAME_VISIBLE_P (f))
+    if (! FRAME_VISIBLE_P (f))
       x_wait_for_event (f, MapNotify);
   }
 }
