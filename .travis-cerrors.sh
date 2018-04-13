c33743670bbd357d3282e8b0097d3c4a68f1e61d
Pip Cet <pipcet@gmail.com>
2018-04-13 11:45:35 +0000

    add a special compile-errors feature, Travis code
    to check that compilation errors in macros are displayed correctly.

---
+++ .travis-cerrors.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# This is a standalone script so we terminate as soon as anything
+# errors. See https://github.com/travis-ci/travis-ci/issues/1066
+set -e
+set -x
+
+export PATH=$PATH:~/.cargo/bin
+
+RUSTFMT_CONFIG_DIR=$DIR/rust_src
+
+echo "Checking compile errors"
+cd "$DIR/rust_src"
+cargo build --features compile-errors 2> err.out || true
+cat err.out
+grep -q 'compile_error!("error 001");' err.out
