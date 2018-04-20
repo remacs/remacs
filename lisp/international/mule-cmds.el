e2ae724460e6d73d3ddcc6066427471799c4bd57
Michael Albinus <michael.albinus@gmx.de>
2018-04-20 14:25:51 +0200

    * lisp/international/mule-cmds.el (view-hello-file):  Use utf-8 coding.

--- lisp/international/mule-cmds.el
+++ lisp/international/mule-cmds.el
@@ -282,7 +282,7 @@ wrong, use this command again to toggle back to the right mode."
   "Display the HELLO file, which lists many languages and characters."
   (interactive)
   ;; We have to decode the file in any environment.
-  (let ((coding-system-for-read 'iso-2022-7bit))
+  (let ((coding-system-for-read 'utf-8))
     (view-file (expand-file-name "HELLO" data-directory))))
 
 (defun universal-coding-system-argument (coding-system)
