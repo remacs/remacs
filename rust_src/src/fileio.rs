1bc503096295d7822af90c9ad7f6907f57a0c53e
nitishch <nitishchandrachinta@gmail.com>
2018-04-23 09:47:43 +0530

    Added directory-name-p function

---
+++ rust_src/src/fileio.rs
@@ -0,0 +1,18 @@
+//! Functions to deal with files
+
+use remacs_macros::lisp_fn;
+use lisp::defsubr;
+use multibyte::LispStringRef;
+use std::path;
+
+/// Return non-nil if NAME ends with a directory separator character.
+#[lisp_fn]
+pub fn directory_name_p(name: LispStringRef) -> bool {
+    if name.len_bytes() > 0 {
+        let b = name.byte_at(name.len_bytes() - 1);
+        return b as char == path::MAIN_SEPARATOR
+    }
+    false
+}
+
+include!(concat!(env!("OUT_DIR"), "/fileio_exports.rs"));
