88682e16c4dc7ce24344ea3a7afafcec941b58d8
nitishch <nitishchandrachinta@gmail.com>
2018-04-23 10:12:28 +0530

    Formatted the code with rustfmt

--- rust_src/src/fileio.rs
+++ rust_src/src/fileio.rs
@@ -1,8 +1,8 @@
 //! Functions to deal with files
 
-use remacs_macros::lisp_fn;
 use lisp::defsubr;
 use multibyte::LispStringRef;
+use remacs_macros::lisp_fn;
 use std::path;
 
 /// Return non-nil if NAME ends with a directory separator character.
@@ -10,7 +10,7 @@ use std::path;
 pub fn directory_name_p(name: LispStringRef) -> bool {
     if name.len_bytes() > 0 {
         let b = name.byte_at(name.len_bytes() - 1);
-        return b as char == path::MAIN_SEPARATOR
+        return b as char == path::MAIN_SEPARATOR;
     }
     false
 }
