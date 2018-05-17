933743939520da42528b0412bf338f693e968ea0
Glenn Barry <gaak99@gaak99.com>
2018-05-17 13:00:58 -0400

    Rust port of system-users from dired.c (post code review)

--- rust_src/src/dired.rs
+++ rust_src/src/dired.rs
@@ -1,20 +1,24 @@
 //! Lisp functions for making directory listings.
 
-use std::ffi::CStr;
-
 use libc;
 
 use remacs_macros::lisp_fn;
-use remacs_sys::Fuser_real_login_name;
+use remacs_sys::{build_string, globals};
 
 use lisp::{defsubr, LispObject};
 use lists::list;
 
-/// Return a list of user names currently registered in the system.
-/// If we don't know how to determine that on this platform, just
-/// return a list with one element, taken from `user-real-login-name'.
-#[lisp_fn]
-pub fn system_users() -> LispObject {
+fn get_user_real_login_name() -> LispObject {
+    LispObject::from_raw(unsafe { globals.f_Vuser_real_login_name })
+}
+
+#[cfg(windows)]
+fn get_users() -> LispObject {
+    get_user_real_login_name()
+}
+
+#[cfg(not(windows))]
+fn get_users() -> LispObject {
     let mut done = false;
     let mut unames = Vec::new();
 
@@ -23,19 +27,24 @@ pub fn system_users() -> LispObject {
         if pw.is_null() {
             done = true;
         } else {
-            let pwnam_string =
-                unsafe { CStr::from_ptr((*pw).pw_name).to_string_lossy().into_owned() };
-            let pwnam_str = &*pwnam_string;
-            unames.push(LispObject::from(pwnam_str));
+            unames.push(LispObject::from_raw(unsafe { build_string((*pw).pw_name) }))
         }
     }
     unsafe { libc::endpwent() };
 
-    if unames.len() < 1 {
-        unames.push(LispObject::from_raw(unsafe { Fuser_real_login_name() }));
+    if unames.is_empty() {
+        unames.push(get_user_real_login_name());
     }
 
     list(&mut unames)
+}
+
+/// Return a list of user names currently registered in the system.
+/// If we don't know how to determine that on this platform, just
+/// return a list with one element, taken from `user-real-login-name'.
+#[lisp_fn]
+pub fn system_users() -> LispObject {
+    get_users()
 }
 
 include!(concat!(env!("OUT_DIR"), "/dired_exports.rs"));
