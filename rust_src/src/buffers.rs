b9d244709e574116a1d50a07f4d31b4c86754a30
Nitish Chandra <nitishchandrachinta@gmail.com>
2018-05-24 09:15:07 -0700

    Replaced emacs_abort with panic!

--- rust_src/src/buffers.rs
+++ rust_src/src/buffers.rs
@@ -10,8 +10,8 @@ use remacs_sys::{Fcons, Fcopy_sequence, Fexpand_file_name, Ffind_file_name_handl
                  Fget_text_property, Fnconc, Fnreverse};
 use remacs_sys::{Qbuffer_read_only, Qget_file_buffer, Qinhibit_read_only, Qnil, Qunbound,
                  Qvoid_variable};
-use remacs_sys::{bget_overlays_after, bget_overlays_before, buffer_local_value, emacs_abort,
-                 fget_buffer_list, fget_buried_buffer_list, get_blv_fwd, get_blv_value, globals,
+use remacs_sys::{bget_overlays_after, bget_overlays_before, buffer_local_value, fget_buffer_list,
+                 fget_buried_buffer_list, get_blv_fwd, get_blv_value, globals,
                  last_per_buffer_idx, set_buffer_internal};
 
 use chartable::LispCharTableRef;
@@ -324,7 +324,10 @@ impl LispBufferRef {
     pub fn set_per_buffer_value_p(&mut self, idx: usize, val: c_uchar) {
         unsafe {
             if idx >= last_per_buffer_idx {
-                emacs_abort()
+                panic!(
+                    "set_per_buffer_value_p called with index greater than {}",
+                    last_per_buffer_idx
+                );
             }
         }
         self.local_flags[idx] = val;
