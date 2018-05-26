809f82c6092178cffe1bc5ffdf9fe193db197c72
Daniel Brooks <db48x@db48x.net>
2018-05-26 13:01:50 -0700

    silence a warning about PhantomData in repr(C) structs
    Since PhantomData is a ZST, it wasn't considered safe to use in a
    repr(C) struct. In practice, however, since the C side doesn't care
    about the PhantomData, it's fine. Also this particular case no longer
    causes the warning on nightlies:
    https://github.com/rust-lang-nursery/rust-bindgen/issues/442

--- rust_src/src/lib.rs
+++ rust_src/src/lib.rs
@@ -7,6 +7,7 @@
 #![allow(non_snake_case)]
 #![allow(private_no_mangle_fns)]
 #![allow(non_camel_case_types, non_snake_case, non_upper_case_globals)]
+#![allow(improper_ctypes)] // we need this to be able to inclde FieldOffsets in C structs
 #![feature(proc_macro)]
 #![cfg_attr(feature = "strict", deny(warnings))]
 #![feature(global_allocator)]
