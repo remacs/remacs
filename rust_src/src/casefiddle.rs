b80c535c406ea1861e3ff8ae6fa5337457c9e0b5
Sean Perry <shaleh@speakeasy.net>
2018-05-27 20:06:00 -0700

    Clean up imports and comments.

--- rust_src/src/casefiddle.rs
+++ rust_src/src/casefiddle.rs
@@ -1,14 +1,15 @@
 //! Case conversion functions.
+use remacs_macros::lisp_fn;
+
 use lisp::LispObject;
 use lisp::defsubr;
-use remacs_macros::lisp_fn;
 use remacs_sys::{casify_object, CaseAction};
 
 /// Convert argument to capitalized form and return that.
 /// This means that each word's first character is converted to either
 /// title case or upper case, and the rest to lower case.
 /// The argument may be a character or string.  The result has the same type.
-/// The argument object is not altered--the value is a copy.  If argument
+/// The argument object is not altered -- the value is a copy.  If argument
 /// is a character, characters which map to multiple code points when
 /// cased, e.g. ﬁ, are returned unchanged.
 #[lisp_fn]
@@ -18,7 +19,7 @@ pub fn capitalize(object: LispObject) -> LispObject {
 
 /// Convert argument to lower case and return that.
 /// The argument may be a character or string.  The result has the same type.
-/// The argument object is not altered--the value is a copy.
+/// The argument object is not altered -- the value is a copy.
 #[lisp_fn]
 pub fn downcase(object: LispObject) -> LispObject {
     unsafe { casify_object(CaseAction::CaseDown, object) }
@@ -26,7 +27,7 @@ pub fn downcase(object: LispObject) -> LispObject {
 
 /// Convert argument to upper case and return that.
 /// The argument may be a character or string.  The result has the same type.
-/// The argument object is not altered--the value is a copy.  If argument
+/// The argument object is not altered -- the value is a copy.  If argument
 /// is a character, characters which map to multiple code points when
 /// cased, e.g. ﬁ, are returned unchanged.
 /// See also `capitalize', `downcase' and `upcase-initials'.
