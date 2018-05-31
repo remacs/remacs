8ca42283939d458e30f74dce2bb1908257ea776f
Lars Ingebrigtsen <larsi@gnus.org>
2018-05-31 13:51:45 +0200

    Fdirectory_files_and_attributes doc string clarification
    * src/dired.c (Fdirectory_files_and_attributes): Clarify what data
    is returned.

--- src/dired.c
+++ src/dired.c
@@ -358,6 +358,16 @@ If NOSORT is non-nil, the list is not sorted--its order is unpredictable.
 DEFUN ("directory-files-and-attributes", Fdirectory_files_and_attributes,
        Sdirectory_files_and_attributes, 1, 5, 0,
        doc: /* Return a list of names of files and their attributes in DIRECTORY.
+The list returned has elements that contain the data from
+`file-attributes' for each file -- with the file name prepended.  So
+the structure of each element is
+`(FILE-NAME FILE-ATTRIBUTE1 FILE-ATTRIBUTE2 ...)'.
+
+For instance, to get the size of the fourth element in a directory,
+you could say:
+
+  (file-attribute-size (cdr (nth 3 (directory-files-and-attributes "/"))))
+
 There are four optional arguments:
 If FULL is non-nil, return absolute file names.  Otherwise return names
  that are relative to the specified directory.
