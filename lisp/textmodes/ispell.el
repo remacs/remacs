8377ca6547d7850c5f308f70b06a1ca4d4f90660
Stefan Monnier <monnier@iro.umontreal.ca>
2018-06-10 22:04:11 -0400

    * lisp/textmodes/ispell.el (ispell--call-enchant-lsmod): Use \' i.s.o $.

--- lisp/textmodes/ispell.el
+++ lisp/textmodes/ispell.el
@@ -1205,9 +1205,11 @@ Internal use.")
   (with-output-to-string
     (with-current-buffer
         standard-output
-      (apply 'ispell-call-process
-             (replace-regexp-in-string "enchant\\(-[0-9]\\)?$" "enchant-lsmod\\1"
-                                       ispell-program-name) nil t nil args))))
+      (apply #'ispell-call-process
+             (replace-regexp-in-string "enchant\\(-[0-9]\\)?\\'"
+                                       "enchant-lsmod\\1"
+                                       ispell-program-name)
+             nil t nil args))))
 
 (defun ispell--get-extra-word-characters (&optional lang)
   "Get the extra word characters for LANG as a character class.
