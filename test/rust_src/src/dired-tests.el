d4a5aa1cc99d4b50a45f2f1b0c1ad9b6da2c05b6
Glenn Barry <gaak99@gaak99.com>
2018-06-29 12:20:27 -0400

    Rust port of file-attributes-lessp from dired.c (refactor test)

--- test/rust_src/src/dired-tests.el
+++ test/rust_src/src/dired-tests.el
@@ -5,19 +5,23 @@
 (require 'ert)
 
 (ert-deftest test-file-attributes-lessp ()
-  (should-error (eval '(file-attributes-lessp '("rms" t)))
-                :type 'wrong-number-of-arguments)
-  (should-error (eval
-                 '(file-attributes-lessp "rms" "wilfred"))
-                :type 'wrong-type-argument)
-  (should-error (eval
-                 '(file-attributes-lessp '("rms" t) "wilfred"))
-                :type 'wrong-type-argument)
-  (should (not (file-attributes-lessp '() '())))
-  (should (not (file-attributes-lessp '() '(""))))
-  (should (file-attributes-lessp '("") '()))
-  (should (file-attributes-lessp '("rms" t) '("wilfred" t)))
-  (should (not (file-attributes-lessp '("wilfred" t) '("rms" t)))))
+  (let ((rstr "rms")
+	(wstr "wilfred")
+	(lnull '())
+	(snull '("")))
+    (should-error (eval '(file-attributes-lessp '(rstr t)))
+		  :type 'wrong-number-of-arguments)
+    (should-error (eval
+		   '(file-attributes-lessp rstr wstr))
+		  :type 'wrong-type-argument)
+    (should-error (eval
+		   '(file-attributes-lessp '(rstr t) wstr))
+		  :type 'wrong-type-argument)
+    (should (not (file-attributes-lessp lnull lnull)))
+    (should (not (file-attributes-lessp lnull snull)))
+    (should (file-attributes-lessp snull lnull))
+    (should (file-attributes-lessp '(rstr t) '(wstr t)))
+    (should (not (file-attributes-lessp '(wstr t) '(rstr t))))))
 
 (ert-deftest test-system-users ()
   (should-error (eval '(system-users 'rms)) :type 'wrong-number-of-arguments)
