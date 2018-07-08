e02d8e29c6009dbaf04d3e1668f476ba000c7f02
Michael Albinus <michael.albinus@gmx.de>
2018-07-08 13:02:19 +0200

    Fix Bug#32084
    * test/lisp/net/dbus-tests.el (dbus-test02-register-service-own-bus):
    Unset $DISPLAY when calling dbus-launch, in order to avoid
    possible X11 authentication errors.  (Bug#32084)

--- test/lisp/net/dbus-tests.el
+++ test/lisp/net/dbus-tests.el
@@ -133,7 +133,8 @@ This includes initialization and closing the bus."
   ;; Start bus.
   (let ((output
 	 (ignore-errors
-	   (shell-command-to-string "dbus-launch --sh-syntax")))
+	   (shell-command-to-string
+            "env DISPLAY= dbus-launch --sh-syntax --close-stderr")))
 	bus pid)
     (skip-unless (stringp output))
     (when (string-match "DBUS_SESSION_BUS_ADDRESS='\\(.+\\)';" output)
