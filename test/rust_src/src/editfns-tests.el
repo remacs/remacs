e0f53ea640b91eda0d17eceac7913696328da339
brotzeit <brotzeitmacher@gmail.com>
2018-07-17 13:24:25 +0200

    add more test cases

--- test/rust_src/src/editfns-tests.el
+++ test/rust_src/src/editfns-tests.el
@@ -6,7 +6,8 @@
 
 (ert-deftest test-char-before ()
   (let ((char1 97)
-        (char2 949))
+        (char2 949)
+        (marker (make-marker)))
     (with-temp-buffer 
       (should (eq (char-before) nil))
       (should (eq (char-before -1) nil))
@@ -15,4 +16,7 @@
       (should (char-equal (char-before) char1))
       (insert "ε")
       (should (char-equal (char-before) char2))
-      (should (char-equal (char-before 2) char1)))))
+      (should (char-equal (char-before 2) char1))
+      (set-marker marker 2)
+      (should (char-equal (char-before marker) char1))
+      (should-error (char-before (current-buffer)) :type 'wrong-type-argument))))
