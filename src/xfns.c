57bcdc76e0518f53cd171c76e726e6bdf646bf9a
Martin Rudalics <rudalics@gmx.at>
2018-09-04 08:53:49 +0200

    Don't call XGetGeometry for frames without outer X window (Bug#32615)
    * src/xfns.c (frame_geometry): Don't call XGetGeometry when
    FRAME has no outer X window; return nil instead.  (Bug#32615)

--- src/xfns.c
+++ src/xfns.c
@@ -5054,7 +5054,7 @@ frame_geometry (Lisp_Object frame, Lisp_Object attribute)
   int menu_bar_height = 0, menu_bar_width = 0;
   int tool_bar_height = 0, tool_bar_width = 0;
 
-  if (FRAME_INITIAL_P (f) || !FRAME_X_P (f))
+  if (FRAME_INITIAL_P (f) || !FRAME_X_P (f) || !FRAME_OUTER_WINDOW (f))
     return Qnil;
 
   block_input ();
