4da29dad8c1bc5db56648590a639bdd7d2061c08
brotzeit <brotzeitmacher@gmail.com>
2018-10-03 12:29:23 +0200

    remove method begv_byte

--- rust_src/src/buffers.rs
+++ rust_src/src/buffers.rs
@@ -97,11 +97,6 @@ impl LispBufferRef {
     }
 
     #[inline]
-    pub fn begv_byte(self) -> ptrdiff_t {
-        self.begv_byte
-    }
-
-    #[inline]
     pub fn beg_addr(self) -> *mut c_uchar {
         unsafe { (*self.text).beg }
     }
@@ -764,12 +759,7 @@ pub extern "C" fn record_buffer_markers(buffer: *mut Lisp_Buffer) {
 
         let buffer = buffer_ref.as_lisp_obj();
         set_marker_both(pt_marker, buffer, buffer_ref.pt(), buffer_ref.pt_byte());
-        set_marker_both(
-            begv_marker,
-            buffer,
-            buffer_ref.begv(),
-            buffer_ref.begv_byte(),
-        );
+        set_marker_both(begv_marker, buffer, buffer_ref.begv(), buffer_ref.begv_byte);
         set_marker_both(zv_marker, buffer, buffer_ref.zv(), buffer_ref.zv_byte);
     }
 }
