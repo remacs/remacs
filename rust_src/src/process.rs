12d1359d543b50fd6bad3859c7531d1f5884d983
brotzeit <brotzeitmacher@gmail.com>
2018-10-06 01:57:21 +0200

    remove some LispProcessRef methods

--- rust_src/src/process.rs
+++ rust_src/src/process.rs
@@ -24,46 +24,6 @@ impl LispProcessRef {
     }
 
     #[inline]
-    fn name(self) -> LispObject {
-        self.name
-    }
-
-    #[inline]
-    fn tty_name(self) -> LispObject {
-        self.tty_name
-    }
-
-    #[inline]
-    fn command(self) -> LispObject {
-        self.command
-    }
-
-    #[inline]
-    fn mark(self) -> LispObject {
-        self.mark
-    }
-
-    #[inline]
-    fn filter(self) -> LispObject {
-        self.filter
-    }
-
-    #[inline]
-    fn sentinel(self) -> LispObject {
-        self.sentinel
-    }
-
-    #[inline]
-    fn plist(self) -> LispObject {
-        self.plist
-    }
-
-    #[inline]
-    fn buffer(self) -> LispObject {
-        self.buffer
-    }
-
-    #[inline]
     fn ptype(self) -> LispObject {
         self.type_
     }
@@ -106,14 +66,14 @@ pub fn get_process(name: LispObject) -> LispObject {
 /// possibly modified to make it unique among process names.
 #[lisp_fn]
 pub fn process_name(process: LispProcessRef) -> LispObject {
-    process.name()
+    process.name
 }
 
 /// Return the buffer PROCESS is associated with.
 /// The default process filter inserts output from PROCESS into this buffer.
 #[lisp_fn]
 pub fn process_buffer(process: LispProcessRef) -> LispObject {
-    process.buffer()
+    process.buffer
 }
 
 /// Return the process id of PROCESS.
@@ -143,7 +103,7 @@ pub fn get_buffer_process(buffer: LispObject) -> LispObject {
     }
     for tail in unsafe { Vprocess_alist }.iter_tails() {
         let p = tail.car().as_cons().unwrap().cdr();
-        if buf.eq(p.as_process().unwrap().buffer()) {
+        if buf.eq(p.as_process().unwrap().buffer) {
             return p;
         }
     }
@@ -155,7 +115,7 @@ pub fn get_buffer_process(buffer: LispObject) -> LispObject {
 /// not the name of the pty that Emacs uses to talk with that terminal.
 #[lisp_fn]
 pub fn process_tty_name(process: LispProcessRef) -> LispObject {
-    process.tty_name()
+    process.tty_name
 }
 
 /// Return the command that was executed to start PROCESS.  This is a
@@ -165,27 +125,27 @@ pub fn process_tty_name(process: LispProcessRef) -> LispObject {
 /// running) or t (process is stopped).
 #[lisp_fn]
 pub fn process_command(process: LispProcessRef) -> LispObject {
-    process.command()
+    process.command
 }
 
 /// Return the filter function of PROCESS.
 /// See `set-process-filter' for more info on filter functions.
 #[lisp_fn]
 pub fn process_filter(process: LispProcessRef) -> LispObject {
-    process.filter()
+    process.filter
 }
 
 /// Return the sentinel of PROCESS.
 /// See `set-process-sentinel' for more info on sentinels.
 #[lisp_fn]
 pub fn process_sentinel(process: LispProcessRef) -> LispObject {
-    process.sentinel()
+    process.sentinel
 }
 
 /// Return the marker for the end of the last output from PROCESS.
 #[lisp_fn]
 pub fn process_mark(process: LispProcessRef) -> LispObject {
-    process.mark()
+    process.mark
 }
 
 /// Return a list of all processes that are Emacs sub-processes.
@@ -197,7 +157,7 @@ pub fn process_list() -> LispObject {
 /// Return the plist of PROCESS.
 #[lisp_fn]
 pub fn process_plist(process: LispProcessRef) -> LispObject {
-    process.plist()
+    process.plist
 }
 
 /// Replace the plist of PROCESS with PLIST.  Return PLIST.
