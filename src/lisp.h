21dfe33206564f2428a1f0b8b8de621d11b82824
Nick Drozd <nicholasdrozd@gmail.com>
2018-10-15 09:44:02 -0500

    Port read
    Porting it as-is for now. This function has some wacky control flow
    that I'll try to fix in another commit.

--- src/lisp.h
+++ src/lisp.h
@@ -3796,6 +3796,7 @@ extern Lisp_Object intern_driver (Lisp_Object, Lisp_Object, Lisp_Object);
 extern Lisp_Object intern_sym (Lisp_Object sym, Lisp_Object obarray, Lisp_Object index);
 extern void init_symbol (Lisp_Object, Lisp_Object);
 extern Lisp_Object oblookup (Lisp_Object, const char *, ptrdiff_t, ptrdiff_t);
+extern Lisp_Object read_internal_start (Lisp_Object, Lisp_Object, Lisp_Object);
 extern void loadhist_attach(Lisp_Object x);
 INLINE void
 LOADHIST_ATTACH (Lisp_Object x)
