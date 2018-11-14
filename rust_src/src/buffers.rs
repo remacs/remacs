aa81f571e44bdbdf93f721a82c1002a46f186e82
Sean Perry <shaleh@speakeasy.net>
2018-11-13 19:57:45 -0800

    Update imports

--- rust_src/src/buffers.rs
+++ rust_src/src/buffers.rs
@@ -15,14 +15,17 @@ use crate::{
     lisp::defsubr,
     lisp::{ExternalPtr, LispObject, LiveBufferIter},
     lists::{car, cdr, list, member},
-    marker::{marker_buffer, marker_position_lisp, set_marker_both, LispMarkerRef},
+    marker::{
+        marker_buffer, marker_position_lisp, marker_position_rust, set_marker_both,
+        unchain_marker_rust, LispMarkerRef,
+    },
     multibyte::{multibyte_length_by_head, string_char},
     numbers::MOST_POSITIVE_FIXNUM,
     remacs_sys::{
         allocate_misc, bset_update_mode_line, buffer_local_flags, buffer_local_value,
-        buffer_window_count, del_range, delete_all_overlays, drop_overlay, globals,
-        last_per_buffer_idx, set_buffer_internal_1, specbind, unbind_to, unchain_both,
-        update_mode_lines,
+        buffer_window_count, del_range, delete_all_overlays, globals, last_per_buffer_idx,
+        lookup_char_property, redisplay_other_windows, set_buffer_internal_1, specbind, unbind_to,
+        unchain_overlay, update_mode_lines,
     },
     remacs_sys::{
         pvec_type, EmacsInt, Lisp_Buffer, Lisp_Buffer_Local_Value, Lisp_Misc_Type, Lisp_Overlay,
@@ -30,7 +33,7 @@ use crate::{
     },
     remacs_sys::{
         windows_or_buffers_changed, Fcons, Fcopy_sequence, Fexpand_file_name,
-        Ffind_file_name_handler, Fget_text_property, Fnconc, Fnreverse, Foverlay_get, Fwiden,
+        Ffind_file_name_handler, Fget_text_property, Fnconc, Fnreverse, Fwiden,
     },
     remacs_sys::{
         Qafter_string, Qbefore_string, Qbuffer_read_only, Qbufferp, Qget_file_buffer,
@@ -38,6 +41,7 @@ use crate::{
     },
     strings::string_equal,
     threads::{c_specpdl_index, ThreadState},
+    windows::selected_window,
 };
 
 pub const BEG: ptrdiff_t = 1;
