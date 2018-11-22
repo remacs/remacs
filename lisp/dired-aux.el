477414ac38399a63fd69f3b2457f5716a02b3eae
Eli Zaretskii <eliz@gnu.org>
2018-11-22 21:05:14 +0200

    Improve documentation of 'dired-do-compress'
    * lisp/dired-aux.el (dired-do-compress): Describe in the doc
    string the effect on directories and on compressed archive.
    (Bug#33450)

--- lisp/dired-aux.el
+++ lisp/dired-aux.el
@@ -1225,7 +1225,12 @@ return t; if SYM is q or ESC, return nil."
 
 ;;;###autoload
 (defun dired-do-compress (&optional arg)
-  "Compress or uncompress marked (or next ARG) files."
+  "Compress or uncompress marked (or next ARG) files.
+If invoked on a directory, compress all of the files in
+the directory and all of its subdirectories, recursively,
+into a .tar.gz archive.
+If invoked on a .tar.gz or a .tgz or a .zip or a .7z archive,
+uncompress and unpack all the files in the archive."
   (interactive "P")
   (dired-map-over-marks-check #'dired-compress arg 'compress t))
 
