2925ce5a7ec1424cfaea9f2f86bd3cab27832584
Eli Zaretskii <eliz@gnu.org>
2018-11-26 19:31:24 +0200

    Support Hunspell 1.7.0 in ispell.el
    * lisp/textmodes/ispell.el
    (ispell-find-hunspell-dictionaries): Invoke Hunspell with an
    additional command-line argument, to work around a misfeature
    in Hunspell 1.7.0 that prevents it from reporting the loaded
    dictionary.  (Bug#33493)

--- lisp/textmodes/ispell.el
+++ lisp/textmodes/ispell.el
@@ -1113,7 +1113,12 @@ dictionary from that list was found."
 				 null-device
 				 t
 				 nil
-				 "-D")
+                                 ;; Hunspell 1.7.0 (and later?) won't
+                                 ;; show LOADED DICTIONARY unless
+                                 ;; there's at least one file argument
+                                 ;; on the command line.  So we feed
+                                 ;; it with the null device.
+				 "-D" null-device)
 	    (buffer-string))
 	  "[\n\r]+"
 	  t))
