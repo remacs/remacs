d6848167b9ac2d5f9f12476342d0540a10db990a
Amanda P. Graven <amanda@amandag.net>
2018-12-19 10:58:13 +0100

    WIP correct type error and write tests

--- rust_src/src/multibyte.rs
+++ rust_src/src/multibyte.rs
@@ -93,7 +93,9 @@ impl LispStringRef {
     /// considered; the validity of the following bytes is not checked.  Tabs in
     /// STRING are always taken to occupy `tab-width' columns.
     pub fn width(self) -> usize {
-        unsafe { lisp_string_width(self.as_lisp_obj(), -1, ptr::null_mut(), ptr::null_mut()) }
+        unsafe {
+            lisp_string_width(self.as_lisp_obj(), -1, ptr::null_mut(), ptr::null_mut()) as usize
+        }
     }
 
     pub fn is_multibyte(self) -> bool {
