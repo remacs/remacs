2eac8de3dc1700ed995b399ac3f848fc4f298d61
Roey Darwish Dror <roey.ghost@gmail.com>
2018-12-31 11:18:27 +0200

    Port list-system-processes (fix #1187)

--- src/process.c
+++ src/process.c
@@ -7177,17 +7177,6 @@ kbd_on_hold_p (void)
 
 /* Enumeration of and access to system processes a-la ps(1).  */
 
-DEFUN ("list-system-processes", Flist_system_processes, Slist_system_processes,
-       0, 0, 0,
-       doc: /* Return a list of numerical process IDs of all running processes.
-If this functionality is unsupported, return nil.
-
-See `process-attributes' for getting attributes of a process given its ID.  */)
-  (void)
-{
-  return list_system_processes ();
-}
-
 DEFUN ("process-attributes", Fprocess_attributes,
        Sprocess_attributes, 1, 1, 0,
        doc: /* Return attributes of the process given by its PID, a number.
@@ -7588,6 +7577,5 @@ returns non-`nil'.  */);
    Fprovide (intern_c_string ("make-network-process"), subfeatures);
  }
 
-  defsubr (&Slist_system_processes);
   defsubr (&Sprocess_attributes);
 }
