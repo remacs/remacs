32a70151f5bdc72aa3fe9bf61a1361018a67af80
brotzeit <brotzeitmacher@gmail.com>
2019-01-02 00:47:26 +0100

    review

--- rust_src/src/data.rs
+++ rust_src/src/data.rs
@@ -824,7 +824,7 @@ pub fn string_to_number_lisp(mut string: LispStringRef, base: Option<EmacsInt>) 
     let b = match base {
         None => 10,
         Some(n) => {
-            if !(n >= 2 && n <= 16) {
+            if n < 2 || n > 16 {
                 args_out_of_range!(base, 2, 16)
             }
             n
