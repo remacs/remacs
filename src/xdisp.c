f136a4bf7c65894acbf491cad8d5298662ba4cd1
Philipp Stephani <phst@google.com>
2019-01-02 18:56:49 +0100

    Avoid a compiler warning on macOS.
    * src/xdisp.c (expose_window_tree): Define variable f only if used.

--- src/xdisp.c
+++ src/xdisp.c
@@ -32390,7 +32390,9 @@ expose_window (struct window *w, XRectangle *fr)
 static bool
 expose_window_tree (struct window *w, XRectangle *r)
 {
+#ifndef HAVE_NS
   struct frame *f = XFRAME (w->frame);
+#endif
   bool mouse_face_overwritten_p = false;
 
   /* NS toolkits may have aleady modified the frame in expectation of
