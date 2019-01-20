27865c8c80965bbbdacb246a59fb5565140f714a
Johannes Altmanninger <aclopte@gmail.com>
2019-01-20 22:11:02 +0100

    Port command-error-or-default-function

--- rust_src/src/emacs.rs
+++ rust_src/src/emacs.rs
@@ -8,6 +8,11 @@ use crate::{
     remacs_sys::Fcopy_sequence,
 };
 
+#[cfg(not(feature = "windowsnt"))]
+use crate::remacs_sys::daemon_type;
+#[cfg(feature = "windowsnt")]
+use crate::remacs_sys::w32_daemon_event;
+
 /// Return the program name that was used to run Emacs.
 /// Any directory names are omitted.
 #[lisp_fn]
@@ -19,6 +24,13 @@ pub fn invocation_name() -> LispObject {
 #[lisp_fn]
 pub fn invocation_directory() -> LispObject {
     unsafe { Fcopy_sequence(globals.Vinvocation_directory) }
+}
+
+pub fn is_daemon() -> bool {
+    #[cfg(feature = "windowsnt")]
+    return unsafe { w32_daemon_event } != std::ptr::null_mut();
+    #[cfg(not(feature = "windowsnt"))]
+    return unsafe { daemon_type } != 0;
 }
 
 include!(concat!(env!("OUT_DIR"), "/emacs_exports.rs"));
