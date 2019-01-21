f98ff4ff3d7ecbea31b3a6247d6f907df996bfa1
Johannes Altmanninger <aclopte@gmail.com>
2019-01-21 16:07:22 +0100

    use cfg-if 0.1.6

--- rust_src/src/emacs.rs
+++ rust_src/src/emacs.rs
@@ -1,5 +1,7 @@
 //! Emacs!
 
+use cfg_if::cfg_if;
+
 use remacs_macros::lisp_fn;
 
 use crate::{
@@ -8,10 +10,19 @@ use crate::{
     remacs_sys::Fcopy_sequence,
 };
 
-#[cfg(not(feature = "windowsnt"))]
-use crate::remacs_sys::daemon_type;
-#[cfg(feature = "windowsnt")]
-use crate::remacs_sys::w32_daemon_event;
+cfg_if! {
+    if #[cfg(windows)] {
+        use crate::remacs_sys::w32_daemon_event;
+        pub fn is_daemon() -> bool {
+            unsafe { w32_daemon_event != std::ptr::null_mut() }
+        }
+    } else {
+        use crate::remacs_sys::daemon_type;
+        pub fn is_daemon() -> bool {
+            unsafe { daemon_type != 0 }
+        }
+    }
+}
 
 /// Return the program name that was used to run Emacs.
 /// Any directory names are omitted.
@@ -24,13 +35,6 @@ pub fn invocation_name() -> LispObject {
 #[lisp_fn]
 pub fn invocation_directory() -> LispObject {
     unsafe { Fcopy_sequence(globals.Vinvocation_directory) }
-}
-
-pub fn is_daemon() -> bool {
-    #[cfg(feature = "windowsnt")]
-    return unsafe { w32_daemon_event } != std::ptr::null_mut();
-    #[cfg(not(feature = "windowsnt"))]
-    return unsafe { daemon_type } != 0;
 }
 
 include!(concat!(env!("OUT_DIR"), "/emacs_exports.rs"));
