c161d51483a465b60a6e07232fdfc9707f0ecc38
Muku TAKEDA <mukuact21@gmail.com>
2019-01-25 15:38:30 +0000

    Use if let instead of car/cdr and change var names

--- rust_src/src/print.rs
+++ rust_src/src/print.rs
@@ -3,22 +3,20 @@
 use remacs_macros::lisp_fn;
 
 use crate::{
+    lisp::defsubr,
     lisp::LispObject,
-    lists::{car, cdr, car_safe, cdr_safe},
+    lists::LispCons,
+    remacs_sys::Vprin1_to_string_buffer,
+    remacs_sys::{print_error_message, set_buffer_internal, Fbuffer_string, Ferase_buffer},
+    remacs_sys::{Qerror, Qnil},
     threads::ThreadState,
-    remacs_sys::{
-        print_error_message, set_buffer_internal, Fbuffer_string, Ferase_buffer, set_buffer_internal,
-    },
-}
-
-/* A buffer which is used to hold output being built by prin1-to-string.  */
-Lisp_Object Vprin1_to_string_buffer;
+};
 
 /// Convert an error value (ERROR-SYMBOL . DATA) to an error message.
 /// See Info anchor `(elisp)Definition of signal' for some details on how this
 /// error message is constructed.
 #[lisp_fn]
-pub fn error_message_string(obj: LispObject) -> LispObject {
+pub fn error_message_string(obj: LispCons) -> LispObject {
     let old = ThreadState::current_buffer_unchecked();
     let value: LispObject;
 
@@ -26,23 +24,26 @@ pub fn error_message_string(obj: LispObject) -> LispObject {
     That is not only faster, it also avoids the need to allocate
     space here when the error is due to memory full.  */
     match obj.into() {
-        Some((car, cdr)) if car.eq(Qerror) => {
-            if cdr.car_safe().is_string() && cdr.cdr_safe().is_nil() {
-                return cdr.car();
+        (error, data) if error.eq(Qerror) => {
+            if let Some((string, nil)) = data.into() {
+                if string.is_string() && nil.is_nil() {
+                    return string;
+                }
             }
         }
         _ => {}
     }
-    unsafe {
-        print_error_message(obj, Vprin1_to_string_buffer, 0, Qnil);
 
-        set_buffer_internal(Vprin1_to_string_buffer.as_buffer());
+    unsafe {
+        print_error_message(obj.into(), Vprin1_to_string_buffer, None, Qnil);
+
+        set_buffer_internal(Vprin1_to_string_buffer.as_buffer_or_error().as_mut());
         value = Fbuffer_string();
 
         Ferase_buffer();
-        set_buffer_internal(old);
+        set_buffer_internal(old.as_mut());
     }
-    return value;
+    value
 }
 
 include!(concat!(env!("OUT_DIR"), "/print_exports.rs"));
