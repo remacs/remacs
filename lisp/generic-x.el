599eee3ebefa4e1c7c6610e5629090ad7ad5159b
Juri Linkov <juri@linkov.net>
2019-01-28 22:18:20 +0200

    * lisp/generic-x.el (etc-passwd-generic-mode): Support backups of passwd,
    group and shadow /etc files.  (Bug#34225)

--- lisp/generic-x.el
+++ lisp/generic-x.el
@@ -1490,7 +1490,7 @@ like an INI file.  You can add this hook to `find-file-hook'."
      '("^\\([^:]+\\):\\([^:]*\\):\\([0-9]+\\):\\(.*\\)$"
        (1 font-lock-type-face)
        (4 font-lock-variable-name-face))))
-  '("/etc/passwd\\'" "/etc/group\\'")
+  '("/etc/passwd-?\\'" "/etc/group-?\\'" "/etc/shadow-?\\'")
   (list
    (function
     (lambda ()
