879d9ce713cb43eadf5bc273619c5bf827bf041c
Michael Albinus <michael.albinus@gmx.de>
2019-01-30 09:28:43 +0100

    * test/lisp/net/tramp-tests.el
    (tramp-test43-asynchronous-requests): Tag it :unstable also for
    emba.  Adapt `accept-process-output' arguments.

--- test/lisp/net/tramp-tests.el
+++ test/lisp/net/tramp-tests.el
@@ -5215,11 +5215,11 @@ Such requests could arrive from timers, process filters and
 process sentinels.  They shall not disturb each other."
   ;; The test fails from time to time, w/o a reproducible pattern.  So
   ;; we mark it as unstable.
+  :tags '(:expensive-test :unstable)
   ;; Recent investigations have uncovered a race condition in
   ;; `accept-process-output'.  Let's check on emba, whether this has
   ;; been solved.
-  :tags
-  (if (getenv "EMACS_EMBA_CI") '(:expensive-test) '(:expensive-test :unstable))
+  ;; (if (getenv "EMACS_EMBA_CI") '(:expensive-test) '(:expensive-test :unstable))
   (skip-unless (tramp--test-enabled))
   (skip-unless (tramp--test-sh-p))
 
@@ -5229,7 +5229,6 @@ process sentinels.  They shall not disturb each other."
   ;; infrequently hangs for hours until killed by the infrastructure.
   (with-timeout (300 (tramp--test-timeout-handler))
     (define-key special-event-map [sigusr1] 'tramp--test-timeout-handler)
-    (tramp--test-instrument-test-case (if (getenv "EMACS_HYDRA_CI") 10 0)
     (let* (;; For the watchdog.
 	   (default-directory (expand-file-name temporary-file-directory))
 	   (watchdog
