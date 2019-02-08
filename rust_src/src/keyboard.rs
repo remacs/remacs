c5123af2052d89e2b90d0048482f4bbdc2879ccc
brotzeit <brotzeitmacher@gmail.com>
2019-02-08 17:49:18 +0100

    review

--- rust_src/src/keyboard.rs
+++ rust_src/src/keyboard.rs
@@ -12,14 +12,14 @@ use crate::{
     lists::{LispCons, LispConsCircularChecks, LispConsEndChecks},
     multibyte::LispStringRef,
     numbers::IsLispNatnum,
+    remacs_sys::globals,
     remacs_sys::{
-        cancel_hourglass, clear_message, command_loop_level, get_input_pending, glyph_row_area,
+        clear_message, command_loop_level, get_input_pending, glyph_row_area,
         interrupt_input_blocked, make_lispy_position, message_log_maybe_newline, minibuf_level,
         output_method, print_error_message, process_special_events, recursive_edit_1,
         recursive_edit_unwind, temporarily_switch_to_single_kboard, totally_unblock_input,
         update_mode_lines, window_box_left_offset,
     },
-    remacs_sys::{display_hourglass_p, globals},
     remacs_sys::{Fdiscard_input, Fkill_emacs, Fpos_visible_in_window_p, Fterpri, Fthrow},
     remacs_sys::{
         Qexit, Qexternal_debugging_output, Qheader_line, Qhelp_echo, Qmode_line, Qnil, Qt,
@@ -28,6 +28,9 @@ use crate::{
     threads::c_specpdl_index,
     windows::{selected_window, LispWindowOrSelected},
 };
+
+#[cfg(feature = "window-system")]
+use crate::remacs_sys::cancel_hourglass;
 
 /// Return position information for buffer position POS in WINDOW.
 /// POS defaults to point in WINDOW; WINDOW defaults to the selected window.
@@ -310,7 +313,7 @@ pub fn top_level() {
     unsafe {
         #[cfg(feature = "window-system")]
         {
-            if display_hourglass_p {
+            if globals.display_hourglass_p {
                 cancel_hourglass();
             }
         }
