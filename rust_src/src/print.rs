f1cec3c44d7c562914652d10acf5400fbbf4256f
Muku TAKEDA <mukuact21@gmail.com>
2019-02-14 13:37:56 +0000

    Several update

--- rust_src/src/print.rs
+++ rust_src/src/print.rs
@@ -3,7 +3,7 @@
 use remacs_macros::lisp_fn;
 
 use crate::{
-    lisp::defsubr,
+    buffers::{as_buffer_or_error, LispBufferRef},
     lisp::LispObject,
     lists::LispCons,
     remacs_sys::Vprin1_to_string_buffer,
@@ -20,9 +20,9 @@ pub fn error_message_string(obj: LispCons) -> LispObject {
     let old = ThreadState::current_buffer_unchecked();
     let value: LispObject;
 
-    /* If OBJ is (error STRING), just return STRING.
-    That is not only faster, it also avoids the need to allocate
-    space here when the error is due to memory full.  */
+    // If OBJ is (error STRING), just return STRING.
+    // That is not only faster, it also avoids the need to allocate
+    // space here when the error is due to memory full.  
     match obj.into() {
         (error, data) if error.eq(Qerror) => {
             if let Some((string, nil)) = data.into() {
@@ -35,9 +35,9 @@ pub fn error_message_string(obj: LispCons) -> LispObject {
     }
 
     unsafe {
-        print_error_message(obj.into(), Vprin1_to_string_buffer, None, Qnil);
+        print_error_message(obj.into(), Vprin1_to_string_buffer, std::ptr::null(), Qnil);
 
-        set_buffer_internal(Vprin1_to_string_buffer.as_buffer_or_error().as_mut());
+        set_buffer_internal(LispBufferRef::from(Vprin1_to_string_buffer).as_mut());
         value = Fbuffer_string();
 
         Ferase_buffer();
