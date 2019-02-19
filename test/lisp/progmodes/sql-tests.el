1a6bcc91e3e468e5a6d3e0b121bb675b576d3362
Michael R. Mauger <michael@mauger.com>
2019-02-18 23:15:54 -0500

    * lisp/progmodes/sql.el: Added password wallet using `auth-source' package. (sql-auth-source-search-wallet): New function. (sql-password-wallet): New variable. (sql-password-search-wallet-function): New variable. (sql-get-login): Handle password wallet search. (sql-product-interactive): Handle password function. * test/lisp/progmodes/sql-test.el: Test wallet changes. (sql-test-login-params): New test variable. (with-sql-test-connect-harness): New macro to wrap test configuration around calls to `sql-connect'. (sql-test-connect, sql-test-connect-password-func) (sql-test-connect-wallet-server-database) (sql-test-connect-wallet-database) (sql-test-connect-wallet-server): New ERT tests. * etc/NEWS: Updated SQL Mode descriptions.

--- test/lisp/progmodes/sql-tests.el
+++ test/lisp/progmodes/sql-tests.el
@@ -53,5 +53,106 @@
           (error "some error"))))
     (should-not (sql-postgres-list-databases))))
 
+(defvar sql-test-login-params nil)
+(defmacro with-sql-test-connect-harness (id login-params connection expected)
+  "Set-up and tear-down SQL connect related test.
+
+Identify tests by ID.  Set :sql-login dialect attribute to
+LOGIN-PARAMS.  Provide the CONNECTION parameters and the EXPECTED
+string of values passed to the comint function for validation."
+  (declare (indent 2))
+  `(cl-letf
+      ((sql-test-login-params ' ,login-params)
+       ((symbol-function 'sql-comint-test)
+        (lambda (product options &optional buf-name)
+          (with-current-buffer (get-buffer-create buf-name)
+            (insert (pp-to-string (list product options sql-user sql-password sql-server sql-database))))))
+       ((symbol-function 'sql-run-test)
+        (lambda (&optional buffer)
+          (interactive "P")
+          (sql-product-interactive 'sqltest buffer)))
+       (sql-user nil)
+       (sql-server nil)
+       (sql-database nil)
+       (sql-product-alist
+        '((ansi)
+          (sqltest
+           :name "SqlTest"
+           :sqli-login sql-test-login-params
+           :sqli-comint-func sql-comint-test)))
+       (sql-connection-alist
+        '((,(format "test-%s" id)
+           ,@connection)))
+       (sql-password-wallet
+        (list
+         (make-temp-file
+          "sql-test-netrc" nil nil
+          (mapconcat #'identity
+                     '("machine aMachine user aUserName password \"netrc-A aPassword\""
+                       "machine aServer user aUserName password \"netrc-B aPassword\""
+                       "machine aMachine server aServer user aUserName password \"netrc-C aPassword\""
+                       "machine aMachine database aDatabase user aUserName password \"netrc-D aPassword\""
+                       "machine aDatabase user aUserName password \"netrc-E aPassword\""
+                       "machine aMachine server aServer database aDatabase user aUserName password \"netrc-F aPassword\""
+                       "machine \"aServer/aDatabase\" user aUserName password \"netrc-G aPassword\""
+                       ) "\n")))))
+
+     (let* ((connection ,(format "test-%s" id))
+            (buffername (format "*SQL: ERT TEST <%s>*" connection)))
+       (when (get-buffer buffername)
+         (kill-buffer buffername))
+       (sql-connect connection buffername)
+       (should (get-buffer buffername))
+       (should (string-equal (with-current-buffer buffername (buffer-string)) ,expected))
+       (when (get-buffer buffername)
+         (kill-buffer buffername))
+     (delete-file (car sql-password-wallet)))))
+
+(ert-deftest sql-test-connect ()
+  "Test of basic `sql-connect'."
+  (with-sql-test-connect-harness 1 (user password server database)
+      ((sql-product 'sqltest)
+       (sql-user "aUserName")
+       (sql-password "test-1 aPassword")
+       (sql-server "aServer")
+       (sql-database "aDatabase"))
+    "(sqltest nil \"aUserName\" \"test-1 aPassword\" \"aServer\" \"aDatabase\")\n"))
+
+(ert-deftest sql-test-connect-password-func ()
+  "Test of password function."
+  (with-sql-test-connect-harness 2 (user password server database)
+      ((sql-product 'sqltest)
+       (sql-user "aUserName")
+       (sql-password (lambda () (concat [?t ?e ?s ?t ?- ?2 ?\s
+                                       ?a ?P ?a ?s ?s ?w ?o ?r ?d])))
+       (sql-server "aServer")
+       (sql-database "aDatabase"))
+    "(sqltest nil \"aUserName\" \"test-2 aPassword\" \"aServer\" \"aDatabase\")\n"))
+
+(ert-deftest sql-test-connect-wallet-server-database ()
+  "Test of password function."
+  (with-sql-test-connect-harness 3 (user password server database)
+      ((sql-product 'sqltest)
+       (sql-user "aUserName")
+       (sql-server "aServer")
+       (sql-database "aDatabase"))
+    "(sqltest nil \"aUserName\" \"netrc-G aPassword\" \"aServer\" \"aDatabase\")\n"))
+
+(ert-deftest sql-test-connect-wallet-database ()
+  "Test of password function."
+  (with-sql-test-connect-harness 4 (user password database)
+      ((sql-product 'sqltest)
+       (sql-user "aUserName")
+       (sql-database "aDatabase"))
+    "(sqltest nil \"aUserName\" \"netrc-E aPassword\" nil \"aDatabase\")\n"))
+
+(ert-deftest sql-test-connect-wallet-server ()
+  "Test of password function."
+  (with-sql-test-connect-harness 5 (user password server)
+      ((sql-product 'sqltest)
+       (sql-user "aUserName")
+       (sql-server "aServer"))
+    "(sqltest nil \"aUserName\" \"netrc-B aPassword\" \"aServer\" nil)\n"))
+
 (provide 'sql-tests)
 ;;; sql-tests.el ends here
