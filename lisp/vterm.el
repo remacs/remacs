2144b516c3256dc15178e4b7352139ac15b5aea5
Lukas Fürmetz <fuermetz@mailbox.org>
2019-02-19 23:18:40 +0100

    Fix multiple vterm sessions

--- lisp/vterm.el
+++ lisp/vterm.el
@@ -122,11 +122,11 @@
   (add-hook 'window-size-change-functions #'vterm-resize-window t t))
 
 (defun vterm-make-process ()
-  (let ((process-environment (nconc '("TERM=xterm"
-                                      "INSIDE_EMACS=vterm"
-                                      "LINES"
-                                      "COLUMNS")
-                                    process-environment)))
+  (let ((process-environment (append '("TERM=xterm"
+                                       "INSIDE_EMACS=vterm"
+                                       "LINES"
+                                       "COLUMNS")
+                                     process-environment)))
     (setq vterm--process
           (make-process
            :name "vterm"
