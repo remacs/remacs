954c9c5bc344cdd6b201c2260b306ec0999aff24
Alan Mackenzie <acm@muc.de>
2019-03-02 18:23:34 +0000

    Check by eassert that a bytepos argument isn't in the middle of a character
    * src/marker.c (buf_bytepos_to_charpos): Add an eassert to check that the
    parameter bytepos is at the beginning of a character or EOB.

--- src/marker.c
+++ src/marker.c
@@ -332,6 +332,10 @@ buf_bytepos_to_charpos (struct buffer *b, ptrdiff_t bytepos)
   if (best_above == best_above_byte)
     return bytepos;
 
+  /* Check bytepos is not in the middle of a character. */
+  eassert (bytepos >= BUF_Z_BYTE (b)
+           || CHAR_HEAD_P (BUF_FETCH_BYTE (b, bytepos)));
+
   best_below = BEG;
   best_below_byte = BEG_BYTE;
 
