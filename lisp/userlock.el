c37bdd00c7dcabaa3ca8405d9dc5122ed36f27e0
Charles A. Roelli <charles@aurox.ch>
2019-03-09 16:47:38 +0100

    Make help for file supersession more readable
    * lisp/userlock.el (ask-user-about-supersession-help): Use
    'substitute-command-keys' to show the command key for
    "\\[revert-buffer]" properly.

--- lisp/userlock.el
+++ lisp/userlock.el
@@ -174,7 +174,9 @@ really edit the buffer? (y, n, r or C-h) "
 
 (defun ask-user-about-supersession-help ()
   (with-output-to-temp-buffer "*Help*"
-    (princ "You want to modify a buffer whose disk file has changed
+    (princ
+     (substitute-command-keys
+      "You want to modify a buffer whose disk file has changed
 since you last read it in or saved it with this buffer.
 
 If you say `y' to go ahead and modify this buffer,
@@ -184,7 +186,7 @@ from the file on disk.
 If you say `n', the change you started to make will be aborted.
 
 Usually, you should type `n' and then `\\[revert-buffer]',
-to get the latest version of the file, then make the change again.")
+to get the latest version of the file, then make the change again."))
     (with-current-buffer standard-output
       (help-mode))))
 
