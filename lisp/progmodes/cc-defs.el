d026d9ad999028c034d69f2477495c6c3e193fc9
Alan Mackenzie <acm@muc.de>
2019-03-10 14:27:38 +0000

    * lisp/progmodes/cc-defs.el: Update c-version to 5.33.2 for Emacs 26.2

--- lisp/progmodes/cc-defs.el
+++ lisp/progmodes/cc-defs.el
@@ -87,7 +87,7 @@
 
 ;;; Variables also used at compile time.
 
-(defconst c-version "5.33.1"
+(defconst c-version "5.33.2"
   "CC Mode version number.")
 
 (defconst c-version-sym (intern c-version))
