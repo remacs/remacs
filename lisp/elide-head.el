01dc2da75ba11f8ddb1ac1802a8c5a7f0caea975
Michał Krzywkowski <mkrzywkow@gmail.com>
2019-04-05 20:33:07 -0400

    Small elide-head.el update
    * lisp/elide-head.el (elide-head-headers-to-hide):
    Also match https for GPL.  (Bug#34919)

--- lisp/elide-head.el
+++ lisp/elide-head.el
@@ -52,7 +52,7 @@
 (defcustom elide-head-headers-to-hide
   '(("is free software[:;] you can redistribute it" . ; GNU boilerplate
      "\\(Boston, MA 0211\\(1-1307\\|0-1301\\), USA\\|\
-If not, see <http://www\\.gnu\\.org/licenses/>\\)\\.")
+If not, see <https?://www\\.gnu\\.org/licenses/>\\)\\.")
     ("The Regents of the University of California\\.  All rights reserved\\." .
      "SUCH DAMAGE\\.")				      ; BSD
     ("Permission is hereby granted, free of charge" . ; X11
