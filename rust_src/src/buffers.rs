2fc8372ab2f659a5acd077f8a7a2d7b1a0aacefe
Amanda P. Graven <amanda@amandag.net>
2019-04-08 21:16:56 +0200

    Fix nconc invocations

--- rust_src/src/buffers.rs
+++ rust_src/src/buffers.rs
@@ -17,7 +17,7 @@ use crate::{
     editfns::{point, widen},
     eval::unbind_to,
     fileio::{expand_file_name, find_file_name_handler},
-    fns::nreverse,
+    fns::{nconc, nreverse},
     frames::LispFrameRef,
     hashtable::LispHashTableRef,
     lisp::{ExternalPtr, LispMiscRef, LispObject, LispStructuralEqual, LiveBufferIter},
@@ -37,7 +37,7 @@ use crate::{
         alloc_buffer_text, allocate_buffer, allocate_misc, block_input, bset_update_mode_line,
         buffer_fundamental_string, buffer_local_flags, buffer_local_value, buffer_memory_full,
         buffer_window_count, concat2, del_range, delete_all_overlays, globals, last_per_buffer_idx,
-        lookup_char_property, make_timespec, marker_position, modify_overlay, nconc2,
+        lookup_char_property, make_timespec, marker_position, modify_overlay,
         notify_variable_watchers, per_buffer_default, set_buffer_internal_1, set_per_buffer_value,
         specbind, unblock_input, unchain_both, unchain_marker, update_mode_lines,
         windows_or_buffers_changed,
@@ -52,7 +52,7 @@ use crate::{
         Qinhibit_read_only, Qmakunbound, Qnil, Qoverlayp, Qpermanent_local, Qpermanent_local_hook,
         Qt, Qunbound, UNKNOWN_MODTIME_NSECS,
     },
-    remacs_sys::{Fcopy_sequence, Fget_text_property, Fmake_marker, Fnconc},
+    remacs_sys::{Fcopy_sequence, Fget_text_property, Fmake_marker},
     strings::string_equal,
     threads::{c_specpdl_index, ThreadState},
     vectors::LispVectorlikeRef,
@@ -188,7 +188,7 @@ impl LispBufferRef {
         // Add the buffer to the alist of live buffers
         let buffer: LispObject = b.into();
         unsafe {
-            Vbuffer_alist = nconc2(Vbuffer_alist, list!((name, buffer)));
+            Vbuffer_alist = nconc(&mut [Vbuffer_alist, list!((name, buffer))]);
             if Vrun_hooks.is_not_nil() {
                 call!(Vrun_hooks, Qbuffer_list_update_hook);
             }
@@ -1175,7 +1175,7 @@ pub fn buffer_list(frame: Option<LispFrameRef>) -> LispObject {
             // Remove any buffer that duplicates one in FRAMELIST or PREVLIST.
             buffers.retain(|e| member(*e, framelist).is_nil() && member(*e, prevlist).is_nil());
 
-            callN_raw!(Fnconc, framelist, list(&buffers), prevlist)
+            nconc(&mut [framelist, list(&buffers), prevlist])
         }
     }
 }
