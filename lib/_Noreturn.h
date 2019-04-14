d209d646220bf1967e12d2de5c37b2be82e29835
Paul Eggert <eggert@cs.ucla.edu>
2019-04-13 21:13:16 -0700

    Update from Gnulib
    This incorporates:
    2019-04-07 Add copyright notices in several files
    * build-aux/config.sub, doc/misc/texinfo.tex, lib/_Noreturn.h:
    Copy from Gnulib.
    * lib/gnulib.mk.in: Regenerate.

--- lib/_Noreturn.h
+++ lib/_Noreturn.h
@@ -1,3 +1,19 @@
+/* A C macro for declaring that a function does not return.
+   Copyright (C) 2011-2019 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published
+   by the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
 #ifndef _Noreturn
 # if (defined __cplusplus \
       && ((201103 <= __cplusplus && !(__GNUC__ == 4 && __GNUC_MINOR__ == 7)) \
