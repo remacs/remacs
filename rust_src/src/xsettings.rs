9f4398597676bff856d9cad387548136fd05b1b0
Ubiratan Pires Alberton <u.alberton@gmail.com>
2019-04-30 13:41:05 -0300

    Fix a typo and add missing use statements

--- rust_src/src/xsettings.rs
+++ rust_src/src/xsettings.rs
@@ -3,13 +3,16 @@
 use remacs_macros::lisp_fn;
 
 use crate::{
-    lisp::LispObject, remacs_sys::build_string, remacs_sys::current_mono_font, remacs_sys::Qnil,
+    lisp::LispObject,
+    remacs_sys::build_string,
+    remacs_sys::Qnil,
+    remacs_sys::{current_font, current_mono_font},
 };
 
 /// Get the system default application font.
 #[lisp_fn]
 pub fn font_get_system_normal_font() -> LispObject {
-    if unsafe { current_font.is_nul() } {
+    if unsafe { current_font.is_null() } {
         Qnil
     } else {
         unsafe { build_string(current_font) }
