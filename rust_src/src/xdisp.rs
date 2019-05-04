b91247b10efe0e29c77f0ac80832c472cf65d64a
Simon Frankau <sgf@arbitrary.name>
2019-05-05 00:40:06 +0100

    Port set-buffer-redisplay

--- rust_src/src/xdisp.rs
+++ rust_src/src/xdisp.rs
@@ -1,12 +1,29 @@
 //! Display generation from window structure and buffer text.
 
-// Remove the feature check when this file gains functions
-// that are not guarded by it.
-#[cfg(feature = "glyph-debug")]
 use remacs_macros::lisp_fn;
 
+use crate::lisp::LispObject;
+use crate::remacs_sys::{bset_update_mode_line, Qnil};
+use crate::threads::ThreadState;
 #[cfg(feature = "glyph-debug")]
 use crate::{interactive::InteractiveNumericPrefix, remacs_sys::trace_redisplay_p};
+
+/// Mark the current buffer for redisplay.
+/// This function may be passed to `add-variable-watcher'.
+#[lisp_fn]
+pub fn set_buffer_redisplay(
+    _symbol: LispObject,
+    _newval: LispObject,
+    _op: LispObject,
+    _where: LispObject,
+) -> LispObject {
+    let mut current_buffer = ThreadState::current_buffer_unchecked();
+    unsafe {
+        bset_update_mode_line(current_buffer.as_mut());
+    }
+    current_buffer.set_prevent_redisplay_optimizations_p(true);
+    Qnil
+}
 
 /// Toggle tracing of redisplay.
 /// With ARG, turn tracing on if and only if ARG is positive.
