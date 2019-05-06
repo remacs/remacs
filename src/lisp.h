f77bd2b4ed59370876f5c177398ae3e1683b71f9
Eli Zaretskii <eliz@gnu.org>
2019-05-06 21:52:19 +0300

    ; * src/lisp.h (DEFSYM): Fix inaccurate comment.

--- src/lisp.h
+++ src/lisp.h
@@ -2010,7 +2010,7 @@ INLINE int
 }
 
 /* Placeholder for make-docfile to process.  The actual symbol
-   definition is done by lread.c's defsym.  */
+   definition is done by lread.c's define_symbol.  */
 #define DEFSYM(sym, name) /* empty */
 
 
