86c8582c9e4497406522c3073e232e1af2eb23d1
Lars Ingebrigtsen <larsi@gnus.org>
2019-05-13 16:16:10 -0400

    Make mml respect the "recipient-filename" parameter
    * lisp/gnus/mml.el (mml-insert-mime-headers): Implement the
    already-documented "recipient-filename" parameter (bug#34654).

--- lisp/gnus/mml.el
+++ lisp/gnus/mml.el
@@ -906,8 +906,14 @@ be \"related\" or \"alternate\"."
 	      (or disposition
 		  (mml-content-disposition type (cdr (assq 'filename cont)))))
       (when parameters
-	(mml-insert-parameter-string
-	 cont mml-content-disposition-parameters))
+	(let ((cont (copy-sequence cont)))
+	  ;; Set the file name to what's specified by the user.
+	  (when-let ((recipient-filename (cdr (assq 'recipient-filename cont))))
+	    (setcdr cont
+		    (cons (cons 'filename recipient-filename)
+			  (cdr cont))))
+	  (mml-insert-parameter-string
+	   cont mml-content-disposition-parameters)))
       (insert "\n"))
     (unless (eq encoding '7bit)
       (insert (format "Content-Transfer-Encoding: %s\n" encoding)))
