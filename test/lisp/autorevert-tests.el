9199643ab2c38cd9e7f81ccff4485463f376e19d
Michael Albinus <michael.albinus@gmx.de>
2019-05-19 11:34:01 +0000

    * test/lisp/autorevert-tests.el (auto-revert--deftest-remote):
    Do not handle errors.

--- test/lisp/autorevert-tests.el
+++ test/lisp/autorevert-tests.el
@@ -140,7 +140,6 @@ This expects `auto-revert--messages' to be bound by
   `(ert-deftest ,(intern (concat (symbol-name test) "-remote")) ()
      ,docstring
      :tags '(:expensive-test)
-     (condition-case err
      (let ((temporary-file-directory
 	    auto-revert-test-remote-temporary-file-directory)
            (auto-revert-remote-files t)
@@ -149,8 +148,7 @@ This expects `auto-revert--messages' to be bound by
        (skip-unless (auto-revert--test-enabled-remote))
        (tramp-cleanup-connection
 	(tramp-dissect-file-name temporary-file-directory) nil 'keep-password)
-       (funcall (ert-test-body ert-test)))
-     (error (message "%s" err)))))
+       (funcall (ert-test-body ert-test)))))
 
 (ert-deftest auto-revert-test00-auto-revert-mode ()
   "Check autorevert for a file."
