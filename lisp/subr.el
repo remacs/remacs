04748e85660b5686cba5528c44db1f8554f31583
Van L <van@scratch.space>
2019-06-23 19:07:38 +0200

    Add a Code: tag to subr.el
    * lisp/subr.el: Add a Code: tag comment (bug#35595).

--- lisp/subr.el
+++ lisp/subr.el
@@ -22,7 +22,7 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
 
-
+;;; Code:
 
 ;; declare-function's args use &rest, not &optional, for compatibility
 ;; with byte-compile-macroexpand-declare-function.
