860880d7848c5a8bc2c7d7859b7238603389228b
Lars Ingebrigtsen <larsi@gnus.org>
2019-07-09 03:56:02 +0200

    Tweak compilation face in the wheatgrass theme
    * etc/themes/wheatgrass-theme.el: The compilation info was too
    light to be readable (bug#36203).

--- etc/themes/wheatgrass-theme.el
+++ etc/themes/wheatgrass-theme.el
@@ -37,6 +37,7 @@ of green, brown, and blue.")
    `(compilation-mode-line-fail ((,class (:foreground "dark green"))))
    `(compilation-mode-line-run  ((,class (:foreground "dark goldenrod"))))
    `(compilation-mode-line-exit ((,class (:foreground "SpringGreen4"))))
+   `(compilation-info ((,class (:foreground "ForestGreen"))))
    ;; Highlighting faces
    `(highlight ((,class (:foreground "white" :background "dark green"))))
    `(region ((,class (:foreground "white" :background "dark green"))))
