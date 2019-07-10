daf5c4309f9fc173c51a5d0bcbc904cbae96ee77
Stefan Kangas <stefankangas@gmail.com>
2019-07-11 00:34:25 +0200

    Use lexical-binding in help-fns-tests.el (Bug#36585)
    * test/lisp/help-fns-tests.el: Use lexical-binding.  Doc fix.

--- test/lisp/help-fns-tests.el
+++ test/lisp/help-fns-tests.el
@@ -1,4 +1,4 @@
-;;; help-fns.el --- tests for help-fns.el
+;;; help-fns-tests.el --- tests for help-fns.el  -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2014-2019 Free Software Foundation, Inc.
 
@@ -123,4 +123,4 @@ Return first line of the output of (describe-function-1 FUNC)."
     (goto-char (point-min))
     (should (looking-at "^font-lock-comment-face is "))))
 
-;;; help-fns.el ends here
+;;; help-fns-tests.el ends here
