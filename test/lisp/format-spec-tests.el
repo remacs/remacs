b41ddb7a8b11c8234e2f010d0db1b13e395468ae
Lars Ingebrigtsen <larsi@gnus.org>
2019-07-11 18:37:55 +0200

    Add tests for format-spec

---
+++ test/lisp/format-spec-tests.el
@@ -0,0 +1,33 @@
+;;; format-spec-tests.el --- tests for format-spec.el -*- lexical-binding: t -*-
+
+;; Copyright (C) 2019 Free Software Foundation, Inc.
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Code:
+
+(require 'ert)
+(require 'format-spec)
+
+(ert-deftest test-format-spec ()
+  (should (equal (format-spec "foo %b zot" '((?b . "bar")))
+                 "foo bar zot"))
+  (should (equal (format-spec "foo %-10b zot" '((?b . "bar")))
+                 "foo bar        zot"))
+  (should (equal (format-spec "foo %10b zot" '((?b . "bar")))
+                 "foo        bar zot")))
+
+;;; format-spec-tests.el ends here
