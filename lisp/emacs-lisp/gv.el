dbd1d2ebf868964fa5b2db5440ba27560846f95f
Lars Ingebrigtsen <larsi@gnus.org>
2019-07-15 11:15:11 +0200

    Tweak gv-define-setter debug setting
    * lisp/emacs-lisp/gv.el (gv-define-setter): Change edebug spec
    from body to def-body (bug#28729).

--- lisp/emacs-lisp/gv.el
+++ lisp/emacs-lisp/gv.el
@@ -214,7 +214,7 @@ The first arg in ARGLIST (the one that receives VAL) receives an expression
 which can do arbitrary things, whereas the other arguments are all guaranteed
 to be pure and copyable.  Example use:
   (gv-define-setter aref (v a i) \\=`(aset ,a ,i ,v))"
-  (declare (indent 2) (debug (&define name sexp body)))
+  (declare (indent 2) (debug (&define name sexp def-body)))
   `(gv-define-expander ,name
      (lambda (do &rest args)
        (declare-function
