acb8e331ca1e6914d8c158a6f387e84ce08c4f3b
Lars Ingebrigtsen <larsi@gnus.org>
2019-08-02 22:04:38 +0200

    Don't but out on ~/.git files in ede
    * lisp/cedet/ede/detect.el (ede--detect-ldf-root-predicate): Make
    this work with ~/ as the dir (bug#19521).

--- lisp/cedet/ede/detect.el
+++ lisp/cedet/ede/detect.el
@@ -135,6 +135,8 @@ Return a cons cell:
 
 (defun ede--detect-ldf-root-predicate (dir)
   "Non-nil if DIR no longer match `ede--detect-nomatch-auto'."
+  ;; `dir' may be "~/".
+  (setq dir (expand-file-name dir))
   (or (ede--detect-stop-scan-p dir)
       ;; To know if DIR is at the top, we need to look just above
       ;; to see if there is a match.
