6f57ef9d3cc5d3d934701e257fd6c8fd9f48f95f
Eli Zaretskii <eliz@gnu.org>
2019-08-10 22:19:31 +0300

    * src/callproc.c (Fcall_process): Doc fix.

--- src/callproc.c
+++ src/callproc.c
@@ -220,7 +220,7 @@ static mode_t const default_output_mode = 0666;
 DEFUN ("call-process", Fcall_process, Scall_process, 1, MANY, 0,
        doc: /* Call PROGRAM synchronously in separate process.
 The remaining arguments are optional.
-The program's input comes from file INFILE (nil means `/dev/null').
+The program's input comes from file INFILE (nil means `null-device').
 
 Third argument DESTINATION specifies how to handle program's output.
 If DESTINATION is a buffer, or t that stands for the current buffer,
