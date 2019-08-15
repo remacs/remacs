bfb83e643f447f9f1ae55852b682ce5e78880185
Lars Ingebrigtsen <larsi@gnus.org>
2019-08-14 17:59:13 -0700

    Don't alter function name face height in manoj-dark theme
    * etc/themes/manoj-dark-theme.el (manoj-dark): Don't alter the
    height of function name faces, because this makes many tabulated
    modes not longer line up (bug#17042).

--- etc/themes/manoj-dark-theme.el
+++ etc/themes/manoj-dark-theme.el
@@ -83,7 +83,7 @@ jarring angry fruit salad look to reduce eye fatigue.")
  '(font-lock-regexp-grouping-construct ((t (:bold t :weight bold))))
  '(font-lock-variable-name-face ((t (:foreground "Aquamarine"))))
  '(font-lock-function-name-face ((t (:foreground "mediumspringgreen"
-				     :weight bold :height 1.1))))
+				     :weight bold))))
  '(font-lock-string-face        ((t (:foreground "RosyBrown1"))))
  '(font-lock-comment-face       ((t (:italic t :slant oblique :foreground "chocolate1"))))
  '(font-lock-comment-delimiter-face ((t (:foreground "Salmon"))))
