0dd4b87e61e2d45b958b7afb888a6de5222ed172
Eli Zaretskii <eliz@gnu.org>
2019-09-21 10:05:20 +0300

    * lisp/completion.el (completion-kill-region): Doc fix.

--- lisp/completion.el
+++ lisp/completion.el
@@ -2138,7 +2138,7 @@ Also sets up so that exiting Emacs will automatically save the file."
   "Kill between point and mark.
 The text is deleted but saved in the kill ring.
 The command \\[yank] can retrieve it from there.
-/(If you want to kill and then yank immediately, use \\[copy-region-as-kill].)
+\(If you want to kill and then yank immediately, use \\[copy-region-as-kill].)
 
 This is the primitive for programs to kill text (as opposed to deleting it).
 Supply two arguments, character positions indicating the stretch of text
