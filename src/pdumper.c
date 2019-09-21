2cc9c45e447bb8a89446a1cd6d3f247a4d01c822
Andreas Schwab <schwab@linux-m68k.org>
2019-09-21 13:54:40 +0200

    Fix compilation with CHECK_STRUCTS
    * src/pdumper.c (dump_buffer): Update hash of struct buffer.

--- src/pdumper.c
+++ src/pdumper.c
@@ -2769,7 +2769,7 @@ dump_hash_table (struct dump_context *ctx,
 static dump_off
 dump_buffer (struct dump_context *ctx, const struct buffer *in_buffer)
 {
-#if CHECK_STRUCTS && !defined HASH_buffer_E34A11C6B9
+#if CHECK_STRUCTS && !defined HASH_buffer_9F2F522174
 # error "buffer changed. See CHECK_STRUCTS comment in config.h."
 #endif
   struct buffer munged_buffer = *in_buffer;
