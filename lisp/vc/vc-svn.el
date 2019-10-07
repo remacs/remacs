604a985a1a1066679895d2ec80c7b351ecbf8ff4
Wolfgang Scherer <wolfgang.scherer@gmx.de>
2019-10-07 06:58:59 +0200

    Set correct svn:ignore property for relative filenames
    * lisp/vc/vc-svn.el: (vc-svn-ignore) Use svn:ignore property of
    immediate parent directory for wildcard specification (bug#37216).

--- lisp/vc/vc-svn.el
+++ lisp/vc/vc-svn.el
@@ -353,15 +353,18 @@ to the SVN command."
 
 (defun vc-svn-ignore (file &optional directory remove)
   "Ignore FILE under Subversion.
-FILE is a file wildcard, relative to the root directory of DIRECTORY."
-  (let* ((ignores (vc-svn-ignore-completion-table directory))
-         (file (file-relative-name file directory))
+FILE is a wildcard specification, either relative to
+DIRECTORY or absolute."
+  (let* ((path (directory-file-name (expand-file-name file directory)))
+         (directory (file-name-directory path))
+         (file (file-name-nondirectory path))
+         (ignores (vc-svn-ignore-completion-table directory))
          (ignores (if remove
                       (delete file ignores)
                     (push file ignores))))
     (vc-svn-command nil 0 nil nil "propset" "svn:ignore"
                     (mapconcat #'identity ignores "\n")
-                    (expand-file-name directory))))
+                    directory)))
 
 (defun vc-svn-ignore-completion-table (directory)
   "Return the list of ignored files in DIRECTORY."
