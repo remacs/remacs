043d88862c43cebe2a29efa56816244db3df4dd2
Lars Ingebrigtsen <larsi@gnus.org>
2019-10-12 22:21:11 +0200

    Make the cl-letf doc string link to the manual
    * lisp/emacs-lisp/cl-macs.el (cl-letf): Add a link to the manual
    from the doc string (bug#24459).

--- lisp/emacs-lisp/cl-macs.el
+++ lisp/emacs-lisp/cl-macs.el
@@ -2577,6 +2577,8 @@ values.  Note that this macro is *not* available in Common Lisp.
 As a special case, if `(PLACE)' is used instead of `(PLACE VALUE)',
 the PLACE is not modified before executing BODY.
 
+See info node `(cl) Function Bindings' for details.
+
 \(fn ((PLACE VALUE) ...) BODY...)"
   (declare (indent 1) (debug ((&rest [&or (symbolp form)
                                           (gate gv-place &optional form)])
