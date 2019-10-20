b6bf5327908d310fabf7012af6790c7abc2114f4
Stefan Kangas <stefankangas@gmail.com>
2019-10-20 23:04:29 +0200

    Remove XEmacs compat code from decipher.el
    * lisp/play/decipher.el (decipher-last-command-char)
    (decipher-keypress): Remove XEmacs compat code.

--- lisp/play/decipher.el
+++ lisp/play/decipher.el
@@ -307,12 +307,6 @@ The most useful commands are:
 ;; Normal key handling:
 ;;--------------------------------------------------------------------
 
-(defmacro decipher-last-command-char ()
-  ;; Return the char which ran this command (for compatibility with XEmacs)
-  (if (fboundp 'event-to-character)
-      '(event-to-character last-command-event)
-    'last-command-event))
-
 (defun decipher-keypress ()
   "Enter a plaintext or ciphertext character."
   (interactive)
@@ -338,7 +332,7 @@ The most useful commands are:
           (let (goal-column)
             (forward-line -1)))
       (let ((char-a (following-char))
-            (char-b (decipher-last-command-char)))
+            (char-b last-command-event))
         (or (and (not (= ?w (char-syntax char-a)))
                  (= char-b ?\s)) ;Spacebar just advances on non-letters
             (funcall decipher-function char-a char-b)))))
