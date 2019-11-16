5ee4edce80ed81619d36ad7f1f661b9a524797ee
James N. V. Cash <james.nvc@gmail.com>
2019-11-16 17:45:22 -0500

    First pass at porting invisible-p

--- rust_src/src/xdisp.rs
+++ rust_src/src/xdisp.rs
@@ -3,10 +3,14 @@
 use remacs_macros::lisp_fn;
 
 use crate::lisp::LispObject;
-use crate::remacs_sys::bset_update_mode_line;
 use crate::threads::ThreadState;
 #[cfg(feature = "glyph-debug")]
 use crate::{interactive::InteractiveNumericPrefix, remacs_sys::trace_redisplay_p};
+use crate::{
+    lists::{LispCons, LispConsCircularChecks, LispConsEndChecks},
+    remacs_sys::bset_update_mode_line,
+    remacs_sys::{EmacsInt, Fget_char_property, Qinvisible, Qnil, Qt},
+};
 
 /// Mark the current buffer for redisplay.
 /// This function may be passed to `add-variable-watcher'.
@@ -35,6 +39,74 @@ pub fn trace_redisplay(arg: Option<InteractiveNumericPrefix>) {
             Some(n) => trace_redisplay_p = n.unwrap() > 0,
         }
     }
+}
+
+fn invisible_prop(propval: LispObject, list: LispCons) -> EmacsInt {
+    let proplist = if propval.is_cons() {
+        propval
+    } else {
+        LispObject::cons(propval, Qnil)
+    };
+    let iter = proplist.iter_cars(LispConsEndChecks::off, LispConsCircularChecks::off);
+
+    for propelt in iter {
+        for tem in list.iter_cars(LispConsEndChecks::off, LispConsCircularChecks::off) {
+            if propelt.eq(tem) {
+                return 1;
+            }
+            if tem.is_cons() && propelt.eq(tem.as_cons().unwrap().car()) {
+                if tem.as_cons().unwrap().cdr().is_nil() {
+                    return 1;
+                } else {
+                    return 2;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
+
+// originally the macro TEXT_PROP_MEANS_INVISIBLE from intervals.h
+pub fn text_prop_means_invisible(prop: LispObject) -> EmacsInt {
+    let cur_buf = ThreadState::current_buffer_unchecked();
+    if cur_buf.invisibility_spec_.is_t() {
+        if prop.is_nil() {
+            1
+        } else {
+            0
+        }
+    } else {
+        invisible_prop(prop, cur_buf.invisibility_spec_.as_cons().unwrap())
+    }
+}
+
+/// Non-nil if text properties at POS cause text there to be currently invisible.
+/// POS should be a marker or a buffer position; the value of the `invisible'
+/// property at that position in the current buffer is examined.
+/// POS can also be the actual value of the `invisible' text or overlay
+/// property of the text of interest, in which case the value itself is
+/// examined.
+///
+/// The non-nil value returned can be t for currently invisible text that is
+/// entirely hidden on display, or some other non-nil, non-t value if the
+/// text is replaced by an ellipsis.
+///
+/// Note that whether text with `invisible' property is actually hidden on
+/// display may depend on `buffer-invisibility-spec', which see.
+#[lisp_fn]
+pub fn invisible_p(pos: LispObject) -> LispObject {
+    let prop = if pos.is_natnum() || pos.is_marker() {
+        unsafe { Fget_char_property(pos, Qinvisible, Qnil) }
+    } else {
+        pos
+    };
+    let invis = text_prop_means_invisible(prop);
+    return match invis {
+        0 => Qnil,
+        1 => Qt,
+        _ => LispObject::from(invis),
+    };
 }
 
 include!(concat!(env!("OUT_DIR"), "/xdisp_exports.rs"));
