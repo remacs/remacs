4f6980ad6bbe130f72904d443e66cf60ff1d71a4
Eli Zaretskii <eliz@gnu.org>
2019-11-21 16:39:15 +0200

    Fix file notifications on macOS
    * src/kqueue.c (Fkqueue_add_watch): Don't use encoded file
    names in objects and APIs that expect decoded multibyte
    strings.  (Bug#38287)

--- src/kqueue.c
+++ src/kqueue.c
@@ -414,7 +414,7 @@ only when the upper directory of the renamed file is watched.  */)
     }
 
   /* Open file.  */
-  file = ENCODE_FILE (file);
+  Lisp_Object encoded_file = ENCODE_FILE (file);
   oflags = O_NONBLOCK;
 #if O_EVTONLY
   oflags |= O_EVTONLY;
@@ -426,7 +426,7 @@ only when the upper directory of the renamed file is watched.  */)
 #else
     oflags |= O_NOFOLLOW;
 #endif
-  fd = emacs_open (SSDATA (file), oflags, 0);
+  fd = emacs_open (SSDATA (encoded_file), oflags, 0);
   if (fd == -1)
     report_file_error ("File cannot be opened", file);
 
