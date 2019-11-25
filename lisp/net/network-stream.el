b006095bc9eb1f963372cf862aa040e9a9d30331
Alex Murray <alex.murray@canonical.com>
2019-11-25 22:18:07 +1030

    Fix auth-source password lookup
    * lisp/net/network-stream.el
    (network-stream-certificate): Ensure :port is specified as a string to
    'auth-source-search' (Bug#38371).
    
    Copyright-paperwork-exempt: yes

--- lisp/net/network-stream.el
+++ lisp/net/network-stream.el
@@ -233,7 +233,7 @@ gnutls-boot (as returned by `gnutls-boot-parameters')."
               (ignore-errors
                 (car (auth-source-search :max 1
                                          :host host
-                                         :port service))))
+                                         :port (format "%s" service)))))
 	     (key (plist-get auth-info :key))
 	     (cert (plist-get auth-info :cert)))
 	(and key cert (file-readable-p key) (file-readable-p cert)
