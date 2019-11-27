e7b4c248a6d2a2eca19a2a362103a7f24cfe30fc
Andrii Kolomoiets <andreyk.mad@gmail.com>
2019-11-27 14:13:12 +0200

    Call vc-setup-buffer in vc-hg-log-incoming and vc-hg-log-outgoing
    * lisp/vc/vc-hg.el (vc-hg-log-incoming, vc-hg-log-outgoing):
    Call vc-setup-buffer.

--- lisp/vc/vc-hg.el
+++ lisp/vc/vc-hg.el
@@ -1378,10 +1378,12 @@ REV is the revision to check out into WORKFILE."
      )))
 
 (defun vc-hg-log-incoming (buffer remote-location)
+  (vc-setup-buffer buffer)
   (vc-hg-command buffer 1 nil "incoming" "-n" (unless (string= remote-location "")
 						remote-location)))
 
 (defun vc-hg-log-outgoing (buffer remote-location)
+  (vc-setup-buffer buffer)
   (vc-hg-command buffer 1 nil "outgoing" "-n" (unless (string= remote-location "")
 						remote-location)))
 
