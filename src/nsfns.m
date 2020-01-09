58412402959d8f88e230f95c5fc7de072e115140
Väinö Järvelä <vaino@jarve.la>
2020-01-09 08:01:59 +0200

    Use NSNumber instead of BOOL (bug#39047)
    * src/nsfns.m (ns_set_represented_filename): Use correct type.
    
    Copyright-paperwork-exempt: yes

--- src/nsfns.m
+++ src/nsfns.m
@@ -493,11 +493,11 @@ ns_set_represented_filename (struct frame *f)
 #if defined (NS_IMPL_COCOA) && defined (MAC_OS_X_VERSION_10_7)
   /* Work around for Mach port leaks on macOS 10.15 (bug#38618).  */
   NSURL *fileURL = [NSURL fileURLWithPath:fstr isDirectory:NO];
-  BOOL isUbiquitousItem = YES;
+  NSNumber *isUbiquitousItem = @YES;
   [fileURL getResourceValue:(id *)&isUbiquitousItem
                      forKey:NSURLIsUbiquitousItemKey
                       error:nil];
-  if (isUbiquitousItem)
+  if ([isUbiquitousItem boolValue])
     fstr = @"";
 #endif
 
