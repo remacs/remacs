34a8e63fa3cfc6bf2dba6f0e1c684dfa9afe7ac7
Sean Perry <shalehperry@gmail.com>
2020-01-12 12:40:52 -0800

    Style updates

--- rust_src/src/image.rs
+++ rust_src/src/image.rs
@@ -1,12 +1,11 @@
 //! Functions for image support on window system.
 
+use crate::lisp::LispObject;
 use remacs_macros::lisp_fn;
+use crate::remacs_sys::lookup_image_type;
 
 #[cfg(feature = "glyph-debug")]
 use crate::remacs_sys::valid_image_p;
-
-use crate::lisp::LispObject;
-use crate::remacs_sys::lookup_image_type;
 
 /// Value is non-nil if SPEC is a valid image specification.
 #[cfg(feature = "glyph-debug")]
@@ -17,6 +16,7 @@ pub fn imagep(spec: LispObject) -> bool {
 
 /// Initialize image library implementing image type TYPE.
 /// Return non-nil if TYPE is a supported image type.
+///
 /// If image libraries are loaded dynamically (currently only the case on
 /// MS-Windows), load the library for TYPE if it is not yet loaded, using
 /// the library file(s) specified by `dynamic-library-alist'.
