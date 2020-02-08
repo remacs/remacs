56b8768b32e9679d3f4f6e2070e9af8f9fc14ff1
Eli Zaretskii <eliz@gnu.org>
2020-02-08 11:38:52 +0200

    More accurate documentation of 'package-menu-hide-package'
    * doc/emacs/package.texi (Package Menu): Improve the description
    of the 'H' command.
    
    * lisp/emacs-lisp/package.el (package-menu-mode-menu): More
    accurate wording of the help-echo string.
    (package-menu-hide-package): Make the doc string more accurate.
    (Bug#39436)

--- lisp/emacs-lisp/package.el
+++ lisp/emacs-lisp/package.el
@@ -2734,7 +2734,7 @@ either a full name or nil, and EMAIL is a valid email address."
      ["Filter by Name" package-menu-filter-by-name :help "Filter packages by name"]
      ["Clear Filter" package-menu-clear-filter :help "Clear package list filter"])
 
-    ["Hide by Regexp" package-menu-hide-package :help "Permanently hide all packages matching a regexp"]
+    ["Hide by Regexp" package-menu-hide-package :help "Hide all packages matching a regexp"]
     ["Display Older Versions" package-menu-toggle-hiding
      :style toggle :selected (not package-menu--hide-packages)
      :help "Display package even if a newer version is already installed"]
@@ -3187,8 +3187,9 @@ function.  The args ARG and NOCONFIRM, passed from
 (define-obsolete-function-alias 'package-menu-refresh 'revert-buffer "27.1")
 
 (defun package-menu-hide-package ()
-  "Hide a package under point in Package Menu.
-If optional arg BUTTON is non-nil, describe its associated package."
+  "Hide in Package Menu packages that match a regexp.
+Prompts for the regexp to match against package names.
+The default regexp will hide only the package whose name is at point."
   (interactive)
   (package--ensure-package-menu-mode)
   (declare (interactive-only "change `package-hidden-regexps' instead."))
